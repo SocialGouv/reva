4d8460b4b62430ae66304690e91082a081b89ae2
Anthonny <anthonny.querouil@gmail.com>
2021-09-23 16:17:03 +0200

    fix(): update footer 20-correction-des-liens-dans-le-footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,33 +1,74 @@
 <template lang="html">
   <div class="">
-
     <footer class="bg-white" aria-labelledby="footer-heading">
       <div id="footer-heading" class="sr-only">Footer</div>
       <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div>
-          <img class="-ml-4 lg:block h-32 w-auto" src="/illustrations/republique-francaise-logo.png" alt="Reva">
+          <img
+            class="-ml-4 lg:block h-32 w-auto"
+            src="/illustrations/republique-francaise-logo.png"
+            alt="Reva"
+          />
           <div class="space-y-8 xl:col-span-1">
             <p class="text-gray-500 text-base">
               REVA - Reconnaître et valider l'expérience
             </p>
             <div class="flex flex-col sm:flex-row sm:space-x-6">
-              <a href="https://beta.gouv.fr" class="font-bold">
-                beta.gouv.fr
-              </a>
-              <a href="https://travail-emploi.gouv.fr/" class="font-bold">
+              <a
+                target="_blank"
+                href="https://travail-emploi.gouv.fr/"
+                class="font-bold"
+              >
                 Ministère du travail, de l'emploi et de l'insertion
               </a>
-              <a href="https://solidarites-sante.gouv.fr/" class="font-bold">
+              <a
+                target="_blank"
+                href="https://solidarites-sante.gouv.fr/"
+                class="font-bold"
+              >
                 Ministère des solidarités et de la santé
               </a>
-              <a href="https://www.education.gouv.fr/" class="font-bold">
+              <a
+                target="_blank"
+                href="https://www.education.gouv.fr/"
+                class="font-bold"
+              >
                 Ministère de l'éducation nationale, de la jeunesse et des sports
+              </a>
+              <a
+                target="_blank"
+                href="https://agriculture.gouv.fr/"
+                class="font-bold"
+              >
+                Ministère de l'Agriculture et de l'Alimentation
+              </a>
+              <a
+                target="_blank"
+                href="https://www.iperia.eu/"
+                class="font-bold"
+              >
+                Branche professionnelles du Particulier Employeur
+              </a>
+              <a
+                target="_blank"
+                href="https://irfss-occitanie.croix-rouge.fr/"
+                class="font-bold"
+              >
+                Croix Rouge
               </a>
             </div>
           </div>
 
-          <div class="mt-12 flex flex-col sm:flex-row sm:gap-8 xl:col-span-2 border-t pt-6">
-
+          <div
+            class="
+              mt-12
+              flex flex-col
+              sm:flex-row sm:gap-8
+              xl:col-span-2
+              border-t
+              pt-6
+            "
+          >
             <!-- <div class="sm:border-r pr-4">
               <a href="#">Plan du site</a>
             </div> -->
@@ -43,27 +84,27 @@
             <!-- <div class="">
               <a href="#">Gestion des cookies</a>
             </div> -->
-
           </div>
-
         </div>
         <div class="pt-8">
           <p class="text-base text-gray-400 xl:text-center">
-            <a class="underline" href="https://github.com/etalab/licence-ouverte/blob/master/LO.md" target="_blank">
-              Sauf mention contraire, tous les textes de ce site sont sous licence etalab-2.0
+            <a
+              class="underline"
+              href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
+              target="_blank"
+            >
+              Sauf mention contraire, tous les textes de ce site sont sous
+              licence etalab-2.0
             </a>
           </p>
         </div>
       </div>
     </footer>
-
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
