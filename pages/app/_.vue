0896a29b5fc6b2bec58bd16e12b63a30f26f21eb
Anthonny <anthonny.querouil@gmail.com>
2021-10-15 20:47:15 +0200

    chore(): init elm app and login screen - wip

---
+++ pages/app/_.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <App :flags="flags" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import ElmComponent from '~/elm_app/elm'
+
+export default defineComponent({
+  name: 'ElmApp',
+  components: {
+    App: ElmComponent(require('~/elm_app/src/Main.elm').Elm.Main),
+  },
+  data: () => {
+    return {
+      flags: {
+        token: null,
+        baseUrl: '/app',
+      },
+    }
+  },
+})
+</script>
