8d3a7e8f2e4a3ddb2ebdb144cf9f4ed82359b744
Anthonny <anthonny.querouil@gmail.com>
2021-10-19 09:43:30 +0200

    fix(): tailwind config to analyze elm file for purge

--- nuxt.config.js
+++ nuxt.config.js
@@ -102,4 +102,19 @@ export default {
     hotjarId: process.env.HOTJAR_ID,
     hotjarVersion: process.env.HOTJAR_VERSION,
   },
+  tailwindcss: {
+    config: {
+      /* Extend the Tailwind config here */
+      purge: {
+        content: [
+          `components/**/*.{vue,js}`,
+          `layouts/**/*.vue`,
+          `pages/**/*.vue`,
+          `plugins/**/*.{js,ts}`,
+          `nuxt.config.{js,ts}`,
+          `elm_app/**/**.elm`,
+        ],
+      },
+    },
+  },
 }
