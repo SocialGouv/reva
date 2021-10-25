ca30cffd1553072533379a8a3600ba5ed6b9e3ee
Anthonny <anthonny.querouil@gmail.com>
2021-10-25 12:46:51 +0200

    fix(): remove diplome for iperia

---
+++ migrations/sqls/20211025084837-remove-iperia-diplome-association-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+delete from cohortes_diplomes_cities
+where cohorte_id in ('f2fb1813-75af-4181-ae3b-e3832a744d90', 'f4f7681f-3671-40a9-9e70-ba7fb3b0cdbf')
+and diplome_id  not in ('e7f900e0-c9b0-4143-b5a6-8bb4ba66179c', '2cfb796f-9392-4f60-9a26-b41fc6b0ffde');
