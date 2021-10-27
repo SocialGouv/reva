5a5429ae5fc82fe7c9192d5bcea092490e58ff2c
Cédric Soulas <soulascedric@gmail.com>
2021-10-27 15:52:41 +0200

    feat(): add candidate directory with details panel

--- elm_app/src/View.elm
+++ elm_app/src/View.elm
@@ -1,148 +1,97 @@
 module View exposing (layout)
 
-import Html.Styled exposing (a, button, div, form, h1, img, input, label, nav, node, span, text)
-import Html.Styled.Attributes exposing (action, alt, attribute, class, for, href, id, method, name, placeholder, src, type_)
-import Html.Styled.Events exposing (onClick, onInput)
+import Html.Styled exposing (Html, a, button, div, img, nav, span, text)
+import Html.Styled.Attributes exposing (alt, attribute, class, href, src, type_)
+import Html.Styled.Events exposing (onClick)
 import View.Icons as Icons
 
 
-layout : { onFilter : String -> msg, onLogout : msg } -> Html.Styled.Html msg -> Html.Styled.Html msg
+layout : { a | onLogout : msg } -> Html msg -> Html.Styled.Html msg
 layout config content =
     div
-        [ class "bg-gray-100 h-screen" ]
+        [ class "bg-gray-white h-screen flex" ]
+        [ mobileMenu config
+        , div
+            [ class "hidden lg:flex lg:flex-shrink-0" ]
+            [ div [ class "flex flex-col w-64" ] [ sideMenu config ] ]
+        , content
+        ]
+
+
+mobileMenu : { a | onLogout : msg } -> Html msg
+mobileMenu config =
+    div
+        -- TODO: Implement open/close mobile menu
+        [ class "hidden fixed inset-0 flex z-40 lg:hidden", attribute "role" "dialog", attribute "aria-modal" "true" ]
         [ div
-            -- TODO: Implement open/close mobile menu
-            [ class "hidden fixed inset-0 flex z-40 md:hidden", attribute "role" "dialog", attribute "aria-modal" "true" ]
-            [ div
-                [ class "fixed inset-0 bg-gray-600 bg-opacity-75", attribute "aria-hidden" "true" ]
-                []
-            , div
-                [ class "relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white" ]
-                [ div
-                    [ class "absolute top-0 right-0 -mr-12 pt-2" ]
-                    [ button
-                        [ type_ "button", class "ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" ]
-                        [ span
-                            [ class "sr-only" ]
-                            [ text "Close sidebar" ]
-                        , Icons.close
-                        ]
-                    ]
-                , div
-                    [ class "flex-shrink-0 flex items-center px-4" ]
-                    [ img
-                        [ class "h-8 w-auto", src "/illustrations/beta-gouv-logo-a3.png", alt "BetaGouv - Reva" ]
-                        []
-                    , text "REVA"
-                    ]
-                , div
-                    [ class "mt-5 flex-1 h-0 overflow-y-auto" ]
-                    [ nav
-                        [ class "px-2 space-y-1" ]
-                        [ a
-                            [ href "#", class "bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md" ]
-                            [ Icons.candidats
-                            , text "Candidats"
-                            ]
-                        , button
-                            [ onClick config.onLogout, class "bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md" ]
-                            [ Icons.signout
-                            , text "Déconnexion"
-                            ]
-                        ]
-                    ]
-                ]
-            , div
-                [ class "flex-shrink-0 w-14", attribute "aria-hidden" "true" ]
-                []
-            ]
+            [ class "fixed inset-0 bg-gray-600 bg-opacity-75", attribute "aria-hidden" "true" ]
+            []
         , div
-            [ class "hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0" ]
+            [ class "relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white" ]
             [ div
-                [ class "flex flex-col flex-grow border-r border-gray-200 pt-5 bg-white overflow-y-auto" ]
-                [ div
-                    [ class "flex items-start flex-shrink-0 px-4" ]
-                    [ img
-                        [ class "h-10 w-auto", src "/illustrations/beta-gouv-logo-a3.png", alt "BetaGouv - Reva" ]
-                        []
-                    , span [ class "font-bold ml-2" ] [ text "REVA" ]
-                    ]
-                , div
-                    [ class "mt-5 flex-grow flex flex-col" ]
-                    [ nav
-                        [ class "flex-1 px-2 pb-4 space-y-1" ]
-                        [ a
-                            [ href "#", class "bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md" ]
-                            [ Icons.candidats
-                            , text "Candidats"
-                            ]
-                        , button
-                            [ onClick config.onLogout, class "w-full text-gray-900 group flex items-center px-2 py-2 text-sm font-base hover:font-medium rounded-md" ]
-                            [ Icons.signout
-                            , text "Déconnexion"
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        , div
-            [ class "md:pl-64 flex flex-col flex-1" ]
-            [ div
-                [ class "sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow" ]
+                [ class "absolute top-0 right-0 -mr-12 pt-2" ]
                 [ button
-                    [ type_ "button", class "px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden" ]
+                    [ type_ "button", class "ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" ]
                     [ span
                         [ class "sr-only" ]
-                        [ text "Open sidebar" ]
-                    , Icons.menu
-                    ]
-                , div
-                    [ class "flex-1 px-4 flex justify-between" ]
-                    [ div
-                        [ class "flex-1 flex" ]
-                        [ div
-                            [ class "w-full flex md:ml-0", action "#", method "GET" ]
-                            [ label
-                                [ for "search-field", class "sr-only" ]
-                                [ text "Rechercher" ]
-                            , div
-                                [ class "relative w-full text-gray-400 focus-within:text-gray-600" ]
-                                [ div
-                                    [ class "absolute inset-y-0 left-0 flex items-center pointer-events-none" ]
-                                    [ Icons.search ]
-                                , input
-                                    [ id "search-field"
-                                    , class "block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                                    , placeholder "Rechercher un nom, un diplôme, une cohorte, une région..."
-                                    , type_ "search"
-                                    , name "search"
-                                    , onInput config.onFilter
-                                    ]
-                                    []
-                                ]
-                            ]
-                        ]
-                    , div
-                        [ class "ml-4 flex items-center md:ml-6" ]
-                        []
+                        [ text "Close sidebar" ]
+                    , Icons.close
                     ]
                 ]
-            , node "main"
-                [ class "flex-1 bg-gray-100 h-full" ]
-                [ div
-                    [ class "py-6" ]
-                    [ div
-                        [ class "max-w-7xl mx-auto px-4 sm:px-6 md:px-8" ]
-                        [ h1
-                            [ class "text-2xl font-semibold text-gray-900" ]
-                            [ text "Candidats" ]
+            , div
+                [ class "flex-shrink-0 flex items-center px-4" ]
+                [ img
+                    [ class "h-8 w-auto", src "/illustrations/beta-gouv-logo-a3.png", alt "BetaGouv - Reva" ]
+                    []
+                , text "REVA"
+                ]
+            , div
+                [ class "mt-5 flex-1 h-0 overflow-y-auto" ]
+                [ nav
+                    [ class "px-2 space-y-1" ]
+                    [ a
+                        [ href "#", class "text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md" ]
+                        [ Icons.candidates
+                        , text "Candidats"
                         ]
-                    , div
-                        [ class "max-w-7xl mx-auto px-4 sm:px-6 md:px-8" ]
-                        [ div
-                            [ class "py-4" ]
-                            [ content ]
+                    , button
+                        [ onClick config.onLogout, class "bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md" ]
+                        [ Icons.signout
+                        , text "Déconnexion"
                         ]
+                    ]
+                ]
+            ]
+        , div
+            -- Force sidebar to shrink to fit close icon
+            [ class "flex-shrink-0 w-14", attribute "aria-hidden" "true" ]
+            []
+        ]
+
+
+sideMenu config =
+    div
+        [ class "flex-1 flex flex-col flex-grow min-h-0 border-r border-gray-200 pt-5 bg-gray-100 overflow-y-auto" ]
+        [ div
+            [ class "flex items-start flex-shrink-0 px-4" ]
+            [ img
+                [ class "h-10 w-auto", src "/illustrations/beta-gouv-logo-a3.png", alt "BetaGouv - Reva" ]
+                []
+            , span [ class "font-bold ml-2" ] [ text "REVA" ]
+            ]
+        , div
+            [ class "mt-5 flex-grow flex flex-col" ]
+            [ nav
+                [ class "flex-1 px-2 pb-4 space-y-1" ]
+                [ a
+                    [ href "#", class "bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md" ]
+                    [ Icons.candidates
+                    , text "Candidats"
+                    ]
+                , button
+                    [ onClick config.onLogout, class "w-full text-gray-900 group flex items-center px-2 py-2 text-sm font-base hover:font-medium rounded-md" ]
+                    [ Icons.signout
+                    , text "Déconnexion"
                     ]
                 ]
             ]
--- elm_app/src/View/Icons.elm
+++ elm_app/src/View/Icons.elm
@@ -1,5 +1,6 @@
-module View.Icons exposing (candidats, close, menu, search, signout)
+module View.Icons exposing (candidates, chevronLeft, close, filter, mail, menu, search, signout, user, userLarge)
 
+import Html.Styled exposing (Html)
 import Html.Styled.Attributes exposing (attribute)
 import Svg.Styled as Svg exposing (svg)
 import Svg.Styled.Attributes exposing (..)
@@ -9,32 +10,60 @@ import Svg.Styled.Attributes exposing (..)
 -- Icons from heroicons.com
 
 
-candidats =
-    svg
-        [ class "text-gray-400 group-hover:text-gray-500 mr-4 flex-shrink-0 h-6 w-6", fill "none", viewBox "0 0 24 24", stroke "currentColor" ]
+candidates : Html msg
+candidates =
+    svg [ class "text-gray-400 group-hover:text-gray-500 mr-4 flex-shrink-0 h-6 w-6", fill "none", viewBox "0 0 24 24", stroke "currentColor" ]
         [ Svg.path [ strokeLinecap "round", strokeLinejoin "round", strokeWidth "2", d "M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" ] [] ]
 
 
+close : Html msg
 close =
-    svg
-        [ class "h-6 w-6 text-white", fill "none", viewBox "0 0 24 24", stroke "currentColor", attribute "aria-hidden" "true" ]
+    svg [ class "h-6 w-6 text-white", fill "none", viewBox "0 0 24 24", stroke "currentColor", attribute "aria-hidden" "true" ]
         [ Svg.path [ strokeLinecap "round", strokeLinejoin "round", strokeWidth "2", d "M6 18L18 6M6 6l12 12" ] [] ]
 
 
+menu : Html msg
 menu =
-    svg
-        [ class "h-6 w-6", fill "none", viewBox "0 0 24 24", stroke "currentColor" ]
+    svg [ class "h-6 w-6", fill "none", viewBox "0 0 24 24", stroke "currentColor" ]
         [ Svg.path [ strokeLinecap "round", strokeLinejoin "round", strokeWidth "2", d "M4 6h16M4 12h16M4 18h7" ] [] ]
 
 
 search =
-    svg
-        [ class "h-5 w-5", viewBox "0 0 20 20", fill "currentColor" ]
+    svg [ class "h-5 w-5", viewBox "0 0 20 20", fill "currentColor" ]
         [ Svg.path [ fillRule "evenodd", d "M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z", clipRule "evenodd" ] [] ]
 
 
+filter : Html msg
+filter =
+    svg [ class "h-5 w-5 text-gray-400", viewBox "0 0 20 20", fill "currentColor" ]
+        [ Svg.path [ fillRule "evenodd", d "M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z", clipRule "evenodd" ] [] ]
+
+
+signout : Html msg
 signout =
     svg [ class "text-gray-400 group-hover:text-gray-500 mr-4 flex-shrink-0 h-6 w-6", viewBox "0 0 20 20", fill "currentColor" ]
-        [ Svg.path [ fillRule "evenodd", d "M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z", clipRule "evenodd" ]
-            []
-        ]
+        [ Svg.path [ fillRule "evenodd", d "M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z", clipRule "evenodd" ] [] ]
+
+
+chevronLeft : Html msg
+chevronLeft =
+    svg [ class "-ml-2 h-5 w-5 text-gray-400", viewBox "0 0 20 20", fill "currentColor" ]
+        [ Svg.path [ fillRule "evenodd", d "M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z", clipRule "evenodd" ] [] ]
+
+
+mail : Html msg
+mail =
+    svg [ class "-ml-1 mr-2 h-5 w-5 text-gray-400", viewBox "0 0 20 20", fill "currentColor" ]
+        [ Svg.path [ d "M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" ] [], Svg.path [ d "M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" ] [] ]
+
+
+user : Html msg
+user =
+    svg [ class "h-8 w-8", viewBox "0 0 20 20", fill "currentColor" ]
+        [ Svg.path [ fillRule "evenodd", d "M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z", clipRule "evenodd" ] [] ]
+
+
+userLarge : Html msg
+userLarge =
+    svg [ class "h-24 w-24", viewBox "0 0 20 20", fill "currentColor" ]
+        [ Svg.path [ fillRule "evenodd", d "M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z", clipRule "evenodd" ] [] ]
