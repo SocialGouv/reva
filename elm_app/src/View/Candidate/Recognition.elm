ede6670cc1ad84916f4373f1cde3bdd9204a243b
Cédric Soulas <soulascedric@gmail.com>
2021-12-07 15:06:31 +0100

    feat: use situation instead of action

--- elm_app/src/View/Candidate/Recognition.elm
+++ elm_app/src/View/Candidate/Recognition.elm
@@ -1,7 +1,7 @@
 module View.Candidate.Recognition exposing (Step(..), view)
 
 import Candidate exposing (Candidate)
-import Html.Styled exposing (Attribute, Html, button, div, h3, h4, input, label, p, span, text, textarea)
+import Html.Styled exposing (Attribute, Html, button, div, h3, h4, h5, input, label, p, span, text, textarea)
 import Html.Styled.Attributes exposing (attribute, class, for, id, name, placeholder, rows, type_)
 import Html.Styled.Events exposing (onClick)
 import List.Extra
@@ -103,7 +103,7 @@ view config step candidate =
 
 introduction : { a | onRecognitionStep : Step -> msg } -> Candidate -> List (Html msg)
 introduction config _ =
-    [ title "Reconnaissance de méta-compétences"
+    [ title3 "Reconnaissance de méta-compétences"
     , p []
         [ text "Bientôt, vous pourrez démarrer ici une démarche de reconnaissance. Accompagné du candidat, vous sélectionnerez une ou plusieurs méta-compétences à reconnaître."
         ]
@@ -137,7 +137,7 @@ selection config _ =
         viewSkills ( firstSkill, nextSkills ) =
             div
                 [ class "flex flex-col items-center mb-4" ]
-                [ subtitle [ text firstSkill.category ]
+                [ title4 [ text firstSkill.category ]
                 , List.map viewSkillButton (firstSkill :: nextSkills)
                     |> div
                         [ class "grid grid-cols-1 gap-6 justify-items-center sm:grid-cols-2 lg:grid-cols-3"
@@ -146,7 +146,7 @@ selection config _ =
                 ]
     in
     popup
-        { title = "Sélectionnez une compétence à reconnaître"
+        { title = "Sélectionnez une compétence"
         , onClose = config.onRecognitionStep Introduction
         , content =
             [ groupByCategory predefinedMetaSkills
@@ -192,8 +192,12 @@ viewSkill attributes skill =
 
 contextualization : { a | onRecognitionStep : Step -> msg } -> Candidate -> MetaSkillReference -> List (Html msg)
 contextualization config _ skill =
+    let
+        commentPlaceholder =
+            "Décrivez au moins une situation pendant laquelle la compétence s'est illustrée"
+    in
     popup
-        { title = "Ajoutez du contexte et des détails"
+        { title = "Décrivez une situation"
         , onClose = config.onRecognitionStep Introduction
         , content =
             [ div
@@ -202,7 +206,7 @@ contextualization config _ skill =
                 ]
                 [ div
                     [ class "text-left w-full" ]
-                    [ div [ class "font-semibold text-gray-800 text-lg mb-2" ] [ text skill.category ]
+                    [ title4 [ text skill.category ]
                     , p
                         [ class "mt-2"
                         , class "text-base text-gray-800"
@@ -210,19 +214,17 @@ contextualization config _ skill =
                         [ text skill.name ]
                     ]
                 , div
-                    [ class "text-left w-full mt-6" ]
+                    [ class "text-left w-full mt-3" ]
                     [ label
-                        [ for "comment"
-                        , class "block font-semibold text-gray-800 text-sm mb-2"
-                        ]
-                        [ text "Contexte et détails (optionnel)" ]
+                        [ for "comment", class "sr-only" ]
+                        [ text commentPlaceholder ]
                     , div
-                        [ class "mt-2" ]
+                        [ class "my-2" ]
                         [ textarea
                             [ rows 4
                             , name "context"
                             , id "context"
-                            , placeholder "La démarche de reconnaissance de cette compétence"
+                            , placeholder commentPlaceholder
                             , class "shadow-base block w-full border-gray-300 rounded-md"
                             , class "focus:ring-indigo-500 focus:border-indigo-500"
                             ]
@@ -293,7 +295,7 @@ popup config =
                     [ class "absolute top-0 w-full bg-white pt-4"
                     , class "border-b"
                     ]
-                    [ title config.title
+                    [ title3 config.title
                     , button
                         [ dataTest "close-popup"
                         , type_ "button"
@@ -324,17 +326,17 @@ groupByCategory =
         (\s1 s2 -> s1.category == s2.category)
 
 
-title : String -> Html msg
-title s =
+title3 : String -> Html msg
+title3 s =
     h3
         [ class "mb-4 text-2xl font-semibold text-gray-800" ]
         [ text s ]
 
 
-subtitle : List (Html msg) -> Html msg
-subtitle content =
+title4 : List (Html msg) -> Html msg
+title4 content =
     h4
-        [ class "font-semibold text-gray-600 text-lg mb-2 mr-2" ]
+        [ class "font-semibold text-gray-700 text-lg mb-2 mr-2" ]
         content
 
 
