62acd460b09364d8c853f87bfc9de12db475d0a3
Anthonny <anthonny.querouil@gmail.com>
2022-02-03 15:35:20 +0100

    chore(*): add build and start app to test on scalingo

--- packages/reva-api/index.ts
+++ packages/reva-api/index.ts
@@ -1,37 +1,63 @@
-import fastify from 'fastify'
-import mercurius from 'mercurius'
+import fastify from 'fastify';
+import mercurius from 'mercurius';
+import path from 'path';
+import proxy from 'fastify-http-proxy';
+import fastifyStatic from 'fastify-static';
+import dotenv from 'dotenv';
 
+dotenv.config({ path: path.join(process.cwd(), '..', '..', '.env') });
 
-const server = fastify({ logger: true })
+const server = fastify({ logger: true });
 
 const schema = `
   type Query {
     add(x: Int, y: Int): Int
   }
-`
+`;
 
 const resolvers = {
   Query: {
-    add: async (_: any, { x, y }: { x: number, y: number }) => x + y
+    add: async (_: any, { x, y }: { x: number, y: number; }) => x + y
   }
-}
+};
 
+if (process.env.NODE_ENV === 'production') {
+  server.register(fastifyStatic, {
+    root: path.join(__dirname, 'website')
+  });
+
+  // server.register(fastifyStatic, {
+  //   root: path.join(__dirname, 'app'),
+  //   prefix: '/app'
+  // });
+
+} else {
+  server.register(proxy, {
+    upstream: 'http://localhost:1234',
+    prefix: '/'
+  });
+
+  server.register(proxy, {
+    upstream: 'http://localhost:1235',
+    prefix: '/app'
+  });
+}
 
 server.register(mercurius, {
   schema,
   resolvers,
-  graphiql: true
-})
+  graphiql: !!process.env.GRAPHIQL
+});
 
 
 server.get('/ping', async (request, reply) => {
-  return 'pong'
-})
+  return 'pong';
+});
 
-server.listen(8080, (err, address) => {
+server.listen(process.env.PORT || 8080, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address} in ${process.env.NODE_ENV}`);
+});
