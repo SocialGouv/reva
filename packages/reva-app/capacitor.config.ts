2638afc89d87513ae3773e10d896b01e025959a0
Anthonny <anthonny.querouil@gmail.com>
2022-02-24 11:31:27 +0100

    chore(mobile): reva-app change the mobile dev and build scripts

--- packages/reva-app/capacitor.config.ts
+++ packages/reva-app/capacitor.config.ts
@@ -1,10 +1,20 @@
+import dotenv from 'dotenv';
 import { CapacitorConfig } from '@capacitor/cli';
+
+dotenv.config();
 
 const config: CapacitorConfig = {
   appId: 'fr.gouv.beta.reva',
   appName: 'reva-app',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: !!process.env.CAPACITOR_WEB_RUNTIME
 };
+
+if (!!process.env.CAPACITOR_LIVE_RELOAD) {
+  config.server = {
+    url: `http://${process.env.HOST_IP}:${process.env.PORT}`,
+    cleartext: true
+  };
+}
 
 export default config;
