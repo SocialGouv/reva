bc26bbcefc15532ec9209f12abc061aca2bc0d0d
Cédric Soulas <soulascedric@gmail.com>
2022-02-24 16:08:33 +0100

    feat: add button example made in storybook styled with tailwind

--- packages/reva-app/src/App.tsx
+++ packages/reva-app/src/App.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "./stories/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="bg-red-500">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button>Hello</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn EpicReact
-        </a>
+        <Button label="Candidater" onClick={() => {}} primary />
       </header>
     </div>
   );
