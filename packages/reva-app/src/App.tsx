c9bbfd767a5bb47e574da4c3072f0fad6a601a2c
Cédric Soulas <soulascedric@gmail.com>
2022-03-14 11:36:53 +0100

    feat(app): move fixtures and interface to separate files

--- packages/reva-app/src/App.tsx
+++ packages/reva-app/src/App.tsx
@@ -2,115 +2,15 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 import { Header } from "./components/atoms/Header";
-import { loremIpsum, loremIpsumShort } from "./components/atoms/LoremIpsum";
+import { loremIpsumShort } from "./components/atoms/LoremIpsum";
 import { TextResult } from "./components/atoms/TextResult";
 import { Card, CardSize } from "./components/organisms/Card";
 import { CardSkeleton } from "./components/organisms/CardSkeleton";
 import { Results } from "./components/organisms/Results";
+import { certificateFixtures } from "./fixtures/certificates";
+import { Certificate } from "./interface";
 
 function App() {
-  interface Certificate {
-    id: string;
-    description: string;
-    label: string;
-    title: string;
-  }
-
-  const certificateFixtures: Certificate[] = [
-    {
-      id: "1",
-      description: loremIpsum,
-      label: "5983",
-      title:
-        "Surveillant - visiteur de nuit en secteur social et médico-social",
-    },
-    {
-      id: "2",
-      description: loremIpsum,
-      label: "13905",
-      title: "Services aux personnes et aux territoires",
-    },
-    {
-      id: "3",
-      description: loremIpsum,
-      label: "35028",
-      title: "Agent de service médico-social",
-    },
-    {
-      id: "4",
-      description: loremIpsum,
-      label: "35830",
-      title: "Aide-Soignant",
-    },
-    {
-      id: "5",
-      description: loremIpsum,
-      label: "34692",
-      title: "Employé familial",
-    },
-    {
-      id: "6",
-      description: loremIpsum,
-      label: "35506",
-      title: "Assistant de vie aux familles",
-    },
-    {
-      id: "7",
-      description: loremIpsum,
-      label: "34690",
-      title: "Assistant de vie dépendance",
-    },
-    {
-      id: "8",
-      description: loremIpsum,
-      label: "25467",
-      title: "Diplôme d'État d'accompagnant éducatif et social",
-    },
-    {
-      id: "9",
-      description: loremIpsum,
-      label: "17163",
-      title: "Conducteur-e accompagnateur-e de personnes à mobilité réduite",
-    },
-    {
-      id: "10",
-      description: loremIpsum,
-      label: "35993",
-      title: "Responsable coordonnateur service au domicile",
-    },
-    {
-      id: "11",
-      description: loremIpsum,
-      label: "34691",
-      title: "Assistant maternel / garde d'enfants",
-    },
-    {
-      id: "12",
-      description: loremIpsum,
-      label: "4500",
-      title: "Assistant familial",
-    },
-    {
-      id: "13",
-      description: loremIpsum,
-      label: "35832",
-      title: "Auxiliaire de puériculture",
-    },
-    {
-      id: "14",
-      description: loremIpsum,
-      label: "4503",
-      title: "Technicien de l'intervention sociale et familiale",
-    },
-    {
-      id: "15",
-      description: loremIpsum,
-      label: "2514",
-      title:
-        "Certificat d'aptitude aux fonctions d'encadrement et de responsable d'unité d'intervention sociale",
-    },
-  ];
-
   const emptyCertificates: Certificate[] = [];
   const [certificates, setCertificates] = useState(emptyCertificates);
 
