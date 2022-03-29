0a4b2a5e21611eb732c62b818eb07566d7499f6e
Cédric Soulas <soulascedric@gmail.com>
2022-03-29 20:44:49 +0200

    feat(app): optimize animations on android

--- packages/reva-app/src/components/organisms/Card/index.tsx
+++ packages/reva-app/src/components/organisms/Card/index.tsx
@@ -1,4 +1,9 @@
-import { motion, useMotionValue } from "framer-motion";
+import {
+  AnimatePresence,
+  LayoutGroup,
+  motion,
+  useMotionValue,
+} from "framer-motion";
 import React from "react";
 import { useState } from "react";
 
@@ -53,64 +58,40 @@ export const Card = React.forwardRef<HTMLLIElement, Card>(
     const isFullscreen = size === "large";
     const isReduced = isSmall || isMedium;
 
-    const zIndex = useMotionValue(isFullscreen ? 20 : 0);
     const transition = isReduced ? transitionOut : transitionIn;
 
-    const decorativeImg = (
-      <motion.img
-        layout
-        transition={transition}
-        className="pointer-events-none"
-        style={
-          isFullscreen
-            ? {
-                position: "relative",
-                marginLeft: "-70px",
-                width: "174px",
-              }
-            : {
-                position: "absolute",
-                left: "-43px",
-                top: "12px",
-                width: isSmall ? "104px" : "240px",
-              }
-        }
-        src={certificateImg}
-      />
-    );
-
-    const summaryParagraph = (
+    const fullscreenDetails = (
       <div
-        style={
-          isFullscreen
-            ? {
-                overflowX: "hidden",
-                overflowY: "auto",
-                height: "calc(100vh - 216px)",
-              }
-            : {}
-        }
+        style={{
+          overflowX: "hidden",
+          overflowY: "auto",
+          height: "calc(100vh - 216px)",
+        }}
+        className={`absolute inset-0 z-50  ${
+          isReduced ? "pointer-events-none opacity-0" : ""
+        }`}
       >
         <motion.div
-          className={`w-full px-6 ${
-            isFullscreen ? "mb-4" : isMedium ? "mt-[250px]" : "mt-[168px]"
-          }`}
+          transition={{ duration: 0 }}
+          layout="position"
+          className="mt-6"
+        >
+          <BackButton
+            color="light"
+            onClick={() => {
+              setSize("small");
+              onClose();
+            }}
+          />
+        </motion.div>
+        <motion.div
           layout="position"
           transition={transition}
+          className={`w-full px-6 mb-4`}
         >
-          <div className={isSmall ? "flex items-end h-20" : ""}>
-            <TextResult
-              size={isSmall ? "small" : "large"}
-              title={
-                isSmall && title.length > SMALL_TITLE_LENGTH
-                  ? `${title.substring(0, SMALL_TITLE_LENGTH)}...`
-                  : title
-              }
-              color="light"
-            />
-          </div>
-          <div className={`transition-opacity ${isReduced && "opacity-0"}`}>
-            <div className="mt-1 mb-4 font-bold">{label}</div>
+          <TextResult size="large" title={title} color="light" />
+          <div>
+            <div className="mt-1 mb-4 font-bold text-white">{label}</div>
             <p className="overflow-auto text-slate-400 text-base leading-normal transition-opacity">
               {summary}
             </p>{" "}
@@ -122,8 +103,54 @@ export const Card = React.forwardRef<HTMLLIElement, Card>(
             Lire tous les détails
           </a>
         </motion.div>
-        {decorativeImg}
+        <img
+          className="pointer-events-none"
+          style={{
+            marginLeft: "-70px",
+            height: "174px",
+            width: "174px",
+          }}
+          src={certificateImg}
+        />
       </div>
+    );
+
+    const reducedInfo = (
+      <>
+        <motion.div
+          layout="position"
+          className={`absolute top-5 right-6 text-right font-bold grow `}
+        >
+          {label}
+          <div className="mt-4 rounded-full flex items-center justify-center h-[46px] w-[46px] bg-blue-500">
+            <div className="w-[18px]">
+              <Add />
+            </div>
+          </div>
+        </motion.div>
+        <motion.img
+          layout
+          className="absolute pointer-events-none"
+          style={{
+            left: "-43px",
+            top: "12px",
+            height: isSmall ? "104px" : "240px",
+            width: isSmall ? "104px" : "240px",
+          }}
+          src={certificateImg}
+        />
+        <motion.div layout className={"flex items-end h-full p-5"}>
+          <TextResult
+            size="small"
+            title={
+              title.length > SMALL_TITLE_LENGTH
+                ? `${title.substring(0, SMALL_TITLE_LENGTH)}...`
+                : title
+            }
+            color="light"
+          />
+        </motion.div>
+      </>
     );
 
     return (
@@ -135,20 +162,16 @@ export const Card = React.forwardRef<HTMLLIElement, Card>(
         }}
       >
         <motion.div
-          initial={isFullscreen ? rounded2xl : false}
-          animate={isFullscreen ? roundedNone : rounded2xl}
-          className={`cursor-pointer overflow-hidden shadow-2xl bg-slate-900 text-white ${
-            isFullscreen ? "rounded-none" : "rounded-3xl"
+          className={`cursor-pointer overflow-hidden bg-slate-900 text-white ${
+            isReduced ? "rounded-2xl" : "rounded-none"
           }`}
           layout
           transition={transition}
           layoutDependency={size}
-          onAnimationStart={() => isFullscreen && zIndex.set(20)}
-          onAnimationComplete={() => isReduced && zIndex.set(0)}
           onClick={() => (isReduced ? (setSize("large"), onOpen()) : {})}
           whileTap={{ scale: isReduced ? 0.92 : 1 }}
           style={{
-            zIndex,
+            zIndex: isOpen ? 20 : 0,
             height: heightConfig[size],
             ...(isFullscreen
               ? {
@@ -161,39 +184,9 @@ export const Card = React.forwardRef<HTMLLIElement, Card>(
           }}
           {...props}
         >
-          {isFullscreen && (
-            <motion.div
-              className="mt-6"
-              transition={transition}
-              layout="position"
-            >
-              <BackButton
-                color="light"
-                onClick={() => {
-                  setSize("small");
-                  onClose();
-                }}
-              />
-            </motion.div>
-          )}
-
-          <motion.div
-            layout="position"
-            transition={transition}
-            className={`transition-opacity absolute top-5 right-6 text-right font-bold grow ${
-              isFullscreen && "pointer-events-none opacity-0"
-            }`}
-          >
-            {label}
-            <div className="mt-4 rounded-full flex items-center justify-center h-[46px] w-[46px] bg-blue-500">
-              <div className="w-[18px]">
-                <Add />
-              </div>
-            </div>
-          </motion.div>
-
-          {summaryParagraph}
+          {isReduced && reducedInfo}
         </motion.div>
+        {fullscreenDetails}
       </li>
     );
   }
