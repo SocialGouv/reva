59841ea2840672ef142052a4f77cec0eda079e44
Cédric Soulas <soulascedric@gmail.com>
2022-03-31 16:50:10 +0200

    feat(app): compute real screen size

--- packages/reva-app/src/App.tsx
+++ packages/reva-app/src/App.tsx
@@ -5,12 +5,18 @@ import { AnimatePresence } from "framer-motion";
 import { Just, Maybe, Nothing } from "purify-ts/Maybe";
 import { useEffect, useState } from "react";
 
+import {
+  loremIpsum,
+  loremIpsumLong,
+  loremIpsumVeryLong,
+} from "./components/atoms/LoremIpsum";
 import { Navigation, Page } from "./components/organisms/Page";
 import { Certificate } from "./interface";
 import { CertificateDetails } from "./pages/CertificateDetails";
 import { Certificates } from "./pages/Certificates";
 import { ProjectGoals } from "./pages/ProjectGoals";
 import { ProjectHome } from "./pages/ProjectHome";
+import useWindowSize from "./utils/useWindowSize";
 
 const GET_CERTIFICATE = gql`
   query Certification($id: ID!) {
@@ -34,6 +40,13 @@ function App() {
     page: initialPage,
   };
   const [navigation, setNavigation] = useState<Navigation>(initialNavigation);
+
+  const windowSize = useWindowSize();
+
+  const appSize =
+    windowSize.width > 540
+      ? { width: 480, height: windowSize.height * 0.85 }
+      : windowSize;
 
   const [getCertification, { data }] = useLazyQuery(GET_CERTIFICATE);
 
@@ -121,50 +134,56 @@ function App() {
     />
   );
 
-  console.log(navigation.page);
-
   return (
-    <div className="App relative flex flex-col items-center justify-center h-screen bg-gray-400">
-      {Capacitor.isNativePlatform() ? (
+    <div className="App relative flex flex-col items-center justify-center bg-slate-200 h-screen px-20">
+      <div className="rounded-xl overflow-hidden shadow-2xl w-full flex items-center justify-between max-w-5xl">
+        {Capacitor.isNativePlatform() ? (
+          <div
+            className={`transition-opacity duration-200 ${
+              maybeCurrentCertificate.isJust() ? "opacity-0" : "opacity-1"
+            } absolute z-50 h-12 top-0 inset-x-0 backdrop-blur-md bg-white/50`}
+          ></div>
+        ) : (
+          <></>
+        )}
+        <div className="flex-1 bg-white text-4xl h-full w-full border-r">
+          <div className="py-12 pl-8 pr-24">
+            <h3 className="font-bold mb-8">REVA</h3>
+            <p className="text-gray-400 text-lg">Version de démonstration.</p>
+          </div>
+        </div>
         <div
-          className={`transition-opacity duration-200 ${
-            maybeCurrentCertificate.isJust() ? "opacity-0" : "opacity-1"
-          } absolute z-50 h-12 top-0 inset-x-0 backdrop-blur-md bg-white/50`}
-        ></div>
-      ) : (
-        <></>
-      )}
-      <div
-        className="relative flex flex-col w-full h-screen bg-white overflow-hidden"
-        style={{ maxWidth: "416px" }}
-      >
-        <AnimatePresence custom={navigation.direction} initial={false}>
-          {navigation.page === "search/results" ||
-          navigation.page === "certificate/summary"
-            ? certificatesPage
-            : navigation.page === "project/home"
-            ? maybeCurrentCertificate.mapOrDefault(
-                projectHomePage,
-                certificatesPage
-              )
-            : navigation.page === "project/goals"
-            ? maybeCurrentCertificate.mapOrDefault(
-                projectGoalsPage,
-                certificatesPage
-              )
-            : maybeCurrentCertificate.mapOrDefault(
-                (certificate) => (
-                  <CertificateDetails
-                    key="show-certificate-details"
-                    certificate={certificate}
-                    navigation={navigation}
-                    setNavigationNext={setNavigationNext}
-                    setNavigationPrevious={setNavigationPrevious}
-                  />
-                ),
-                <></>
-              )}
-        </AnimatePresence>
+          className="flex-1 shadow-xl relative flex flex-col w-full bg-white overflow-hidden"
+          style={appSize}
+        >
+          <AnimatePresence custom={navigation.direction} initial={false}>
+            {navigation.page === "search/results" ||
+            navigation.page === "certificate/summary"
+              ? certificatesPage
+              : navigation.page === "project/home"
+              ? maybeCurrentCertificate.mapOrDefault(
+                  projectHomePage,
+                  certificatesPage
+                )
+              : navigation.page === "project/goals"
+              ? maybeCurrentCertificate.mapOrDefault(
+                  projectGoalsPage,
+                  certificatesPage
+                )
+              : maybeCurrentCertificate.mapOrDefault(
+                  (certificate) => (
+                    <CertificateDetails
+                      key="show-certificate-details"
+                      certificate={certificate}
+                      navigation={navigation}
+                      setNavigationNext={setNavigationNext}
+                      setNavigationPrevious={setNavigationPrevious}
+                    />
+                  ),
+                  <></>
+                )}
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   );
