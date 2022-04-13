4d7edf41e28994c01a3424d1c48215a2f55965b3
Cédric Soulas <soulascedric@gmail.com>
2022-04-13 15:24:56 +0200

    cleanup

--- packages/reva-app/src/components/atoms/Select/index.tsx
+++ packages/reva-app/src/components/atoms/Select/index.tsx
@@ -38,21 +38,19 @@ export const Select = ({
           {label}
         </label>
       )}
-      <div className="relative">
-        <select
-          id={name}
-          name={name}
-          className="px-6 rounded-t flex items-center w-full h-16 border-0 bg-gray-100 border-b-[3px] border-gray-600 focus:ring-0 focus:border-blue-600 text-lg"
-          {...props}
-        >
-          <option value="" selected disabled hidden>
-            Selectionnez une option
-          </option>
-          {options.map((opt) => (
-            <option value={opt.value}>{opt.label}</option>
-          ))}
-        </select>
-      </div>
+      <select
+        id={name}
+        name={name}
+        className="px-6 rounded-t flex items-center w-full h-16 border-0 bg-gray-100 border-b-[3px] border-gray-600 focus:ring-0 focus:border-blue-600 text-lg"
+        {...props}
+      >
+        <option value="" selected disabled hidden>
+          Selectionnez une option
+        </option>
+        {options.map((opt) => (
+          <option value={opt.value}>{opt.label}</option>
+        ))}
+      </select>
     </div>
   );
 };
