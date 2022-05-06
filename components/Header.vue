c451b18ee3d88c3ae0760862274ae7521db05f71
Cédric Soulas <soulascedric@gmail.com>
2022-05-06 14:06:37 +0200

    feat(website): add reva xp logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <header>
-    <nav class="bg-white">
+    <nav class="bg-white border-b">
       <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div class="relative flex justify-between py-1">
           <div class="flex-1 flex items-stretch justify-start">
@@ -15,32 +15,10 @@
               <a href="https://beta.gouv.fr/" target="_blank">
                 <img
                   class="h-4 lg:h-10 w-auto ml-2"
-                  src="/illustrations/beta-gouv-logo-a3.png"
+                  src="/illustrations/reva.svg"
                   alt="beta.gouv"
                 />
               </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-
-    <nav class="bg-white border border-t">
-      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div class="relative flex justify-between py-2">
-          <div
-            class="
-              flex-1 flex
-              items-center
-              justify-center
-              sm:items-stretch sm:justify-start
-            "
-          >
-            <div class="ml-2 text-gray-800">
-              <nuxt-link to="/" class="hover:text-gray-600 font-bold">
-                REVA
-              </nuxt-link>
-              <span>- Reconnaître et valider l'expérience</span>
             </div>
           </div>
         </div>
