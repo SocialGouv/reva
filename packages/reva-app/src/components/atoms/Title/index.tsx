da36cc5f2ac6a3faae4165330e58f0a54a62e4ea
Cédric Soulas <soulascedric@gmail.com>
2022-05-17 11:23:43 +0200

    feat(app): improve title levels

--- packages/reva-app/src/components/atoms/Title/index.tsx
+++ packages/reva-app/src/components/atoms/Title/index.tsx
@@ -1,17 +1,28 @@
 type Size = "small" | "large";
+type Level = 1 | 2;
 
 interface TitleProps {
   label: string;
+  level?: Level;
   size?: Size;
 }
 
-export const Title = ({ label = "", size = "small", ...props }: TitleProps) => (
-  <h3
-    className={`mt-4 text-slate-800 font-bold ${
-      size == "small" ? "text-xl" : "text-2xl"
-    }`}
-    {...props}
-  >
-    {label}
-  </h3>
-);
+export const Title = ({
+  label = "",
+  level = 1,
+  size = "small",
+  ...props
+}: TitleProps) => {
+  const className = `mt-4 text-slate-800 font-bold ${
+    size == "small" ? "text-xl" : "text-2xl"
+  }`;
+  return level === 1 ? (
+    <h1 className={className} {...props}>
+      {label}
+    </h1>
+  ) : (
+    <h2 className={className} {...props}>
+      {label}
+    </h2>
+  );
+};
