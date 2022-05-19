db1f39bb01386d55c294da7232cf9058192da7d4
Anthonny <anthonny.querouil@gmail.com>
2022-05-19 21:11:04 +0200

    fix(api): fix tests

--- packages/reva-api/domains/candidacy/candidacy.test.ts
+++ packages/reva-api/domains/candidacy/candidacy.test.ts
@@ -3,42 +3,8 @@ import { FunctionalCodeError, FunctionalError } from "../FunctionalError";
 import { CandidacyInput, createCandidacy } from "./index";
 
 describe("domain candidacy", () => {
-  test("given an unknown companion, when createCandidacy is call, then an error COMPANION_NOT_FOUND should occured", async () => {
-    const createCandidacyWithDeps = createCandidacy({
-      existsCompanion: () => Promise.resolve(Left('Error')),
-      getCandidacyFromDeviceId: (id: string) => Promise.resolve(Left('Not found')),
-      createCandidacy: () => Promise.resolve(Right(
-        {
-          id: '123',
-          deviceId: '12',
-          companionId: '456',
-          experiences: [],
-          goals: [],
-        }))
-    });
-
-
-    const result = await createCandidacyWithDeps({
-      candidacy: {
-        deviceId: '12',
-        companionId: '456',
-        experiences: [],
-        goals: [],
-      }
-    });
-
-    expect(result.extract()).toBeInstanceOf(FunctionalError);
-    expect(result.extract()).toEqual(
-      expect.objectContaining({
-        code: FunctionalCodeError.COMPANION_NOT_FOUND
-      }));
-  });
-
   test("given an device with an existing candidacy, when createCandidacy is call, then an error CANDIDACY_ALREADY_EXISTS should occured", async () => {
     const createCandidacyWithDeps = createCandidacy({
-      existsCompanion: () => Promise.resolve(Right({
-        id: 'companion_123'
-      })),
       getCandidacyFromDeviceId: (id: string) => Promise.resolve(Right(
         {
           id: '123',
@@ -46,6 +12,10 @@ describe("domain candidacy", () => {
           companionId: '456',
           experiences: [],
           goals: [],
+          phone: '0600000000',
+          email: 'john.doe@reva.beta.gouv.fr',
+          createdAt: new Date(),
+          certificationId: '1234'
         })),
       createCandidacy: () => Promise.resolve(Right(
         {
@@ -54,17 +24,17 @@ describe("domain candidacy", () => {
           companionId: '456',
           experiences: [],
           goals: [],
+          phone: '0600000000',
+          email: 'john.doe@reva.beta.gouv.fr',
+          createdAt: new Date(),
+          certificationId: '1234'
         }))
     });
 
 
     const result = await createCandidacyWithDeps({
-      candidacy: {
-        deviceId: '12',
-        companionId: '456',
-        experiences: [],
-        goals: [],
-      }
+      deviceId: '12',
+      certificationId: '1234'
     });
 
     expect(result.extract()).toBeInstanceOf(FunctionalError);
@@ -76,21 +46,14 @@ describe("domain candidacy", () => {
 
   test("given an device without any candidacy, when createCandidacy is call and an error occurs, then an error CANDIDACY_NOT_CREATED should occured", async () => {
     const createCandidacyWithDeps = createCandidacy({
-      existsCompanion: () => Promise.resolve(Right({
-        id: 'companion_123'
-      })),
       getCandidacyFromDeviceId: (id: string) => Promise.resolve(Left('Not found')),
       createCandidacy: () => Promise.resolve(Left('Error while saving'))
     });
 
 
     const result = await createCandidacyWithDeps({
-      candidacy: {
-        deviceId: '12',
-        companionId: '456',
-        experiences: [],
-        goals: [],
-      }
+      deviceId: '12',
+      certificationId: '1234'
     });
 
     expect(result.extract()).toBeInstanceOf(FunctionalError);
@@ -102,35 +65,34 @@ describe("domain candidacy", () => {
 
   test("given an device without any candidacy, when createCandidacy is call, then ", async () => {
     const createCandidacyWithDeps = createCandidacy({
-      existsCompanion: () => Promise.resolve(Right({
-        id: 'companion_123'
-      })),
       getCandidacyFromDeviceId: (id: string) => Promise.resolve(Left('Not found')),
-      createCandidacy: (params: {candidacy: CandidacyInput}) => Promise.resolve(Right(
+      createCandidacy: (params: { deviceId: string; certificationId: string }) => Promise.resolve(Right(
         {
-          ...params.candidacy,
+          ...params,
           id: '123',
+          companionId: '456',
+          experiences: [],
+          goals: [],
+          phone: '0600000000',
+          email: 'john.doe@reva.beta.gouv.fr',
+          createdAt: new Date()
         }))
     });
 
 
     const result = await createCandidacyWithDeps({
-      candidacy: {
-        deviceId: '12',
-        companionId: '456',
-        experiences: [],
-        goals: [],
-      }
+      deviceId: '12',
+      certificationId: '1234'
     });
 
     expect(result.extract()).not.toBeInstanceOf(FunctionalError);
     expect(result.extract()).toEqual(
       expect.objectContaining({
-          id: '123',
-          deviceId: '12',
-          companionId: '456',
-          experiences: [],
-          goals: [],
-        }));
+        id: '123',
+        deviceId: '12',
+        companionId: '456',
+        experiences: [],
+        goals: [],
+      }));
   });
 });
