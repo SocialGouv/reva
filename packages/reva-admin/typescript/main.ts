292a8c8279f01854c7d3197cc6243cdad347bc76
Cédric Soulas <soulascedric@gmail.com>
2022-06-08 09:49:52 +0200

    remove logs

--- packages/reva-admin/typescript/main.ts
+++ packages/reva-admin/typescript/main.ts
@@ -1,8 +1,6 @@
 // @ts-ignore
 import { Elm } from "../src/Main.elm";
 
-console.log(process.env.API_GRAPHQL)
-
 const app = Elm.Main.init({
      flags: {
         endpoint: process.env.API_GRAPHQL || "http://localhost:8080/graphql",
