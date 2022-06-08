d08d2023dc9300a746fff508eb498378517bb834
Cédric Soulas <soulascedric@gmail.com>
2022-06-08 08:45:54 +0200

    feat(admin): add candidacy deletion

--- packages/reva-admin/typescript/main.ts
+++ packages/reva-admin/typescript/main.ts
@@ -1,6 +1,8 @@
 // @ts-ignore
 import { Elm } from "../src/Main.elm";
 
+console.log(process.env.API_GRAPHQL)
+
 const app = Elm.Main.init({
      flags: {
         endpoint: import.meta.env.API_GRAPHQL || "http://localhost:8080/graphql",
