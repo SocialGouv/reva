fc7a52b2928f6641b57b93d0aad032eb35f1845b
Cédric Soulas <soulascedric@gmail.com>
2022-06-10 11:34:30 +0200

    feat(app): update project submitted page (#355)

--- packages/reva-app/src/components/molecules/ProgressTitle/index.tsx
+++ packages/reva-app/src/components/molecules/ProgressTitle/index.tsx
@@ -1,26 +1,39 @@
 import { Title } from "../../atoms/Title";
 
 type Size = "small" | "large";
+type Theme = "dark" | "light";
 
 interface Props {
   progress: number;
   size?: Size;
+  theme?: Theme;
   title: string;
 }
 
-export const ProgressTitle = ({ progress, size = "small", title }: Props) => {
+export const ProgressTitle = ({
+  progress,
+  size = "small",
+  theme = "light",
+  title,
+}: Props) => {
   return (
     <>
       <div className="flex items-end justify-between">
-        <Title label={title} size={size} />
+        <Title label={title} size={size} theme={theme} />
         <div
           data-test="progress-title-value"
-          className="font-semibold text-base text-gray-500"
+          className={`font-semibold text-base ${
+            theme == "dark" ? "text-white" : "text-gray-500"
+          }`}
         >
           {progress}%
         </div>
       </div>
-      <div className="mt-2 w-full bg-slate-300 rounded-full h-[5px]">
+      <div
+        className={`mt-2 w-full rounded-full h-[5px] ${
+          theme == "dark" ? "bg-gray-900" : "bg-slate-300"
+        }`}
+      >
         <div
           className={`${
             size == "small" ? "bg-blue-600" : "bg-slate-900"
