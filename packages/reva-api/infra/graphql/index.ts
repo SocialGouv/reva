8cc0d3121a303a581db5371a59bfacd082ac413f
Anthonny Quérouil <anthonny.querouil@gmail.com>
2022-08-04 16:56:57 +0200

    chore(api): use graphql-scalars lib instead of custom resolver (#381)

--- packages/reva-api/infra/graphql/index.ts
+++ packages/reva-api/infra/graphql/index.ts
@@ -2,7 +2,8 @@ import * as path from "path";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { GraphQLScalarType } from "graphql"
+import { GraphQLScalarType } from "graphql";
+import { VoidTypeDefinition, VoidResolver, TimestampTypeDefinition, TimestampResolver } from 'graphql-scalars';
 
 // Resolvers
 
@@ -10,41 +11,21 @@ import * as search from "./search";
 import * as candidacy from "./candidacy";
 import * as referential from "./referential";
 
-const dateScalar = new GraphQLScalarType({
-  name: 'Date',
-  parseValue(value: any) {
-    return new Date(value);
-  },
-  serialize(value: any) {
-    return value.getTime();
-  },
-})
-
-const Void = new GraphQLScalarType({
-    name: 'Void',
-    description: 'Represents NULL values',
-    serialize() {
-        return null
-    },
-    parseValue() {
-        return null
-    },
-    parseLiteral() {
-        return null
-    }
-})
-
 const typeDefs = loadFilesSync(path.join(__dirname, "."), {
   extensions: ["graphql"],
   recursive: true,
 });
+
 const resolvers = mergeResolvers([search.resolvers, candidacy.resolvers, referential.resolvers]);
-resolvers.Date = dateScalar
-resolvers.Void = Void
+resolvers.Void = VoidResolver;
+resolvers.Timestamp = TimestampResolver;
 
 export const graphqlConfiguration = {
   schema: makeExecutableSchema({
-    typeDefs: mergeTypeDefs(typeDefs),
+    typeDefs: mergeTypeDefs([...typeDefs
+      , TimestampTypeDefinition
+      , VoidTypeDefinition
+    ]),
     resolvers,
   }),
   graphiql: !!process.env.GRAPHIQL,
