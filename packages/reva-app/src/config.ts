cca2ece678d0cf99e1b6128095a2f82339618f1d
Cédric Soulas <soulascedric@gmail.com>
2022-08-26 10:03:42 +0200

    fix candidate button not being hidden

--- packages/reva-app/src/config.ts
+++ packages/reva-app/src/config.ts
@@ -4,9 +4,12 @@ const visible = { y: 0, scale: 1, opacity: 1 };
 
 export const buttonVariants = {
   hidden: (page: State) =>
-    ["searchResults", "searchResultsError", "loadingCertifications"].includes(
-      page
-    )
+    [
+      "applicationDataLoaded",
+      "searchResults",
+      "searchResultsError",
+      "loadingCertifications",
+    ].includes(page)
       ? { y: 120, scale: 0.98, opacity: 1 }
       : visible,
   visible,
