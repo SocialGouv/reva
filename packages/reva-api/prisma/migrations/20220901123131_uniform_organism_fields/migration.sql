05d2baa3f4bd9ca804ea13ea4c79ba42e423cd8d
Anthonny <anthonny.querouil@gmail.com>
2022-09-01 16:08:00 +0200

    feat(api): rework sql migration to use new field of organism type in seed

--- packages/reva-api/prisma/migrations/20220901123131_uniform_organism_fields/migration.sql
+++ packages/reva-api/prisma/migrations/20220901123131_uniform_organism_fields/migration.sql
@@ -2,27 +2,13 @@
 -- DropIndex
 DROP INDEX "organism_contact_administratif_key";
 
--- Add columns
+-- Rename old columns and default
 ALTER TABLE "organism" 
-  ADD COLUMN     "address" TEXT DEFAULT '' NOT NULL,
-  ADD COLUMN     "contact_administrative_email" VARCHAR(255) DEFAULT '' NOT NULL,
-  ADD COLUMN     "zip" VARCHAR(5) DEFAULT '' NOT NULL;
-
-
--- Copy current content
-UPDATE "organism"
-  SET "address" = "adress",
-      "zip" = "cp",
-      "contact_administrative_email" = "contact_administratif";
-
--- Remove old columns and default
+  RENAME COLUMN "adress" TO "address";
 ALTER TABLE "organism" 
-  DROP COLUMN "adress",
-  DROP COLUMN "contact_administratif",
-  DROP COLUMN "cp",
-  ALTER COLUMN "address" DROP DEFAULT,
-  ALTER COLUMN "contact_administrative_email" DROP DEFAULT,
-  ALTER COLUMN "zip" DROP DEFAULT;
+  RENAME COLUMN "contact_administratif" TO "contact_administrative_email";
+ALTER TABLE "organism" 
+  RENAME COLUMN "cp" TO "zip";
 
 -- CreateIndex
 CREATE UNIQUE INDEX "organism_contact_administrative_email_key" ON "organism"("contact_administrative_email");
