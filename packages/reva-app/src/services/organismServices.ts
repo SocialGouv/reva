a884e16a8a415d807c179bab107e95ab5163a7a5
Beh <dev@behrouze.com>
2022-09-01 14:40:34 +0200

    Ajouter un service de sauvegarde d'un organisme

--- packages/reva-app/src/services/organismServices.ts
+++ packages/reva-app/src/services/organismServices.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, gql } from "@apollo/client";
 
 const GET_ORGANISMS_FOR_CANDIDACY = gql`
-  query OrganismsForCandidacy($query: UUID!) {
+  query getOrganismsForCandidacy($query: UUID!) {
     getOrganismsForCandidacy(candidacyId: $query) {
       id
       label
@@ -19,4 +19,27 @@ export const getOrganismsForCandidacy =
     client.query({
       query: GET_ORGANISMS_FOR_CANDIDACY,
       variables: { query },
+    });
+
+const SET_ORGANISMS_FOR_CANDIDACY = gql`
+  mutation setOrganismsForCandidacy($candidacyId: UUID!, $organismId: UUID!) {
+    candidacy_selectOrganism(
+      candidacyId: $candidacyId
+      organismId: $organismId
+    ) {
+      id
+      candidacyId
+      organismId
+      regionId
+      createdAt
+    }
+  }
+`;
+
+export const setOrganismsForCandidacy =
+  (client: ApolloClient<object>) =>
+  ({ candidacyId, organismId }: { candidacyId: string; organismId: string }) =>
+    client.mutate({
+      mutation: SET_ORGANISMS_FOR_CANDIDACY,
+      variables: { candidacyId, organismId },
     });
