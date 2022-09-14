af6523a6d998b7355507a5cc752f7414a30514f3
Beh <dev@behrouze.com>
2022-09-14 14:32:03 +0200

    first intercepts

--- packages/reva-tests/cypress/e2e/project.cy.js
+++ packages/reva-tests/cypress/e2e/project.cy.js
@@ -1,12 +1,17 @@
 context("Project", () => {
-  beforeEach(() => {
-    cy.visit("/");
-  });
-
   it("attempt to validate project", function () {
+    cy.intercept('/graphql', { fixture: 'getCandidacy' }).as('getCandidacy')
+    cy.visit("/");
+
+    cy.intercept('/graphql', { fixture: 'certifications' }).as('certifications')
     cy.get("#select_region").select("11");
+
+    cy.intercept('/graphql', { fixture: 'getCertification' }).as('getCertification')
     cy.get('[data-test="results"] [data-type="card"]').eq(4).click();
+
+    cy.intercept('/graphql', { fixture: 'createCandidacy' }).as('createCandidacy')
     cy.get('[data-test="certification-submit"]').click();
+
     cy.get('[data-test="submission-home-show-project-home"]').click();
     cy.get('[data-test="project-home-validate-locked"]').click();
     cy.get('[data-test="project-help"]').should("exist");
