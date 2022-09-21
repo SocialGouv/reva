debada5013918ca935d1bdce001c60cca9f38189
Beh <dev@behrouze.com>
2022-09-21 16:26:09 +0200

    checkbox & state

--- packages/reva-app/src/pages/TrainingProgram.tsx
+++ packages/reva-app/src/pages/TrainingProgram.tsx
@@ -1,5 +1,5 @@
 import { useActor } from "@xstate/react";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Interpreter } from "xstate";
 
 import { CardBasic } from "../components/atoms/CardBasic";
@@ -14,6 +14,7 @@ interface Props {
 
 export const TrainingProgram: FC<Props> = ({ mainService }) => {
   const [state] = useActor(mainService);
+  const [checkedCondition, setCheckedCondition] = useState(false);
 
   if (!state.context.trainingProgram) return <></>;
 
@@ -39,12 +40,10 @@ export const TrainingProgram: FC<Props> = ({ mainService }) => {
         <h1 className="text-white	text-3xl font-extrabold mb-8">
           Votre parcours personnalisé
         </h1>
-
         <CardBasic
           title="Mon accompagnateur"
           text={state.context.organism?.label}
         />
-
         <dl>
           <DescriptionSimple
             term="Diplome visé"
@@ -81,9 +80,26 @@ export const TrainingProgram: FC<Props> = ({ mainService }) => {
           <DescriptionSimple term="Autre" detail={otherTraining} />
         </dl>
 
+        <div className="my-8">
+          <input
+            type="checkbox"
+            id="accept-conditions"
+            className="checked:bg-blue-500"
+            onClick={() => setCheckedCondition(!checkedCondition)}
+          />
+          <label htmlFor="accept-conditions" className="ml-4 text-slate-400">
+            J'ai bien compris qu'il s'agissait des étapes et prestations
+            nécessaires pour que j'obtienne mon diplôme et je m'engage à les
+            suivre ou informer mon accompagnateur de tout abandon dans les 48h.
+            J'accepte que les résultats de mon étude personnalisée ainsi que le
+            résultat à ma session de jury me soient transmis ainsi qu'à mon
+            accompagnateur.
+          </label>
+        </div>
         <button
           className="bg-gray-200 text-gray-600 font-bold py-2 px-4 rounded"
           data-test="submit-confirm-path"
+          disabled={!checkedCondition}
           onClick={
             () => alert("SUBMIT_CONFIRM_PATH")
             // send({
