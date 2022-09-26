c5d154ce633ccf36750f34fe6cc90273ae29f048
Beh <dev@behrouze.com>
2022-09-26 16:16:33 +0200

    the TAB is back

--- packages/reva-app/src/pages/TrainingProgram.tsx
+++ packages/reva-app/src/pages/TrainingProgram.tsx
@@ -39,7 +39,7 @@ export const TrainingProgram: FC<Props> = ({ mainService }) => {
       direction={state.context.direction}
     >
       <div className="px-4 pb-8 flex flex-col">
-        <h1 className="text-white	text-3xl font-extrabold mb-8">
+        <h1 className="text-white text-3xl font-extrabold mb-8">
           Votre parcours personnalisé
         </h1>
         <CardBasic
