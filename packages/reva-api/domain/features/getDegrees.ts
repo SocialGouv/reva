ccdfeda4e6299a5aebac686c1f6e7925426cba86
Anthonny <anthonny.querouil@gmail.com>
2022-09-27 16:06:15 +0200

    feat(api): add degrees and vulnerability indicators

---
+++ packages/reva-api/domain/features/getDegrees.ts
@@ -0,0 +1,11 @@
+import { Either, EitherAsync } from "purify-ts";
+import {  Degree } from "../types/candidacy";
+import { FunctionalCodeError, FunctionalError } from "../types/functionalError";
+
+interface GetDegreesDeps {
+    getDegrees: () => Promise<Either<string, Degree[]>>;
+}
+
+export const getDegrees = (deps: GetDegreesDeps) => async () => 
+    EitherAsync.fromPromise(() => deps.getDegrees())
+        .mapLeft(() => new FunctionalError(FunctionalCodeError.TECHNICAL_ERROR, 'Erreur lors de la récupération des diplômes'));
