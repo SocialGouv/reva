bf6c526b394722ce115ea3d7118fe3fbcacd5e34
Cédric Soulas <soulascedric@gmail.com>
2022-10-06 18:47:10 +0200

    add hover on footer links

--- packages/reva-app/src/components/organisms/Footer/index.tsx
+++ packages/reva-app/src/components/organisms/Footer/index.tsx
@@ -1,20 +1,26 @@
+import { FC } from "react";
+
 import logoImg from "../../atoms/Logo/logo.png";
 
-const Divider = () => <span className="text-gray-600">|</span>;
-
+const Divider = () => <span className="text-gray-400">|</span>;
+const Link: FC<{ href: string }> = ({ children, href }) => (
+  <a className="hover:text-gray-900" href={href}>
+    {children}
+  </a>
+);
 export const Footer = () => (
   <footer>
     <div className="w-full hidden sm:flex items-end justify-center text-gray-600 text-sm pb-6">
       <div className="space-x-2">
-        <a href="https://reva.incubateur.net/">Site Reva</a>
+        <Link href="https://reva.incubateur.net/">Site Reva</Link>
         <Divider />
-        <a href="https://reva.incubateur.net/mentions-l%C3%A9gale">
+        <Link href="https://reva.incubateur.net/mentions-l%C3%A9gale">
           Mentions légales
-        </a>
+        </Link>
         <Divider />
-        <a href="https://reva.incubateur.net/politique-de-confidentialit%C3%A9">
+        <Link href="https://reva.incubateur.net/politique-de-confidentialit%C3%A9">
           Données personnelles
-        </a>
+        </Link>
       </div>
     </div>
     <div className="fixed bottom-0 left-0 hidden sm:block p-6">
