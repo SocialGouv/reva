37bcca62a48441b6b97fcf49cbfef4234da00382
Cédric Soulas <soulascedric@gmail.com>
2022-10-07 13:27:06 +0200

    feat(app): add web app footer and layout (#467)

---
+++ packages/reva-app/src/components/organisms/Footer/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+
+import logoImg from "../../atoms/Logo/logo.png";
+
+const Divider = () => <span className="text-gray-400">|</span>;
+const Link: FC<{ href: string }> = ({ children, href }) => (
+  <a className="hover:text-gray-900" href={href}>
+    {children}
+  </a>
+);
+export const Footer = () => (
+  <footer>
+    <div className="w-full hidden sm:flex items-end justify-center text-gray-600 text-sm pb-6">
+      <div className="space-x-2">
+        <Link href="/">Site Reva</Link>
+        <Divider />
+        <Link href="/mentions-l%C3%A9gale">Mentions légales</Link>
+        <Divider />
+        <Link href="/politique-de-confidentialit%C3%A9">
+          Données personnelles
+        </Link>
+      </div>
+    </div>
+    <div className="fixed bottom-0 left-0 hidden sm:block p-6">
+      <a href="/">
+        <img alt="Reva" className="h-6" src={logoImg} />
+      </a>
+    </div>
+  </footer>
+);
