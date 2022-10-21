b88e261a95bf06f6301a97e5ac2bedb2c289c93b
Anthonny <anthonny.querouil@gmail.com>
2022-10-21 12:10:08 +0200

    feat(api): change password generation for candidate

--- packages/reva-api/infra/graphql/candidate/index.ts
+++ packages/reva-api/infra/graphql/candidate/index.ts
@@ -50,6 +50,28 @@ const getCandidateAccountInIAM = (keycloakAdmin: KeycloakAdminClient) => async (
   }
 };
 
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const integers = "0123456789";
+const exCharacters = "!@#$%^&*_-=+";
+const createPassword = (length: number, hasNumbers: boolean, hasSymbols: boolean) => {
+    let chars = alpha;
+    if (hasNumbers) {
+        chars += integers;
+    }
+    if (hasSymbols) {
+        chars += exCharacters;
+    }
+    return generatePassword(length, chars);
+};
+
+const generatePassword = (length: number, chars: string) => {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+};
+
 const createCandidateAccountInIAM = (keycloakAdmin: KeycloakAdminClient) => async (account: {
   email: string,
   firstname?: string;
@@ -73,7 +95,7 @@ const createCandidateAccountInIAM = (keycloakAdmin: KeycloakAdminClient) => asyn
 };
 
 const generateIAMToken = (keycloakAdmin: KeycloakAdminClient) => async (userId: string) => {
-  const randomPassword = `${Date.now()}`
+  const randomPassword = createPassword(20, true, true)
 
   const user = await keycloakAdmin.users.findOne({
     id: userId,
