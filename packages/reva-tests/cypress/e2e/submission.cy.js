59814561a357caf78c9655019800f6b003762289
Cédric Soulas <soulascedric@gmail.com>
2022-10-24 22:12:15 +0200

    fix(app): display submission home when project taken over (#494)

--- packages/reva-tests/cypress/e2e/submission.cy.js
+++ packages/reva-tests/cypress/e2e/submission.cy.js
@@ -12,4 +12,16 @@ context("Submission", () => {
 
     cy.get('[data-test="submission-home-project-submitted"');
   });
+
+  it("log on a project taken over", function () {
+    cy.intercept("POST", "/graphql", (req) => {
+      stubMutation(req, "candidate_login", "candidate2-taken-over.json");
+      stubQuery(req, "getReferential", "referential.json");
+    });
+    cy.visit("/login?token=abc");
+    cy.wait("@candidate_login");
+    cy.wait("@getReferential");
+
+    cy.get('[data-test="submission-home-project-submitted"');
+  });
 });
