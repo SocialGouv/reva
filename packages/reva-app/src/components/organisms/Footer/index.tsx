8c72cbcfc737d8db96653b99116fb0341bf214ab
Cédric Soulas <soulascedric@gmail.com>
2022-10-25 10:01:35 +0200

    feat(app): open footer links to new tab (#496)

--- packages/reva-app/src/components/organisms/Footer/index.tsx
+++ packages/reva-app/src/components/organisms/Footer/index.tsx
@@ -3,8 +3,12 @@ import { FC } from "react";
 import logoImg from "../../atoms/Logo/logo.png";
 
 const Divider = () => <span className="text-gray-400">|</span>;
-const Link: FC<{ href: string }> = ({ children, href }) => (
-  <a className="hover:text-gray-900" href={href}>
+const Link: FC<{ href: string; target?: string }> = ({
+  children,
+  href,
+  target = "_self",
+}) => (
+  <a className="hover:text-gray-900" href={href} target={target}>
     {children}
   </a>
 );
@@ -12,13 +16,19 @@ export const Footer = () => (
   <footer>
     <div className="w-full hidden sm:flex items-end justify-center text-gray-600 text-sm pb-6">
       <div className="space-x-2">
-        <Link href="/">Site Reva</Link>
+        <Link href="/" target="_blank">
+          Site Reva
+        </Link>
         <Divider />
-        <Link href="/cgu">CGU</Link>
+        <Link href="/cgu" target="_blank">
+          CGU
+        </Link>
         <Divider />
-        <Link href="/mentions-l%C3%A9gale">Mentions légales</Link>
+        <Link href="/mentions-l%C3%A9gale" target="_blank">
+          Mentions légales
+        </Link>
         <Divider />
-        <Link href="/politique-de-confidentialit%C3%A9">
+        <Link href="/politique-de-confidentialit%C3%A9" target="_blank">
           Données personnelles
         </Link>
       </div>
