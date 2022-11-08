ca50a39f8e84aa0461f2703b7e65ca93aa66760f
Cédric Soulas <soulascedric@gmail.com>
2022-11-08 16:56:57 +0100

    intercept auth on staging

--- packages/reva-tests/cypress/e2e/certificates.cy.js
+++ packages/reva-tests/cypress/e2e/certificates.cy.js
@@ -2,6 +2,22 @@ import { stubMutation, stubQuery } from "../utils/graphql";
 
 context("Certificates", () => {
   beforeEach(() => {
+    cy.intercept(
+      "**/realms/reva-app/protocol/openid-connect/3p-cookies/step1.html",
+      {
+        fixture: "auth-step1.html",
+      }
+    );
+
+    cy.intercept("GET", "**/realms/reva-app/protocol/openid-connect/auth*", {
+      headers: {
+        Location: `${
+          Cypress.config().baseUrl
+        }/app/silent-check-sso.html#error=login_required&state=6a5b9f5c-c131-421c-86e0-5b1d6d5bf44b`,
+      },
+      statusCode: 302,
+    });
+
     cy.intercept("POST", "/graphql", (req) => {
       stubMutation(req, "candidate_login", "candidate1.json");
       stubQuery(req, "getReferential", "referential.json");
