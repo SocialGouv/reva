407b316ddff9155465cd88fb8891b7500f35a054
Cédric Soulas <soulascedric@gmail.com>
2022-11-24 18:25:44 +0100

    feat(admin): enforce requirement on vulnerability indicator and degree

--- packages/reva-api/domain/features/updateCandidate.ts
+++ packages/reva-api/domain/features/updateCandidate.ts
@@ -1,17 +1,43 @@
 import { Candidate } from "@prisma/client";
 import { Either, EitherAsync, Left } from "purify-ts";
+
 import { FunctionalCodeError, FunctionalError } from "../types/functionalError";
 
 interface UpdateCandidate {
-    hasRole: (role: string) => boolean;
-    updateCandidate: (id: string, candidate: Candidate) => Promise<Either<string, Candidate>>;
-};
-
-export const updateCandidate = (deps: UpdateCandidate) => (id: string, candidate: Candidate) => {
-    if (deps.hasRole("admin") || deps.hasRole("manage_candidacy")) {
-        return EitherAsync.fromPromise(() => deps.updateCandidate(id, candidate))
-            .mapLeft(() => new FunctionalError(FunctionalCodeError.CANDIDATE_NOT_SAVED, `Erreur lors de l'enregistrement du candidat ${candidate.email}`));
-    } else {
-        return Left(new FunctionalError(FunctionalCodeError.NOT_AUTHORIZED, `Vous n'êtes pas autorisé à modifier ce candidat`));
-    }
+  hasRole: (role: string) => boolean;
+  updateCandidate: (
+    id: string,
+    candidate: Candidate
+  ) => Promise<Either<string, Candidate>>;
 }
+
+export const updateCandidate =
+  (deps: UpdateCandidate) => (id: string, candidate: Candidate) => {
+    if (!candidate.highestDegreeId || !candidate.vulnerabilityIndicatorId) {
+      return Left(
+        new FunctionalError(
+          FunctionalCodeError.CANDIDATE_NOT_SAVED,
+          `Veuillez sélectionner un diplôme et indicateur de public fragile`
+        )
+      );
+    }
+
+    if (deps.hasRole("admin") || deps.hasRole("manage_candidacy")) {
+      return EitherAsync.fromPromise(() =>
+        deps.updateCandidate(id, candidate)
+      ).mapLeft(
+        () =>
+          new FunctionalError(
+            FunctionalCodeError.CANDIDATE_NOT_SAVED,
+            `Erreur lors de l'enregistrement du candidat ${candidate.email}`
+          )
+      );
+    }
+
+    return Left(
+      new FunctionalError(
+        FunctionalCodeError.NOT_AUTHORIZED,
+        `Vous n'êtes pas autorisé à modifier ce candidat`
+      )
+    );
+  };
