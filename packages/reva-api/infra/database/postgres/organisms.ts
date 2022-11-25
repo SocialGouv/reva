0b0bfea310587ba38e0f1772b2b78dcbf2100cb9
Anthonny Quérouil <anthonny.querouil@gmail.com>
2022-11-25 18:39:19 +0100

    Feat/get companions (#532)

--- packages/reva-api/infra/database/postgres/organisms.ts
+++ packages/reva-api/infra/database/postgres/organisms.ts
@@ -1,60 +1,88 @@
+import { Prisma } from "@prisma/client";
 import { Either, Left, Maybe, Right } from "purify-ts";
-import * as domain from '../../../domain/types/candidacy';
+
+import * as domain from "../../../domain/types/candidacy";
 import { prismaClient } from "./client";
 
-export const getAAPOrganisms = async (params: { candidacyId: string; }): Promise<Either<string, domain.Organism[]>> => {
-    try {
-
-        const candidacy = await prismaClient.candidacy.findFirst({
-            where: {
-                id: params.candidacyId
-            },
-            include: {
-                certificationsAndRegions:{
-                    where: {
-                        isActive: true
-                    },
-                    select: {
-                        certificationId: true,
-                        regionId: true
-                    }
-                }
-            }
-        })
-
-        if (!candidacy) {
-            return Right([])
-        }
-
-        const organisms = await prismaClient.organism.findMany({
-            where: {
-                regionsAndCertifications: {
-                    some: {
-                        certificationId: candidacy.certificationsAndRegions[0].certificationId,
-                        regionId: candidacy.certificationsAndRegions[0].regionId,
-                        isArchitect: true
-                    }
-                },
-                isActive: true
-            }
-        })
-
-        return Right(organisms);
-    } catch (e) {
-        return Left(`error while retrieving organisms`);
-    };
+export const getAAPOrganisms = async (params: {
+  candidacyId: string;
+}): Promise<Either<string, domain.Organism[]>> => {
+  return getOrganisms({ candidacyId: params.candidacyId, isArchitect: true });
 };
 
-export const getOrganismById = async (organismId: string) : Promise<Either<string, Maybe<domain.Organism>>> => {
-    try {
-        const organism = await prismaClient.organism.findFirst({
-            where: {
-                id: organismId
-            }
-        })
+export const getOrganismById = async (
+  organismId: string
+): Promise<Either<string, Maybe<domain.Organism>>> => {
+  try {
+    const organism = await prismaClient.organism.findFirst({
+      where: {
+        id: organismId,
+      },
+    });
 
-        return Right(Maybe.fromNullable(organism))
-    } catch(e) {
-        return Left(`error while retrieving organism ${organismId}`);
+    return Right(Maybe.fromNullable(organism));
+  } catch (e) {
+    return Left(`error while retrieving organism ${organismId}`);
+  }
+};
+
+export const getCompanionOrganisms = async (params: {
+  candidacyId: string;
+}): Promise<Either<string, domain.Organism[]>> => {
+  return getOrganisms({ candidacyId: params.candidacyId, isCompanion: true });
+};
+
+const getOrganisms = async (params: {
+  candidacyId: string;
+  isArchitect?: boolean;
+  isCompanion?: boolean;
+}) => {
+  try {
+    const candidacy = await prismaClient.candidacy.findFirst({
+      where: {
+        id: params.candidacyId,
+      },
+      include: {
+        certificationsAndRegions: {
+          where: {
+            isActive: true,
+          },
+          select: {
+            certificationId: true,
+            regionId: true,
+          },
+        },
+      },
+    });
+
+    if (!candidacy) {
+      return Right([]);
     }
-}
+
+    let filters = {
+      certificationId: candidacy.certificationsAndRegions[0].certificationId,
+      regionId: candidacy.certificationsAndRegions[0].regionId,
+    } as Prisma.OrganismsOnRegionsAndCertificationsWhereInput;
+
+    if (params.isArchitect !== undefined) {
+      filters = { ...filters, isArchitect: params.isArchitect };
+    }
+
+    if (params.isCompanion !== undefined) {
+      filters = { ...filters, isCompanion: params.isCompanion };
+    }
+
+    const organisms = await prismaClient.organism.findMany({
+      where: {
+        regionsAndCertifications: {
+          some: filters,
+        },
+        isActive: true,
+      },
+    });
+
+    return Right(organisms);
+  } catch (e) {
+    return Left(`error while retrieving organisms`);
+  }
+};
--- packages/reva-api/infra/graphql/candidacy/index.ts
+++ packages/reva-api/infra/graphql/candidacy/index.ts
@@ -1,35 +1,36 @@
+import KeycloakAdminClient from "@keycloak/keycloak-admin-client";
+import Keycloak from "keycloak-connect";
+import mercurius from "mercurius";
+
 import { addExperienceToCandidacy } from "../../../domain/features/addExperienceToCandidacy";
 import { archiveCandidacy } from "../../../domain/features/archiveCandidacy";
 import { createCandidacy } from "../../../domain/features/createCandidacy";
 import { deleteCandidacy } from "../../../domain/features/deleteCandidacy";
-import { getCandidacy } from "../../../domain/features/getCandidacy";
+import { getBasicSkills } from "../../../domain/features/getBasicSkills";
 import { getCandidacySummaries } from "../../../domain/features/getCandidacies";
-import { getCompanions } from "../../../domain/features/getCompanions";
+import { getCandidacy } from "../../../domain/features/getCandidacy";
+import { getCompanionsForCandidacy } from "../../../domain/features/getCompanionsForCandidacy";
 import { getDeviceCandidacy } from "../../../domain/features/getDeviceCandidacy";
 import { getAAPOrganismsForCandidacy } from "../../../domain/features/getOrganismsForCandidacy";
+import { getTrainings } from "../../../domain/features/getTrainings";
+import { selectOrganismForCandidacy } from "../../../domain/features/selectOrganismForCandidacy";
 import { submitCandidacy } from "../../../domain/features/submitCandidacy";
+import { submitTraining } from "../../../domain/features/submitTrainingForm";
 import { takeOverCandidacy } from "../../../domain/features/takeOverCandidacy";
 import { updateAppointmentInformations } from "../../../domain/features/updateAppointmentInformations";
 import { updateCertificationOfCandidacy } from "../../../domain/features/updateCertificationOfCandidacy";
 import { updateContactOfCandidacy } from "../../../domain/features/updateContactOfCandidacy";
 import { updateExperienceOfCandidacy } from "../../../domain/features/updateExperienceOfCandidacy";
 import { updateGoalsOfCandidacy } from "../../../domain/features/updateGoalsOfCandidacy";
+import { confirmTrainingFormByCandidate } from "../../../domain/features/validateTrainingFormByCandidate";
+import { Candidacy } from "../../../domain/types/candidacy";
+import * as basicSkillDb from "../../database/postgres/basicSkills";
 import * as candidacyDb from "../../database/postgres/candidacies";
-import * as organismDb from "../../database/postgres/organisms";
 import * as experienceDb from "../../database/postgres/experiences";
 import * as goalDb from "../../database/postgres/goals";
+import * as organismDb from "../../database/postgres/organisms";
 import * as trainingDb from "../../database/postgres/trainings";
-import * as basicSkillDb from "../../database/postgres/basicSkills";
-import mercurius from "mercurius";
-import { getTrainings } from "../../../domain/features/getTrainings";
-import { selectOrganismForCandidacy } from "../../../domain/features/selectOrganismForCandidacy";
 import { notifyNewCandidacy } from "../../mattermost";
-import KeycloakAdminClient from "@keycloak/keycloak-admin-client";
-import Keycloak from 'keycloak-connect';
-import { getBasicSkills } from "../../../domain/features/getBasicSkills";
-import { submitTraining } from "../../../domain/features/submitTrainingForm";
-import { Candidacy } from "../../../domain/types/candidacy";
-import { confirmTrainingFormByCandidate } from "../../../domain/features/validateTrainingFormByCandidate";
 
 const withBasicSkills = (c: Candidacy) => ({
   ...c,
@@ -40,7 +41,7 @@ const withBasicSkills = (c: Candidacy) => ({
   }, []),
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  basicSkills: c.basicSkills.map(bs => bs.basicSkill),
+  basicSkills: c.basicSkills.map((bs) => bs.basicSkill),
 });
 
 const withMandatoryTrainings = (c: Candidacy) => ({
@@ -52,83 +53,154 @@ const withMandatoryTrainings = (c: Candidacy) => ({
   }, []),
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  mandatoryTrainings: c.trainings.map(t => t.training)
+  mandatoryTrainings: c.trainings.map((t) => t.training),
 });
 
 export const resolvers = {
   Query: {
-    getCandidacy: async (other: unknown, { deviceId }: { deviceId: string; }, context: any) => {
-      const result = await getDeviceCandidacy({ getCandidacyFromDeviceId: candidacyDb.getCandidacyFromDeviceId })({ deviceId });
+    getCandidacy: async (
+      other: unknown,
+      { deviceId }: { deviceId: string },
+      context: any
+    ) => {
+      const result = await getDeviceCandidacy({
+        getCandidacyFromDeviceId: candidacyDb.getCandidacyFromDeviceId,
+      })({ deviceId });
       return result
         .map(withBasicSkills)
         .map(withMandatoryTrainings)
-        .mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
-    getCandidacyById: async (other: unknown, { id }: { id: string; }, context: any) => {
-      const result = await getCandidacy({ getCandidacyFromId: candidacyDb.getCandidacyFromId })({ id });
+    getCandidacyById: async (
+      other: unknown,
+      { id }: { id: string },
+      context: any
+    ) => {
+      const result = await getCandidacy({
+        getCandidacyFromId: candidacyDb.getCandidacyFromId,
+      })({ id });
       return result
         .map(withBasicSkills)
         .map(withMandatoryTrainings)
-        .mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
-    getCandidacies: async (_: unknown, params: { deviceId: string; }, context: { reply: any, auth: any, app: { keycloak: Keycloak.Keycloak, getKeycloakAdmin: () => KeycloakAdminClient; }; }) => {
-      
+    getCandidacies: async (
+      _: unknown,
+      params: { deviceId: string },
+      context: {
+        reply: any;
+        auth: any;
+        app: {
+          keycloak: Keycloak.Keycloak;
+          getKeycloakAdmin: () => KeycloakAdminClient;
+        };
+      }
+    ) => {
       const result = await getCandidacySummaries({
         hasRole: context.auth.hasRole,
         getCandidacySummaries: candidacyDb.getCandidacies,
-        getCandidacySummariesForUser: candidacyDb.getCandidaciesForUser
+        getCandidacySummariesForUser: candidacyDb.getCandidaciesForUser,
       })({ IAMId: context.auth.userInfo?.sub });
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
     getTrainings: async () => {
-      const result = await getTrainings({ getTrainings: trainingDb.getTrainings })();
+      const result = await getTrainings({
+        getTrainings: trainingDb.getTrainings,
+      })();
 
       return result.extract();
     },
-    getOrganismsForCandidacy: async (_: unknown, params: { candidacyId: string; }) => {
+    getOrganismsForCandidacy: async (
+      _: unknown,
+      params: { candidacyId: string }
+    ) => {
       const result = await getAAPOrganismsForCandidacy({
-        getAAPOrganisms: organismDb.getAAPOrganisms
+        getAAPOrganisms: organismDb.getAAPOrganisms,
       })({ candidacyId: params.candidacyId });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
     getBasicSkills: async () => {
-      const result = await getBasicSkills({ getBasicSkills: basicSkillDb.getBasicSkills })();
+      const result = await getBasicSkills({
+        getBasicSkills: basicSkillDb.getBasicSkills,
+      })();
 
       return result.extract();
-    }
+    },
+    getCompanionsForCandidacy: async (
+      _: unknown,
+      params: { candidacyId: string }
+    ) => {
+      const result = await getCompanionsForCandidacy({
+        getCompanionsForCandidacy: organismDb.getCompanionOrganisms,
+      })({ candidacyId: params.candidacyId });
+
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
+    },
   },
   Mutation: {
-    candidacy_createCandidacy: async (_: unknown, { candidacy }: { candidacy: { deviceId: string; certificationId: string; regionId: string; departmentId: string }; }) => {
-
+    candidacy_createCandidacy: async (
+      _: unknown,
+      {
+        candidacy,
+      }: {
+        candidacy: {
+          deviceId: string;
+          certificationId: string;
+          regionId: string;
+          departmentId: string;
+        };
+      }
+    ) => {
       const result = await createCandidacy({
         createCandidacy: candidacyDb.insertCandidacy,
         getCandidacyFromDeviceId: candidacyDb.getCandidacyFromDeviceId,
-        notifyTeam: notifyNewCandidacy
-      })({ deviceId: candidacy.deviceId, certificationId: candidacy.certificationId, regionId: candidacy.regionId, departmentId: candidacy.departmentId });
+        notifyTeam: notifyNewCandidacy,
+      })({
+        deviceId: candidacy.deviceId,
+        certificationId: candidacy.certificationId,
+        regionId: candidacy.regionId,
+        departmentId: candidacy.departmentId,
+      });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
-    candidacy_submitCandidacy: async (_: unknown, payload: { deviceId: string; candidacyId: string; }) => {
-
+    candidacy_submitCandidacy: async (
+      _: unknown,
+      payload: { deviceId: string; candidacyId: string }
+    ) => {
       const result = await submitCandidacy({
         updateCandidacyStatus: candidacyDb.updateCandidacyStatus,
         getCandidacyFromId: candidacyDb.getCandidacyFromId,
       })({ candidacyId: payload.candidacyId });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
-    candidacy_updateCertification: async (_: unknown, payload: any) => { 
+    candidacy_updateCertification: async (_: unknown, payload: any) => {
       const result = await updateCertificationOfCandidacy({
         updateCertification: candidacyDb.updateCertification,
         getCandidacyFromId: candidacyDb.getCandidacyFromId,
       })({
         candidacyId: payload.candidacyId,
         certificationId: payload.certificationId,
-        departmentId: payload.departmentId
+        departmentId: payload.departmentId,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
     candidacy_addExperience: async (_: unknown, payload: any) => {
       const result = await addExperienceToCandidacy({
@@ -136,10 +208,12 @@ export const resolvers = {
         getCandidacyFromId: candidacyDb.getCandidacyFromId,
       })({
         candidacyId: payload.candidacyId,
-        experience: payload.experience
+        experience: payload.experience,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
     candidacy_updateExperience: async (_: unknown, payload: any) => {
       const result = await updateExperienceOfCandidacy({
@@ -149,24 +223,29 @@ export const resolvers = {
       })({
         candidacyId: payload.candidacyId,
         experienceId: payload.experienceId,
-        experience: payload.experience
+        experience: payload.experience,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
-    candidacy_removeExperience: async (_: unknown, payload: any) => { console.log("candidacy_removeExperience", payload); },
+    candidacy_removeExperience: async (_: unknown, payload: any) => {
+      console.log("candidacy_removeExperience", payload);
+    },
 
-
-    candidacy_updateGoals: async (_: unknown, payload: any) => { 
+    candidacy_updateGoals: async (_: unknown, payload: any) => {
       const result = await updateGoalsOfCandidacy({
         updateGoals: goalDb.updateGoals,
         getCandidacyFromId: candidacyDb.getCandidacyFromId,
       })({
         candidacyId: payload.candidacyId,
-        goals: payload.goals
+        goals: payload.goals,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
 
     candidacy_updateContact: async (_: unknown, payload: any) => {
@@ -176,54 +255,73 @@ export const resolvers = {
       })({
         candidacyId: payload.candidacyId,
         phone: payload.phone,
-        email: payload.email
+        email: payload.email,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
 
     candidacy_deleteById: async (_: unknown, payload: any) => {
       const result = await deleteCandidacy({
         deleteCandidacyFromId: candidacyDb.deleteCandidacyFromId,
       })({
-        candidacyId: payload.candidacyId
+        candidacyId: payload.candidacyId,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
 
     candidacy_archiveById: async (_: unknown, payload: any) => {
       const result = await archiveCandidacy({
         updateCandidacyStatus: candidacyDb.updateCandidacyStatus,
       })({
-        candidacyId: payload.candidacyId
+        candidacyId: payload.candidacyId,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
 
-    candidacy_updateAppointmentInformations: async (_: unknown, payload: any) => {
+    candidacy_updateAppointmentInformations: async (
+      _: unknown,
+      payload: any
+    ) => {
       const result = await updateAppointmentInformations({
-        updateAppointmentInformations: candidacyDb.updateAppointmentInformations,
+        updateAppointmentInformations:
+          candidacyDb.updateAppointmentInformations,
       })({
         candidacyId: payload.candidacyId,
         candidateTypologyInformations: payload.candidateTypologyInformations,
-        appointmentInformations: payload.appointmentInformations
+        appointmentInformations: payload.appointmentInformations,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
 
-    candidacy_takeOver: async (_: unknown, payload: any, context: { auth: any;  }) => {
+    candidacy_takeOver: async (
+      _: unknown,
+      payload: any,
+      context: { auth: any }
+    ) => {
       const result = await takeOverCandidacy({
         hasRole: context.auth.hasRole,
-        existsCandidacyWithActiveStatus: candidacyDb.existsCandidacyWithActiveStatus,
+        existsCandidacyWithActiveStatus:
+          candidacyDb.existsCandidacyWithActiveStatus,
         updateCandidacyStatus: candidacyDb.updateCandidacyStatus,
       })({
-        candidacyId: payload.candidacyId
+        candidacyId: payload.candidacyId,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
 
     candidacy_selectOrganism: async (_: unknown, payload: any) => {
@@ -232,33 +330,48 @@ export const resolvers = {
         getCandidacyFromId: candidacyDb.getCandidacyFromId,
       })({
         candidacyId: payload.candidacyId,
-        organismId: payload.organismId
+        organismId: payload.organismId,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
-    candidacy_submitTrainingForm: async (_: unknown, payload: any, context: {  auth: any; }) => {
+    candidacy_submitTrainingForm: async (
+      _: unknown,
+      payload: any,
+      context: { auth: any }
+    ) => {
       const result = await submitTraining({
         hasRole: context.auth.hasRole,
         updateTrainingInformations: candidacyDb.updateTrainingInformations,
-        existsCandidacyWithActiveStatus: candidacyDb.existsCandidacyWithActiveStatus,
+        existsCandidacyWithActiveStatus:
+          candidacyDb.existsCandidacyWithActiveStatus,
         updateCandidacyStatus: candidacyDb.updateCandidacyStatus,
       })({
         candidacyId: payload.candidacyId,
-        training: payload.training
+        training: payload.training,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
     },
-    candidacy_confirmTrainingForm: async (_: unknown, { candidacyId }: { candidacyId: string; }) => {
+    candidacy_confirmTrainingForm: async (
+      _: unknown,
+      { candidacyId }: { candidacyId: string }
+    ) => {
       const result = await confirmTrainingFormByCandidate({
-        existsCandidacyWithActiveStatus: candidacyDb.existsCandidacyWithActiveStatus,
+        existsCandidacyWithActiveStatus:
+          candidacyDb.existsCandidacyWithActiveStatus,
         updateCandidacyStatus: candidacyDb.updateCandidacyStatus,
       })({
-        candidacyId: candidacyId
+        candidacyId: candidacyId,
       });
 
-      return result.mapLeft(error => new mercurius.ErrorWithProps(error.message, error)).extract();
-    }
-  }
+      return result
+        .mapLeft((error) => new mercurius.ErrorWithProps(error.message, error))
+        .extract();
+    },
+  },
 };
