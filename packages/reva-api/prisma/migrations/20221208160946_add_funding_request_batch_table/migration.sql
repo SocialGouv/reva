cbbd81e2a36914f0359df49f530b5c17331e5335
Alexandre Garbe <alexandre.garbe@gmail.com>
2022-12-14 17:06:04 +0100

    feat(api): removed batch_file_name column from fundingRequestBatch

--- packages/reva-api/prisma/migrations/20221208160946_add_funding_request_batch_table/migration.sql
+++ packages/reva-api/prisma/migrations/20221208160946_add_funding_request_batch_table/migration.sql
@@ -3,14 +3,11 @@ CREATE TABLE "funding_request_batch" (
     "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
     "funding_request_id" UUID NOT NULL,
     "sent" BOOLEAN NOT NULL DEFAULT false,
-    "batch_file_name" TEXT,
     "content" JSONB NOT NULL,
-
     CONSTRAINT "funding_request_batch_pkey" PRIMARY KEY ("id")
 );
-
 -- CreateIndex
 CREATE UNIQUE INDEX "funding_request_batch_funding_request_id_key" ON "funding_request_batch"("funding_request_id");
-
 -- AddForeignKey
-ALTER TABLE "funding_request_batch" ADD CONSTRAINT "funding_request_batch_funding_request_id_fkey" FOREIGN KEY ("funding_request_id") REFERENCES "funding_request"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "funding_request_batch"
+ADD CONSTRAINT "funding_request_batch_funding_request_id_fkey" FOREIGN KEY ("funding_request_id") REFERENCES "funding_request"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
