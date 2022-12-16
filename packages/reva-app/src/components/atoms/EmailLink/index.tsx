2364902d35a31fe624759958863cf4350f58bd1f
Vincent Tournebise <vincent.tournebise@tutanota.com>
2022-12-16 09:29:56 +0100

    style(app): styling projectDroppedOut page

--- packages/reva-app/src/components/atoms/EmailLink/index.tsx
+++ packages/reva-app/src/components/atoms/EmailLink/index.tsx
@@ -1,10 +1,10 @@
 interface EmailLinkProps {
     email: string;
-  }
+}
   
 export const EmailLink = ({email}: EmailLinkProps) => {
     const mailtoHref = `mailto:${email}`;
     return (
-      <a href={mailtoHref}>{email}</a>
+      <a className="text-gray-500 underline hover:text-blue-800 hover:font-semibold" href={mailtoHref}>{email}</a>
     )
 }
--- packages/reva-app/src/pages/ProjectDroppedOut.tsx
+++ packages/reva-app/src/pages/ProjectDroppedOut.tsx
@@ -22,17 +22,22 @@ export const ProjectDroppedOut = ({mainService, candidateName, candidateEmail, s
       className="z-[80] overflow-hidden h-full flex flex-col bg-white pt-6"
       direction={state.context.direction}
     >
-      <Title label={`Bonjour ${candidateName},`} />
-      <p className="bold">
-        Email: {candidateEmail}
-      </p>
-      <p>
-        Nous avons enregistré l'interruption de votre parcours.<br/>
-        Nous vous informons que vous ne pourrez pas candidater à nouveau dans le cadre de cette expérimentation.
-      </p>
-      <p>
-        Vous avez une question ? <EmailLink email={supportEmail}/>
-      </p>
+      <div className="relative overflow-y-auto flex flex-col rounded-xl p-12 text-slate-600 leading-loose">
+
+        <Title label={`Bonjour ${candidateName},`} size="large" />
+
+        <p className="font-bold mt-6">
+          Email: {candidateEmail}
+        </p>
+
+        <p className="mt-8">
+          Nous avons enregistré l'interruption de votre parcours.<br/>
+          Nous vous informons que vous ne pourrez pas candidater à nouveau dans le cadre de cette expérimentation.
+        </p>
+        <p className="mt-8">
+          Vous avez une question ? <EmailLink email={supportEmail}/>
+        </p>
+      </div>
     </Page>
   );
 };
