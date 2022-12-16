574469dfdd80d09c34a20ba20c0f1d34f21dd84b
Vincent Tournebise <vincent.tournebise@tutanota.com>
2022-12-16 13:32:40 +0100

    wip

--- packages/reva-tests/cypress/e2e/dropped-out.cy.js
+++ packages/reva-tests/cypress/e2e/dropped-out.cy.js
@@ -1,6 +1,10 @@
 import { stubMutation, stubQuery } from "../utils/graphql";
 
-context("Submission", () => {
+const firstname = "John";
+const lastname = "Doe";
+const email1 = "email@example.com";
+
+context("Dropped out", () => {
   it("log on a dropped-out project", function () {
     cy.intercept("POST", "/graphql", (req) => {
       stubMutation(req, "candidate_login", "candidate2-dropped-out.json");
@@ -10,6 +14,19 @@ context("Submission", () => {
     cy.wait("@candidate_login");
     cy.wait("@getReferential");
 
-    cy.get('[data-test="home-project-dropped-out"');
+    cy.get('[data-test="home-project-dropped-out"]');
   });
+
+
+  // it("dropped-out project page should display candidate info", function () {
+  //   cy.intercept("POST", "/graphql", (req) => {
+  //     stubMutation(req, "candidate_login", "candidate2-dropped-out.json");
+  //     stubQuery(req, "getReferential", "referential.json");
+  //   });
+  //   cy.login();
+  //   cy.wait("@candidate_login");
+  //   cy.wait("@getReferential");
+
+  //   cy.get('[data-test="home-project-dropped-out"');
+  // });
 });
