f66f3e0355e2598e11ba7381275298d68083445f
Vincent Tournebise <vincent.tournebise@tutanota.com>
2022-12-16 14:24:08 +0100

    feat(api): add candidacy dropout reason tables

---
+++ packages/reva-api/prisma/migrations/20221216132238_add_drop_out_info/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "drop_out_reason" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "label" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "drop_out_reason_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "drop_out_reason_on_candidacies" (
+    "candidacy_id" UUID NOT NULL,
+    "drop_out_reason_id" UUID NOT NULL,
+    "status" "CandidacyStatus" NOT NULL,
+    "drop_out_date" TIMESTAMP(3) NOT NULL,
+    "other_reason_content" TEXT,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "drop_out_reason_on_candidacies_pkey" PRIMARY KEY ("candidacy_id","drop_out_reason_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "drop_out_reason_label_key" ON "drop_out_reason"("label");
+
+-- AddForeignKey
+ALTER TABLE "drop_out_reason_on_candidacies" ADD CONSTRAINT "drop_out_reason_on_candidacies_candidacy_id_fkey" FOREIGN KEY ("candidacy_id") REFERENCES "candidacy"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "drop_out_reason_on_candidacies" ADD CONSTRAINT "drop_out_reason_on_candidacies_drop_out_reason_id_fkey" FOREIGN KEY ("drop_out_reason_id") REFERENCES "drop_out_reason"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
