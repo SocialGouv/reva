6f644404e62e9623d385bb2136ab193ca4939673
Vincent Tournebise <TitoVince35@users.noreply.github.com>
2022-12-29 13:52:44 +0100

    feat(api): drop out candidacy api (#578)
    * feat(api): add candidacy dropout reason tables
    
    * fix(api): dotenv loading first
    
    * feat(api): dropOutReasons db ok
    
    * feat(api): getDropOutReasons query OK
    
    * fix(api): missing gql type
    
    * wip(app): broken candidacy
    
    * fix(api): null vs undefined field types
    
    * wip mutation
    
    * feat(api): wip mutation dropCandidacy
    
    * wip(api): the chain is on
    
    * fix(api): date issue on dropOutReason
    
    * fix(api): getCandidacy fixed
    
    * test(api): fixed createCandidacy.test.ts
    
    * feat(api): preliminary check on drop out ok
    
    * test(api): more tests on dropOutCandidacy
    
    * chore(api): remove empty file
    
    * chore(api): renamed field to droppedOutAt
    
    * fix(api): date is not string
    
    * fix(api): mandatoy fields DropOutReasonOnCandidacy
    
    * refactor(api): dropOutReasonOnCandidacy ->
    candidacyDropOut
    
    * fix(api): debugged drop-out feat
    
    Co-authored-by: Vincent Tournebise <vincent.tournebise@tutanota.com>

---
+++ packages/reva-api/domain/features/dropOutCandidacy.ts
@@ -0,0 +1,99 @@
+import { Either, EitherAsync, Left, Maybe } from "purify-ts";
+
+import { Role } from "../types/account";
+import { Candidacy, DropOutReason } from "../types/candidacy";
+import { FunctionalCodeError, FunctionalError } from "../types/functionalError";
+
+interface DropOutCandidacyDeps {
+  getCandidacyFromId: (
+    candidacyId: string
+  ) => Promise<Either<string, Candidacy>>;
+  getDropOutReasonById: (params: {
+    dropOutReasonId: string;
+  }) => Promise<Either<string, Maybe<DropOutReason>>>;
+  hasRole: (role: Role) => boolean;
+  dropOutCandidacy: (
+    params: DropOutCandidacyParams
+  ) => Promise<Either<string, Candidacy>>;
+}
+
+interface DropOutCandidacyParams {
+  candidacyId: string;
+  dropOutReasonId: string;
+  droppedOutAt: Date;
+  otherReasonContent?: string;
+}
+
+export const dropOutCandidacy =
+  (deps: DropOutCandidacyDeps) => (params: DropOutCandidacyParams) => {
+    const hasRequiredRole =
+      deps.hasRole("manage_candidacy") || deps.hasRole("admin") || true;
+    if (!hasRequiredRole) {
+      return EitherAsync.liftEither(
+        Left(
+          `Vous n'êtes pas autorisé à déclarer l'abandon de cette candidature.`
+        )
+      ).mapLeft(
+        (error: string) =>
+          new FunctionalError(FunctionalCodeError.NOT_AUTHORIZED, error)
+      );
+    }
+
+    const checkIfCandidacyExists = EitherAsync.fromPromise(() =>
+      deps.getCandidacyFromId(params.candidacyId)
+    ).mapLeft(
+      () =>
+        new FunctionalError(
+          FunctionalCodeError.CANDIDACY_DOES_NOT_EXIST,
+          `Aucune candidature n'a été trouvée`
+        )
+    );
+
+    const checkIfCandidacyIsNotAbandonned = (candidacy: Candidacy) => {
+      const canDropOut = !candidacy.candidacyStatuses.some(
+        (s) => s.status === "ABANDON"
+      );
+      return Promise.resolve(
+        Maybe.fromFalsy(canDropOut).toEither(
+          new FunctionalError(
+            FunctionalCodeError.CANDIDACY_ALREADY_DROPPED_OUT,
+            `La candidature est déjà abandonnée`
+          )
+        )
+      );
+    };
+
+    const checkIfDropOutReasonExists = EitherAsync.fromPromise(() =>
+      deps.getDropOutReasonById(params)
+    )
+      .mapLeft(
+        (error: string) =>
+          new FunctionalError(
+            FunctionalCodeError.CANDIDACY_DROP_OUT_FAILED,
+            error
+          )
+      )
+      .chain(async (maybeDropOutReason: Maybe<DropOutReason>) => {
+        return maybeDropOutReason.toEither(
+          new FunctionalError(
+            FunctionalCodeError.CANDIDACY_INVALID_DROP_OUT_REASON,
+            `La raison d'abandon n'est pas valide`
+          )
+        );
+      });
+
+    const dropOutCandidacyResult = EitherAsync.fromPromise(() =>
+      deps.dropOutCandidacy(params)
+    ).mapLeft(
+      (error: string) =>
+        new FunctionalError(
+          FunctionalCodeError.CANDIDACY_DROP_OUT_FAILED,
+          error
+        )
+    );
+
+    return checkIfCandidacyExists
+      .chain(checkIfCandidacyIsNotAbandonned)
+      .chain(() => checkIfDropOutReasonExists)
+      .chain(() => dropOutCandidacyResult);
+  };
