493f1685aa6c23845c35042cd5b28396cd93052d
Anthonny Quérouil <anthonny.querouil@gmail.com>
2023-01-04 11:04:40 +0100

    feat(admin): add crisp, clean tsconfig.json (#583)
    * feat(admin): add crisp, clean tsconfig.json
    
    * feat(admin): add crisp env in example

--- packages/reva-admin/typescript/main.ts
+++ packages/reva-admin/typescript/main.ts
@@ -2,12 +2,16 @@
 import { Elm } from "../src/Main.elm";
 import keycloakElement from "./custom-elements/keycloak";
 import Keycloak from "keycloak-js";
+import { Crisp } from "crisp-sdk-web";
 
 customElements.define(keycloakElement.name, keycloakElement.clazz);
+const crispId = import.meta.env.VITE_CRISP_ID;
+if (crispId) {
+   Crisp.configure(crispId);
+}
 
 const app = Elm.Main.init({
    flags: {
-      // @ts-ignore
       endpoint: import.meta.env.VITE_API_GRAPHQL || "http://localhost:8080/graphql",
       baseUrl: 'admin',
       keycloakConfiguration: {
