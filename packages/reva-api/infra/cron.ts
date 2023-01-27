ac3c449265be8cdb90e953dda05be538073f2ee9
Vincent Tournebise <vincent.tournebise@beta.gouv.fr>
2023-01-27 22:32:02 +0100

    fet(api): paymentRequestProof job (WIP)

--- packages/reva-api/infra/cron.ts
+++ packages/reva-api/infra/cron.ts
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import pino from "pino";
 
 import { batchFundingRequest } from "./batch/fundingRequest";
+import uploadSpoolerFiles from "./batch/paymentRequestProofJob";
 
 dotenv.config({ path: path.join(process.cwd(), "..", "..", ".env") });
 
@@ -13,7 +14,6 @@ const logger = pino();
 const fundingRequest = new cron.CronJob({
   cronTime: process.env.BATCH_FUNDING_REQUEST_CRONTIME || "*/5 * * * *",
   onTick: async function () {
-    const now = new Date();
     logger.info("Batch fundingRequest ticked");
     await batchFundingRequest();
   },
@@ -21,4 +21,16 @@ const fundingRequest = new cron.CronJob({
   timeZone: "Europe/Paris",
 });
 
+const paymentRequestProofUpload = new cron.CronJob({
+  cronTime: process.env.BATCH_PAYMENT_REQUEST_PROOF_CRONTIME || "*/2 * * * *",
+  onTick: async function () {
+    logger.info("Batch paymentRequestProofUpload ticked");
+    await uploadSpoolerFiles();
+  },
+  start: true,
+  timeZone: "Europe/Paris",
+});
+
 logger.info("Started cron jobs");
+
+uploadSpoolerFiles(); // TODO: KILL ME
