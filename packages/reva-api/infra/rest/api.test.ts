1e706f6130e605d3c452f770670066d3a9a84a6c
Vincent Tournebise <vincent.tournebise@beta.gouv.fr>
2023-02-13 14:30:35 +0100

    fix(api): ping pong test with fastify-test-env

--- packages/reva-api/infra/rest/api.test.ts
+++ packages/reva-api/infra/rest/api.test.ts
@@ -1,20 +1,8 @@
-import path from "path";
-
-import dotenv from "dotenv";
-import { FastifyInstance } from "fastify";
-
-import { buildApp } from "../server/app";
-import keycloakPluginMock from "../../test/mocks/keycloak-plugin.mock";
-dotenv.config({ path: path.join(process.cwd(), "..", "..", ".env") });
-
-let fastify: FastifyInstance;
-
-beforeAll(async () => {
-  fastify = await buildApp({keycloakPluginMock});
-});
-
+/**
+ * @jest-environment ./test/fastify-test-env.ts
+ */
 test("Ping should pong", async () => {
-  const response = await fastify.inject({
+  const response = await (global as any).fastify.inject({
     method: "GET",
     url: "/ping",
   });
