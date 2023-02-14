38b71b00cf76b9eeea0bfcdd4b9e6eeeba140e3a
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-02-14 13:48:30 +0100

    feat(website): added dsfr ui-kit

--- packages/reva-website/src/pages/_app.tsx
+++ packages/reva-website/src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { createNextDsfrIntegrationApi } from "@codegouvfr/react-dsfr/next-pagesdir";
+import Link from "next/link";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Only in TypeScript projects
+declare module "@codegouvfr/react-dsfr/next-pagesdir" {
+  interface RegisterLink {
+    Link: typeof Link;
+  }
 }
+
+const { withDsfr, dsfrDocumentApi } = createNextDsfrIntegrationApi({
+  defaultColorScheme: "system",
+  Link,
+});
+
+export { dsfrDocumentApi };
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default withDsfr(App);
