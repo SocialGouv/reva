79522a23a13edb319a444a57bb97985b31e1334f
Vincent Tournebise <vincent.tournebise@beta.gouv.fr>
2023-02-17 17:23:03 +0100

    feat(api): rejectSubscriptionRequest ok

--- packages/reva-api/infra/graphql/subscription/domain/index.ts
+++ packages/reva-api/infra/graphql/subscription/domain/index.ts
@@ -2,3 +2,4 @@ export { createSubscriptionRequest } from "./createSubscriptionRequest";
 export { getSubscriptionRequest } from "./getSubscriptionRequest";
 export { getSubscriptionRequests } from "./getSubscriptionRequests";
 export { validateSubscriptionRequest } from "./validateSubscriptionRequest";
+export { rejectSubscriptionRequest } from "./rejectSubscriptionRequest";
