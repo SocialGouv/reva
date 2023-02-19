a411342a7d170f0fd0706ccc10f6a4e4e707042c
Vincent Tournebise <vincent.tournebise@beta.gouv.fr>
2023-02-19 15:48:31 +0100

    feat(api): pagination for real

--- packages/reva-api/infra/graphql/module.d.ts
+++ packages/reva-api/infra/graphql/module.d.ts
@@ -1,7 +1,17 @@
-
 type Sort = "asc" | "desc";
 interface FilteredPaginatedListArgs {
   limit?: number;
   offset?: number;
   filter?: string;
+}
+
+interface PaginationInfo {
+  totalRows: number;
+  currentPage: number;
+  totalPages: number;
+  pageLength: number;
+}
+interface PaginatedListResult<T> {
+  rows: T[];
+  info: PaginationInfo;
 }
