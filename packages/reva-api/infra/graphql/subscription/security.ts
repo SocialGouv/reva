818358e360a899aa43eed13d31bbe1ed36df6c75
Vincent Tournebise <vincent.tournebise@beta.gouv.fr>
2023-03-07 16:41:55 +0100

    fix(api): security on subscriptions

--- packages/reva-api/infra/graphql/subscription/security.ts
+++ packages/reva-api/infra/graphql/subscription/security.ts
@@ -1,9 +1,8 @@
-import { isAnyone, isAdmin } from "../security/presets";
+import { isAdmin, isAnyone } from "../security/presets";
 
-export const resolversSecurityMap = {};
-// {
-//   "Mutation.subscription_createSubscriptionRequest": isAnyone,
-//   "Mutation.subscription_validateSubscription": isAdmin,
-//   "Mutation.subscription_rejectSubscription": isAdmin,
-//   "Query.*": isAdmin,
-// };
+export const resolversSecurityMap = {
+  "Mutation.subscription_createSubscriptionRequest": isAnyone,
+  "Mutation.subscription_validateSubscription": isAdmin,
+  "Mutation.subscription_rejectSubscription": isAdmin,
+  "Query.*": isAdmin,
+};
