e7130899dcb269664e8ad62743e3f5a95bcf5063
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-03-10 11:31:19 +0100

    feat(app): small layout rework on training program summary page

--- packages/reva-app/src/components/organisms/PageWithBackground/index.tsx
+++ packages/reva-app/src/components/organisms/PageWithBackground/index.tsx
@@ -1,4 +1,3 @@
-import { AnimatePresence, motion } from "framer-motion";
 import { FC, ReactNode } from "react";
 
 import { PageHeaders } from "../../../components/molecules/PageHeaders";
@@ -20,38 +19,31 @@ export const PageWithBackground: FC<Props> = ({
   children,
 }) => {
   return (
-    <Page className="z-[80] flex flex-col bg-neutral-100" direction={direction}>
-      <AnimatePresence>
-        <motion.div
-          key="home-screen"
-          className="flex flex-col h-full relative overflow-hidden"
-          initial={direction === "next" ? { opacity: 0, y: 10 } : false}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.5, duration: 0.5, ease: "easeOut" }}
-        >
-          <img
-            className="pointer-events-none"
-            alt=""
-            role="presentation"
-            style={{
-              position: "absolute",
-              left: "-53px",
-              top: "58px",
-              width: "106px",
-            }}
-            src={certificationImg}
-          />
-          <h1 className="mt-12 -mb-12 text-center font-bold">Reva</h1>
-          <div className="grow overflow-y-auto mt-36 px-12 pb-8">
-            <PageHeaders
-              codeRncp={certification.codeRncp}
-              title={certification.label}
-              candidacyCreatedAt={candidacyCreatedAt}
-            />
-            {children}
-          </div>
-        </motion.div>
-      </AnimatePresence>
+    <Page
+      className="relative flex flex-col bg-neutral-100 overflow-hidden w-full !max-w-none"
+      direction={direction}
+    >
+      <img
+        className="pointer-events-none"
+        alt=""
+        role="presentation"
+        style={{
+          position: "absolute",
+          left: "-53px",
+          top: "58px",
+          width: "106px",
+        }}
+        src={certificationImg}
+      />
+      <h1 className="mt-12 -mb-12 text-center font-bold">Reva</h1>
+      <div className="grow overflow-y-auto mt-36 px-12 pb-8">
+        <PageHeaders
+          codeRncp={certification.codeRncp}
+          title={certification.label}
+          candidacyCreatedAt={candidacyCreatedAt}
+        />
+        {children}
+      </div>
     </Page>
   );
 };
