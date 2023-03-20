4510698c416d90ec6ac19c4b2cc369f5874a198e
Vincent Tournebise <vincent.tournebise@beta.gouv.fr>
2023-03-20 21:18:51 +0100

    feat(website): update ProfessionalSpaceInfos fields

--- packages/reva-website/src/components/professional-space/creation/form/StepThreeForm.tsx
+++ packages/reva-website/src/components/professional-space/creation/form/StepThreeForm.tsx
@@ -8,11 +8,10 @@ import { useProfessionalSpaceCreationContext } from "../context/ProfessionalSpac
 import { useState } from "react";
 
 const zodSchema = z.object({
-  companyBic: z.string().length(8, "8 caractères"),
-  companyIban: z
-    .string()
-    .min(1, "obligatoire")
-    .max(34, "34 caractères maximum"),
+  accountFirstname: z.string().min(1, "obligatoire"),
+  accountLastname: z.string().min(1, "obligatoire"),
+  accountEmail: z.string().email("mauvais format"),
+  accountPhoneNumber: z.string().min(1, "obligatoire"),
 });
 
 type StepThreeFormSchema = z.infer<typeof zodSchema>;
@@ -43,7 +42,7 @@ export const StepThreeForm = () => {
   return (
     <div className="flex flex-col min-w-[70vw]">
       <Stepper
-        title="Saisir vos informations confidentielles"
+        title="Saisir vos informations générales"
         currentStep={3}
         stepCount={3}
       />
@@ -56,16 +55,28 @@ export const StepThreeForm = () => {
       <form className="flex flex-col" onSubmit={handleSubmit(handleFormSubmit)}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8">
           <Input
-            label="BIC *"
-            state={errors.companyBic ? "error" : "default"}
-            stateRelatedMessage={errors.companyBic?.message}
-            nativeInputProps={{ ...register("companyBic") }}
+            label="Prénom *"
+            state={errors.accountFirstname ? "error" : "default"}
+            stateRelatedMessage={errors.accountFirstname?.message}
+            nativeInputProps={{ ...register("accountFirstname") }}
           />
           <Input
-            label="IBAN *"
-            state={errors.companyIban ? "error" : "default"}
-            stateRelatedMessage={errors.companyIban?.message}
-            nativeInputProps={{ ...register("companyIban") }}
+            label="Nom *"
+            state={errors.accountLastname ? "error" : "default"}
+            stateRelatedMessage={errors.accountLastname?.message}
+            nativeInputProps={{ ...register("accountLastname") }}
+          />
+          <Input
+            label="Adresse email de l'architete de parcours *"
+            state={errors.accountEmail ? "error" : "default"}
+            stateRelatedMessage={errors.accountEmail?.message}
+            nativeInputProps={{ ...register("accountEmail") }}
+          />
+          <Input
+            label="Téléphone de l'architecte de parcours *"
+            state={errors.accountPhoneNumber ? "error" : "default"}
+            stateRelatedMessage={errors.accountPhoneNumber?.message}
+            nativeInputProps={{ ...register("accountPhoneNumber") }}
           />
         </div>
         <div className="flex gap-2 ml-auto mt-4">
