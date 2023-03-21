79871bc294d759ba8d733937cfa9343400cdbf80
Vincent Tournebise <vincent.tournebise@beta.gouv.fr>
2023-03-21 09:07:00 +0100

    fix(website): default value for NEXT_PUBLIC_WEBSITE_API_GRAPHQL

--- packages/reva-website/src/components/professional-space/creation/context/ProfessionalSpaceCreationContext.tsx
+++ packages/reva-website/src/components/professional-space/creation/context/ProfessionalSpaceCreationContext.tsx
@@ -62,7 +62,7 @@ type ProfessionalSpaceCreationContext = ProfessionalSpaceCreationState & {
 
 const GRAPHQL_API_URL =
   process.env.NEXT_PUBLIC_WEBSITE_API_GRAPHQL ||
-  "http://localhost:8080/graphql";
+  "http://localhost:8080/api/graphql";
 
 const ProfessionalSpaceCreationContext =
   createContext<ProfessionalSpaceCreationContext>(
