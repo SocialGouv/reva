583eabc36f69a25c4f04e50c9ed8ac6a0c8f6082
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-03-28 16:30:11 +0200

    feat(website): added skiplinks

--- packages/reva-website/src/components/layout/main-layout/MainLayout.tsx
+++ packages/reva-website/src/components/layout/main-layout/MainLayout.tsx
@@ -1,11 +1,24 @@
 import { ReactNode } from "react";
 import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
+import { SkipLinks } from "@codegouvfr/react-dsfr/SkipLinks";
 
 export const MainLayout = (props: { children?: ReactNode }) => (
   <div className="min-h-screen flex flex-col">
+    <SkipLinks
+      links={[
+        {
+          anchor: "#content",
+          label: "Contenu",
+        },
+        {
+          anchor: "#footer",
+          label: "Pied de page",
+        },
+      ]}
+    />
     <Header />
-    <main role="main" className="flex-1 flex flex-col">
+    <main role="main" id="content" className="flex-1 flex flex-col">
       {props.children}
     </main>
     <Footer />
