75fa821bde038aad942cb51a672b43064c3ac6a1
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-03-28 16:33:44 +0200

    fix(website): changed homepage title tag to h1

--- packages/reva-website/src/pages/index.tsx
+++ packages/reva-website/src/pages/index.tsx
@@ -9,7 +9,7 @@ const IndexPage = () => {
         <title>Accueil - France VAE</title>
       </Head>
       <div className="flex-1 flex justify-center mt-[10%] m-5">
-        <h2>Bienvenue sur France VAE</h2>
+        <h1>Bienvenue sur France VAE</h1>
       </div>
     </MainLayout>
   );
