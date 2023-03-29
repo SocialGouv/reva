96e155e38aa2d2a6daefb7876d451fd056e301f5
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-03-29 10:14:03 +0200

    fix(build): increased stack size for website build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 npm run build --workspaces --if-present
 npm i --prefix "./packages/reva-website"
-npm run package --prefix "./packages/reva-website"
+npm run package --stack-size=10000 --prefix "./packages/reva-website"
