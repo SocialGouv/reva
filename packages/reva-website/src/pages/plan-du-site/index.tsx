d8928acd44f5afe69a3f7aa87f1ccadb082e1557
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-04-03 10:26:19 +0200

    feat(website): updated sitemap page

--- packages/reva-website/src/pages/plan-du-site/index.tsx
+++ packages/reva-website/src/pages/plan-du-site/index.tsx
@@ -9,10 +9,34 @@ const SiteMapPage = () => {
         <title>Plan du site - France VAE</title>
       </Head>
       <h1 className="text-3xl mb-5">Plan du site</h1>
-      <Link href="/">Accueil</Link>
-      <Link href="/espace-professionnel/creation">
-        Création d'un espace professionnel
-      </Link>
+      <ul>
+        <li>
+          <Link href="/">Accueil</Link>
+        </li>
+        <li>
+          <Link href="/espace-candidat">Espace candidat</Link>
+          <ul>
+            <li>
+              <Link href="/app">Connexion candidat</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link href="/espace-professionnel">Espace professionnel</Link>
+          <ul>
+            <li>
+              <Link href="/espace-professionnel/creation">
+                Création d'un espace professionnel
+              </Link>
+              <ul>
+                <li>
+                  <Link href="/admin">Connexion professionnel</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </MainLayout>
   );
 };
