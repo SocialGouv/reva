23b01278a479b35950330d7d0119158efb6547fa
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-04-03 19:00:20 +0200

    fix(website): moved candidate space components to the components folder

--- packages/reva-website/src/pages/espace-candidat/index.tsx
+++ packages/reva-website/src/pages/espace-candidat/index.tsx
@@ -1,12 +1,12 @@
 import { MainLayout } from "@/components/layout/main-layout/MainLayout";
 import Head from "next/head";
-import { Accompagnement } from "./components/Accompagnement";
-import { CommentCaMarche } from "./components/CommentCaMarche";
-import { Eligibilite } from "./components/Eligibilite";
-import { ExperienceReconnue } from "./components/ExperienceReconnue";
-import { Financement } from "./components/Financement";
-import { Professionnels } from "./components/Professionnels";
-import { QuestcequelaVae } from "./components/QuestcequelaVae";
+import { Accompagnement } from "../../components/candidate-space/components/Accompagnement";
+import { CommentCaMarche } from "../../components/candidate-space/components/CommentCaMarche";
+import { Eligibilite } from "../../components/candidate-space/components/Eligibilite";
+import { ExperienceReconnue } from "../../components/candidate-space/components/ExperienceReconnue";
+import { Financement } from "../../components/candidate-space/components/Financement";
+import { Professionnels } from "../../components/candidate-space/components/Professionnels";
+import { QuestcequelaVae } from "../../components/candidate-space/components/QuestcequelaVae";
 
 const IndexCandidatPage = () => {
   return (
