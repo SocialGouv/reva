c3862f4410bca886632ef98ff7a531e4dbfefce7
Cédric Soulas <soulascedric@gmail.com>
2023-05-03 17:44:00 +0200

    feat(admin): add subscription data type and api call

--- packages/reva-admin/src/Api/Subscription.elm
+++ packages/reva-admin/src/Api/Subscription.elm
@@ -3,13 +3,18 @@ module Api.Subscription exposing (getSubscriptions, reject, validate)
 import Admin.Enum.Sort exposing (Sort)
 import Admin.Mutation as Mutation
 import Admin.Object
+import Admin.Object.SubscriptionRequest
+import Admin.Object.SubscriptionRequestOnConventionCollective
+import Admin.Object.SubscriptionRequestOnDomaine
 import Admin.Object.SubscriptionRequestSummary
 import Admin.Object.SubscriptionRequestsPaginated
 import Admin.Query as Query
 import Admin.Scalar exposing (Id(..), Timestamp(..), Uuid(..))
 import Api.Auth as Auth
+import Api.Referential
 import Api.RemoteData exposing (nothingToError)
 import Api.Token exposing (Token)
+import Data.Referential
 import Data.Subscription
 import Graphql.OptionalArgument exposing (OptionalArgument(..))
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet, with)
@@ -56,18 +61,35 @@ getSubscriptions :
 getSubscriptions endpointGraphql token toMsg =
     Query.subscription_getSubscriptionRequests
         (\optionals -> { optionals | limit = Present 50, orderBy = Present { accountLastname = Absent, companyName = Absent, createdAt = Present Admin.Enum.Sort.Desc } })
-        selection
+        subscriptionsSelection
         |> Auth.makeQuery endpointGraphql token toMsg
 
 
-selection : SelectionSet (List Data.Subscription.SubscriptionSummary) Admin.Object.SubscriptionRequestsPaginated
-selection =
+get :
+    String
+    -> Token
+    -> (RemoteData String Data.Subscription.Subscription -> msg)
+    -> String
+    -> Cmd msg
+get endpointGraphql token toMsg subscriptionId =
+    let
+        requiredArgs =
+            Query.SubscriptionGetSubscriptionRequestRequiredArguments (Id subscriptionId)
+    in
+    Query.subscription_getSubscriptionRequest
+        requiredArgs
+        selection
+        |> Auth.makeQuery endpointGraphql token (nothingToError "Cette inscription est introuvable" >> toMsg)
+
+
+subscriptionsSelection : SelectionSet (List Data.Subscription.SubscriptionSummary) Admin.Object.SubscriptionRequestsPaginated
+subscriptionsSelection =
     SelectionSet.succeed identity
-        |> with (Admin.Object.SubscriptionRequestsPaginated.rows subscriptionSelection)
+        |> with (Admin.Object.SubscriptionRequestsPaginated.rows subscriptionSummarySelection)
 
 
-subscriptionSelection : SelectionSet Data.Subscription.SubscriptionSummary Admin.Object.SubscriptionRequestSummary
-subscriptionSelection =
+subscriptionSummarySelection : SelectionSet Data.Subscription.SubscriptionSummary Admin.Object.SubscriptionRequestSummary
+subscriptionSummarySelection =
     SelectionSet.succeed Data.Subscription.SubscriptionSummary
         |> with (SelectionSet.map (\(Id id) -> id) Admin.Object.SubscriptionRequestSummary.id)
         |> with Admin.Object.SubscriptionRequestSummary.accountLastname
@@ -76,3 +98,41 @@ subscriptionSelection =
         |> with Admin.Object.SubscriptionRequestSummary.companyName
         |> with Admin.Object.SubscriptionRequestSummary.companyAddress
         |> with Admin.Object.SubscriptionRequestSummary.createdAt
+
+
+selection : SelectionSet Data.Subscription.Subscription Admin.Object.SubscriptionRequest
+selection =
+    SelectionSet.succeed Data.Subscription.Subscription
+        |> with (SelectionSet.map (\(Id id) -> id) Admin.Object.SubscriptionRequest.id)
+        |> with Admin.Object.SubscriptionRequest.companySiret
+        |> with Admin.Object.SubscriptionRequest.companyLegalStatus
+        |> with Admin.Object.SubscriptionRequest.companyName
+        |> with Admin.Object.SubscriptionRequest.companyAddress
+        |> with Admin.Object.SubscriptionRequest.companyZipCode
+        |> with Admin.Object.SubscriptionRequest.companyCity
+        |> with Admin.Object.SubscriptionRequest.companyBillingContactFirstname
+        |> with Admin.Object.SubscriptionRequest.companyBillingContactLastname
+        |> with Admin.Object.SubscriptionRequest.companyBillingEmail
+        |> with Admin.Object.SubscriptionRequest.companyBillingPhoneNumber
+        |> with Admin.Object.SubscriptionRequest.companyBic
+        |> with Admin.Object.SubscriptionRequest.companyIban
+        |> with Admin.Object.SubscriptionRequest.accountFirstname
+        |> with Admin.Object.SubscriptionRequest.accountLastname
+        |> with Admin.Object.SubscriptionRequest.accountEmail
+        |> with Admin.Object.SubscriptionRequest.accountPhoneNumber
+        |> with Admin.Object.SubscriptionRequest.typology
+        |> with (Admin.Object.SubscriptionRequest.subscriptionRequestOnDomaine subscriptionRequestOnDomainSelection)
+        |> with (Admin.Object.SubscriptionRequest.subscriptionRequestOnConventionCollective subscriptionRequestOnConventionCollectiveSelection)
+        |> with (Admin.Object.SubscriptionRequest.departmentsWithOrganismMethods Api.Referential.departmentWithOrganismMethodsSelection)
+
+
+subscriptionRequestOnDomainSelection : SelectionSet Data.Referential.Domain Admin.Object.SubscriptionRequestOnDomaine
+subscriptionRequestOnDomainSelection =
+    SelectionSet.succeed identity
+        |> with (Admin.Object.SubscriptionRequestOnDomaine.domaine Api.Referential.domainSelection)
+
+
+subscriptionRequestOnConventionCollectiveSelection : SelectionSet Data.Referential.ConventionCollective Admin.Object.SubscriptionRequestOnConventionCollective
+subscriptionRequestOnConventionCollectiveSelection =
+    SelectionSet.succeed identity
+        |> with (Admin.Object.SubscriptionRequestOnConventionCollective.ccn Api.Referential.conventionCollectiveSelection)
