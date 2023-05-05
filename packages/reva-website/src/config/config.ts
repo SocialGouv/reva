5fb1388b698b23f848cbbdee20cf0abe89156030
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-05-05 11:19:53 +0200

    feat(website): added basic matomo tracking

--- packages/reva-website/src/config/config.ts
+++ packages/reva-website/src/config/config.ts
@@ -1,3 +1,6 @@
 export const GRAPHQL_API_URL =
   process.env.NEXT_PUBLIC_WEBSITE_API_GRAPHQL ||
   "http://localhost:8080/api/graphql";
+
+export const MATOMO_CONTAINER_URL =
+  process.env.NEXT_PUBLIC_MATOMO_CONTAINER_URL;
