924803b39bf37120edfbb4fd3d5156e61c84b896
Cédric Soulas <soulascedric@gmail.com>
2023-05-11 18:05:29 +0200

    feat(admin): add subscription typology and departments details

--- packages/reva-admin/src/Page/Subscription.elm
+++ packages/reva-admin/src/Page/Subscription.elm
@@ -8,10 +8,12 @@ module Page.Subscription exposing
 
 import Accessibility exposing (dd, dl, dt, h1, h2, h3)
 import Admin.Enum.LegalStatus as LegalStatus
+import Admin.Enum.OrganismTypology exposing (OrganismTypology(..))
 import Api.Subscription
 import BetaGouv.DSFR.Button as Button
 import Browser.Navigation as Nav
 import Data.Context exposing (Context)
+import Data.Referential exposing (DepartmentWithOrganismMethods)
 import Data.Subscription exposing (Subscription, SubscriptionSummary)
 import Html exposing (Html, div, li, p, text, ul)
 import Html.Attributes exposing (attribute, class)
@@ -101,19 +103,41 @@ viewContent context errors subscription =
             [ View.errors errors
             , h1 [ class "w-full mb-0" ] [ text subscription.accountFirstname, text " ", text subscription.accountLastname ]
             , viewTitle "Informations générales"
-            , viewInfo "Adresse email de l'architecte de parcours" [ subscription.accountEmail ]
-            , viewInfo "Téléphone de l'architecte de parcours" [ subscription.accountPhoneNumber ]
+            , viewInfoText "Adresse email de l'architecte de parcours" [ subscription.accountEmail ]
+            , viewInfoText "Téléphone de l'architecte de parcours" [ subscription.accountPhoneNumber ]
             , viewTitle "Informations juridiques de la structure"
-            , viewInfo "SIRET de la structure" [ subscription.companySiret ]
-            , viewInfo "Forme juridique" [ subscription.companyLegalStatus |> LegalStatus.toString ]
-            , viewInfo "Raison sociale" [ subscription.companyName ]
-            , viewInfo "Adresse de la structure" [ subscription.companyAddress, subscription.companyZipCode, subscription.companyCity ]
+            , viewInfoText "SIRET de la structure" [ subscription.companySiret ]
+            , viewInfoText "Forme juridique" [ subscription.companyLegalStatus |> LegalStatus.toString ]
+            , viewInfoText "Raison sociale" [ subscription.companyName ]
+            , viewInfoText "Adresse de la structure"
+                [ subscription.companyAddress
+                , subscription.companyZipCode
+                , subscription.companyCity
+                ]
+            , viewTitle "Typologie et zone d'intervention"
+            , viewInfoText "Typologie" [ subscription.typology |> typologyToString ]
+            , case subscription.typology of
+                ExpertBranche ->
+                    viewInfo "Conventions collectives" [ viewCategory subscription.ccns ]
+
+                ExpertFiliere ->
+                    viewInfo "Filière(s)" [ viewCategory subscription.domains ]
+
+                Generaliste ->
+                    div [ class "w-full" ] []
+            , viewInfo "Zone d'intervention en présentiel"
+                [ viewDepartements subscription.departmentsWithOrganismMethods .isOnSite ]
+            , viewInfo "Zone d'intervention en distanciel"
+                [ viewDepartements subscription.departmentsWithOrganismMethods .isRemote ]
             , viewTitle "Informations de facturation"
-            , viewInfo "Contact de facturation" [ subscription.companyBillingContactFirstname, subscription.companyBillingContactLastname ]
-            , viewInfo "Adresse email de facturation" [ subscription.companyBillingEmail ]
-            , viewInfo "Téléphone du contact de facturation" [ subscription.companyBillingPhoneNumber ]
-            , viewInfo "BIC" [ subscription.companyBic ]
-            , viewInfo "IBAN" [ subscription.companyIban ]
+            , viewInfoText "Contact de facturation"
+                [ subscription.companyBillingContactFirstname
+                , subscription.companyBillingContactLastname
+                ]
+            , viewInfoText "Adresse email de facturation" [ subscription.companyBillingEmail ]
+            , viewInfoText "Téléphone du contact de facturation" [ subscription.companyBillingPhoneNumber ]
+            , viewInfoText "BIC" [ subscription.companyBic ]
+            , viewInfoText "IBAN" [ subscription.companyIban ]
             , div
                 [ class "flex items-center justify-end space-x-4 w-full my-8" ]
                 [ Button.new { onClick = Just (ClickedRejection subscription.id), label = "Rejeter" }
@@ -126,20 +150,51 @@ viewContent context errors subscription =
         ]
 
 
+typologyToString : OrganismTypology -> String
+typologyToString typology =
+    case typology of
+        Generaliste ->
+            "Généraliste"
+
+        ExpertFiliere ->
+            "Expert de filière(s)"
+
+        ExpertBranche ->
+            "Expert de branche(s)"
+
+
+viewDepartements : List DepartmentWithOrganismMethods -> (DepartmentWithOrganismMethods -> Bool) -> Html msg
+viewDepartements departments predicate =
+    departments
+        |> List.filter predicate
+        |> List.map (\d -> li [] [ text <| Data.Referential.departmentToString d.department ])
+        |> ul [ class "my-0" ]
+
+
+viewCategory : List { a | label : String } -> Html msg
+viewCategory category =
+    category
+        |> List.map (\c -> li [] [ text c.label ])
+        |> ul [ class "my-0" ]
+
+
 viewTitle : String -> Accessibility.Html msg
 viewTitle s =
     h2 [ class "w-full mt-6 mb-0 text-xl" ] [ text s ]
 
 
-viewInfo : String -> List String -> Accessibility.Html msg
-viewInfo term data =
+viewInfo : String -> List (Accessibility.Html msg) -> Accessibility.Html msg
+viewInfo term content =
     dl
         [ class "w-full sm:w-1/2 my-2" ]
-        [ dt [ class "font-normal text-sm text-gray-600" ] [ text term ]
-        , dd
-            [ class "my-0" ]
-            (List.intersperse " " data |> List.map text)
+        [ dt [ class "font-normal text-sm text-gray-600 mb-1" ] [ text term ]
+        , dd [ class "my-0" ] content
         ]
+
+
+viewInfoText : String -> List String -> Accessibility.Html msg
+viewInfoText term data =
+    viewInfo term <| (List.intersperse " " data |> List.map text)
 
 
 
