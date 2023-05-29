d5ca5cd1ba83aca85989e22aa9b684f8a9595278
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-05-29 11:16:50 +0200

    fix(app): updated footer to be a11y compliant (aria-label in external links)

--- packages/reva-app/src/components/organisms/Footer/index.tsx
+++ packages/reva-app/src/components/organisms/Footer/index.tsx
@@ -1,16 +1,84 @@
-import { Footer as DsfrFooter } from "@codegouvfr/react-dsfr/Footer";
-export const Footer = (props: { className?: string }) => (
-  <DsfrFooter
-    className={props.className}
-    accessibility="non compliant"
-    brandTop={<></>}
-    contentDescription="Reva"
-    cookiesManagementLinkProps={{
-      href: "#",
-    }}
-    homeLinkProps={{
-      href: "/",
-      title: "Accueil - Reva - l'expérimentation de la VAE",
-    }}
-  />
+export const Footer = () => (
+  <footer className="fr-footer" role="contentinfo" id="footer">
+    <div className="fr-container">
+      <div className="fr-footer__body">
+        <div className="fr-footer__brand fr-enlarge-link">
+          <a href="/" title="Accueil - Reva - l'expérimentation de la VAE">
+            <p className="fr-logo"></p>
+          </a>
+        </div>
+        <div className="fr-footer__content">
+          <p className="fr-footer__content-desc">Reva</p>
+          <ul className="fr-footer__content-list">
+            <li className="fr-footer__content-item">
+              <a
+                className="fr-footer__content-link"
+                target="_blank"
+                href="https://legifrance.gouv.fr"
+                aria-label="legifrance.gouv.fr nouvelle page"
+                rel="noreferrer"
+              >
+                legifrance.gouv.fr
+              </a>
+            </li>
+            <li className="fr-footer__content-item">
+              <a
+                className="fr-footer__content-link"
+                target="_blank"
+                href="https://gouvernement.fr"
+                aria-label="gouvernement.fr nouvelle page"
+                rel="noreferrer"
+              >
+                gouvernement.fr
+              </a>
+            </li>
+            <li className="fr-footer__content-item">
+              <a
+                className="fr-footer__content-link"
+                target="_blank"
+                href="https://service-public.fr"
+                aria-label="service-public.fr nouvelle page"
+                rel="noreferrer"
+              >
+                service-public.fr
+              </a>
+            </li>
+            <li className="fr-footer__content-item">
+              <a
+                className="fr-footer__content-link"
+                target="_blank"
+                href="https://data.gouv.fr"
+                aria-label="data.gouv.fr nouvelle page"
+                rel="noreferrer"
+              >
+                data.gouv.fr
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="fr-footer__bottom">
+        <ul className="fr-footer__bottom-list">
+          <li className="fr-footer__bottom-item">
+            <span className="fr-footer__bottom-link">
+              Accessibilité: non conforme
+            </span>
+          </li>
+        </ul>
+        <div className="fr-footer__bottom-copy">
+          <p>
+            Sauf mention contraire, tous les contenus de ce site sont sous{" "}
+            <a
+              href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
+              target="_blank"
+              aria-label="licence etalab-2.0 nouvelle page"
+              rel="noreferrer"
+            >
+              licence etalab-2.0
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
 );
