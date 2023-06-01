ad693e5bf4bae775668f19eb0c30c5308bebb9c5
Vincent Tournebise <vincent.tournebise@beta.gouv.fr>
2023-06-01 16:03:38 +0200

    faet(app): searchbar replacement for rgaa crit 7.1

---
+++ packages/reva-app/src/components/molecules/SearchBar/SearchBar.tsx
@@ -0,0 +1,38 @@
+import Button from "@codegouvfr/react-dsfr/Button";
+import Input from "@codegouvfr/react-dsfr/Input";
+import { SyntheticEvent } from "react";
+
+interface SearchBarProps {
+  label: string;
+  className: string;
+  nativeInputProps: {
+    defaultValue: string;
+    onChange: (e: any) => void;
+  };
+}
+export const SearchBar = (props: SearchBarProps) => {
+  const handleClickSearch = (e: SyntheticEvent) => {
+    return props.nativeInputProps.onChange({
+      ...e,
+      target: document.getElementById("searchbarreplacement"),
+    });
+  };
+  return (
+    <div className="fr-search-bar fr-search-bar--lg" role="search">
+      <Input
+        label="Rechercher un diplôme"
+        hideLabel
+        nativeInputProps={{
+          type: "search",
+          placeholder: "Rechercher un diplôme",
+        }}
+      />
+      <Button
+        onClick={(e) => handleClickSearch(e)}
+        iconId="fr-icon-search-line"
+        priority="tertiary no outline"
+        title="Lancer la recherche"
+      />
+    </div>
+  );
+};
