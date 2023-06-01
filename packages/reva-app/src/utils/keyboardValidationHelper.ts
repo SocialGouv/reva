0af79b4c85b61f0f18d7fecf5d32ecfa0e18003a
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-06-01 14:29:01 +0200

    feat(app): added certification card selection on space key

--- packages/reva-app/src/utils/keyboardValidationHelper.ts
+++ packages/reva-app/src/utils/keyboardValidationHelper.ts
@@ -1,7 +1,7 @@
 export const onKeyboardValidation =
   (handler?: () => void) => (e: React.KeyboardEvent<HTMLElement>) => {
     console.log("keyUp", e);
-    if (handler && e.code === "Enter") {
+    if (handler && (e.code === "Enter" || e.code === "Space")) {
       handler();
     }
   };
