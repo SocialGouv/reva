95bcdf3427f2740f1fd883e9113407792ee03b1a
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-06-07 12:31:37 +0200

    fix(website): fixed "Des démarches simplifiées pour les candidats" block background color in candidate homepage

--- packages/reva-website/src/components/candidate-space/components/CommentCaMarche.tsx
+++ packages/reva-website/src/components/candidate-space/components/CommentCaMarche.tsx
@@ -8,22 +8,20 @@ import {
 export const CommentCaMarche = () => (
   <section
     id="comment-ca-marche"
-    className="mt-20 lg:mt-[200px] pb-48 bg-[#1B1B35] text-white"
+    className="mt-20 lg:mt-[200px] pb-48 bg-dsfrGray-altblueFrance"
   >
     <div className="relative w-full mx-auto px-5 lg:space-x-12">
       <div className="w-full mt-[100px] mx-auto text-center">
         <header className="flex flex-col items-center">
-          <SectionSubHeader className="text-[#FEF7DA]">
-            Le parcours Reva
-          </SectionSubHeader>
-          <h1 className="text-white lg:text-[40px] lg:leading-[44px] xl:text-[80px] xl:leading-[88px] max-w-screen-xl">
+          <SectionSubHeader>Le parcours Reva</SectionSubHeader>
+          <h1 className=" lg:text-[40px] lg:leading-[44px] xl:text-[80px] xl:leading-[88px] max-w-screen-xl">
             Des démarches simplifiées pour les candidats
           </h1>
         </header>
       </div>
       <div className="flex flex-col lg:flex-row mt-[100px] justify-around lg:min-w-[1248px]">
         <section className="w-full lg:w-[450px] lg:h-[400px] lg:pl-[120px] lg:pt-[80px] lg:pr-[32px] lg:bg-[url('/candidate-space/hexa-group-1.svg')] bg-[url('/candidate-space/hexa-gp-sm-1.png')] bg-no-repeat bg-[top_left_20px] px-4 pt-[108px] mb-[50px]">
-          <div className="flex flex-col lg:ml-6 ">
+          <div className="flex flex-col lg:ml-6 text-white">
             <header>
               <SubSectionHeader>Confirmation de votre projet</SubSectionHeader>
             </header>
@@ -34,7 +32,7 @@ export const CommentCaMarche = () => (
           </div>
         </section>
         <section className="w-[360px] lg:w-[450px] lg:h-[400px] lg:pl-[128px] pr-[32px] lg:bg-left-top lg:bg-[url('/candidate-space/hexa-group-2.svg')] bg-[url('/candidate-space/hexa-gp-sm-2.png')] bg-no-repeat bg-[top_left_20px] px-4 pt-[108px] mb-[50px]">
-          <div className="flex flex-col lg:-mt-6">
+          <div className="flex flex-col lg:-mt-6 text-white">
             <header>
               <SubSectionHeader>
                 Préparation du dossier d’expérience
@@ -47,7 +45,7 @@ export const CommentCaMarche = () => (
           </div>
         </section>
         <section className="w-[360px] lg:w-[450px] lg:h-[400px] lg:pl-[128px] pr-[36px] lg:bg-[url('/candidate-space/hexa-group-3.svg')] bg-[url('/candidate-space/hexa-gp-sm-3.png')]  bg-no-repeat bg-[top_left_20px] px-4 pt-[108px] mb-[50px]">
-          <div className="flex flex-col lg:-mt-9 lg:ml-5">
+          <div className="flex flex-col lg:-mt-9 lg:ml-5 text-white">
             <header>
               <SubSectionHeader>Entretien avec le jury</SubSectionHeader>
             </header>
