f7cd0fac88d1480efac07bf818fe4770cb275852
Cédric Soulas <soulascedric@gmail.com>
2023-06-08 17:19:17 +0200

    chore(admin): fork mda dsfr pictograms for ready to use component

---
+++ packages/reva-website/types/primitives.d.ts
@@ -0,0 +1,11 @@
+declare module "@codegouvfr/react-dsfr/*.svg" {
+  export interface SVG {
+    height: number;
+    src: string;
+    width: number;
+  }
+
+  const content: SVG;
+
+  export = content;
+}
