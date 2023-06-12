79fc711c0329b37a737d211edef1942ab92906d6
Cédric Soulas <soulascedric@gmail.com>
2023-06-12 16:24:42 +0200

    feat(app): replace show more about certification with a francecompetence link

--- packages/reva-app/src/pages/CertificateDetails.tsx
+++ packages/reva-app/src/pages/CertificateDetails.tsx
@@ -2,87 +2,47 @@ import Button from "@codegouvfr/react-dsfr/Button";
 import { BackButton } from "components/molecules/BackButton";
 import { Page } from "components/organisms/Page";
 import { useMainMachineContext } from "contexts/MainMachineContext/MainMachineContext";
-import parse from "html-react-parser";
-import { ReactNode, useMemo, useState } from "react";
-
-const Section = ({
-  title,
-  children,
-}: {
-  title: string;
-  children?: ReactNode;
-}) => {
-  return (
-    <section>
-      <h2 className="text-lg font-bold mb-6">{title}</h2>
-      {children}
-    </section>
-  );
-};
 
 export const CertificateDetails = () => {
   const {
     state,
     mainService: { send },
   } = useMainMachineContext();
-  const [showMore, setShowMore] = useState(false);
-  const certification = useMemo(
-    () => state.context.selectedCertification,
-    [state.context.selectedCertification]
-  );
 
-  return certification ? (
-    <Page title={`Détails de la certification ${certification.label}`}>
+  const selectedCertification = state.context.selectedCertification;
+
+  return selectedCertification ? (
+    <Page title={`Détails de la certification ${selectedCertification.label}`}>
       <BackButton className="mb-6" />
-      <h1 className="mb-4 text-2xl font-bold text-black">
-        {certification.label}
-      </h1>
-      <h2 className="mb-6">
-        Code RNCP: {state.context.selectedCertification?.codeRncp}
-      </h2>
-      <Section title="Objectif du diplôme">
-        <p>{certification.summary}</p>
-      </Section>
-      {showMore && (
-        <div className="flex flex-col gap-6 mt-6 mb-2">
-          {certification.activities && (
-            <Section title="Activités visées">
-              <p>{parse(certification.activities)}</p>
-            </Section>
-          )}
-          {certification.abilities && (
-            <Section title="Compétences attestées">
-              <p>{parse(certification.abilities)}</p>
-            </Section>
-          )}
-
-          {certification.activityArea && (
-            <Section title="Secteurs d'activités">
-              <p>{parse(certification.activityArea)}</p>
-            </Section>
-          )}
-          {certification.accessibleJobType && (
-            <Section title="Types d'emplois accessibles">
-              <p>{parse(certification.accessibleJobType)}</p>
-            </Section>
-          )}
-        </div>
-      )}
-      <Button
-        priority="tertiary no outline"
-        className="p-0 mt-4"
-        onClick={() => setShowMore((showMore) => !showMore)}
-        nativeButtonProps={{
-          "aria-expanded": showMore,
-        }}
+      <h1
+        data-test="certification-label"
+        className="mb-4 text-2xl font-bold text-black"
       >
-        Lire {showMore ? "moins" : "plus"}
-      </Button>
+        {selectedCertification.label}
+      </h1>
+      <h2 data-test="certification-code-rncp" className="mb-3">
+        Code RNCP: {selectedCertification.codeRncp}
+      </h2>
+      <p>
+        <a
+          data-test="certification-more-info-link"
+          target="_blank"
+          rel="noreferrer"
+          href={`https://www.francecompetences.fr/recherche/rncp/${selectedCertification.codeRncp}/`}
+        >
+          Lire les détails de la fiche diplôme
+        </a>
+      </p>
       <div className="flex flex-col md:flex-row gap-6 mt-6">
         <Button
           data-test="submit-certification-button"
           className="justify-center w-[100%]  md:w-fit"
-          onClick={() => send({ type: "SUBMIT_CERTIFICATION", certification })}
+          onClick={() =>
+            send({
+              type: "SUBMIT_CERTIFICATION",
+              certification: selectedCertification,
+            })
+          }
         >
           Choisir ce diplôme
         </Button>
