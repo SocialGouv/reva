60f2f00e79d125f96d5a1b87d8f028840d481173
Cédric Soulas <soulascedric@gmail.com>
2023-06-21 14:27:54 +0200

    fix(website): fix blue introduction block on mobile

--- packages/reva-website/src/components/layout/blue-layout/BlueLayout.tsx
+++ packages/reva-website/src/components/layout/blue-layout/BlueLayout.tsx
@@ -6,19 +6,20 @@ export const BlueLayout = (props: {
   children?: ReactNode;
 }) => (
   <div className="flex-1 flex flex-col relative">
-    <div className="bg-gradient-to-r from-[#557AFF] to-[#2400FF] w-screen left-0 absolute z-0 pb-[400px]"></div>
-    <div className="flex flex-col items-center z-1 relative pb-14 fr-container">
-      <div className="w-full py-8">
+    <div className="bg-gradient-to-r from-[#557AFF] to-[#2400FF] w-screen w-full pt-8 pb-16 md:pb-20 px-4 -mb-12">
+      <div className="fr-container">
         <h1 className="text-white text-3xl">{props.title}</h1>
         {typeof props.description === "string" ? (
-          <p className="text-white text-xl leading-relaxed mb-0">
+          <p className="text-white text-sm md:text-xl md:leading-relaxed mb-0">
             {props.description}
           </p>
         ) : (
           props.description
         )}
       </div>
-      <div className="bg-white drop-shadow-lg flex flex-col px-5 md:px-24 py-14 mb-8">
+    </div>
+    <div className="flex flex-col items-center relative pb-14 fr-container">
+      <div className="bg-white md:drop-shadow-lg flex flex-col px-5 md:px-24 pt-4 pb-6 md:py-14 mb-8">
         {props.children}
       </div>
     </div>
