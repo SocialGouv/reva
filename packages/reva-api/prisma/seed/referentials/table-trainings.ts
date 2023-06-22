0aaa2c95c9302054a708a024095c844d0daed23f
Cédric Soulas <soulascedric@gmail.com>
2023-06-22 17:56:57 +0200

    feat(api): add new trainings

--- packages/reva-api/prisma/seed/referentials/table-trainings.ts
+++ packages/reva-api/prisma/seed/referentials/table-trainings.ts
@@ -40,4 +40,34 @@ export async function insertTrainingsIfNone(prisma: PrismaClient) {
       label: `Systèmes d'attaches`,
     },
   });
+
+  await prisma.training.upsert({
+    where: {
+      label: `Prévention et secours civiques de niveau 1 (PSC1)`,
+    },
+    update: {},
+    create: {
+      label: `Prévention et secours civiques de niveau 1 (PSC1)`,
+    },
+  });
+
+  await prisma.training.upsert({
+    where: {
+      label: `Premiers secours en équipe de niveau 1 (PSE1)`,
+    },
+    update: {},
+    create: {
+      label: `Premiers secours en équipe de niveau 1 (PSE1)`,
+    },
+  });
+
+  await prisma.training.upsert({
+    where: {
+      label: `Brevet national de sécurité et de sauvetage aquatique (BNSSA)`,
+    },
+    update: {},
+    create: {
+      label: `Brevet national de sécurité et de sauvetage aquatique (BNSSA)`,
+    },
+  });
 }
