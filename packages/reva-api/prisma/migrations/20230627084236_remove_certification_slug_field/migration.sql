842d2f30042848dcf26cb15f434bddf3a30be4e6
Cédric Soulas <soulascedric@gmail.com>
2023-06-27 10:47:39 +0200

    chore(api): remove unused data-seed and slug certification field

---
+++ packages/reva-api/prisma/migrations/20230627084236_remove_certification_slug_field/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `slug` on the `certification` table. All the data in the column will be lost.
+
+*/
+
+-- AlterTable
+ALTER TABLE "certification" DROP COLUMN "slug";
