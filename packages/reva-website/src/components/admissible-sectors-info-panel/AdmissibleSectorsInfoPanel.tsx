b7e07283fbdf1c5f6a18cb6f2928f4eae45c4612
Cédric Soulas <soulascedric@gmail.com>
2023-07-04 11:19:15 +0200

    feat(website): improve warning of available certifications

--- packages/reva-website/src/components/admissible-sectors-info-panel/AdmissibleSectorsInfoPanel.tsx
+++ packages/reva-website/src/components/admissible-sectors-info-panel/AdmissibleSectorsInfoPanel.tsx
@@ -3,19 +3,36 @@ export const AdmissibleSectorsInfoPanel = ({
 }: {
   className?: string;
 }) => (
-  <div className={`fr-alert fr-alert--info max-w-[550px] ${className || ""}`}>
-    <p className="text-sm">
-      Les premières filières éligibles sont : sanitaire et social, grande
-      distribution, industrie métallurgique et métiers du sport.
+  <div
+    className={`fr-alert fr-alert--warning max-w-[580px] ${className || ""}`}
+  >
+    <h3 className="fr-alert__title">Attention</h3>
+    <p>Tous les diplômes ne sont pas encore couverts par France VAE.</p>
+    <p className="font-semibold">
+      Seules les filières suivantes sont actuellement éligibles :
     </p>
-    <a
-      className="fr-link !text-sm"
-      title="Voir tous les diplômes disponibles - ouvre une nouvelle fenêtre"
-      target="_blank"
-      referrerPolicy="no-referrer"
-      href="https://airtable.com/shrTDCbwwBI4xLLo9/tblWDa9HN0cuqLnAl"
-    >
-      Voir tous les diplômes disponibles
-    </a>
+    <ul>
+      <li>sanitaire et social,</li>
+      <li>grande distribution,</li>
+      <li>industrie métallurgique,</li>
+      <li>métiers du sport.</li>
+    </ul>
+    <p>
+      <a
+        className="fr-link font-semibold !text-sm"
+        title="Voir tous les diplômes disponibles - ouvre une nouvelle fenêtre"
+        target="_blank"
+        referrerPolicy="no-referrer"
+        href="https://airtable.com/shrTDCbwwBI4xLLo9/tblWDa9HN0cuqLnAl"
+      >
+        Consultez les diplômes actuellement disponibles via France VAE
+      </a>
+    </p>
+    <p>
+      Si le diplôme que vous visez ne fait pas partie de cette liste, veuillez
+      consulter le site{" "}
+      <a href="https://vae.centre-inffo.fr">vae.centre-inffo.fr</a> pour
+      continuer votre parcours VAE.
+    </p>
   </div>
 );
