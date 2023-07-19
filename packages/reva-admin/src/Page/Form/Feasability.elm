4ea26d08dffabaa4b3ec39175e0f619eccc70d0a
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-07-19 16:04:36 +0200

    feat(admin): added firstname, lastname and certification of candidate in feasability tab

--- packages/reva-admin/src/Page/Form/Feasability.elm
+++ packages/reva-admin/src/Page/Form/Feasability.elm
@@ -1,14 +1,36 @@
 module Page.Form.Feasability exposing (..)
 
-import Data.Candidacy exposing (Candidacy, CandidacyId, CandidacySummary)
+import Data.Candidacy exposing (Candidacy)
 import Data.Form exposing (FormData)
 import Data.Referential exposing (Referential)
-import Page.Form exposing (Form)
+import Page.Form as Form exposing (Form)
 
 
 form : FormData -> ( Candidacy, Referential ) -> Form
-form formData _ =
-    { elements = []
+form _ ( candidacy, _ ) =
+    let
+        elements =
+            [ ( "heading"
+              , Form.Section <|
+                    case candidacy.candidate of
+                        Just candidate ->
+                            String.concat [ candidate.firstname, " ", candidate.lastname ]
+
+                        Nothing ->
+                            ""
+              )
+            , ( "heading2"
+              , Form.Section <|
+                    case candidacy.certification of
+                        Just certification ->
+                            certification.label
+
+                        Nothing ->
+                            ""
+              )
+            ]
+    in
+    { elements = elements
     , saveLabel = Nothing
     , submitLabel = "Valider"
     , title = "Dossier de faisabilité"
