355ad2249ce67da697dfaeb89961d5d5e4d59c53
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-08-01 17:03:28 +0200

    feat(admin): added a button link pointing to the feasibility details to the feasibility card component

--- packages/reva-admin/src/View/Feasibility/FeasibilityCard.elm
+++ packages/reva-admin/src/View/Feasibility/FeasibilityCard.elm
@@ -1,10 +1,12 @@
 module View.Feasibility.FeasibilityCard exposing (..)
 
 import Admin.Scalar
+import BetaGouv.DSFR.Button as Button
 import Data.Context exposing (Context)
 import Data.Feasibility exposing (FeasibilitySummary)
 import Html exposing (Html, div, li, text)
 import Html.Attributes exposing (attribute, class)
+import Route
 import View.Helpers exposing (dataTest)
 
 
@@ -16,6 +18,14 @@ view context feasibilitySummary =
     in
     li [ dataTest "directory-item", attribute "style" "--li-bottom:0" ]
         [ div
-            [ class "border py-5 pl-6 pr-4 my-8" ]
-            [ text (idToString feasibilitySummary.id) ]
+            [ class "flex flex-col border py-5 pl-6 pr-4 my-8" ]
+            [ text (idToString feasibilitySummary.id)
+            , div
+                [ class "ml-0 lg:ml-auto mt-4" ]
+                [ Button.new { onClick = Nothing, label = "Accéder au dossier" }
+                    |> Button.linkButton (Route.toString context.baseUrl (Route.Feasibility (idToString feasibilitySummary.id)))
+                    |> Button.withAttrs [ attribute "title" "Accéder au dossier" ]
+                    |> Button.view
+                ]
+            ]
         ]
