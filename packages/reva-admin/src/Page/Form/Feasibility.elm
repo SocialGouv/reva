d287a4bd0f4135eba139adb4845913e2b3f70651
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-08-07 16:15:01 +0200

    feat(admin): added a smal help panel at the end of the aap feasibility form

--- packages/reva-admin/src/Page/Form/Feasibility.elm
+++ packages/reva-admin/src/Page/Form/Feasibility.elm
@@ -4,8 +4,8 @@ import Data.Candidacy exposing (Candidacy)
 import Data.Form exposing (FormData)
 import Data.Form.Feasibility exposing (keys)
 import Data.Referential exposing (Referential)
-import Html exposing (div, h6, p, strong, text)
-import Html.Attributes exposing (class)
+import Html exposing (a, div, h6, p, strong, text)
+import Html.Attributes exposing (class, href, target, title)
 import Html.Attributes.Extra exposing (role)
 import Page.Form as Form exposing (Form)
 
@@ -19,7 +19,43 @@ form _ ( candidacy, _ ) =
                     (div
                         [ class "fr-alert fr-alert--warning ml-2 mt-2 mb-8", role "alert" ]
                         [ h6 [] [ text "Attention" ]
-                        , p [] [ text "Ne joignez pas la Carte d’Identité du candidat dans ce formulaire. Pour le moment", strong [] [ text ", la Carte d’Identité est toujours à communiquer par email." ] ]
+                        , p []
+                            [ text "Ne joignez pas la Carte d’Identité du candidat dans ce formulaire. Pour le moment"
+                            , strong [] [ text ", la Carte d’Identité est toujours à communiquer par email." ]
+                            ]
+                        ]
+                    )
+              )
+            ]
+
+        helpPanel =
+            [ ( "help panel"
+              , Form.StaticHtml
+                    (div
+                        [ class "fr-notice fr-notice--info ml-2 mt-10" ]
+                        [ div [ class "fr-container" ]
+                            [ div [ class "fr-notice__body" ]
+                                [ div [ class "fr-notice__title" ]
+                                    [ text "Pour retrouver les dossiers de faisabilité et être guidé dans le remplissage, "
+                                    , a
+                                        [ class "fr-link text-[#0063cb]"
+                                        , href "https://www.notion.so/Dossiers-de-faisabilit-c5bff6e7fa7744859cda85c935fd928f"
+                                        , target "_blank"
+                                        , title "consultez la documentation - nouvelle fenêtre"
+                                        ]
+                                        [ text "consultez la documentation" ]
+                                    , text " ou "
+                                    , a
+                                        [ class "fr-link text-[#0063cb]"
+                                        , href "https://www.notion.so/NOTICE-D-UTILISATION-DU-DOSSIER-DE-FAISABILITE-92a8744b294a47259396fe1efd226043"
+                                        , target "_blank"
+                                        , title "la notice d’utilisation du dossier de faisabilité - nouvelle fenêtre"
+                                        ]
+                                        [ text "la notice d’utilisation du dossier de faisabilité" ]
+                                    , text "."
+                                    ]
+                                ]
+                            ]
                         ]
                     )
               )
@@ -88,6 +124,7 @@ form _ ( candidacy, _ ) =
                                 "Certificateur Inconnu"
                           )
                         ]
+                , helpPanel
                 ]
     in
     { elements = elements
