b07478c74f5cd44723dcf96e7b437d6bbedab776
Florian Bonniec <f.bonniec@me.com>
2023-08-23 08:57:50 +0200

    feat(website): improved wording of register confirmation by an APP

--- packages/reva-website/src/pages/espace-professionnel/creation/confirmation.tsx
+++ packages/reva-website/src/pages/espace-professionnel/creation/confirmation.tsx
@@ -1,5 +1,7 @@
 import { BlueLayout } from "@/components/layout/blue-layout/BlueLayout";
 import { MainLayout } from "@/components/layout/main-layout/MainLayout";
+import Alert from "@codegouvfr/react-dsfr/Alert";
+import Button from "@codegouvfr/react-dsfr/Button";
 import Head from "next/head";
 import Image from "next/image";
 import * as React from "react";
@@ -29,12 +31,46 @@ const ProfessionalSpaceCreationConfirmationPage = () => {
             height={60}
           />
           <p className="text-xl">
-            Félicitations, votre demande de création de compte a bien été
-            enregistrée ! Vous recevrez un email contenant un lien d'activation
-            du compte valable 4 jours, dès qu’un administrateur France VAE
-            l’aura traitée. En cas de question vous pouvez contacter :
-            support@vae.gouv.fr
+            Votre demande de création de compte a bien été enregistrée.
           </p>
+          <p className="text-xl">
+            L’équipe France VAE va maintenant analyser votre inscription. Cela
+            peut prendre quelques jours.
+          </p>
+          <p className="text-xl">
+            Une fois votre demande validée par nos conseillers, vous recevrez un
+            e-mail contenant un lien d'activation qui sera valable pendant 4
+            jours.
+          </p>
+          <p className="text-xl">
+            En cas de question, vous pouvez contacter :{" "}
+            <a href="mailto:support@vae.gouv.fr">support@vae.gouv.fr</a>
+          </p>
+
+          <Alert
+            severity="info"
+            title={
+              <div className="flex flex-col items-start gap-2 font-normal">
+                <p>
+                  Afin de vous assurer que vous recevez bien nos e-mails,{" "}
+                  <strong>
+                    pensez à vérifier votre dossier de courrier indésirable ou
+                    votre outil de filtrage de spams
+                  </strong>{" "}
+                  si votre structure en utilise un (ex : Mail in Black).
+                </p>
+              </div>
+            }
+          />
+
+          <Button
+            priority="primary"
+            className="self-center !w-full  lg:!w-fit mt-8"
+            linkProps={{ href: "/espace-professionnel" }}
+            size="large"
+          >
+            C'est compris !
+          </Button>
         </div>
       </BlueLayout>
     </MainLayout>
