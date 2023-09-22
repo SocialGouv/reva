ca0d2a38c19621f5f79a75f66814cec7d07af947
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-09-22 16:13:35 +0200

    feat(api): added a dry run mode to the create_accounts script (create_auth_accounts_dry_run)

--- packages/reva-api/prisma/seed/certificateurs/create-accounts.ts
+++ packages/reva-api/prisma/seed/certificateurs/create-accounts.ts
@@ -51,7 +51,11 @@ const createAccountForCertificationAuthority = (params: {
     username: cleanEmail(params.contactEmail),
   });
 
-async function createCertificationAuthoritiesAccounts() {
+async function createCertificationAuthoritiesAccounts({
+  dryRun,
+}: {
+  dryRun: boolean;
+}) {
   await keycloakAdmin.auth({
     grantType: "client_credentials",
     clientId: process.env.KEYCLOAK_ADMIN_CLIENTID || "admin-cli",
@@ -95,10 +99,24 @@ async function createCertificationAuthoritiesAccounts() {
     `Accounts for the following authorities will be created`
   );
 
+  if (!dryRun) {
+    for (const authority of authoritiesToCreateAccountFor) {
       logger.info(`creating account for ${authority.contactEmail}`);
+      (await createAccountForCertificationAuthority(authority)).mapLeft(
+        logger.error
+      );
+    }
   }
 }
 
 (async () => {
-  await createCertificationAuthoritiesAccounts();
+  const dryRun = process.argv[2] === "DRY_RUN";
+
+  logger.info(
+    "Running in " +
+      (dryRun
+        ? "DRY RUN MODE: Accounts WILL be created"
+        : "PRODUCTON MODE: Accounts will NOT be created")
+  );
+  await createCertificationAuthoritiesAccounts({ dryRun });
 })();
