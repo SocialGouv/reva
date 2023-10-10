ccf01f1a48fd77c1540953567cb1bee408f64da4
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-10-10 14:38:47 +0200

    feat(website): upped padding of FullHeightBlueLayout from 8px to 16px

--- packages/reva-website/src/components/layout/full-height-blue-layout/FullHeightBlueLayout.tsx
+++ packages/reva-website/src/components/layout/full-height-blue-layout/FullHeightBlueLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const FullHeightBlueLayout = (props: { children?: ReactNode }) => (
   <div className="flex-1 flex bg-gradient-to-r from-[#557AFF] to-[#2400FF]  p-0 md:p-16">
-    <div className="max-w-none flex-1 bg-white flex flex-col p-2 md:p-16">
+    <div className="max-w-none flex-1 bg-white flex flex-col p-4 md:p-16">
       {props.children}
     </div>
   </div>
