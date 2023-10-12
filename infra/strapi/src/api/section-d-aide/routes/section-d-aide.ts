ccd5be165bd312a3baf82f46a7133f8f1c44d6b4
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-10-12 11:11:58 +0200

    feat(strapi): added help center collections

---
+++ infra/strapi/src/api/section-d-aide/routes/section-d-aide.ts
@@ -0,0 +1,7 @@
+/**
+ * section-d-aide router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::section-d-aide.section-d-aide');
---
+++ infra/strapi/src/api/section-d-aide/services/section-d-aide.ts
@@ -0,0 +1,7 @@
+/**
+ * section-d-aide service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::section-d-aide.section-d-aide');
