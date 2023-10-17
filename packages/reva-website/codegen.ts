96d79850e252429b512bf731d62e230b614f9479
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-10-17 13:49:08 +0200

    feat(strapi): added env variable for strapi api url

--- packages/reva-website/codegen.ts
+++ packages/reva-website/codegen.ts
@@ -1,7 +1,11 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "../reva-api/modules/generated-graphql-schema.graphql",
+  schema: [
+    "../reva-api/modules/generated-graphql-schema.graphql",
+    process.env.NEXT_PUBLIC_WEBSITE_STRAPI_BASE_URL + "/graphql" ||
+      "http://localhost:1337/graphql",
+  ],
   documents: ["src/**/*.tsx"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
