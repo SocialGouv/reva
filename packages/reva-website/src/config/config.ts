89688c5b69d06600012ece6c3588dc9651c26644
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-10-17 14:54:35 +0200

    feat(website): first pass at help center pages

--- packages/reva-website/src/config/config.ts
+++ packages/reva-website/src/config/config.ts
@@ -2,9 +2,10 @@ export const GRAPHQL_API_URL =
   process.env.NEXT_PUBLIC_WEBSITE_API_GRAPHQL ||
   "http://localhost:8080/api/graphql";
 
-export const STRAPI_GRAPHQL_API_URL =
-  process.env.NEXT_PUBLIC_WEBSITE_STRAPI_API_GRAPHQL ||
-  "http://localhost:1337/graphql";
+export const STRAPI_BASE_URL =
+  process.env.NEXT_PUBLIC_WEBSITE_STRAPI_BASE_URL || "http://localhost:1337";
+
+export const STRAPI_GRAPHQL_API_URL = STRAPI_BASE_URL + "/graphql";
 
 export const MATOMO = {
   URL: process.env.NEXT_PUBLIC_MATOMO_URL,
---
+++ packages/reva-website/src/pages/centre-aide/index.tsx
@@ -0,0 +1,127 @@
+import { MainLayout } from "@/components/layout/main-layout/MainLayout";
+import { STRAPI_BASE_URL, STRAPI_GRAPHQL_API_URL } from "@/config/config";
+import { graphql } from "@/graphql/generated";
+import { useQuery } from "@tanstack/react-query";
+import request from "graphql-request";
+import { truncate } from "lodash";
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+const sectionsQuery = graphql(`
+  query getSectionDAides {
+    sectionDAides {
+      data {
+        id
+        attributes {
+          titre
+          article_d_aides {
+            data {
+              id
+              attributes {
+                titre
+                vignette {
+                  data {
+                    attributes {
+                      url
+                      alternativeText
+                    }
+                  }
+                }
+                description
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+const HelpSection = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) => (
+  <div>
+    <h3 className="text-4xl font-bold mb-8">{title}</h3>
+    <div className="flex flex-wrap gap-8">{children}</div>
+  </div>
+);
+
+const HelpArticle = ({
+  title,
+  description,
+  thumbnailUrl,
+  thumbnailAlt,
+  url,
+}: {
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  thumbnailAlt: string;
+  url: string;
+}) => (
+  <Link href={url} className="!bg-none">
+    <div className="grid grid-rows-2 h-[585px] w-[535px] rounded-[32px] shadow-[0px_8px_24px_0px_rgba(11,11,248,0.16)]">
+      <div className="flex">
+        <Image
+          src={STRAPI_BASE_URL + thumbnailUrl}
+          alt={thumbnailAlt}
+          width={535}
+          height={292}
+          className="object-cover w-full rounded-t-[32px]"
+        />
+      </div>
+      <div className="p-8 overflow-hidden ">
+        <h4 className="text-4xl font-bold">{title}</h4>
+        <div>{description}</div>
+      </div>
+    </div>
+  </Link>
+);
+
+const CentreAidePage = () => {
+  const sections = useQuery({
+    queryKey: ["sections"],
+    queryFn: async () => request(STRAPI_GRAPHQL_API_URL, sectionsQuery),
+  });
+
+  return (
+    <MainLayout>
+      <div className="flex flex-col items-center p-32 pt-16">
+        <h1 className="text-7xl font-bold">Centre d’aide France VAE</h1>
+        <h2 className="text-2xl font-bold">
+          Trouvez des réponses à vos questions à propos de votre VAE.
+        </h2>
+        <div className="self-start mt-20 flex flex-col gap-20">
+          {sections.data?.sectionDAides?.data?.map((sa) => (
+            <HelpSection key={sa.id} title={sa.attributes?.titre || ""}>
+              {sa.attributes?.article_d_aides?.data?.map((aa) => (
+                <HelpArticle
+                  key={aa.id}
+                  title={aa.attributes?.titre || ""}
+                  description={truncate(aa.attributes?.description || "", {
+                    length: 300,
+                  })}
+                  thumbnailUrl={
+                    aa.attributes?.vignette?.data?.attributes?.url || ""
+                  }
+                  thumbnailAlt={
+                    aa.attributes?.vignette?.data?.attributes
+                      ?.alternativeText || ""
+                  }
+                  url={"/centre-aide/articles/" + aa.id}
+                />
+              ))}
+            </HelpSection>
+          ))}
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default CentreAidePage;
