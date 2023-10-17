d8e7101e7ad38e88fb7d75c46d0d871091dcc191
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-10-17 13:49:08 +0200

    feat(strapi): added env variable for strapi api url

--- packages/reva-website/src/config/config.ts
+++ packages/reva-website/src/config/config.ts
@@ -2,6 +2,10 @@ export const GRAPHQL_API_URL =
   process.env.NEXT_PUBLIC_WEBSITE_API_GRAPHQL ||
   "http://localhost:8080/api/graphql";
 
+export const STRAPI_GRAPHQL_API_URL =
+  process.env.NEXT_PUBLIC_WEBSITE_STRAPI_API_GRAPHQL ||
+  "http://localhost:1337/graphql";
+
 export const MATOMO = {
   URL: process.env.NEXT_PUBLIC_MATOMO_URL,
   SITE_ID: process.env.NEXT_PUBLIC_MATOMO_SITE_ID,
