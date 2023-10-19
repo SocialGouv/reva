9c71fb087a188a92e86d873d284a780121de6367
Cédric Soulas <soulascedric@gmail.com>
2023-10-19 14:40:22 +0200

    feat(website): update cgu pro

--- packages/reva-website/src/components/cgu/Cgu.tsx
+++ packages/reva-website/src/components/cgu/Cgu.tsx
@@ -2,11 +2,14 @@ export const Cgu = () => (
   <div>
     <p>
       Les présentes conditions générales d’utilisation (dites « CGU ») fixent le
-      cadre juridique de la plateforme France VAE et définissent les conditions
-      d’accès et d’utilisation des services par l’Utilisateur. Ce dispositif est
-      un téléservice conformément aux dispositions de l’ordonnance 2005-1516 du
-      8 décembre 2005 relative aux échanges électroniques entre les usagers et
-      les autorités administratives et entre les autorités administratives.
+      cadre juridique de la Plateforme France VAE et définissent les conditions
+      d’accès et d’utilisation des services par l’Utilisateur.
+    </p>
+    <p>
+      Ce dispositif est un téléservice conformément aux dispositions de
+      l’ordonnance n° 2005-1516 du 8 décembre 2005 relative aux échanges
+      électroniques entre les usagers et les autorités administratives et entre
+      les autorités administratives.
     </p>
     <h2>Article 1 – Présentation de France VAE</h2>
     <p>
@@ -21,7 +24,6 @@ export const Cgu = () => (
       la certification est limité à certaines certifications du secteur « santé,
       soin, dépendance ».
     </p>
-
     <h2>Article 2 – Définitions</h2>
     <p>
       Les « Services » sont les fonctionnalités offertes par France VAE pour
@@ -31,64 +33,77 @@ export const Cgu = () => (
       Les « Structures Professionnelles » de France VAE sont des organismes
       chargés du suivi de la validation des acquis expérientiels des candidats.
     </p>
-
     <h2>Article 3 – Fonctionnalités de la plateforme</h2>
     <h3>3.1 L’espace PRO</h3>
     <p>
-      Chaque Structure professionnelle dispose d’un compte et d’un parcours
-      personnalisé lui permettant de suivre les candidatures des candidats (sur
-      la base de la certification choisie et du secteur géographique de ce
-      dernier).
+      Chaque Structure professionnelle Architecte Accompagnateur de Parcours
+      (AAP) dispose d’un compte et d’un parcours personnalisé lui permettant de
+      suivre les candidatures des candidats (sur la base de la certification
+      choisie et du secteur géographique de ce dernier).
     </p>
     <p>Il leur est possible de :</p>
     <ul>
       <li>
         consulter une candidature et les informations renseignées par le
-        candidat lors de sa candidature
+        candidat lors de sa candidature ;
       </li>
       <li>
         renseigner les informations liées aux rendez-vous pédagogiques et à la
-        recevabilité du candidat
+        recevabilité du candidat ;
       </li>
       <li>
         définir le parcours d’accompagnement prévisionnel du candidat (en nombre
-        d’heure) et de l’envoyer au candidat pour validation
+        d’heure) et de l’envoyer au candidat pour validation ;
       </li>
       <li>
         estimer le coût de la prise en charge (en nombre d’heures, coût horaire)
-        des personnes souhaitant obtenir une certification
+        des personnes souhaitant obtenir une certification ;
       </li>
       <li>
         demander le paiement final des prestations réalisées sur la base de
         pièces justificatives (facture et attestation de présence du candidat
-        par exemple)
+        par exemple) ;
       </li>
-      <li>déclarer l’abandon ou la réorientation du candidat</li>
-      <li>déclarer les dates prévisionnelles de jury du candidat</li>
+      <li>déclarer l’abandon ou la réorientation du candidat ;</li>
+      <li>déclarer les dates prévisionnelles de jury du candidat ;</li>
+      <li>
+        remplir un dossier avec toutes les expériences du candidat pour
+        l’envoyer au certificateur (l’accompagnateur pour justifier de
+        l’identité du candidat joint au dossier la carte nationale d’identité de
+        ce dernier).
+      </li>
     </ul>
     <p>
-      La Structure Professionnelle renseigne notamment, en s’inscrivant, la date
-      de validation de sa certification Qualiopi, des données de contact (nom,
-      prénom et adresse e-mail). Elle sélectionne également sa typologie
+      La Structure Professionnelle AAP renseigne notamment, en s’inscrivant, la
+      date de validation de sa certification Qualiopi, des données de contact
+      (nom, prénom et adresse e-mail). Elle sélectionne également sa typologie
       d’architecte accompagnateur et peut se positionner sur un ensemble de
       certifications (en lien avec un secteur, une filière ou à une convention
-      collective) en fonction de ce critère. La Structure Professionnelle doit
-      également choisir une zone d’intervention basée sur le département ou le
-      distanciel.
+      collective) en fonction de ce critère. La Structure Professionnelle AAP
+      doit également choisir une zone d’intervention basée sur le département en
+      présentiel et/ou en distanciel.
     </p>
     <p>
-      L’administrateur France VAE valide ou invalide l’accès à la plateforme de
-      la Structure Professionnelle selon les données saisies.
+      L’administrateur France VAE valide ou invalide l’accès à la Plateforme de
+      la Structure Professionnelle AAP selon les données saisies.
     </p>
-
-    <h3>3.2 Boîte à outils</h3>
     <p>
-      La Structure Professionnelle en qualité d’architecte de parcours bénéficie
-      de documents avec accès restreint via l’espace PRO.
+      La liste des professionnels Architectes Accompagnateurs de Parcours
+      validés par l'équipe France VAE pourra être partagée (Nom de
+      l'établissement, SIRET et coordonnées) aux instances concernées (Pôle
+      emploi par exemple), notamment à des fins de traitements ou contrôle
+      administratifs.
     </p>
-
+    <h3>3.2 Espace Documentaire</h3>
+    <p>
+      La Structure Professionnelle en qualité d’architecte accompagnateur de
+      parcours bénéficie de l’accès à un espace documentaire dont le lien est
+      réservé à l’usage seul des AAP et certificateurs. Le professionnel y
+      retrouvera notamment le cahier des charges, les dossiers de faisabilités,
+      le calendrier des webinaires...
+    </p>
     <h2>Article 4 – Engagements respectifs</h2>
-    <h3>4.1 L’éditeur de la plateforme</h3>
+    <h3>4.1 L’Éditeur de la Plateforme</h3>
     <p>
       Les sources des informations diffusées sur notre plateforme sont réputées
       fiables mais nous ne garantissons pas qu’il soit exempt de défauts,
@@ -96,39 +111,45 @@ export const Cgu = () => (
     </p>
     <p>
       Tout événement dû à un cas de force majeure ayant pour conséquence un
-      dysfonctionnement de notre plateforme et sous réserve de toute
+      dysfonctionnement de notre Plateforme et sous réserve de toute
       interruption ou modification en cas de maintenance, n'engage pas la
       responsabilité de France VAE.
     </p>
     <p>
-      L’éditeur s’autorise à suspendre ou révoquer n'importe quel compte et
+      L’Éditeur s’autorise à suspendre ou révoquer n'importe quel compte et
       toutes les actions réalisées par ce biais, s’il estime que l’usage réalisé
       du service porte préjudice à son image ou ne correspond pas aux exigences
       de sécurité.
     </p>
     <p>
-      L’éditeur s’engage à la sécurisation de la plateforme, notamment en
+      L’Éditeur s’engage à la sécurisation de la Plateforme, notamment en
       prenant toutes les mesures nécessaires permettant de garantir la sécurité
       et la confidentialité des informations fournies.
     </p>
     <p>
-      L’éditeur fournit les moyens nécessaires et raisonnables pour assurer un
+      L’Éditeur fournit les moyens nécessaires et raisonnables pour assurer un
       accès continu, sans contrepartie financière à la plateforme. Il se réserve
       la liberté de faire évoluer, de modifier ou de suspendre, sans préavis, la
-      plateforme pour des raisons de maintenance ou pour tout autre motif jugé
+      Plateforme pour des raisons de maintenance ou pour tout autre motif jugé
       nécessaire.
     </p>
-
-    <h3>4.2 La Structure Professionnelle de la plateforme</h3>
+    <h3>4.2 La Structure Professionnelle de la Plateforme</h3>
     <p>
-      La structure professionnelle s'assure de garder son mot de passe secret.
-      Toute divulgation du mot de passe, quelle que soit sa forme, est
+      La Structure professionnelle AAP s'assure de garder son mot de passe
+      secret. Toute divulgation du mot de passe, quelle que soit sa forme, est
       interdite. Il assume les risques liés à l'utilisation de son identifiant
       et mot de passe.
     </p>
     <p>
-      Il s’engage à ne pas commercialiser les données reçues et à ne pas les
+      Elle s’engage à ne pas commercialiser les données reçues et à ne pas les
       communiquer à des tiers en dehors des cas prévus par la loi.
+    </p>
+    <p>
+      La Structure professionnelle s’engage à utiliser la carte nationale
+      d’identité du candidat uniquement pour justifier de son identité auprès du
+      certificateur et à prendre toutes les mesures nécessaires pour sécuriser
+      les données. La carte nationale d’identité du candidat n’est conservée que
+      jusqu’à la transmission du dossier au certificateur.
     </p>
     <p>
       Toute information transmise par la Structure Professionnelle est de sa
@@ -138,7 +159,7 @@ export const Cgu = () => (
       pouvant aller jusqu’à trois ans d’emprisonnement et 45 000 euros d’amende.
     </p>
     <p>
-      La structure professionnelle s'engage à ne pas mettre en ligne de contenus
+      La Structure professionnelle s'engage à ne pas mettre en ligne de contenus
       ou informations contraires aux dispositions légales et réglementaires en
       vigueur.
     </p>
@@ -158,16 +179,15 @@ export const Cgu = () => (
       les éléments de paiement. Le financeur libérera les fonds résiduels du
       candidat.
     </p>
-
     <h2>Article 5 – Mise à jour des CGU</h2>
     <p>
-      Les termes des présentes conditions d’utilisation peuvent être amendés à
-      tout moment, sans préavis, en fonction des modifications apportées à la
-      plateforme, de l’évolution de la législation ou pour tout autre motif jugé
-      nécessaire. Chaque modification des CGU donne lieu à une nouvelle version
-      qui doit être acceptée par les parties.
+      Les termes des présentes conditions générales d’utilisation peuvent être
+      amendés à tout moment, sans préavis, en fonction des modifications
+      apportées à la Plateforme, de l’évolution de la législation ou pour tout
+      autre motif jugé nécessaire. Chaque modification des CGU donne lieu à une
+      nouvelle version qui doit être acceptée par les parties.
     </p>
     <br />
-    <p>Dernière version des CGU : 14/03/2023</p>
+    <p>Dernière version des CGU : 26/09/2023</p>
   </div>
 );
