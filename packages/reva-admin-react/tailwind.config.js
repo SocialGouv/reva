2064c85c8950beefc436042be4560490a0066c0e
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-10-30 17:00:57 +0100

    fix(reva-admin-react): moved tailwing config from ts to js since it was not working inside the npm workspace ...

--- packages/reva-admin-react/tailwind.config.js
+++ packages/reva-admin-react/tailwind.config.js
@@ -1,6 +1,4 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+module.exports= {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -17,4 +15,3 @@ const config: Config = {
   },
   plugins: [],
 }
-export default config
