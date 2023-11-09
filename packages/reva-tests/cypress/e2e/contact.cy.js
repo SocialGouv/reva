79901286605185ac54dbbaba5af9c750c366359a
Cédric Soulas <soulascedric@gmail.com>
2023-11-09 08:50:24 +0100

    chore(test): move contact test to proper folder and add activeFeaturesForConnectedUser stub

--- packages/reva-tests/cypress/e2e/contact.cy.js
+++ packages/reva-tests/cypress/e2e/contact.cy.js
@@ -1,75 +1,55 @@
 import { stubMutation, stubQuery } from "../utils/graphql";
 
-const firstname = "John";
-const lastname = "Doe";
+const firstname1 = "John";
+const firstname2 = "John 2";
+
+const lastname1 = "Doe";
+const lastname2 = "Doe 2";
 
 const email1 = "email@example.com";
 const email2 = "email2@example.com";
 
-const phone1 = "06-01-02-03-04";
-const phone2 = "06-01-02-03-05";
+const phone1 = "06 01 02 03 04";
+const phone2 = "06 01 02 03 05";
 
-context.skip("Contact", () => {
-  it("add a contact", function () {
-    cy.intercept("POST", "/api/graphql", (req) => {
-      stubQuery(req, "getDepartments", "departments.json");
-      stubQuery(req, "candidate_askForRegistration", "registration.json");
-    });
-    cy.visit("/");
-
-    cy.get("#firstname").type(firstname);
-    cy.get("#lastname").type(lastname);
-    cy.get("#email").type(email1);
-    cy.get("#phone").type(phone1);
-
-    cy.get('[data-test="project-contact-add"]').click();
-    cy.wait("@candidate_askForRegistration");
-
-    cy.get('[data-test="project-contact-confirmation"]');
-  });
-
-  it("retrieve contact info from registration", function () {
+context("Candidate account", () => {
+  it("update all account information", function () {
     cy.intercept("POST", "/api/graphql", (req) => {
       stubMutation(req, "candidate_login", "candidate1.json");
       stubQuery(req, "getReferential", "referential.json");
-      stubQuery(req, "update_contact", "contact2.json");
-    });
-    cy.login();
-    cy.wait("@candidate_login");
-    cy.wait("@getReferential");
-
-    cy.get('[data-test="progress-title-value"]').should("have.text", "20%");
-
-    cy.get('[data-test="project-home-contact-email').contains(email1);
-    cy.get('[data-test="project-home-contact-phone').contains(phone1);
-
-    cy.get('[data-test="project-home-edit-contact"]').click();
-
-    cy.get("#firstname").should("have.value", firstname);
-    cy.get("#lastname").should("have.value", lastname);
-    cy.get("#phone").should("have.value", phone1);
-    cy.get("#email").should("have.value", email1);
-  });
-
-  it("update email and phone", function () {
-    cy.intercept("POST", "/api/graphql", (req) => {
-      stubMutation(req, "candidate_login", "candidate1.json");
-      stubQuery(req, "getReferential", "referential.json");
+      stubQuery(req, "activeFeaturesForConnectedUser", "features.json");
       stubQuery(req, "update_contact", "contact.json");
     });
     cy.login();
     cy.wait("@candidate_login");
     cy.wait("@getReferential");
 
-    cy.get('[data-test="project-home-edit-contact"]').click();
+    cy.get('[data-test="project-home-update-contact"]').click();
 
-    cy.get("#phone").type(`{selectAll}${phone2}`);
-    cy.get("#email").type(`{selectAll}${email2}`);
+    cy.get("[name=firstname]").should("have.value", firstname1);
+    cy.get("[name=lastname]").should("have.value", lastname1);
+    cy.get("[name=phone]").should("have.value", phone1);
+    cy.get("[name=email]").should("have.value", email1);
+
+    cy.get("[name=firstname]").type(`{selectAll}${firstname2}`);
+    cy.get("[name=lastname]").type(`{selectAll}${lastname2}`);
+    cy.get("[name=phone]").type(`{selectAll}${phone2}`);
+    cy.get("[name=email]").type(`{selectAll}${email2}`);
 
     cy.get('[data-test="project-contact-save"]').click();
     cy.wait("@update_contact");
 
+    cy.get('[data-test="project-home-fullname"]').contains(
+      `${firstname2} ${lastname2}`
+    );
     cy.get('[data-test="project-home-contact-phone"]').contains(phone2);
     cy.get('[data-test="project-home-contact-email"]').contains(email2);
+
+    cy.get('[data-test="project-home-update-contact"]').click();
+
+    cy.get("[name=firstname]").should("have.value", firstname2);
+    cy.get("[name=lastname]").should("have.value", lastname2);
+    cy.get("[name=phone]").should("have.value", phone2);
+    cy.get("[name=email]").should("have.value", email2);
   });
 });
