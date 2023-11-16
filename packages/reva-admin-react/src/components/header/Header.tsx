f17389033a50b5bd097588d87082e3739659a252
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-11-16 15:23:41 +0100

    feat(admin-react): added nav menu

--- packages/reva-admin-react/src/components/header/Header.tsx
+++ packages/reva-admin-react/src/components/header/Header.tsx
@@ -1,34 +1,70 @@
 "use client";
+import { ADMIN_ELM_URL } from "@/config/config";
 import { Header as DsfrHeader } from "@codegouvfr/react-dsfr/Header";
 import { signOut } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
-export const Header = () => (
-  <DsfrHeader
-    brandTop={
-      <>
-        république
-        <br />
-        française
-      </>
-    }
-    homeLinkProps={{
-      href: "../",
-      title: "Accueil - France VAE",
-    }}
-    operatorLogo={{
-      alt: "France VAE",
-      imgUrl: "/admin2/fvae_logo.svg",
-      orientation: "horizontal",
-    }}
-    quickAccessItems={[
-      {
-        buttonProps: {
-          onClick: () => signOut({ redirect: false }),
-          className: "!text-sm !px-3 !py-1 !mb-4 !mx-1",
+export const Header = () => {
+  const currentPathName = usePathname();
+  return (
+    <DsfrHeader
+      brandTop={
+        <>
+          république
+          <br />
+          française
+        </>
+      }
+      homeLinkProps={{
+        href: "../",
+        title: "Accueil - France VAE",
+      }}
+      operatorLogo={{
+        alt: "France VAE",
+        imgUrl: "/admin2/fvae_logo.svg",
+        orientation: "horizontal",
+      }}
+      quickAccessItems={[
+        {
+          buttonProps: {
+            onClick: () => signOut({ redirect: false }),
+            className: "!text-sm !px-3 !py-1 !mb-4 !mx-1",
+          },
+          iconId: "ri-logout-box-r-line",
+          text: "Se déconnecter",
         },
-        iconId: "ri-logout-box-r-line",
-        text: "Se déconnecter",
-      },
-    ]}
-  />
-);
+      ]}
+      navigation={[
+        {
+          text: "Candidatures",
+          linkProps: {
+            href: ADMIN_ELM_URL + "/candidacies",
+            target: "_self",
+          },
+        },
+        {
+          text: "Paramètres du compte",
+          linkProps: {
+            href: "/account-parameters",
+            target: "_self",
+          },
+          isActive: currentPathName.startsWith("/account-parameters"),
+        },
+        {
+          text: "Inscriptions",
+          linkProps: {
+            href: ADMIN_ELM_URL + "/subscritpions",
+            target: "_self",
+          },
+        },
+        {
+          text: "Comptes",
+          linkProps: {
+            href: ADMIN_ELM_URL + "/accounts",
+            target: "_self",
+          },
+        },
+      ]}
+    />
+  );
+};
--- packages/reva-admin-react/src/config/config.ts
+++ packages/reva-admin-react/src/config/config.ts
@@ -1,3 +1,6 @@
 export const GRAPHQL_API_URL =
   process.env.NEXT_PUBLIC_ADMIN_REACT_GRAPHQL_API_URL ||
   "http://localhost:8080/api/graphql";
+
+export const ADMIN_ELM_URL =
+  process.env.NEXT_PUBLIC_ADMIN_ELM_URL || "http://localhost:3000/admin";
