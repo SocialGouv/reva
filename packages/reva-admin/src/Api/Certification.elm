f12f693607b8ec08605fd1c83a4270900b813bdb
Cédric Soulas <soulascedric@gmail.com>
2023-11-20 16:43:30 +0100

    feat(admin): add api client code to retrieve all certifications

--- packages/reva-admin/src/Api/Certification.elm
+++ packages/reva-admin/src/Api/Certification.elm
@@ -1,10 +1,44 @@
-module Api.Certification exposing (selection, summarySelection)
+module Api.Certification exposing (getCertifications, selection, summarySelection)
 
 import Admin.Object
 import Admin.Object.Certification
+import Admin.Object.CertificationPage
 import Admin.Object.CertificationSummary
+import Admin.Query as Query
+import Api.Auth as Auth
+import Api.Pagination exposing (pageInfoSelection)
+import Api.Token exposing (Token)
 import Data.Certification
+import Graphql.OptionalArgument as OptionalArgument exposing (OptionalArgument(..))
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet, with)
+import RemoteData exposing (RemoteData(..))
+
+
+getCertifications :
+    String
+    -> Token
+    -> Int
+    -> (RemoteData (List String) Data.Certification.CertificationPage -> msg)
+    -> Maybe String
+    -> Cmd msg
+getCertifications endpointGraphql token page toMsg searchText =
+    Query.getCertifications
+        (\optionals ->
+            { optionals
+                | limit = Present 10
+                , offset = Present ((page - 1) * 10)
+                , searchText = OptionalArgument.fromMaybe searchText
+            }
+        )
+        pageSelection
+        |> Auth.makeQuery "getCertifications" endpointGraphql token toMsg
+
+
+pageSelection : SelectionSet Data.Certification.CertificationPage Admin.Object.CertificationPage
+pageSelection =
+    SelectionSet.succeed Data.Certification.CertificationPage
+        |> with (Admin.Object.CertificationPage.rows selection)
+        |> with (Admin.Object.CertificationPage.info pageInfoSelection)
 
 
 selection : SelectionSet Data.Certification.Certification Admin.Object.Certification
