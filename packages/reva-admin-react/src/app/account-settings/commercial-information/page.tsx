9a4094daa2957a66e0486e191286ddf44569ba85
Alexandre Garbe <alexandre.garbe@gmail.com>
2023-12-01 10:22:17 +0100

    feat(react-admin): information commerciales form now displays an error message when an error occurs

--- packages/reva-admin-react/src/app/account-settings/commercial-information/page.tsx
+++ packages/reva-admin-react/src/app/account-settings/commercial-information/page.tsx
@@ -66,152 +66,168 @@ const CommercialInformationPage = () => {
   });
 
   return (
-    informationsCommercialesStatus === "success" && (
-      <div className="flex flex-col">
-        <h1 className="leading-6 font-bold text-2xl mb-8">
-          Informations commerciales
-        </h1>
+    <div className="flex flex-col">
+      <h1 className="leading-6 font-bold text-2xl mb-8">
+        Informations commerciales
+      </h1>
 
+      {informationsCommercialesStatus === "error" && (
         <Alert
-          severity="info"
-          title=""
-          description="Les informations suivantes seront affichées aux candidats dans les résultats de recherche d’un AAP et dans le message récapitulant leur candidature. Si elles ne sont pas renseignées, les informations juridiques et administrateur seront prises par défaut."
+          className="mb-6"
+          severity="error"
+          title="Une erreur est survenue pensant la récupération des informations commerciales."
         />
+      )}
+      {createOrUpdateInformationsCommerciales.status === "error" && (
+        <Alert
+          className="mb-6"
+          severity="error"
+          title="Une erreur est survenue pensant l'enregistrement des informations commerciales."
+        />
+      )}
+      {informationsCommercialesStatus === "success" && (
+        <>
+          <Alert
+            severity="info"
+            title=""
+            description="Les informations suivantes seront affichées aux candidats dans les résultats de recherche d’un AAP et dans le message récapitulant leur candidature. Si elles ne sont pas renseignées, les informations juridiques et administrateur seront prises par défaut."
+          />
 
-        <form
-          className="flex flex-col mt-10"
-          onSubmit={handleFormSubmit}
-          onReset={(e) => {
-            e.preventDefault();
-            reset(informationsCommerciales as FormData);
-          }}
-        >
-          <fieldset className="flex flex-col gap-4">
-            <div>
-              <Input
-                className="!mb-4"
-                label="Nom commercial (optionnel)"
-                nativeInputProps={{ ...register("nom") }}
-              />
-              <SmallNotice>
-                Si vous ne renseignez pas ce champ, votre raison commerciale
-                sera affichée par défaut.
-              </SmallNotice>
-            </div>
-            <div>
-              <Input
-                className="!mb-4"
-                label="Téléphone (optionnel)"
-                nativeInputProps={{
-                  ...register("telephone"),
-                }}
-              />
-              <SmallNotice>
-                Ce numéro de téléphone sera également envoyé aux candidats dans
-                le message récapitulant leur inscription.
-              </SmallNotice>
-            </div>
-            <div>
-              <Input
-                className="!mb-4"
-                label="Site internet de l'établissement (optionnel)"
-                nativeInputProps={{
-                  ...register("siteInternet"),
-                }}
-              />
-              <SmallNotice>
-                Ajouter le lien de votre établissement peut permettre à celui-ci
-                de se démarquer lorsque le candidat choisira son organisme
-                accompagnateur.
-              </SmallNotice>
-            </div>
-            <div>
-              <Input
-                className="!mb-4"
-                label="E-mail de contact (optionnel)"
-                state={errors.emailContact ? "error" : "default"}
-                stateRelatedMessage={errors.emailContact?.message}
-                nativeInputProps={{
-                  ...register("emailContact"),
-                }}
-              />
-              <SmallNotice>
-                Cet e-mail sera également envoyé aux candidats dans le message
-                récapitulant leur inscription.
-              </SmallNotice>
-            </div>
-          </fieldset>
-          <fieldset className="mt-8">
-            <legend className="text-2xl font-bold mb-4">
-              Adresse du lieu d’accueil (optionnel)
-            </legend>
-            <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-x-8">
-              <Input
-                label="Numéro et nom de rue (optionnel)"
-                nativeInputProps={{
-                  ...register("adresseNumeroEtNomDeRue"),
-                }}
-              />
-              <Input
-                label="Informations complémentaires (optionnel)"
-                nativeInputProps={{
-                  ...register("adresseInformationsComplementaires"),
-                }}
-              />
-              <Input
-                label="Code Postal (optionnel)"
-                nativeInputProps={{
-                  ...register("adresseCodePostal"),
-                }}
-              />
-              <Input
-                label="Ville (optionnel)"
-                nativeInputProps={{
-                  ...register("adresseVille"),
-                }}
-              />
-            </div>
-          </fieldset>
+          <form
+            className="flex flex-col mt-10"
+            onSubmit={handleFormSubmit}
+            onReset={(e) => {
+              e.preventDefault();
+              reset(informationsCommerciales as FormData);
+            }}
+          >
+            <fieldset className="flex flex-col gap-4">
+              <div>
+                <Input
+                  className="!mb-4"
+                  label="Nom commercial (optionnel)"
+                  nativeInputProps={{ ...register("nom") }}
+                />
+                <SmallNotice>
+                  Si vous ne renseignez pas ce champ, votre raison commerciale
+                  sera affichée par défaut.
+                </SmallNotice>
+              </div>
+              <div>
+                <Input
+                  className="!mb-4"
+                  label="Téléphone (optionnel)"
+                  nativeInputProps={{
+                    ...register("telephone"),
+                  }}
+                />
+                <SmallNotice>
+                  Ce numéro de téléphone sera également envoyé aux candidats
+                  dans le message récapitulant leur inscription.
+                </SmallNotice>
+              </div>
+              <div>
+                <Input
+                  className="!mb-4"
+                  label="Site internet de l'établissement (optionnel)"
+                  nativeInputProps={{
+                    ...register("siteInternet"),
+                  }}
+                />
+                <SmallNotice>
+                  Ajouter le lien de votre établissement peut permettre à
+                  celui-ci de se démarquer lorsque le candidat choisira son
+                  organisme accompagnateur.
+                </SmallNotice>
+              </div>
+              <div>
+                <Input
+                  className="!mb-4"
+                  label="E-mail de contact (optionnel)"
+                  state={errors.emailContact ? "error" : "default"}
+                  stateRelatedMessage={errors.emailContact?.message}
+                  nativeInputProps={{
+                    ...register("emailContact"),
+                  }}
+                />
+                <SmallNotice>
+                  Cet e-mail sera également envoyé aux candidats dans le message
+                  récapitulant leur inscription.
+                </SmallNotice>
+              </div>
+            </fieldset>
+            <fieldset className="mt-8">
+              <legend className="text-2xl font-bold mb-4">
+                Adresse du lieu d’accueil (optionnel)
+              </legend>
+              <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-x-8">
+                <Input
+                  label="Numéro et nom de rue (optionnel)"
+                  nativeInputProps={{
+                    ...register("adresseNumeroEtNomDeRue"),
+                  }}
+                />
+                <Input
+                  label="Informations complémentaires (optionnel)"
+                  nativeInputProps={{
+                    ...register("adresseInformationsComplementaires"),
+                  }}
+                />
+                <Input
+                  label="Code Postal (optionnel)"
+                  nativeInputProps={{
+                    ...register("adresseCodePostal"),
+                  }}
+                />
+                <Input
+                  label="Ville (optionnel)"
+                  nativeInputProps={{
+                    ...register("adresseVille"),
+                  }}
+                />
+              </div>
+            </fieldset>
 
-          <fieldset className="mt-8">
-            <RadioButtons
-              legend="Votre établissement est-il conforme aux normes d'accessibilité et peut
+            <fieldset className="mt-8">
+              <RadioButtons
+                legend="Votre établissement est-il conforme aux normes d'accessibilité et peut
             recevoir du public à mobilité réduite ?"
-              options={[
-                {
-                  label: "Oui",
-                  nativeInputProps: {
-                    value: "CONFORME",
-                    ...register("conformeNormesAccessbilite"),
+                options={[
+                  {
+                    label: "Oui",
+                    nativeInputProps: {
+                      value: "CONFORME",
+                      ...register("conformeNormesAccessbilite"),
+                    },
                   },
-                },
-                {
-                  label: "Non",
-                  nativeInputProps: {
-                    value: "NON_CONFORME",
-                    ...register("conformeNormesAccessbilite"),
+                  {
+                    label: "Non",
+                    nativeInputProps: {
+                      value: "NON_CONFORME",
+                      ...register("conformeNormesAccessbilite"),
+                    },
                   },
-                },
-                {
-                  label: "Cet établissement ne reçoit pas de public",
-                  nativeInputProps: {
-                    value: "ETABLISSEMENT_NE_RECOIT_PAS_DE_PUBLIC",
-                    ...register("conformeNormesAccessbilite"),
+                  {
+                    label: "Cet établissement ne reçoit pas de public",
+                    nativeInputProps: {
+                      value: "ETABLISSEMENT_NE_RECOIT_PAS_DE_PUBLIC",
+                      ...register("conformeNormesAccessbilite"),
+                    },
                   },
-                },
-              ]}
-            />
-          </fieldset>
+                ]}
+              />
+            </fieldset>
 
-          <div className="flex flex-col md:flex-row gap-4 self-center md:self-end mt-8">
-            <Button priority="secondary" type="reset">
-              Annuler les modifications
-            </Button>
-            <Button disabled={isSubmitting}>Valider les modifications</Button>
-          </div>
-        </form>
-      </div>
-    )
+            <div className="flex flex-col md:flex-row gap-4 self-center md:self-end mt-8">
+              <Button priority="secondary" type="reset">
+                Annuler les modifications
+              </Button>
+              <Button disabled={isSubmitting}>Valider les modifications</Button>
+            </div>
+          </form>
+        </>
+      )}
+    </div>
   );
 };
 
