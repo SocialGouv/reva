4dda98b09e26c1c0a7e6eee0ce7acbb34f1ac9e9
Florian Bonniec <f.bonniec@me.com>
2023-12-07 17:37:33 +0100

    fix(api): update timeout of seed-candidacy-convention-collective

--- packages/reva-api/prisma/seed/referentials/seed-candidacy-convention-collective.ts
+++ packages/reva-api/prisma/seed/referentials/seed-candidacy-convention-collective.ts
@@ -5,27 +5,30 @@ import { PrismaClient } from "@prisma/client";
 import { readCsvRows } from "../read-csv";
 
 export async function seedCandidacyConventionCollective(prisma: PrismaClient) {
-  await prisma.$transaction(async (tx) => {
-    await tx.$executeRawUnsafe(
-      `SET CONSTRAINTS "candidacy_ccn_id_fkey" DEFERRED;`
-    );
+  await prisma.$transaction(
+    async (tx) => {
+      await tx.$executeRawUnsafe(
+        `SET CONSTRAINTS "candidacy_ccn_id_fkey" DEFERRED;`
+      );
 
-    await tx.candidacyConventionCollective.deleteMany();
+      await tx.candidacyConventionCollective.deleteMany();
 
-    const ccn = await readCsvRows<{
-      id: string;
-      idcc: string;
-      label: string;
-    }>({
-      filePath: "./referentials/candidacy-convention-collective.csv",
-      headersDefinition: ["id", "idcc", "label"],
-    });
+      const ccn = await readCsvRows<{
+        id: string;
+        idcc: string;
+        label: string;
+      }>({
+        filePath: "./referentials/candidacy-convention-collective.csv",
+        headersDefinition: ["id", "idcc", "label"],
+      });
 
-    await tx.candidacyConventionCollective.createMany({
-      data: ccn,
-    });
-    await tx.$executeRawUnsafe(
-      `SET CONSTRAINTS "candidacy_ccn_id_fkey" IMMEDIATE;`
-    );
-  });
+      await tx.candidacyConventionCollective.createMany({
+        data: ccn,
+      });
+      await tx.$executeRawUnsafe(
+        `SET CONSTRAINTS "candidacy_ccn_id_fkey" IMMEDIATE;`
+      );
+    },
+    { maxWait: 5000, timeout: 15000 }
+  );
 }
