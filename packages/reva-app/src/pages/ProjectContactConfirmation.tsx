6b5f486e12e71ba3fb27c723d1e3e23eb9ebd419
Florian Bonniec <f.bonniec@me.com>
2023-12-15 11:19:02 +0100

    feat(app): improve wording of ProjectContactConfirmation

--- packages/reva-app/src/pages/ProjectContactConfirmation.tsx
+++ packages/reva-app/src/pages/ProjectContactConfirmation.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@codegouvfr/react-dsfr/Button";
+
 import { Page } from "../components/organisms/Page";
 
 export const ProjectContactConfirmation = () => (
@@ -6,14 +8,31 @@ export const ProjectContactConfirmation = () => (
     title="Confirmation de création de compte"
   >
     <div role="status">
-      <h1 className="text-3xl font-bold text-dsfrBlue-500">Félicitations !</h1>
+      <h1 className="text-3xl font-bold text-dsfrBlue-500">
+        Votre demande de création de compte a bien été enregistrée.
+      </h1>
       <p className="mt-4 text-lg font-bold text-dsfrGray-500">
-        Vous allez recevoir un email pour finaliser votre inscription.
+        Vous allez recevoir un mail contenant un lien d'activation qui sera
+        valable pendant 4 jours.
       </p>
       <p className="mt-4 text-dsfrGray-500">
-        Cliquez sur le lien de validation dans cet email pour accéder à votre
-        espace candidat.
+        Afin de vous assurer que vous recevez bien nos e-mails, pensez à
+        vérifiez votre dossier de courrier indésirable (spams).
+        <br />
+        En cas de question, vous pouvez contacter :{" "}
+        <a
+          data-test="register-mailto"
+          rel="noreferrer"
+          href="mailto:support@vae.gouv.fr"
+        >
+          support@vae.gouv.fr
+        </a>
       </p>
+    </div>
+    <div className="flex flex-col justify-center items-center text-center">
+      <Button className="mt-6" linkProps={{ href: "/" }}>
+        Retour à l'accueil
+      </Button>
     </div>
   </Page>
 );
