03e45c89e8d07f2ae29101322498c016a83e4722
ThomasDos <dosanjos.thomas@gmail.com>
2024-01-03 14:48:39 +0100

    feat(admin-react): add content in AgencesPage

--- packages/reva-admin-react/src/app/agences/page.tsx
+++ packages/reva-admin-react/src/app/agences/page.tsx
@@ -1,3 +1,18 @@
+import { PageTitle } from "@/components/page/page-title/PageTitle";
+
 export default function AgencesPage() {
-  return null;
+  return (
+    <div className="flex flex-col items-center justify-center ox-4 sm:px-10">
+      <PageTitle>Ajoutez et gérez vos agences locales ici.</PageTitle>
+
+      <div className="my-4 self-start text-lg">Vous pouvez :</div>
+
+      <ul className="self-start pl-10">
+        <li className="list-disc">
+          déclarer votre structure actuelle en tant qu'agence,
+        </li>
+        <li className="list-disc">ajouter une ou plusieurs agences</li>
+      </ul>
+    </div>
+  );
 }
