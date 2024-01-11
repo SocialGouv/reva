da3cff73e98e2f24feaa5f8c7ea48426ff914d39
ThomasDos <dosanjos.thomas@gmail.com>
2024-01-11 17:47:55 +0100

    feat(website): remove banner image and rework article page

--- packages/reva-website/src/pages/savoir-plus/articles/[slug]/index.tsx
+++ packages/reva-website/src/pages/savoir-plus/articles/[slug]/index.tsx
@@ -4,7 +4,6 @@ import { graphql } from "@/graphql/generated";
 import Button from "@codegouvfr/react-dsfr/Button";
 import { useQuery } from "@tanstack/react-query";
 import request from "graphql-request";
-import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
 const articleQuery = graphql(`
@@ -48,43 +47,31 @@ const ArticleAidePage = () => {
   return (
     <MainLayout>
       {
-        <div className="relative">
+        <div className="flex flex-col sm:flex-row w-full gap-8 sm:gap-16 fr-container p-32 pt-16">
           <Button
-            className="absolute top-4 left-4 !bg-white"
-            priority="tertiary no outline"
+            className="h-fit"
+            priority="tertiary"
             iconPosition="left"
             iconId="fr-icon-arrow-left-line"
             linkProps={{ href: "/savoir-plus" }}
             size="large"
           >
-            Revenir au centre d'aide
+            Retour
           </Button>
-          <Image
-            src={article.attributes?.vignette?.data?.attributes?.url || ""}
-            alt={
-              article.attributes?.vignette?.data?.attributes?.alternativeText ||
-              ""
-            }
-            height={360}
-            width={100}
-            className="object-cover w-screen h-[360px]"
-          />
-          <div className="p-4">
-            <div className="fr-container p-32 pt-16 flex flex-col max-w-4xl items-center">
-              <h1 className="text-7xl font-bold mb-16">
-                {article.attributes?.titre}
-              </h1>
-              <div
-                className="ck-content"
-                dangerouslySetInnerHTML={{
-                  __html:
-                    article.attributes?.contenu?.replaceAll(
-                      "<a",
-                      "<a target='_'"
-                    ) || "",
-                }}
-              />
-            </div>
+          <div className="flex flex-col max-w-4xl">
+            <h1 className="font-bold mb-16" style={{ fontSize: "40px" }}>
+              {article.attributes?.titre}
+            </h1>
+            <div
+              className="ck-content"
+              dangerouslySetInnerHTML={{
+                __html:
+                  article.attributes?.contenu?.replaceAll(
+                    "<a",
+                    "<a target='_'"
+                  ) || "",
+              }}
+            />
           </div>
         </div>
       }
