9d5900b5c983838f9876fe6c083b182f3dc4a2c2
Cédric Soulas <soulascedric@gmail.com>
2024-01-16 14:21:07 +0100

    feat(api): allow multi-words search in candidacies and feasibilities

--- packages/reva-api/modules/shared/search/search.ts
+++ packages/reva-api/modules/shared/search/search.ts
@@ -10,7 +10,19 @@ export const getWhereClauseFromSearchFilter = (
   searchFilter?: string
 ) => {
   if (searchFilter) {
-    return createWhereInput(searchFilter);
+    if (searchFilter.length > 100) {
+      throw new Error(
+        "Veuillez réduire le nombre de caractères présents dans votre recherche."
+      );
+    }
+
+    const words = searchFilter.split(/\s+/);
+    if (words.length > 12) {
+      throw new Error(
+        "Veuillez réduire le nombre de mots présents dans votre recherche."
+      );
+    }
+    return { AND: words.map(createWhereInput) };
   }
   return {};
 };
