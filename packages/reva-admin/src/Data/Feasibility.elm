31db528b8b3e63a5354429112a95797cdbf164c2
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-01-23 12:57:27 +0100

    chore(admin): removed unused code

--- packages/reva-admin/src/Data/Feasibility.elm
+++ packages/reva-admin/src/Data/Feasibility.elm
@@ -1,10 +1,8 @@
-module Data.Feasibility exposing (Candidate, Decision(..), Feasibility, FeasibilityCountByCategory, FeasibilitySummary, FeasibilitySummaryPage, feasibilityCategoryFilterToReadableString)
+module Data.Feasibility exposing (Candidate, Decision(..), Feasibility)
 
-import Admin.Enum.FeasibilityCategoryFilter as FeasibilityCategoryFilter exposing (FeasibilityCategoryFilter)
 import Data.CertificationAuthority exposing (CertificationAuthority)
 import Data.File exposing (File)
 import Data.Organism exposing (Organism)
-import Data.Pagination exposing (PaginationInfo)
 import Data.Scalar
 
 
@@ -34,48 +32,3 @@ type alias Feasibility =
     , decision : Decision
     , decisionSentAt : Maybe Data.Scalar.Timestamp
     }
-
-
-type alias FeasibilitySummary =
-    { id : Data.Scalar.Id
-    , feasibilityFileSentAt : Data.Scalar.Timestamp
-    , certificationLabel : Maybe String
-    , candidateFirstname : Maybe String
-    , candidateLastname : Maybe String
-    , departmentLabel : Maybe String
-    , departmentCode : Maybe String
-    }
-
-
-type alias FeasibilitySummaryPage =
-    { rows : List FeasibilitySummary
-    , info : PaginationInfo
-    }
-
-
-type alias FeasibilityCountByCategory =
-    { all : Int
-    , pending : Int
-    , admissible : Int
-    , incomplete : Int
-    , rejected : Int
-    }
-
-
-feasibilityCategoryFilterToReadableString : FeasibilityCategoryFilter -> String
-feasibilityCategoryFilterToReadableString categoryFilter =
-    case categoryFilter of
-        FeasibilityCategoryFilter.Pending ->
-            "Dossiers en attente de recevabilité"
-
-        FeasibilityCategoryFilter.Admissible ->
-            "Dossiers recevables"
-
-        FeasibilityCategoryFilter.Rejected ->
-            "Dossiers non recevables"
-
-        FeasibilityCategoryFilter.Incomplete ->
-            "Dossiers incomplets"
-
-        _ ->
-            "Tous les dossiers de faisabilité"
