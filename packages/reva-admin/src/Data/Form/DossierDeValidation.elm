ddffe51e81fded7262715747911d53d99a812b24
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-01-23 17:21:23 +0100

    feat(admin): added checks and validation to dossier de validation form

--- packages/reva-admin/src/Data/Form/DossierDeValidation.elm
+++ packages/reva-admin/src/Data/Form/DossierDeValidation.elm
@@ -1,15 +1,26 @@
 module Data.Form.DossierDeValidation exposing (keys, validate)
 
-import Data.Candidacy exposing (Candidacy, CandidacyId)
+import Data.Candidacy exposing (Candidacy)
 import Data.Form exposing (FormData)
+import Data.Form.Helper as Helper
 import Data.Referential exposing (Referential)
-import Dict exposing (Dict)
 
 
 keys =
-    { dossierDeValidationFile = "dossierDeValidationFile" }
+    { dossierDeValidationFile = "dossierDeValidationFile", dossierDeValidationFileCheck = "dossierDeValidationFile", otherFilesCheck = "otherFilesCheck" }
 
 
 validate : ( Candidacy, Referential ) -> FormData -> Result (List String) ()
 validate ( _, _ ) formData =
-    Ok ()
+    let
+        decode =
+            Helper.decode keys formData
+
+        error =
+            Err [ "Veuillez vérifier les pièces jointes et cocher toutes les cases" ]
+    in
+    if decode.bool .dossierDeValidationFileCheck False && decode.bool .otherFilesCheck False then
+        Ok ()
+
+    else
+        error
--- packages/reva-admin/src/Page/Form/DossierDeValidation.elm
+++ packages/reva-admin/src/Page/Form/DossierDeValidation.elm
@@ -4,6 +4,8 @@ import Data.Candidacy exposing (Candidacy)
 import Data.Form exposing (FormData)
 import Data.Form.DossierDeValidation
 import Data.Referential exposing (Referential)
+import Html exposing (div, h2, text)
+import Html.Attributes exposing (class)
 import Page.Form as Form exposing (Form)
 
 
@@ -12,12 +14,35 @@ form _ ( candidacy, _ ) =
     let
         keys =
             Data.Form.DossierDeValidation.keys
+
+        filesChecklistTitle =
+            ( ""
+            , Form.StaticHtml <|
+                div
+                    [ class "mt-6 -mb-8"
+                    , class "pt-6 px-8 bg-neutral-100 w-full"
+                    , class "[&+div]:!w-full [&+div]:!mr-0 [&+div]:bg-neutral-100 [&+div]:px-8"
+                    ]
+                    [ h2 [ class "text-xl" ] [ text "Avant de finaliser l' envoi :" ] ]
+            )
+
+        filesChecklist =
+            [ ( keys.dossierDeValidationFileCheck
+              , "J’ai bien vérifié que le dossier de validation était complet et lisible."
+              )
+            , ( keys.otherFilesCheck
+              , "J’ai bien vérifié que tous les autres documents étaient lisibles."
+              )
+            ]
     in
     { elements =
         [ ( ""
           , Form.Text "Le dossier de validation doit être rédigé par le candidat.Des pièces jointes supplémentaires peuvent être ajoutées selon les attendus du certificateur." Nothing
           )
+        , ( "", Form.Title1 "Pièces jointes" )
         , ( keys.dossierDeValidationFile, Form.FileRequired "Joindre le dossier de validation" "Format supporté : PDF uniquement avec un poids maximum de 2Mo" )
+        , filesChecklistTitle
+        , ( "filesChecklist", Form.CheckboxList "" filesChecklist )
         ]
     , saveLabel = Nothing
     , submitLabel = "Enregistrer"
