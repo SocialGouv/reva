2a8c6694e70aa9e2c7764fe20bd501503f601a54
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-02-01 18:07:59 +0100

    feat(api): added security to dossierDeValidation_signalProblem mutation

--- packages/reva-api/modules/dossier-de-validation/dossier-de-validation.security.ts
+++ packages/reva-api/modules/dossier-de-validation/dossier-de-validation.security.ts
@@ -1,6 +1,21 @@
+import { hasRole } from "../shared/security/middlewares";
+import { defaultSecurity } from "../shared/security/presets";
 import { canManageDossierDeValidation } from "./security/canManageDossierDeValidation";
 
 export const dossierDeValidationResolversSecurityMap = {
+  "Mutation.*": defaultSecurity,
+  "Query.*": defaultSecurity,
+
   "Query.dossierDeValidation_getDossierDeValidationById":
     canManageDossierDeValidation,
+
+  "Query.dossierDeValidation_dossierDeValidationCountByCategory": [
+    hasRole(["admin", "manage_feasibility"]),
+  ],
+
+  "Query.dossierDeValidation_getDossiersDeValidation": [
+    hasRole(["admin", "manage_feasibility"]),
+  ],
+
+  "Mutation.dossierDeValidation_signalProblem": canManageDossierDeValidation,
 };
