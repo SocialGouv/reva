51df1f112201a91c73ed26f2e42536ea9c61b3de
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-02-07 16:36:05 +0100

    feat(api): readyForJuryEstimatedAt can only between set between today and 2 years from now

--- packages/reva-api/modules/candidacy/features/setReadyForJuryEstimatedAt.ts
+++ packages/reva-api/modules/candidacy/features/setReadyForJuryEstimatedAt.ts
@@ -1,11 +1,33 @@
+import {
+  addYears,
+  isAfter,
+  isBefore,
+  startOfDay,
+  startOfToday,
+} from "date-fns";
 import { prismaClient } from "../../../prisma/client";
 
-export const setReadyForJuryEstimatedAt = async (params: {
+export const setReadyForJuryEstimatedAt = async ({
+  candidacyId,
+  readyForJuryEstimatedAt,
+}: {
   candidacyId: string;
   readyForJuryEstimatedAt: Date;
 }) => {
+  const now = startOfToday();
+
+  if (isBefore(readyForJuryEstimatedAt, now)) {
+    throw new Error("La date prévisionnelle ne peut pas être dans le passé.");
+  }
+
+  if (isAfter(startOfDay(readyForJuryEstimatedAt), addYears(now, 2))) {
+    throw new Error(
+      "La date prévisionnelle doit être comprise dans les deux prochaines années",
+    );
+  }
+
   const candidacy = await prismaClient.candidacy.findUnique({
-    where: { id: params.candidacyId },
+    where: { id: candidacyId },
   });
 
   if (!candidacy) {
@@ -17,9 +39,9 @@ export const setReadyForJuryEstimatedAt = async (params: {
   }
 
   return prismaClient.candidacy.update({
-    where: { id: params.candidacyId },
+    where: { id: candidacyId },
     data: {
-      readyForJuryEstimatedAt: params.readyForJuryEstimatedAt,
+      readyForJuryEstimatedAt,
     },
   });
 };
