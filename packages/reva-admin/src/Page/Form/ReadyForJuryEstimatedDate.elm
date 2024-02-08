6539c68863ce1df4ee699b3b32c0143e8241a834
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-02-08 17:21:00 +0100

    fix(admin): wording changes on ReadyForJuryEstimatedDate instead of DossierDeValidation

--- packages/reva-admin/src/Page/Form/ReadyForJuryEstimatedDate.elm
+++ packages/reva-admin/src/Page/Form/ReadyForJuryEstimatedDate.elm
@@ -1,9 +1,11 @@
 module Page.Form.ReadyForJuryEstimatedDate exposing (..)
 
+import Accessibility exposing (li, text, ul)
 import Data.Candidacy exposing (Candidacy)
 import Data.Form exposing (FormData)
 import Data.Form.ReadyForJuryEstimatedDate
 import Data.Referential exposing (Referential)
+import Html.Attributes exposing (class)
 import Page.Form as Form exposing (Form)
 import View.Date
 
@@ -15,21 +17,23 @@ form _ ( candidacy, _ ) =
             Data.Form.ReadyForJuryEstimatedDate.keys
     in
     { elements =
-        case candidacy.readyForJuryEstimatedAt of
+        [ ( ""
+          , Form.Text "La date prévisionnelle correspond :" Nothing
+          )
+        , ( ""
+          , Form.StaticHtml <|
+                ul [ class "mb-8" ]
+                    [ li [] [ text "à la date à laquelle le candidat aura finalisé son dossier de validation pour les certifications du Ministère du Travail et des Branches Professionnelles" ]
+                    , li [] [ text "à la date de dépôt du dossier de validation pour les autres certifications" ]
+                    ]
+          )
+        , case candidacy.readyForJuryEstimatedAt of
             Just date ->
-                [ ( ""
-                  , Form.Text "Date prévisionnelle à laquelle le candidat sera potentiellement prêt pour son passage devant le jury." Nothing
-                  )
-                , ( keys.estimatedDate, Form.Info "Date prévisionnelle" (View.Date.toFullFormat date) )
-                ]
+                ( keys.estimatedDate, Form.Info "Date prévisionnelle" (View.Date.toFullFormat date) )
 
             Nothing ->
-                [ ( ""
-                  , Form.Text "Afin de faciliter la tenue du jury pour le candidat, merci de renseigner la date prévisionnelle à laquelle le candidat sera potentiellement prêt pour son passage devant le jury." Nothing
-                  )
-                  , ("", Form.Text "Pour toute précision, merci de vous rapprocher du certificateur." Nothing)
-                , ( keys.estimatedDate, Form.Date "Date prévisionnelle" )
-                ]
+                ( keys.estimatedDate, Form.Date "Date prévisionnelle" )
+        ]
     , saveLabel = Nothing
     , submitLabel = "Enregistrer"
     , title = ""
