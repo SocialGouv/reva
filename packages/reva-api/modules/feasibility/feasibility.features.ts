6512ab782d762e67c6e47985c4ec84e1e8a39010
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-02-12 17:27:35 +0100

    fix(api): wrong feasibility url in new feasibility email

--- packages/reva-api/modules/feasibility/feasibility.features.ts
+++ packages/reva-api/modules/feasibility/feasibility.features.ts
@@ -218,7 +218,7 @@ export const createFeasibility = async ({
     if (emails.length) {
       sendNewFeasibilitySubmittedEmail({
         emails,
-        feasibilityUrl: `${baseUrl}/admin2/candidacies/feasibilities/${feasibility.id}`,
+        feasibilityUrl: `${baseUrl}/admin2/candidacies/${candidacy.id}/feasibility`,
       });
     }
   }
