5f2666869651cb3adcc08e304739902df7231fc2
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-02-12 15:16:08 +0100

    feat(admin-react): extracted UpdateOrReplaceCertificationForm component

---
+++ packages/reva-admin-react/src/app/(admin)/certifications/[certificationId]/_components/UpdateOrReplaceCertificationForm.tsx
@@ -0,0 +1,224 @@
+"use client";
+import { Certification } from "@/graphql/generated/graphql";
+import { Button } from "@codegouvfr/react-dsfr/Button";
+import { Select } from "@codegouvfr/react-dsfr/Select";
+import { Input } from "@codegouvfr/react-dsfr/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { useController, useForm } from "react-hook-form";
+import { z } from "zod";
+import { useEffect } from "react";
+import { errorToast } from "@/components/toast/toast";
+
+const schema = z.object({
+  codeRncp: z.string(),
+  label: z.string(),
+  expiresAt: z.date(),
+  availableAt: z.date(),
+  typeDiplomeId: z.string(),
+  degreeLevel: z.number(),
+  conventionCollectiveId: z.string(),
+  domaineId: z.string(),
+  certificationAuthorityTag: z.string(),
+});
+
+export type UpdateOrReplaceCertificationFormData = z.infer<typeof schema>;
+
+const certificationToFormData = (
+  c: Partial<Certification>,
+): UpdateOrReplaceCertificationFormData | undefined =>
+  c
+    ? {
+        codeRncp: c.codeRncp || "",
+        label: c.label || "",
+        expiresAt: format(
+          new Date(c.expiresAt || ""),
+          "yyyy-MM-dd",
+        ) as unknown as Date,
+        availableAt: format(
+          new Date(c.availableAt || ""),
+          "yyyy-MM-dd",
+        ) as unknown as Date,
+        typeDiplomeId: c.typeDiplome?.id || "",
+        certificationAuthorityTag: c.certificationAuthorityTag || "",
+        degreeLevel: c.degree?.level || -1,
+        conventionCollectiveId: c.conventionsCollectives?.[0]?.id || "",
+        domaineId: c.domaines?.[0]?.id || "",
+      }
+    : undefined;
+
+const UpdateOrReplaceCertificationForm = ({
+  certification,
+  typeDiplomes,
+  domaines,
+  conventionCollectives,
+  degrees,
+  onSubmit,
+}: {
+  certification: Certification;
+  typeDiplomes: { id: string; label: string }[];
+  domaines: { id: string; label: string }[];
+  conventionCollectives: { id: string; label: string }[];
+  degrees: { id: string; level: number; longLabel: string }[];
+  onSubmit: (data: UpdateOrReplaceCertificationFormData) => void;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<UpdateOrReplaceCertificationFormData>({
+    resolver: zodResolver(schema),
+    defaultValues: certificationToFormData(certification),
+  });
+
+  useEffect(() => {
+    certification && reset(certificationToFormData(certification));
+  }, [certification, reset]);
+
+  const handleFormSubmit = handleSubmit(
+    (data) => onSubmit(data),
+    (e) => errorToast(JSON.stringify(e)),
+  );
+
+  const typeDiplomeController = useController({
+    control,
+    name: "typeDiplomeId",
+  });
+
+  const degreeController = useController({
+    control,
+    name: "degreeLevel",
+  });
+
+  const domaineController = useController({
+    control,
+    name: "domaineId",
+  });
+
+  const conventionCollectiveController = useController({
+    control,
+    name: "conventionCollectiveId",
+  });
+
+  return (
+    <form
+      className="grid grid-cols-1  md:grid-cols-2 gap-8"
+      onSubmit={handleFormSubmit}
+      onReset={(e) => {
+        e.preventDefault();
+        reset(certificationToFormData(certification as Certification));
+      }}
+    >
+      <Input
+        label="Libellé"
+        className="md:col-span-2"
+        nativeInputProps={{ ...register("label") }}
+        state={errors.label ? "error" : "default"}
+        stateRelatedMessage={errors.label?.message}
+      />
+      <Input
+        label="Code RNCP"
+        nativeInputProps={{ ...register("codeRncp") }}
+        state={errors.codeRncp ? "error" : "default"}
+        stateRelatedMessage={errors.codeRncp?.message}
+      />
+      <Input
+        label="Tag certificateur"
+        nativeInputProps={{
+          ...register("certificationAuthorityTag"),
+        }}
+        state={errors.certificationAuthorityTag ? "error" : "default"}
+        stateRelatedMessage={errors.certificationAuthorityTag?.message}
+      />
+      <Input
+        className="col-start-1"
+        label="Disponible à partir du"
+        nativeInputProps={{
+          ...register("availableAt", { valueAsDate: true }),
+          type: "date",
+        }}
+        state={errors.availableAt ? "error" : "default"}
+        stateRelatedMessage={errors.availableAt?.message}
+      />
+      <Input
+        label="Expire à partir du"
+        nativeInputProps={{
+          ...register("expiresAt", { valueAsDate: true }),
+          type: "date",
+        }}
+        state={errors.expiresAt ? "error" : "default"}
+        stateRelatedMessage={errors.expiresAt?.message}
+      />
+      <Select
+        label="Niveau de la certification"
+        nativeSelectProps={{
+          onChange: (event) =>
+            degreeController.field.onChange(
+              Number.parseInt(event.target.value),
+            ),
+          value: degreeController.field.value,
+        }}
+      >
+        {degrees?.map((d) => (
+          <option key={d.id} value={d.level}>
+            {d.longLabel}
+          </option>
+        ))}
+      </Select>
+      <Select
+        label="Type de la certification"
+        nativeSelectProps={{
+          onChange: (event) =>
+            typeDiplomeController.field.onChange(event.target.value),
+          value: typeDiplomeController.field.value,
+        }}
+      >
+        {typeDiplomes?.map((t) => (
+          <option key={t.id} value={t.id}>
+            {t.label}
+          </option>
+        ))}
+      </Select>
+      <Select
+        label="Domaine de la certification"
+        nativeSelectProps={{
+          onChange: (event) =>
+            domaineController.field.onChange(event.target.value),
+          value: domaineController.field.value,
+        }}
+      >
+        <option />
+        {domaines?.map((d) => (
+          <option key={d.id} value={d.id}>
+            {d.label}
+          </option>
+        ))}
+      </Select>
+      <Select
+        label="Convention collective de la certification"
+        nativeSelectProps={{
+          onChange: (event) =>
+            conventionCollectiveController.field.onChange(event.target.value),
+          value: conventionCollectiveController.field.value,
+        }}
+      >
+        <option />
+        {conventionCollectives?.map((c) => (
+          <option key={c.id} value={c.id}>
+            {c.label}
+          </option>
+        ))}
+      </Select>
+      <div className="flex flex-col md:flex-row gap-4 items-center md:col-start-2 md:ml-auto md:mt-8">
+        <Button priority="secondary" type="reset">
+          Annuler
+        </Button>
+        <Button disabled={isSubmitting}>Valider</Button>
+      </div>
+    </form>
+  );
+};
+
+export default UpdateOrReplaceCertificationForm;
--- packages/reva-admin-react/src/app/(admin)/certifications/[certificationId]/update/page.tsx
+++ packages/reva-admin-react/src/app/(admin)/certifications/[certificationId]/update/page.tsx
@@ -1,59 +1,13 @@
 "use client";
 import { useCertificationQueries } from "@/app/(admin)/certifications/[certificationId]/certificationQueries";
-import { Certification } from "@/graphql/generated/graphql";
-import { Button } from "@codegouvfr/react-dsfr/Button";
-import { Select } from "@codegouvfr/react-dsfr/Select";
-import { Input } from "@codegouvfr/react-dsfr/Input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { format, parse } from "date-fns";
 import { useParams } from "next/navigation";
-import { useController, useForm } from "react-hook-form";
-import { z } from "zod";
 import { PageTitle } from "@/components/page/page-title/PageTitle";
 import Link from "next/link";
-import { useEffect } from "react";
-import {
-  errorToast,
-  graphqlErrorToast,
-  successToast,
-} from "@/components/toast/toast";
-
-const schema = z.object({
-  codeRncp: z.string(),
-  label: z.string(),
-  expiresAt: z.date(),
-  availableAt: z.date(),
-  typeDiplomeId: z.string(),
-  degreeLevel: z.number(),
-  conventionCollectiveId: z.string(),
-  domaineId: z.string(),
-  certificationAuthorityTag: z.string(),
-});
-
-type FormData = z.infer<typeof schema>;
-
-const certificationToFormData = (
-  c: Partial<Certification>,
-): FormData | undefined =>
-  c
-    ? {
-        codeRncp: c.codeRncp || "",
-        label: c.label || "",
-        expiresAt: format(
-          new Date(c.expiresAt || ""),
-          "yyyy-MM-dd",
-        ) as unknown as Date,
-        availableAt: format(
-          new Date(c.availableAt || ""),
-          "yyyy-MM-dd",
-        ) as unknown as Date,
-        typeDiplomeId: c.typeDiplome?.id || "",
-        certificationAuthorityTag: c.certificationAuthorityTag || "",
-        degreeLevel: c.degree?.level || -1,
-        conventionCollectiveId: c.conventionsCollectives?.[0]?.id || "",
-        domaineId: c.domaines?.[0]?.id || "",
-      }
-    : undefined;
+import { graphqlErrorToast, successToast } from "@/components/toast/toast";
+import UpdateOrReplaceCertificationForm, {
+  UpdateOrReplaceCertificationFormData,
+} from "@/app/(admin)/certifications/[certificationId]/_components/UpdateOrReplaceCertificationForm";
+import { Certification } from "@/graphql/generated/graphql";
 
 const UpdateCertificationPage = () => {
   const { certificationId } = useParams<{ certificationId: string }>();
@@ -68,202 +22,60 @@ const UpdateCertificationPage = () => {
     certificationId,
   });
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    control,
-    formState: { errors, isSubmitting },
-  } = useForm<FormData>({
-    resolver: zodResolver(schema),
-    defaultValues: certificationToFormData(certification as Certification),
-  });
+  const handleFormSubmit = async (
+    data: UpdateOrReplaceCertificationFormData,
+  ) => {
+    try {
+      await updateCertification.mutateAsync({
+        label: data.label,
+        level: data.degreeLevel,
+        codeRncp: data.codeRncp,
+        typeDiplomeId: data.typeDiplomeId,
+        certificationAuthorityTag: data.certificationAuthorityTag,
+        domaineIds: data.domaineId ? [data.domaineId] : [],
+        conventionCollectiveIds: data.conventionCollectiveId
+          ? [data.conventionCollectiveId]
+          : [],
+        availableAt: data.availableAt.getTime(),
+        expiresAt: data.expiresAt.getTime(),
+      });
 
-  useEffect(() => {
-    certification &&
-      reset(certificationToFormData(certification as Certification));
-  }, [certification, reset]);
-
-  const handleFormSubmit = handleSubmit(
-    async (data) => {
-      try {
-        await updateCertification.mutateAsync({
-          label: data.label,
-          level: data.degreeLevel,
-          codeRncp: data.codeRncp,
-          typeDiplomeId: data.typeDiplomeId,
-          certificationAuthorityTag: data.certificationAuthorityTag,
-          domaineIds: data.domaineId ? [data.domaineId] : [],
-          conventionCollectiveIds: data.conventionCollectiveId
-            ? [data.conventionCollectiveId]
-            : [],
-          availableAt: data.availableAt.getTime(),
-          expiresAt: data.expiresAt.getTime(),
-        });
-
-        successToast("Certification enregistrée");
-      } catch (e) {
-        graphqlErrorToast(e);
-      }
-    },
-    (e) => errorToast(JSON.stringify(e)),
-  );
-
-  const typeDiplomeController = useController({
-    control,
-    name: "typeDiplomeId",
-  });
-
-  const degreeController = useController({
-    control,
-    name: "degreeLevel",
-  });
-
-  const domaineController = useController({
-    control,
-    name: "domaineId",
-  });
-
-  const conventionCollectiveController = useController({
-    control,
-    name: "conventionCollectiveId",
-  });
+      successToast("Certification enregistrée");
+    } catch (e) {
+      graphqlErrorToast(e);
+    }
+  };
 
   return (
     <div className="flex flex-col w-full">
-      {certification && (
-        <>
-          <Link
-            href={`/certifications/${certificationId}`}
-            className="fr-icon-arrow-left-line fr-link--icon-left text-blue-900 text-lg mr-auto mb-8"
-          >
-            Retour
-          </Link>
-          <PageTitle> Modifier une certification</PageTitle>
-          <p>
-            Sauf mention contraire “(optionnel)” dans le label, tous les champs
-            sont obligatoires.
-          </p>
-          <br />
-          <form
-            className="grid grid-cols-1  md:grid-cols-2 gap-8"
-            onSubmit={handleFormSubmit}
-            onReset={(e) => {
-              e.preventDefault();
-              reset(certificationToFormData(certification as Certification));
-            }}
-          >
-            <Input
-              label="Libellé"
-              className="md:col-span-2"
-              nativeInputProps={{ ...register("label") }}
-              state={errors.label ? "error" : "default"}
-              stateRelatedMessage={errors.label?.message}
-            />
-            <Input
-              label="Code RNCP"
-              nativeInputProps={{ ...register("codeRncp") }}
-              state={errors.codeRncp ? "error" : "default"}
-              stateRelatedMessage={errors.codeRncp?.message}
-            />
-            <Input
-              label="Tag certificateur"
-              nativeInputProps={{
-                ...register("certificationAuthorityTag"),
-              }}
-              state={errors.certificationAuthorityTag ? "error" : "default"}
-              stateRelatedMessage={errors.certificationAuthorityTag?.message}
-            />
-            <Input
-              className="col-start-1"
-              label="Disponible à partir du"
-              nativeInputProps={{
-                ...register("availableAt", { valueAsDate: true }),
-                type: "date",
-              }}
-              state={errors.availableAt ? "error" : "default"}
-              stateRelatedMessage={errors.availableAt?.message}
-            />
-            <Input
-              label="Expire à partir du"
-              nativeInputProps={{
-                ...register("expiresAt", { valueAsDate: true }),
-                type: "date",
-              }}
-              state={errors.expiresAt ? "error" : "default"}
-              stateRelatedMessage={errors.expiresAt?.message}
-            />
-            <Select
-              label="Niveau de la certification"
-              nativeSelectProps={{
-                onChange: (event) =>
-                  degreeController.field.onChange(
-                    Number.parseInt(event.target.value),
-                  ),
-                value: degreeController.field.value,
-              }}
+      {certification &&
+        typeDiplomes &&
+        domaines &&
+        conventionCollectives &&
+        degrees && (
+          <>
+            <Link
+              href={`/certifications/${certificationId}`}
+              className="fr-icon-arrow-left-line fr-link--icon-left text-blue-900 text-lg mr-auto mb-8"
             >
-              {degrees?.map((d) => (
-                <option key={d.id} value={d.level}>
-                  {d.longLabel}
-                </option>
-              ))}
-            </Select>
-            <Select
-              label="Type de la certification"
-              nativeSelectProps={{
-                onChange: (event) =>
-                  typeDiplomeController.field.onChange(event.target.value),
-                value: typeDiplomeController.field.value,
-              }}
-            >
-              {typeDiplomes?.map((t) => (
-                <option key={t.id} value={t.id}>
-                  {t.label}
-                </option>
-              ))}
-            </Select>
-            <Select
-              label="Domaine de la certification"
-              nativeSelectProps={{
-                onChange: (event) =>
-                  domaineController.field.onChange(event.target.value),
-                value: domaineController.field.value,
-              }}
-            >
-              <option />
-              {domaines?.map((d) => (
-                <option key={d.id} value={d.id}>
-                  {d.label}
-                </option>
-              ))}
-            </Select>
-            <Select
-              label="Convention collective de la certification"
-              nativeSelectProps={{
-                onChange: (event) =>
-                  conventionCollectiveController.field.onChange(
-                    event.target.value,
-                  ),
-                value: conventionCollectiveController.field.value,
-              }}
-            >
-              <option />
-              {conventionCollectives?.map((c) => (
-                <option key={c.id} value={c.id}>
-                  {c.label}
-                </option>
-              ))}
-            </Select>
-            <div className="flex flex-col md:flex-row gap-4 items-center md:col-start-2 md:ml-auto md:mt-8">
-              <Button priority="secondary" type="reset">
-                Annuler les modifications
-              </Button>
-              <Button disabled={isSubmitting}>Valider les modifications</Button>
-            </div>
-          </form>
-        </>
-      )}
+              Retour
+            </Link>
+            <PageTitle> Modifier une certification</PageTitle>
+            <p>
+              Sauf mention contraire “(optionnel)” dans le label, tous les
+              champs sont obligatoires.
+            </p>
+            <br />
+            <UpdateOrReplaceCertificationForm
+              certification={certification as Certification}
+              typeDiplomes={typeDiplomes}
+              domaines={domaines}
+              conventionCollectives={conventionCollectives}
+              degrees={degrees}
+              onSubmit={handleFormSubmit}
+            />
+          </>
+        )}
     </div>
   );
 };
