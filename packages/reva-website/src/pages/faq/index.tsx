0cc43cd5e4740acfe47de904504775858b52d6e0
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-02-19 12:21:30 +0100

    feat(website): css & layout on faq page

--- packages/reva-website/src/pages/faq/index.tsx
+++ packages/reva-website/src/pages/faq/index.tsx
@@ -3,9 +3,8 @@ import { STRAPI_GRAPHQL_API_URL } from "@/config/config";
 import { graphql } from "@/graphql/generated";
 import { GetSectionFaqsQuery } from "@/graphql/generated/graphql";
 import Accordion from "@codegouvfr/react-dsfr/Accordion";
-import Button from "@codegouvfr/react-dsfr/Button";
+import { Tile } from "@codegouvfr/react-dsfr/Tile";
 import request from "graphql-request";
-
 const sectionFaqs = graphql(`
   query getSectionFaqs {
     sectionFaqs(sort: "ordre") {
@@ -39,28 +38,34 @@ const sectionFaqs = graphql(`
 const FaqPage = ({ sections }: { sections: GetSectionFaqsQuery }) => (
   <MainLayout>
     <div className="flex flex-col w-full gap-8 fr-container p-32 pt-16">
-      <h1 className="text-5xl font-bold  bg-white mb-4">
+      <h1 className="text-4xl font-bold  bg-white mt-12 mb-6 self-center">
         Questions fréquentes
       </h1>
-      <div className="flex flex-wrap justify-start md:justify-center gap-4 mt-4">
+      <div className="flex flex-wrap justify-start md:justify-center gap-16">
         {sections.sectionFaqs?.data?.map((s) => (
-          <Button key={s.id} linkProps={{ href: `#section-${s.id}` }}>
-            {s.attributes?.titre}
-          </Button>
+          <Tile
+            key={s.id}
+            enlargeLink
+            linkProps={{
+              href: `#section-${s.id}`,
+            }}
+            title={s.attributes?.titre}
+          />
         ))}
       </div>
-      <div className="flex flex-col py-4 mt-4">
+      <div className="flex flex-col py-4 mt-12">
         {sections.sectionFaqs?.data?.map((s) => (
           <div
             key={s.id}
             id={`section-${s.id}`}
-            className="font-bold text-4xl text-black mb-6"
+            className="font-bold text-3xl text-black mb-12"
           >
             {s.attributes?.titre}
             <ul className="list-none p-0">
               {s.attributes?.sous_section_faqs?.data?.map((ss) => (
-                <li className="text-lg mt-4" key={ss.id}>
-                  {ss.attributes?.titre}
+                <li className="text-2xl mt-4" key={ss.id}>
+                  {(s.attributes?.sous_section_faqs?.data?.length || 0) > 1 &&
+                    ss.attributes?.titre}
                   <div className="fr-accordions-group mt-4">
                     {ss.attributes?.article_faqs?.data?.map((a) => (
                       <span key={a.id} id={`article-${s.id}`}>
