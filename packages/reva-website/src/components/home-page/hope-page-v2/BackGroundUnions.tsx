ac1954a22aa8ef4fdb2eabe8ef2b783ad0c0c86a
ThomasDos <dosanjos.thomas@gmail.com>
2024-02-19 12:25:51 +0100

    style(website): update background unions

--- packages/reva-website/src/components/home-page/hope-page-v2/BackGroundUnions.tsx
+++ packages/reva-website/src/components/home-page/hope-page-v2/BackGroundUnions.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const BackGroundUnions = () => (
   <>
-    <div className="absolute -z-10 w-full top-32 lg:-top-[60px] hidden lg:block">
+    <div className="absolute -z-10 w-full top-32 lg:top-[25px] hidden lg:block">
       <Image
         src="/home-page/unions-background/union-background1.svg"
         width={3000}
@@ -11,12 +11,12 @@ const BackGroundUnions = () => (
         alt="rayon rose en fond d'écran"
       />
     </div>
-    <div className="absolute -z-10 w-full top-[1150px] hidden lg:block">
+    <div className="absolute -z-10 w-full top-[1190px] hidden lg:block">
       <Image
         src="/home-page/unions-background/union-background2.svg"
-        width={2158}
-        height={586}
-        style={{ width: "100%", height: "586px" }}
+        width={3000}
+        height={1074}
+        style={{ width: "100%" }}
         alt="rayon rose en fond d'écran"
       />
     </div>
