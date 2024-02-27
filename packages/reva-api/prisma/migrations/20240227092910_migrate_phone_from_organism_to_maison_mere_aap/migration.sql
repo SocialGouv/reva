ea6866e3865f2b94f3c496bc7840075de410cade
Florian Bonniec <f.bonniec@me.com>
2024-02-27 15:01:19 +0100

    fix(api): improve migration of phone from organism to maison_mere_aap

--- packages/reva-api/prisma/migrations/20240227092910_migrate_phone_from_organism_to_maison_mere_aap/migration.sql
+++ packages/reva-api/prisma/migrations/20240227092910_migrate_phone_from_organism_to_maison_mere_aap/migration.sql
@@ -1,14 +1,7 @@
-UPDATE maison_mere_aap mm1 SET phone = organism.contact_administrative_phone
-FROM maison_mere_aap mm2
-JOIN (
-     SELECT *
-     FROM organism
-     WHERE maison_mere_aap_id IN (
-        SELECT organism.maison_mere_aap_id
-        FROM organism
-        GROUP BY organism.maison_mere_aap_id
-     )
-     ORDER BY organism.created_at ASC
-) AS organism
-ON mm2.id = organism.maison_mere_aap_id
-WHERE mm1.id = mm2.id;
+UPDATE maison_mere_aap SET phone = (
+    SELECT contact_administrative_phone
+    FROM organism
+    WHERE organism.maison_mere_aap_id = maison_mere_aap.id
+    ORDER BY organism.created_at ASC
+    LIMIT 1
+)
