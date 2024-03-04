fdd33e9619cec25a25a2fa5353a196cff3383a03
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-03-04 17:22:32 +0100

    feat(admin-react): added admin name to candidacy log page

--- packages/reva-admin-react/src/app/(admin)/candidacies/[candidacyId]/logs/page.tsx
+++ packages/reva-admin-react/src/app/(admin)/candidacies/[candidacyId]/logs/page.tsx
@@ -4,11 +4,12 @@ import { PageTitle } from "@/components/page/page-title/PageTitle";
 import { ADMIN_ELM_URL } from "@/config/config";
 import { graphql } from "@/graphql/generated";
 import {
-  CandidacyLog,
+  CandidacyLogUser,
   CandidacyLogUserProfile,
 } from "@/graphql/generated/graphql";
 import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
+import { capitalize, toLower, toUpper, truncate } from "lodash";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
@@ -28,6 +29,10 @@ const getCandidacyLogsQuery = graphql(`
         createdAt
         message
         userProfile
+        user {
+          firstname
+          lastname
+        }
       }
     }
   }
@@ -50,12 +55,18 @@ const CandidacyLogsPage = () => {
   const candidate = candidacy?.candidate;
   const candidacyLogs = candidacy?.candidacyLogs;
 
-  const getUserProfileText = (
-    candidacyLogUserProfile: CandidacyLogUserProfile,
-  ) => {
-    switch (candidacyLogUserProfile) {
+  const getUserProfileText = ({
+    userProfile,
+    user,
+  }: {
+    userProfile: CandidacyLogUserProfile;
+    user: CandidacyLogUser;
+  }) => {
+    switch (userProfile) {
       case "ADMIN":
-        return "Administrateur";
+        return `Administrateur (${toUpper(
+          truncate(user.firstname, { length: 2, omission: "." }),
+        )} ${capitalize(toLower(user.lastname))})`;
       case "AAP":
         return "AAP";
       case "CERTIFICATEUR":
@@ -88,7 +99,13 @@ const CandidacyLogsPage = () => {
                 {format(l.createdAt, "dd/MM/yyyy - HH:mm")}
               </span>
               <span>
-                <strong>{getUserProfileText(l.userProfile)} : </strong>
+                <strong>
+                  {getUserProfileText({
+                    userProfile: l.userProfile,
+                    user: l.user,
+                  })}{" "}
+                  :{" "}
+                </strong>
                 {l.message}
               </span>
             </li>
