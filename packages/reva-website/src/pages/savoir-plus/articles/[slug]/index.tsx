6d7922c02853c85fc167f7f978bf06d26bbd1450
ThomasDos <dosanjos.thomas@gmail.com>
2024-03-05 14:47:37 +0100

    feat(website): add metadata to savoir plus' articles

--- packages/reva-website/src/pages/savoir-plus/articles/[slug]/index.tsx
+++ packages/reva-website/src/pages/savoir-plus/articles/[slug]/index.tsx
@@ -4,6 +4,7 @@ import { graphql } from "@/graphql/generated";
 import { GetArticleDAideQuery } from "@/graphql/generated/graphql";
 import Button from "@codegouvfr/react-dsfr/Button";
 import request from "graphql-request";
+import Head from "next/head";
 
 const articleQuery = graphql(`
   query getArticleDAide($filters: ArticleDAideFiltersInput!) {
@@ -21,6 +22,7 @@ const articleQuery = graphql(`
             }
           }
           contenu
+          description
         }
       }
     }
@@ -33,37 +35,50 @@ const ArticleAidePage = ({ articles }: { articles: GetArticleDAideQuery }) => {
   if (!article) return null;
 
   return (
-    <MainLayout>
-      {
-        <div className="flex flex-col sm:flex-row w-full gap-8 sm:gap-16 fr-container p-32 pt-16">
-          <Button
-            className="h-fit"
-            priority="tertiary"
-            iconPosition="left"
-            iconId="fr-icon-arrow-left-line"
-            linkProps={{ href: "/savoir-plus" }}
-            size="large"
-          >
-            Retour
-          </Button>
-          <div className="flex flex-col max-w-4xl">
-            <h1 className="font-bold mb-16" style={{ fontSize: "40px" }}>
-              {article.attributes?.titre}
-            </h1>
-            <div
-              className="ck-content"
-              dangerouslySetInnerHTML={{
-                __html:
-                  article.attributes?.contenu?.replaceAll(
-                    "<a",
-                    "<a target='_'"
-                  ) || "",
-              }}
-            />
+    <>
+      <Head>
+        <title>France VAE | {article.attributes?.titre ?? ""}</title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content={article.attributes?.description ?? ""}
+        />
+        <meta name="keywords" content="Gouvernement, France, VAE, France VAE" />
+        <meta name="author" content="France VAE" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <MainLayout>
+        {
+          <div className="flex flex-col sm:flex-row w-full gap-8 sm:gap-16 fr-container p-32 pt-16">
+            <Button
+              className="h-fit"
+              priority="tertiary"
+              iconPosition="left"
+              iconId="fr-icon-arrow-left-line"
+              linkProps={{ href: "/savoir-plus" }}
+              size="large"
+            >
+              Retour
+            </Button>
+            <div className="flex flex-col max-w-4xl">
+              <h1 className="font-bold mb-16" style={{ fontSize: "40px" }}>
+                {article.attributes?.titre}
+              </h1>
+              <div
+                className="ck-content"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    article.attributes?.contenu?.replaceAll(
+                      "<a",
+                      "<a target='_'"
+                    ) || "",
+                }}
+              />
+            </div>
           </div>
-        </div>
-      }
-    </MainLayout>
+        }
+      </MainLayout>
+    </>
   );
 };
 
