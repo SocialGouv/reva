d48914013e1b3739e76b41da0b2d48b27a510cf0
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-03-15 10:15:33 +0100

    feat(admin-react): updated ADMIN_ELM_URL default value in order to point to the api proxy

--- packages/reva-admin-react/src/config/config.ts
+++ packages/reva-admin-react/src/config/config.ts
@@ -7,7 +7,7 @@ export const REST_API_URL =
   "http://localhost:8080/api";
 
 export const ADMIN_ELM_URL =
-  process.env.NEXT_PUBLIC_ADMIN_ELM_URL || "http://localhost:3000/admin";
+  process.env.NEXT_PUBLIC_ADMIN_ELM_URL || "http://localhost:8080/admin";
 
 export const KEYCLOAK_CLIENT_ID = process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID;
 export const KEYCLOAK_REALM = process.env.NEXT_PUBLIC_KEYCLOAK_REALM;
