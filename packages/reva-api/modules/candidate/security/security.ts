90549d72ab487b689ec08373e2221f3f59b001f8
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-03-19 16:43:59 +0100

    fix(api): updated candidate_updateCandidate security

--- packages/reva-api/modules/candidate/security/security.ts
+++ packages/reva-api/modules/candidate/security/security.ts
@@ -1,6 +1,6 @@
 import {
   defaultSecurity,
-  isAdminOrManager,
+  isAdminOrCandidacyCompanion,
   isAnyone,
 } from "../../shared/security/presets";
 
@@ -10,5 +10,5 @@ export const resolversSecurityMap = {
   "Mutation.candidate_askForRegistration": isAnyone,
   "Mutation.candidate_login": isAnyone,
   "Mutation.candidate_askForLogin": isAnyone,
-  "Mutation.candidate_updateCandidate": isAdminOrManager,
+  "Mutation.candidate_updateCandidate": isAdminOrCandidacyCompanion,
 };
