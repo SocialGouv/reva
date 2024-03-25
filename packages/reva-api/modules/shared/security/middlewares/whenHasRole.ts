426dcd11cef252760a78c63a640d96336d22960b
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-03-25 16:54:55 +0100

    fix(api): patched whenHasRole in order to go forth the middleware chain when user do not have the given role

--- packages/reva-api/modules/shared/security/middlewares/whenHasRole.ts
+++ packages/reva-api/modules/shared/security/middlewares/whenHasRole.ts
@@ -5,20 +5,22 @@ export const whenHasRole =
   (
     role: KeyCloakUserRole,
     ifHasRoleResolver: (
-      next: IFieldResolver<unknown>
-    ) => IFieldResolver<unknown>
+      next: IFieldResolver<unknown>,
+    ) => IFieldResolver<unknown>,
   ) =>
   (next: IFieldResolver<unknown>) =>
   async (
     root: any,
     args: Record<string, any>,
     context: MercuriusContext,
-    info: any
+    info: any,
   ) => {
     const log = debug("gql:security:whenHasRole");
     log("user:", context.auth.userInfo.sub);
     if (context.auth.hasRole(role)) {
       log("user have required role");
       return ifHasRoleResolver(next)(root, args, context, info);
+    } else {
+      return next(root, args, context, info);
     }
   };
