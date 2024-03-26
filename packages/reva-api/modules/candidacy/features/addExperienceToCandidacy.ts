5eab7c64b2e01e87fd0346f8155279d095ff0374
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-03-26 11:50:42 +0100

    fix(api): typo in error message

--- packages/reva-api/modules/candidacy/features/addExperienceToCandidacy.ts
+++ packages/reva-api/modules/candidacy/features/addExperienceToCandidacy.ts
@@ -19,30 +19,30 @@ export const addExperienceToCandidacy =
   (deps: AddExperienceToCandidacyDeps) =>
   async (params: { candidacyId: string; experience: ExperienceInput }) => {
     const checkIfCandidacyExists = EitherAsync.fromPromise(() =>
-      deps.getCandidacyFromId(params.candidacyId)
+      deps.getCandidacyFromId(params.candidacyId),
     ).mapLeft(
       () =>
         new FunctionalError(
           FunctionalCodeError.CANDIDACY_DOES_NOT_EXIST,
-          `Aucune candidature n'a été trouvée`
-        )
+          `Aucune candidature n'a été trouvée`,
+        ),
     );
 
     const createExperience = EitherAsync.fromPromise(() =>
-      deps.createExperience(params)
+      deps.createExperience(params),
     ).mapLeft(
       () =>
         new FunctionalError(
           FunctionalCodeError.EXPERIENCE_NOT_CREATED,
-          `Erreur lors de la creation de l'expérience`
-        )
+          `Erreur lors de la creation de l'expérience`,
+        ),
     );
 
     if (
       !(await canCandidateUpdateCandidacy({ candidacyId: params.candidacyId }))
     ) {
       throw new Error(
-        "Impossible de mettre à jour la candidature une fois le premier entretien effetué"
+        "Impossible de mettre à jour la candidature une fois le premier entretien effectué",
       );
     }
 
