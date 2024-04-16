35f20ffe934738e0e7f9aaf47674d107b49da305
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-04-16 14:46:33 +0200

    fix(admin-react): "Paramètres du compte" menu should not be visible to admins

--- packages/reva-admin-react/src/components/header/Header.tsx
+++ packages/reva-admin-react/src/components/header/Header.tsx
@@ -37,6 +37,7 @@ export const Header = () => {
           : []),
 
         ...(isFeatureActive("AGENCIES_SETTINGS") &&
+        !isAdmin &&
         (isOrganism || isGestionnaireMaisonMereAAP)
           ? [
               {
