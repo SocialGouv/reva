15ca1a7e3044980d9683293837d554a5c815efed
Cédric Soulas <soulascedric@gmail.com>
2024-04-25 11:57:25 +0200

    feat(api): add explanation to the message sent to previous email after email update

--- packages/reva-api/modules/candidacy/mails/sendPreviousEmailCandidateEmail.ts
+++ packages/reva-api/modules/candidacy/mails/sendPreviousEmailCandidateEmail.ts
@@ -10,7 +10,7 @@ export const sendPreviousEmailCandidateEmail = ({
     templateMail({
       content: `
       <p>Bonjour,</p>
-      <p>Vous avez demandé à changer votre e-mail sur France VAE, sachez que la modification a été effectuée.</p>
+      <p>Une demande de changement de votre email sur France VAE a été envoyée, si vous êtes à l'origine de cette demande veuillez confirmer la nouvelle adresse email en consultant l'email envoyé sur cette même nouvelle adresse</p>
       <p>Si vous n'êtes pas à l'origine de cette demande, contactez immédiatement le support par email à <a href='mailto:support@vae.gouv.fr'>support@vae.gouv.fr</a>.</p>
       <p>L’équipe France VAE.</p>
         `,
