ff765bee1623d96727ce8e208f93b11add6d27c1
Florian Bonniec <f.bonniec@me.com>
2024-04-29 11:47:47 +0200

    feat(website): improve creation-suspendue page for aap

--- packages/reva-website/src/pages/espace-professionnel/creation-suspendue/index.tsx
+++ packages/reva-website/src/pages/espace-professionnel/creation-suspendue/index.tsx
@@ -1,12 +1,12 @@
 import { MainLayout } from "@/components/layout/main-layout/MainLayout";
 import { SectionParagraph } from "@/components/section-content/SectionContent";
-import { Button } from "@codegouvfr/react-dsfr/Button";
+import Notice from "@codegouvfr/react-dsfr/Notice";
 import Head from "next/head";
 import Image from "next/image";
 
 const SuspendedCreationPage = () => {
   return (
-    <MainLayout className="py-20 gap-32 lg:gap-64 lg:pb-80">
+    <MainLayout className="py-20 gap-32 lg:gap-24 lg:pb-80">
       <Head>
         <title>
           France VAE | L’outil qui facilite le suivi des candidats à la VAE
@@ -16,28 +16,20 @@ const SuspendedCreationPage = () => {
           content="L’espace professionnel centralise le suivi de vos candidats depuis leur inscription jusqu’à leur passage devant le jury."
         />
       </Head>
+
       <section className="fr-container flex flex-col gap-10 lg:flex-row lg:gap-10">
         <div className="flex flex-col basis-3/5">
           <header>
-            <h1>Service indisponible</h1>
+            <h1>Service momentanément indisponible</h1>
           </header>
           <SectionParagraph className="lg:!text-[20px] lg:!leading-[28px]">
             Les inscriptions reprendront courant juin 2024.
           </SectionParagraph>
           <SectionParagraph className="lg:!text-[14px] lg:!leading-[28px]">
             Nous prenons ce temps pour étudier la conformité des dossiers des
-            professionnels déjà inscrits. Nous vous invitons à revenir plus tard
-            pour rejoindre la communauté France VAE.
+            professionnels déjà référencés. Nous vous invitons à revenir plus
+            tard pour rejoindre la communauté France VAE.
           </SectionParagraph>
-
-          <Button
-            priority="secondary"
-            className="justify-center !w-full lg:!w-fit mt-4"
-            linkProps={{ href: "/nous-contacter" }}
-            size="large"
-          >
-            Nous contacter
-          </Button>
         </div>
 
         <div className="relative min-h-[282px] mt-10 lg:mt-0 basis-2/5">
@@ -49,6 +41,46 @@ const SuspendedCreationPage = () => {
               objectFit: "contain",
             }}
           />
+        </div>
+      </section>
+
+      <section className="fr-container">
+        <div className="fr-callout flex flex-col items-start">
+          <h4>Préparez votre inscription : </h4>
+          <p>
+            En attendant la réouverture des inscriptions, collectez toutes les
+            pièces justificatives dont vous aurez besoin :
+          </p>
+
+          <br />
+
+          <ul>
+            <li>
+              Une attestation de vigilance ou une attestation fiscale de
+              l'URSSAF
+            </li>
+            <li>
+              Une copie du justificatif d'identité du dirigeant "certifiée
+              conforme à l'original' signée par lui-même.
+            </li>
+          </ul>
+
+          <br />
+
+          <p>
+            Si l'administrateur de la plateforme n'est pas le dirigeant, il vous
+            faudra également fournir :
+          </p>
+
+          <br />
+
+          <ul>
+            <li>Une lettre de délégation signée par le dirigeant</li>
+            <li>
+              Une copie du justificatif d'identité de la personne ayant reçu
+              délégation.
+            </li>
+          </ul>
         </div>
       </section>
     </MainLayout>
