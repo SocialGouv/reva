d3927270ae99ec546def3e0d14460cc78faacc78
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-05-03 14:19:35 +0200

    feat(admin-react): FormButtons component now has a className prop and the  back button is optionnal

--- packages/reva-admin-react/src/components/form/form-footer/FormButtons.tsx
+++ packages/reva-admin-react/src/components/form/form-footer/FormButtons.tsx
@@ -3,19 +3,25 @@ import { Button } from "@codegouvfr/react-dsfr/Button";
 export const FormButtons = ({
   backUrl,
   formState: { isDirty, isSubmitting },
+  className,
 }: {
-  backUrl: string;
+  backUrl?: string;
   formState: {
     isDirty: boolean;
     isSubmitting: boolean;
   };
+  className?: string;
 }) => {
   return (
-    <div className="flex gap-4 items-center justify-between mt-10">
-      <Button priority="secondary" linkProps={{ href: backUrl }}>
-        Retour
-      </Button>
-      <div className="flex gap-x-2">
+    <div
+      className={`flex gap-4 items-center justify-between mt-10 ${className || ""}`}
+    >
+      {backUrl && (
+        <Button priority="secondary" linkProps={{ href: backUrl }}>
+          Retour
+        </Button>
+      )}
+      <div className="flex gap-x-2 ml-auto">
         <Button type="reset" priority="tertiary no outline" disabled={!isDirty}>
           Réinitialiser
         </Button>
