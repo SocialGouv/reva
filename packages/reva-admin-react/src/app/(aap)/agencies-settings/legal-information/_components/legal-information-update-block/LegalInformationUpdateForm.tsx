d38f07145854f4a0801bb7e609ccb52a77348e6c
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-05-07 11:54:54 +0200

    feat(admin-react): better form validation for maison mere aap legal information update form

--- packages/reva-admin-react/src/app/(aap)/agencies-settings/legal-information/_components/legal-information-update-block/LegalInformationUpdateForm.tsx
+++ packages/reva-admin-react/src/app/(aap)/agencies-settings/legal-information/_components/legal-information-update-block/LegalInformationUpdateForm.tsx
@@ -11,29 +11,52 @@ import { REST_API_URL } from "@/config/config";
 import { useKeycloakContext } from "@/components/auth/keycloakContext";
 import { errorToast, successToast } from "@/components/toast/toast";
 
-const schema = z.object({
-  managerFirstname: z.string().min(1, "Ce champ est obligatoire."),
-  managerLastname: z.string().min(1, "Ce champ est obligatoire."),
-  attestationURSSAF: z.object({
-    0: z.undefined().or(z.instanceof(File)),
-  }),
-  justificatifIdentiteGestionnaire: z
-    .object({
-      0: z.undefined().or(z.instanceof(File)),
-    })
-    .optional(),
-  delegataire: z.boolean(),
-  lettreDeDelegation: z
-    .object({
-      0: z.undefined().or(z.instanceof(File)),
-    })
-    .optional(),
-  justificatifIdentiteDelegataire: z
-    .object({
-      0: z.undefined().or(z.instanceof(File)),
-    })
-    .optional(),
-});
+const schema = z
+  .object({
+    managerFirstname: z.string().min(1, "Ce champ est obligatoire."),
+    managerLastname: z.string().min(1, "Ce champ est obligatoire."),
+    attestationURSSAF: z.object({
+      0: z.instanceof(File, { message: "Ce champ est obligatoire" }),
+    }),
+    justificatifIdentiteGestionnaire: z.object({
+      0: z.instanceof(File, { message: "Ce champ est obligatoire" }),
+    }),
+
+    delegataire: z.boolean(),
+    lettreDeDelegation: z
+      .object({
+        0: z.undefined().or(z.instanceof(File)),
+      })
+      .optional(),
+    justificatifIdentiteDelegataire: z
+      .object({
+        0: z.undefined().or(z.instanceof(File)),
+      })
+      .optional(),
+  })
+  .superRefine(
+    (
+      { lettreDeDelegation, justificatifIdentiteDelegataire, delegataire },
+      { addIssue },
+    ) => {
+      if (delegataire) {
+        if (!lettreDeDelegation?.[0]) {
+          addIssue({
+            path: ["lettreDeDelegation[0]"],
+            message: "Ce champ est obligatoire",
+            code: z.ZodIssueCode.custom,
+          });
+        }
+        if (!justificatifIdentiteDelegataire?.[0]) {
+          addIssue({
+            path: ["justificatifIdentiteDelegataire[0]"],
+            message: "Ce champ est obligatoire",
+            code: z.ZodIssueCode.custom,
+          });
+        }
+      }
+    },
+  );
 
 type FormData = z.infer<typeof schema>;
 
@@ -54,47 +77,50 @@ export const LegalInformationUpdateForm = ({
   const { delegataire } = useWatch({ control });
   const { accessToken } = useKeycloakContext();
 
-  const handleFormSubmit = handleSubmit(async (data) => {
-    const formData = new FormData();
-    formData.append("managerFirstname", data.managerFirstname);
-    formData.append("managerLastname", data.managerLastname);
-    formData.append("delegataire", data.delegataire.toString());
+  const handleFormSubmit = handleSubmit(
+    async (data) => {
+      const formData = new FormData();
+      formData.append("managerFirstname", data.managerFirstname);
+      formData.append("managerLastname", data.managerLastname);
+      formData.append("delegataire", data.delegataire.toString());
 
-    if (data.attestationURSSAF?.[0]) {
-      formData.append("attestationURSSAF", data.attestationURSSAF?.[0]);
-    }
-    if (data.justificatifIdentiteGestionnaire?.[0]) {
-      formData.append(
-        "justificatifIdentiteGestionnaire",
-        data.justificatifIdentiteGestionnaire?.[0],
-      );
-    }
-    if (data.lettreDeDelegation?.[0]) {
-      formData.append("lettreDeDelegation", data.lettreDeDelegation?.[0]);
-    }
-    if (data.justificatifIdentiteDelegataire?.[0]) {
-      formData.append(
-        "justificatifIdentiteDelegataire",
-        data.justificatifIdentiteDelegataire?.[0],
-      );
-    }
+      if (data.attestationURSSAF?.[0]) {
+        formData.append("attestationURSSAF", data.attestationURSSAF?.[0]);
+      }
+      if (data.justificatifIdentiteGestionnaire?.[0]) {
+        formData.append(
+          "justificatifIdentiteGestionnaire",
+          data.justificatifIdentiteGestionnaire?.[0],
+        );
+      }
+      if (data.lettreDeDelegation?.[0]) {
+        formData.append("lettreDeDelegation", data.lettreDeDelegation?.[0]);
+      }
+      if (data.justificatifIdentiteDelegataire?.[0]) {
+        formData.append(
+          "justificatifIdentiteDelegataire",
+          data.justificatifIdentiteDelegataire?.[0],
+        );
+      }
 
-    const result = await fetch(
-      `${REST_API_URL}/maisonMereAAP/${maisonMereAAPId}/legal-information`,
-      {
-        method: "post",
-        headers: {
-          authorization: `Bearer ${accessToken}`,
+      const result = await fetch(
+        `${REST_API_URL}/maisonMereAAP/${maisonMereAAPId}/legal-information`,
+        {
+          method: "post",
+          headers: {
+            authorization: `Bearer ${accessToken}`,
+          },
+          body: formData,
         },
-        body: formData,
-      },
-    );
-    if (result.ok) {
-      successToast("Modifications enregistrées");
-    } else {
-      errorToast(await result.text());
-    }
-  });
+      );
+      if (result.ok) {
+        successToast("Modifications enregistrées");
+      } else {
+        errorToast(await result.text());
+      }
+    },
+    (e) => console.log({ e }),
+  );
 
   return (
     <div className="flex flex-col">
@@ -108,12 +134,14 @@ export const LegalInformationUpdateForm = ({
           label="Prénom"
           nativeInputProps={{ ...register("managerFirstname") }}
           state={errors.managerFirstname ? "error" : "default"}
-          stateRelatedMessage={errors.managerLastname?.message?.toString()}
+          stateRelatedMessage={errors.managerFirstname?.message}
         />
         <Input
           className="my-0"
           label="Nom"
           nativeInputProps={{ ...register("managerLastname") }}
+          state={errors.managerLastname ? "error" : "default"}
+          stateRelatedMessage={errors.managerLastname?.message}
         />
 
         <FancyUpload
@@ -122,6 +150,8 @@ export const LegalInformationUpdateForm = ({
           description="L’attestation URSSAF doit afficher le code de vérification -
          Exemples : attestation de vigilance, attestation fiscale."
           nativeInputProps={{ type: "file", ...register("attestationURSSAF") }}
+          state={errors.attestationURSSAF ? "error" : "default"}
+          stateRelatedMessage={errors.attestationURSSAF?.[0]?.message}
         />
         <FancyUpload
           className="col-span-2"
@@ -144,6 +174,10 @@ export const LegalInformationUpdateForm = ({
             type: "file",
             ...register("justificatifIdentiteGestionnaire"),
           }}
+          state={errors.justificatifIdentiteGestionnaire ? "error" : "default"}
+          stateRelatedMessage={
+            errors.justificatifIdentiteGestionnaire?.[0]?.message
+          }
         />
 
         <Checkbox
@@ -167,6 +201,8 @@ export const LegalInformationUpdateForm = ({
                 type: "file",
                 ...register("lettreDeDelegation"),
               }}
+              state={errors.lettreDeDelegation ? "error" : "default"}
+              stateRelatedMessage={errors.lettreDeDelegation?.[0]?.message}
             />
 
             <FancyUpload
@@ -188,6 +224,12 @@ export const LegalInformationUpdateForm = ({
                 type: "file",
                 ...register("justificatifIdentiteDelegataire"),
               }}
+              state={
+                errors.justificatifIdentiteDelegataire ? "error" : "default"
+              }
+              stateRelatedMessage={
+                errors.justificatifIdentiteDelegataire?.[0]?.message
+              }
             />
           </>
         )}
@@ -205,14 +247,18 @@ const FancyUpload = ({
   description,
   className,
   nativeInputProps,
+  state,
+  stateRelatedMessage,
 }: {
   title: string;
-  description: string | ReactNode;
+  description: ReactNode;
   className?: string;
   nativeInputProps?: DetailedHTMLProps<
     InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
   >;
+  state?: "error" | "success" | "default";
+  stateRelatedMessage?: ReactNode;
 }) => (
   <Upload
     className={`border bg-dsfr-light-neutral-grey-1000 p-8 ${className || ""}`}
@@ -231,5 +277,7 @@ const FancyUpload = ({
     }
     hint="Format supporté : PDF uniquement avec un poids maximum de 2Mo"
     nativeInputProps={nativeInputProps}
+    state={state}
+    stateRelatedMessage={stateRelatedMessage}
   />
 );
