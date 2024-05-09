1d2830eeeca1a00d0f6bcc5107d5058258691826
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-05-09 10:21:08 +0200

    feat(api): user can now re-submit a maison mere aap legal information documents

--- packages/reva-api/modules/organism/features/submitMaisonMereAAPLegalInformationDocuments.ts
+++ packages/reva-api/modules/organism/features/submitMaisonMereAAPLegalInformationDocuments.ts
@@ -1,8 +1,33 @@
 import { FileService, UploadedFile } from "../../shared/file";
 import { prismaClient } from "../../../prisma/client";
 import { v4 as uuidV4 } from "uuid";
+import { File } from "@prisma/client";
 
-export const submitMaisonMereAAPLegalInformationDocuments = async ({
+export const submitMaisonMereAAPLegalInformationDocuments = async (params: {
+  maisonMereAAPId: string;
+  managerFirstname: string;
+  managerLastname: string;
+  delegataire: boolean;
+  attestationURSSAF: UploadedFile;
+  justificatifIdentiteDirigeant: UploadedFile;
+  lettreDeDelegation?: UploadedFile;
+  justificatifIdentiteDelegataire?: UploadedFile;
+}) => {
+  const { maisonMereAAPId } = params;
+  const oldDocuments =
+    await prismaClient.maisonMereAAPLegalInformationDocuments.findUnique({
+      where: { maisonMereAAPId },
+      select: { id: true },
+    });
+
+  if (oldDocuments) {
+    await deleteOldMaisonMereAAPLegalInformationDocuments({ maisonMereAAPId });
+  }
+
+  return createMaisonMereAAPLegalInformationDocuments(params);
+};
+
+const createMaisonMereAAPLegalInformationDocuments = async ({
   maisonMereAAPId,
   managerFirstname,
   managerLastname,
@@ -108,6 +133,42 @@ export const submitMaisonMereAAPLegalInformationDocuments = async ({
   });
 };
 
+const deleteOldMaisonMereAAPLegalInformationDocuments = async ({
+  maisonMereAAPId,
+}: {
+  maisonMereAAPId: string;
+}) => {
+  const oldDocuments =
+    await prismaClient.maisonMereAAPLegalInformationDocuments.findUnique({
+      where: { maisonMereAAPId },
+      include: {
+        justificatifIdentiteDelegataireFile: true,
+        attestationURSSAFFile: true,
+        justificatifIdentiteDirigeantFile: true,
+        lettreDeDelegationFile: true,
+      },
+    });
+
+  const files = [
+    oldDocuments?.attestationURSSAFFile,
+    oldDocuments?.justificatifIdentiteDirigeantFile,
+    oldDocuments?.lettreDeDelegationFile,
+    oldDocuments?.justificatifIdentiteDelegataireFile,
+  ].filter((d) => !!d) as File[];
+
+  for (const file of files) {
+    await deleteFile({ filePath: file.path });
+  }
+
+  await prismaClient.maisonMereAAPLegalInformationDocuments.delete({
+    where: { maisonMereAAPId },
+  });
+
+  await prismaClient.file.deleteMany({
+    where: { id: { in: files.map((f) => f.id) } },
+  });
+};
+
 const getFilePath = ({
   maisonMereAAPId,
   fileId,
@@ -130,3 +191,6 @@ const uploadFile = ({
     },
     file._buf,
   );
+
+const deleteFile = ({ filePath }: { filePath: string }) =>
+  FileService.getInstance().deleteFile({ fileKeyPath: filePath });
