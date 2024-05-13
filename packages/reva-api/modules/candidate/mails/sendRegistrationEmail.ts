6a4cf8a172d6058c55befc8bb36d4af69962235d
Pierre A <pierre@avizou.eu>
2024-05-13 17:40:24 +0200

    feat(api): improve registration email

--- packages/reva-api/modules/candidate/mails/sendRegistrationEmail.ts
+++ packages/reva-api/modules/candidate/mails/sendRegistrationEmail.ts
@@ -1,25 +1,38 @@
 import mjml2html from "mjml";
 
-import {
-  sendEmailWithLinkPurifyJS,
-  templateEmailPurifyJS,
-} from "../../shared/email";
+import { sendEmailWithLinkPurifyJS, templateMail } from "../../shared/email";
 
 export const sendRegistrationEmail = async (email: string, token: string) => {
   const htmlContent = (url: string) =>
     mjml2html(
-      templateEmailPurifyJS({
-        headline:
-          "<strong>Commencez</strong> dès maintenant votre parcours France VAE en cliquant sur le bouton ci-dessous.",
-        labelCTA: "Démarrer mon parcours",
+      templateMail({
+        content: `
+        <p>Bonjour,</p>
+        <p>Vous venez de créer votre compte France VAE afin de commencer un parcours de <b>Validation des Acquis de l’Expérience.</b></p>
+        
+        <h3 style="margin-bottom:0">Le parcours VAE</h3>
+        <p style="margin-top:5px">Vous allez pouvoir commencer votre parcours en cliquant sur le bouton ci-dessous. Il vous sera demandé de remplir des informations concernant vos expériences professionnelles et personnelles qui vous paraissent être en relation avec le diplôme que vous visez. </p>
+        `,
+        labelCTA: "Je commence mon parcours",
         url,
+        bottomLine: `
+        <h3 style="margin-bottom:0">Et si je n’ai pas le temps de tout faire maintenant ?</h3>
+        <p style="margin-top:5px">Pas de panique, rien ne vous oblige à compléter tout votre profil dès maintenant, vous pourrez toujours revenir plus tard <a href="${url}">en vous connectant à nouveau</a> pour finaliser votre dossier.</p>
+        
+        <h3 style="margin-bottom:0">Besoin d’aide ?</h3>
+        <p style="margin-top:5px">Nos équipes ont élaboré <a href="https://scribehow.com/shared/Parcours_candidat__vp9k4YzATvmheao9kAoKjw">un guide en ligne pour vous aider dans cette démarche</a>.</p>
+
+        <p>L’équipe France VAE</p>
+        `,
       }),
     );
 
   return sendEmailWithLinkPurifyJS({
     email,
+    subject:
+      "Votre compte France VAE est créé, vous pouvez commencer votre parcours",
     token,
-    action: "registration",
+    action: "login",
     htmlContent,
   });
 };
