3f50979a3e5683140cfc5a87a42a8d782bf58116
ThomasDos <dosanjos.thomas@gmail.com>
2024-05-22 15:34:51 +0200

    feat(admin-react): serialize phone manage 3 digits phone codes

--- packages/reva-admin-react/src/utils/serializeStringToPhoneNumberStructure.util.ts
+++ packages/reva-admin-react/src/utils/serializeStringToPhoneNumberStructure.util.ts
@@ -1,17 +1,41 @@
+const phoneCodesThreeDigits = [
+  "590",
+  "596",
+  "594",
+  "262",
+  "508",
+  "681",
+  "689",
+  "687",
+];
+
 export function serializeStringToPhoneNumberStructure(phoneNumber: string) {
-  // Check if phoneNumber is a valid string
   if (typeof phoneNumber !== "string") {
     return "Invalid input";
   }
+  const phoneNumberWithoutSpaces = phoneNumber.replace(/\s/g, "");
 
-  // Remove non-numeric characters except the plus sign at the beginning
+  if (phoneNumber.startsWith("00")) {
+    phoneNumber = "+" + phoneNumber.slice(2);
+  }
+
   phoneNumber = phoneNumber.replace(/(?!^\+)[^0-9]/g, "");
 
-  // Reformat the phone number dynamically
-  let formattedNumber = phoneNumber
-    .replace(/(\+\d{2})(\d?)(\d{0,2})/g, "$1 $2 $3")
-    .trim();
-  formattedNumber = formattedNumber.replace(/(\d{2})(?=\d)/g, "$1 ");
+  const isThreeDigitCode = phoneCodesThreeDigits.includes(
+    phoneNumberWithoutSpaces.match(/^\+\d{3}/)?.[0]?.split("+")[1] ?? "",
+  );
+
+  let formattedNumber = phoneNumber;
+  if (isThreeDigitCode) {
+    formattedNumber = phoneNumber.replace(
+      /(\+\d{3})(\d?)(\d{0,2})(\d{0,2})(\d{0,2})(\d{0,2})/,
+      "$1 $2 $3 $4 $5 $6",
+    );
+  } else {
+    formattedNumber = phoneNumber
+      .replace(/(\+\d{2})(\d?)(\d{0,2})/g, "$1 $2 $3")
+      .replace(/(\d{2})(?=\d)/g, "$1 ");
+  }
 
   return formattedNumber.trim();
 }
