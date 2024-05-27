4c6cd0dea5b72b30e9f1a14e4472169e02bddb00
Cédric Soulas <soulascedric@gmail.com>
2024-05-27 13:27:54 +0200

    feat(api): update rejected subscription email

--- packages/reva-api/modules/subscription/mail/rejection.ts
+++ packages/reva-api/modules/subscription/mail/rejection.ts
@@ -15,18 +15,18 @@ export const sendRejectionEmail = async ({
   const htmlContent = mjml2html(
     template({
       headline: "Bonjour,",
-      message: `<p>Nous vous remercions pour votre demande de référencement en tant qu'Architecte Accompagnateur de Parcours (AAP) sur la plateforme France VAE.</p>
-        <p>Après vérification des éléments fournis concernant votre structure, nous ne pouvons malheureusement pas valider votre référencement.</p>
-        <p>Nous avons constaté les erreurs suivantes :</p>
+      message: `<p>Nous vous remercions pour votre demande de référencement en tant qu'Architecte         Accompagnateur de Parcours (AAP) sur la plateforme France VAE.</p>
+        <p>Après avoir examiné votre dossier, nous avons identifié certaines erreurs qui nécessitent votre attention :</p>
         <ul>${reason
           .split("\n")
           .map((line: string) => `<li>${line}</li>`)
           .join("")}
         </ul>
-        <p>Nous vous invitons à recommencer votre inscription en corrigeant ces points.</p>
-        <p>Si vous ne l’avez pas déjà fait, nous vous invitons à <a href="https://tally.so/r/mVjVeN">participer à un de nos webinaires de présentation de France VAE</a>.</p>
-        <p>Si des questions subsistent, n'hésitez pas à nous contacter via la messagerie instantanée ou par email à <a href="mailto:support@vae.gouv.fr">support@vae.gouv.fr</a>.</p>
-        <p>Très cordialement</p>
+        <p>Nous vous recommandons de corriger ces points si vous souhaitez déposer une nouvelle demande d’inscription.</p>
+        <p>Si vous ne l’avez pas déjà fait, nous vous invitons à <a href="https://tally.so/r/mVjVeN">participer à l’un de nos webinaires de présentation de France VAE</a>. La participation à ce webinaire est un prérequis pour être référencé !</p>
+        <p>Inscrivez-vous ici : <a href="https://tally.so/r/mVjVeN">Webinaires France VAE</a>.</p>
+        <p>Pour toute question, vous pouvez consulter notre <a href="https://vae.gouv.fr/faq/">FAQ</a>, nous contacter via notre <a href="https://vae.gouv.fr/nous-contacter/">formulaire de contact</a> ou encore nous écrire à <a href="mailto:support@vae.gouv.fr">support@vae.gouv.fr</a>.</p>
+        <p>Cordialement</p>
         <p>L’équipe France VAE.</p>
       `,
     }),
