7bdd14f4c5dd7b32761f409ee751aa29a478b038
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-05-30 13:38:24 +0200

    chore(api): removed commented code

--- packages/reva-api/modules/organism/organism.resolvers.ts
+++ packages/reva-api/modules/organism/organism.resolvers.ts
@@ -126,9 +126,6 @@ const unsafeResolvers = {
       }),
   },
   MaisonMereAAPLegalInformationDocuments: {
-    // File: {
-    //   previewUrl: (_file: File) => 'yaaaay',
-    // },
     attestationURSSAFFile: async (
       { maisonMereAAPId }: { maisonMereAAPId: string },
       _: unknown,
