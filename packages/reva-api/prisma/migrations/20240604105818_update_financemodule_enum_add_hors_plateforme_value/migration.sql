858ee3ff5544d477041fbf8c3c768d026b849242
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-06-04 13:27:46 +0200

    feat(api): added "hors_plateforme" value financeModule enum and set it as the default value for candidacy.financeModule

---
+++ packages/reva-api/prisma/migrations/20240604105818_update_financemodule_enum_add_hors_plateforme_value/migration.sql
@@ -0,0 +1,2 @@
+-- AlterEnum
+ALTER TYPE "FinanceModule" ADD VALUE 'hors_plateforme';
---
+++ packages/reva-api/prisma/migrations/20240604110602_update_candidacy_finance_module_default_to_hors_plateforme/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "candidacy" ALTER COLUMN "finance_module" SET DEFAULT 'hors_plateforme';
