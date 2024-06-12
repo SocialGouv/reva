521febed270548866f082efc680d17b9a4905fd5
Florian Bonniec <f.bonniec@me.com>
2024-06-12 15:32:51 +0200

    feat(api): improved script to clean candidacies

--- packages/reva-api/scripts/resetOrganismAndCertificationForCandidacies.ts
+++ packages/reva-api/scripts/resetOrganismAndCertificationForCandidacies.ts
@@ -1,53 +1,24 @@
 import { prismaClient } from "../prisma/client";
 
 const resetOrganismAndCertificationForCandidacies = async () => {
-  // Get last version of Professional CGU
-  const lastProfessional = await prismaClient.professionalCgu.findFirst({
-    orderBy: { createdAt: "desc" },
-  });
-
-  if (!lastProfessional) {
-    return;
-  }
-
-  // Get all organisms where cgu version of MaisonMere not equal to last version of Professional CGU
-  const organisms = await prismaClient.organism.findMany({
+  const candidacyStatuses = await prismaClient.candidaciesStatus.findMany({
     where: {
-      maisonMereAAP: {
-        OR: [
-          {
-            cguVersion: { equals: null },
-          },
-          {
-            cguVersion: { not: lastProfessional.version },
-          },
-        ],
-      },
+      isActive: true,
+      status: "PROJET",
     },
   });
 
-  // Clean all candidacies for retrived organisms
-  for (const organism of organisms) {
-    const candidacyStatuses = await prismaClient.candidaciesStatus.findMany({
-      where: {
-        isActive: true,
-        status: "PROJET",
-        candidacy: { organismId: organism.id },
-      },
+  for (const candidacyStatus of candidacyStatuses) {
+    const { candidacyId } = candidacyStatus;
+
+    await prismaClient.candidaciesOnRegionsAndCertifications.deleteMany({
+      where: { candidacyId },
     });
 
-    for (const candidacyStatus of candidacyStatuses) {
-      const { candidacyId } = candidacyStatus;
-
-      await prismaClient.candidaciesOnRegionsAndCertifications.deleteMany({
-        where: { candidacyId },
-      });
-
-      await prismaClient.candidacy.update({
-        where: { id: candidacyId },
-        data: { organismId: null },
-      });
-    }
+    await prismaClient.candidacy.update({
+      where: { id: candidacyId },
+      data: { organismId: null },
+    });
   }
 };
 
