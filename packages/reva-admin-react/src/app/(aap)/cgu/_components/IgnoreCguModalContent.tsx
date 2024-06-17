a86914ad2fc7dbaf7df1cbc59f3a4930367900d6
Cédric Soulas <soulascedric@gmail.com>
2024-06-17 10:03:16 +0200

    feat(admin-react): update cgu ignore modal text with punctuation

--- packages/reva-admin-react/src/app/(aap)/cgu/_components/IgnoreCguModalContent.tsx
+++ packages/reva-admin-react/src/app/(aap)/cgu/_components/IgnoreCguModalContent.tsx
@@ -3,15 +3,16 @@ export const IgnoreCguModalContent = () => (
     <ul className="mb-4 [&_li]:my-2">
       <li>
         Votre établissement restera non visible et n’apparaîtra pas dans les
-        recherches des candidats
+        recherches des candidats&nbsp;;
       </li>
       <li>
         Vous pourrez toutefois continuer l’accompagnement des candidatures en
-        cours dans votre espace professionnel
+        cours dans votre espace professionnel&nbsp;;
       </li>
       <li>
         Si vous souhaitez réapparaître dans les résultats, il faudra accepter
-        les nouvelles CGU (accessibles depuis le bandeau en haut de votre écran)
+        les nouvelles CGU (accessibles depuis le bandeau en haut de votre
+        écran).
       </li>
     </ul>
     <p>
