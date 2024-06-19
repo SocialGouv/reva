f8b6d03e6e6588f4f9192d68b33290fc53761c0d
Pierre A <pierre@avizou.eu>
2024-06-19 17:42:44 +0200

    chore(admin-react):extract logic into hook and change candidacy deletion logic

---
+++ packages/reva-admin-react/src/app/(aap)/candidacies/[candidacyId]/_components/candidacy.hook.ts
@@ -0,0 +1,68 @@
+import { useAuth } from "@/components/auth/auth";
+import { isCandidacyStatusEqualOrAbove } from "@/utils/isCandidacyStatusEqualOrAbove";
+import { useMemo } from "react";
+import { CandidacyStatusStep } from "@/graphql/generated/graphql";
+
+export type CandidacyForStatus = {
+  id: string;
+  candidacyStatuses: {
+    status: CandidacyStatusStep;
+    isActive: boolean;
+  }[];
+  reorientationReason?:
+    | {
+        id?: string;
+        label: string;
+        disabled: boolean;
+      }
+    | {
+        id: string;
+      }
+    | null;
+    candidacyDropOut?: { droppedOutAt: number } | null;
+};
+
+export const useCandidacyStatus = (candidacy: CandidacyForStatus) => {
+  const isCandidacyReoriented = !!candidacy.reorientationReason;
+  const isCandidacyArchived = candidacy.candidacyStatuses.some(
+    (s) => s.isActive && s.status === "ARCHIVE",
+  );
+
+  const { isAdmin } = useAuth();
+
+  const candidacyCurrentActiveStatus = useMemo(() => {
+    return candidacy.candidacyStatuses.find((s) => s.isActive)?.status;
+  }, [candidacy]);
+
+  const isCandidacyArchivedAndNotReoriented =
+    isCandidacyArchived && !isCandidacyReoriented;
+
+  const isCandidacyDroppedOut = !!candidacy.candidacyDropOut;
+
+  const canBeArchived =
+    candidacyCurrentActiveStatus &&
+    !isCandidacyArchivedAndNotReoriented &&
+    (!isCandidacyStatusEqualOrAbove(
+      candidacyCurrentActiveStatus,
+      "PARCOURS_CONFIRME",
+    ) ||
+      isAdmin);
+
+  const canBeRestored =
+    isCandidacyArchivedAndNotReoriented && isAdmin;
+
+  const canDroput =
+    !isCandidacyDroppedOut && !isCandidacyArchivedAndNotReoriented;
+
+  const canCancelDropout =
+    isCandidacyDroppedOut && !isCandidacyArchivedAndNotReoriented && !!isAdmin;
+
+  return {
+    candidacyCurrentActiveStatus,
+    isCandidacyDroppedOut,
+    canBeArchived,
+    canBeRestored,
+    canDroput,
+    canCancelDropout,
+  };
+};
