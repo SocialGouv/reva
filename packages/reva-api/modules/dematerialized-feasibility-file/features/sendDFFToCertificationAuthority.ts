e2174f97688499e20488b4c87fd4f97605bef574
ThomasDos <dosanjos.thomas@gmail.com>
2024-07-09 11:18:17 +0200

    feat(api): update candidacy status when dff has been sent to the certification authority

--- packages/reva-api/modules/dematerialized-feasibility-file/features/sendDFFToCertificationAuthority.ts
+++ packages/reva-api/modules/dematerialized-feasibility-file/features/sendDFFToCertificationAuthority.ts
@@ -4,18 +4,38 @@ import { sendDFFNotificationToCertificationAuthorityEmail } from "../emails";
 export const sendDFFToCertificationAuthority = async ({
   dematerializedFeasibilityFileId,
   certificationAuthorityId,
+  candidacyId,
 }: {
   dematerializedFeasibilityFileId: string;
   certificationAuthorityId: string;
+  candidacyId: string;
 }) => {
   const now = new Date().toISOString();
-  await prismaClient.dematerializedFeasibilityFile.update({
-    where: { id: dematerializedFeasibilityFileId },
-    data: {
-      sentToCertificationAuthorityAt: now,
-      certificationAuthorityId,
-    },
-  });
+
+  await prismaClient.$transaction([
+    prismaClient.candidaciesStatus.updateMany({
+      where: {
+        candidacyId: candidacyId,
+      },
+      data: {
+        isActive: false,
+      },
+    }),
+    prismaClient.candidaciesStatus.create({
+      data: {
+        status: "DOSSIER_FAISABILITE_ENVOYE",
+        isActive: true,
+        candidacyId,
+      },
+    }),
+    prismaClient.dematerializedFeasibilityFile.update({
+      where: { id: dematerializedFeasibilityFileId },
+      data: {
+        sentToCertificationAuthorityAt: now,
+        certificationAuthorityId,
+      },
+    }),
+  ]);
 
   const dff = await prismaClient.dematerializedFeasibilityFile.findUnique({
     where: { id: dematerializedFeasibilityFileId },
