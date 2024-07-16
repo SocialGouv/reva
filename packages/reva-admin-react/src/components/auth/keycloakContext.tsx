5c87305b486e2d1d6c165ae2923e856a6f4e37da
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-07-16 13:57:37 +0200

    fix(admin-react): re-enabled post-login redirect page

--- packages/reva-admin-react/src/components/auth/keycloakContext.tsx
+++ packages/reva-admin-react/src/components/auth/keycloakContext.tsx
@@ -112,7 +112,7 @@ export const KeycloakProvider = ({
             setTimeoutRefreshToken();
           } else {
             keycloakInstance.login({
-              redirectUri: window.location.href,
+              redirectUri: window.location.origin + "/admin2/post-login",
             });
           }
 
