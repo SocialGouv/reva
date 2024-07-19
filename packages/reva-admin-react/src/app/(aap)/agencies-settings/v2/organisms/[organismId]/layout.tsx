a31e1d63539a96e8211323494f3bfa69d54bc7b4
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-07-19 11:26:32 +0200

    feat(admin-react): added a badge in agencies settings for the admin when an agency is not visible in the search results

---
+++ packages/reva-admin-react/src/app/(aap)/agencies-settings/v2/organisms/[organismId]/layout.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useGraphQlClient } from "@/components/graphql/graphql-client/GraphqlClient";
+import { graphql } from "@/graphql/generated";
+import { Badge } from "@codegouvfr/react-dsfr/Badge";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { ReactNode } from "react";
+
+const getOrganismQuery = graphql(`
+  query getOrganismForOrganismLayout($organismId: ID!) {
+    organism_getOrganism(id: $organismId) {
+      id
+      isOnSite
+      isRemote
+      fermePourAbsenceOuConges
+      managedDegrees {
+        id
+      }
+      domaines {
+        id
+      }
+      conventionCollectives {
+        id
+      }
+    }
+  }
+`);
+
+const OrganismDetailsLayout = ({ children }: { children: ReactNode }) => {
+  const { graphqlClient } = useGraphQlClient();
+  const { organismId } = useParams<{ organismId: string }>();
+
+  const { data: getOrganismResponse, status: getOrganismStatus } = useQuery({
+    queryKey: ["organisms", "agencies-settings-aap-organism-layout"],
+    queryFn: () => graphqlClient.request(getOrganismQuery, { organismId }),
+  });
+
+  const organism = getOrganismResponse?.organism_getOrganism;
+
+  if (!organism) {
+    return null;
+  }
+
+  const {
+    isOnSite,
+    isRemote,
+    fermePourAbsenceOuConges,
+    managedDegrees,
+    domaines,
+    conventionCollectives,
+  } = organism;
+
+  const isVisibleInSearchResults =
+    (isOnSite || isRemote) &&
+    !fermePourAbsenceOuConges &&
+    managedDegrees.length &&
+    (domaines.length || conventionCollectives.length);
+
+  return (
+    <div className="flex flex-col">
+      {!isVisibleInSearchResults && (
+        <Badge severity="warning" className="mb-4">
+          Actuellement, vous n’êtes pas visible dans les résultats de recherche.
+        </Badge>
+      )}
+      {children}
+    </div>
+  );
+};
+
+export default OrganismDetailsLayout;
