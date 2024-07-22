00358784df9c129bcac392927386918b193413fc
Pierre A <pierre@avizou.eu>
2024-07-22 14:36:59 +0200

    feat(candidate):Suspense and skeleton for certifications

---
+++ packages/reva-candidate/src/app/(project)/set-certification/(components)/CertificationResults.tsx
@@ -0,0 +1,161 @@
+import { Results } from "@/components/legacy/organisms/Results";
+import { Pagination } from "@/components/pagination/Pagination";
+import CertificationResultCard from "./CertificationResultCard";
+import { searchCertifications } from "../set-certification.loaders";
+import SubmitButton from "@/components/forms/SubmitButton";
+import Button from "@codegouvfr/react-dsfr/Button";
+import Notice from "@codegouvfr/react-dsfr/Notice";
+import Link from "next/link";
+import { updateCertification } from "../set-certification.actions";
+
+export default async function CertificationResults({
+  searchParams,
+  candidateDepartmentId,
+  candidacyId,
+  financementHorsPlateformeFeatureActive,
+}: {
+  searchParams: Record<string, string>;
+  candidateDepartmentId: string;
+  candidacyId: string;
+  financementHorsPlateformeFeatureActive: boolean;
+}) {
+  const urlSearchParams = new URLSearchParams(searchParams);
+  const page = urlSearchParams.get("page");
+  const searchFilter = urlSearchParams.get("search") || "";
+  const departmentId = urlSearchParams.get("departmentId");
+  const selectedCertificationId = urlSearchParams.get("certificationId");
+  const currentPage = page ? Number.parseInt(page) : 1;
+
+  const searchCertificationsForCandidate = await searchCertifications({
+    departmentId: departmentId || candidateDepartmentId || "",
+    searchText: searchFilter,
+    currentPage,
+  });
+
+  let searchParamsWithoutPage = {};
+  urlSearchParams.forEach((value, key) => {
+    if (key.toLowerCase() !== "page") {
+      searchParamsWithoutPage = { ...searchParamsWithoutPage, [key]: value };
+    }
+  });
+
+  const searchParamsWithoutCertification = new URLSearchParams(urlSearchParams);
+  searchParamsWithoutCertification.delete("certificationId");
+
+  const info = searchCertificationsForCandidate.info;
+  const rows = searchCertificationsForCandidate.rows;
+
+  const selectedCertification = rows?.find(
+    (certification) => certification.id == selectedCertificationId,
+  );
+
+  return (
+    <>
+      {
+        !selectedCertification && (
+          <div id="results" className="flex flex-col justify-center">
+            <Results
+              title=""
+              listClassName="flex flex-wrap justify-center lg:justify-start items-center mb-4 gap-4"
+            >
+              <CertificationResultCard rows={rows} />
+            </Results>
+
+            {info && (
+              <Pagination
+                totalPages={info.totalPages}
+                currentPage={currentPage}
+                baseHref={"/set-certification/"}
+                className="mx-auto"
+                baseParams={searchParamsWithoutPage}
+              />
+            )}
+          </div>
+        )
+      }
+      {selectedCertification && (
+        <form action={updateCertification}>
+          <input
+            type="hidden"
+            name="certificationId"
+            value={selectedCertification.id}
+          />
+          <input
+            type="hidden"
+            name="candidacyId"
+            value={candidacyId}
+          />
+          <input
+            type="hidden"
+            name="departmentId"
+            value={departmentId || candidateDepartmentId}
+          />
+          <h2
+            data-test="certification-label"
+            className="mt-6 mb-2 text-2xl font-bold text-black "
+          >
+            {selectedCertification.label}
+          </h2>
+          <p data-test="certification-code-rncp" className="text-xs mb-3">
+            Code RNCP: {selectedCertification.codeRncp}
+          </p>
+
+          {financementHorsPlateformeFeatureActive &&
+            selectedCertification.financeModule === "hors_plateforme" && (
+              <Notice
+                className="mt-2 mb-3"
+                title={
+                  <span>
+                    <p className="mb-4">
+                      Ce diplôme peut être financé par les dispositifs comme Mon
+                      Compte Formation, l’aide des régions, l’aide de France
+                      Travail.
+                    </p>
+                    <p className="mb-4">
+                      Votre accompagnateur explorera avec vous les dispositifs
+                      de financement dont vous pouvez bénéficier. Il vous
+                      indiquera les démarches nécessaires et, le cas échéant,
+                      vous accompagnera pour les réaliser.
+                    </p>
+                    <p>
+                      Pour information, le coût moyen constaté d’un parcours est
+                      de 2516€.
+                    </p>
+                  </span>
+                }
+              />
+            )}
+          <p>
+            <a
+              data-test="certification-more-info-link"
+              target="_blank"
+              rel="noreferrer"
+              href={`https://www.francecompetences.fr/recherche/rncp/${selectedCertification.codeRncp}/`}
+            >
+              Lire les détails de la fiche diplôme
+            </a>
+          </p>
+
+          <div className="flex flex-col md:flex-row gap-6 mt-6">
+            <SubmitButton
+              className="justify-center w-[100%]  md:w-fit"
+              data-test="submit-certification-button"
+              label="Choisir ce diplôme"
+            />
+            <Button
+              priority="secondary"
+              className="justify-center w-[100%] p-0 md:w-fit"
+            >
+              <Link
+                className="px-4 py-2"
+                href={`/set-certification/?${searchParamsWithoutCertification.toString()}`}
+              >
+                Retour
+              </Link>
+            </Button>
+          </div>
+        </form>
+      )}
+    </>
+  );
+}
--- packages/reva-candidate/src/app/(project)/set-certification/page.tsx
+++ packages/reva-candidate/src/app/(project)/set-certification/page.tsx
@@ -1,19 +1,13 @@
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import Button from "@codegouvfr/react-dsfr/Button";
-import { Notice } from "@codegouvfr/react-dsfr/Notice";
 import { isFeatureActive } from "@/utils/getActiveFeatures";
 import { FormOptionalFieldsDisclaimer } from "@/components/legacy/atoms/FormOptionalFieldsDisclaimer/FormOptionalFieldsDisclaimer";
-import { Pagination } from "@/components/pagination/Pagination";
-import { Results } from "@/components/legacy/organisms/Results";
 import { getCandidacy } from "@/app/home.loaders";
-import { searchCertifications } from "./set-certification.loaders";
 import CertificationSearchBar from "./(components)/CertificationSearchBar";
-import CertificationResultCard from "./(components)/CertificationResultCard";
 import SelectCertificationDepartment from "./(components)/SelectCertificationDepartment";
 import { getDepartments } from "@/components/select-department/SelectDepartment.loaders";
-import { updateCertification } from "./set-certification.actions";
-import SubmitButton from "@/components/forms/SubmitButton";
+import CertificationResults from "./(components)/CertificationResults";
+import { Suspense } from "react";
+import CertificationsSkeleton from "./(components)/CertificationsSkeleton";
 
 export default async function SetCertification({
   searchParams,
@@ -21,11 +15,8 @@ export default async function SetCertification({
   searchParams: Record<string, string>;
 }) {
   const urlSearchParams = new URLSearchParams(searchParams);
-  const page = urlSearchParams.get("page");
-  const departmentId = urlSearchParams.get("departmentId");
   const searchFilter = urlSearchParams.get("search") || "";
   const selectedCertificationId = urlSearchParams.get("certificationId");
-  const currentPage = page ? Number.parseInt(page) : 1;
 
   const financementHorsPlateformeFeatureActive = await isFeatureActive(
     "FINANCEMENT_HORS_PLATEFORME",
@@ -37,11 +28,6 @@ export default async function SetCertification({
   }
 
   const departments = await getDepartments();
-  const searchCertificationsForCandidate = await searchCertifications({
-    departmentId: departmentId || candidate.department.id || "",
-    searchText: searchFilter,
-    currentPage,
-  });
 
   let searchParamsWithoutPage = {};
   urlSearchParams.forEach((value, key) => {
@@ -53,15 +39,9 @@ export default async function SetCertification({
   const searchParamsWithoutCertification = new URLSearchParams(urlSearchParams);
   searchParamsWithoutCertification.delete("certificationId");
 
-  const info = searchCertificationsForCandidate.info;
-  const rows = searchCertificationsForCandidate.rows;
-  const selectedCertification = rows?.find(
-    (certification) => certification.id == selectedCertificationId,
-  );
-
   return (
     <>
-      {!selectedCertification && (
+      {!selectedCertificationId && (
         <>
           <h2 className="mt-6 mb-2">Nouveau parcours VAE - Choix du diplôme</h2>
           <FormOptionalFieldsDisclaimer
@@ -74,111 +54,19 @@ export default async function SetCertification({
             defaultValue={candidate.department.id}
           />
           <CertificationSearchBar searchFilter={searchFilter} />
-
-          <div id="results" className="flex flex-col justify-center">
-            <Results
-              title=""
-              listClassName="flex flex-wrap justify-center lg:justify-start items-center mb-4 gap-4"
-            >
-              <CertificationResultCard rows={rows} />
-            </Results>
-
-            {info && (
-              <Pagination
-                totalPages={info.totalPages}
-                currentPage={currentPage}
-                baseHref={"/set-certification/"}
-                className="mx-auto"
-                baseParams={searchParamsWithoutPage}
-              />
-            )}
-          </div>
         </>
       )}
 
-      {selectedCertification && (
-        <form action={updateCertification}>
-          <input
-            type="hidden"
-            name="certificationId"
-            value={selectedCertification.id}
-          />
-          <input
-            type="hidden"
-            name="candidacyId"
-            value={candidate.candidacy.id}
-          />
-          <input
-            type="hidden"
-            name="departmentId"
-            value={departmentId || candidate.department.id}
-          />
-          <h2
-            data-test="certification-label"
-            className="mt-6 mb-2 text-2xl font-bold text-black "
-          >
-            {selectedCertification.label}
-          </h2>
-          <p data-test="certification-code-rncp" className="text-xs mb-3">
-            Code RNCP: {selectedCertification.codeRncp}
-          </p>
-
-          {financementHorsPlateformeFeatureActive &&
-            selectedCertification.financeModule === "hors_plateforme" && (
-              <Notice
-                className="mt-2 mb-3"
-                title={
-                  <span>
-                    <p className="mb-4">
-                      Ce diplôme peut être financé par les dispositifs comme Mon
-                      Compte Formation, l’aide des régions, l’aide de France
-                      Travail.
-                    </p>
-                    <p className="mb-4">
-                      Votre accompagnateur explorera avec vous les dispositifs
-                      de financement dont vous pouvez bénéficier. Il vous
-                      indiquera les démarches nécessaires et, le cas échéant,
-                      vous accompagnera pour les réaliser.
-                    </p>
-                    <p>
-                      Pour information, le coût moyen constaté d’un parcours est
-                      de 2516€.
-                    </p>
-                  </span>
-                }
-              />
-            )}
-          <p>
-            <a
-              data-test="certification-more-info-link"
-              target="_blank"
-              rel="noreferrer"
-              href={`https://www.francecompetences.fr/recherche/rncp/${selectedCertification.codeRncp}/`}
-            >
-              Lire les détails de la fiche diplôme
-            </a>
-          </p>
-
-          <div className="flex flex-col md:flex-row gap-6 mt-6">
-            <SubmitButton
-              className="justify-center w-[100%]  md:w-fit"
-              data-test="submit-certification-button"
-              label="Choisir ce diplôme"
-            />
-            <Button
-              priority="secondary"
-              className="justify-center w-[100%] p-0 md:w-fit"
-            >
-              <Link
-                className="px-4 py-2"
-                href={`/set-certification/?${searchParamsWithoutCertification.toString()}`}
-              >
-                Retour
-              </Link>
-            </Button>
-          </div>
-        </form>
-      )}
+      <Suspense fallback={<CertificationsSkeleton />}>
+        <CertificationResults
+          candidateDepartmentId={candidate.department.id}
+          candidacyId={candidate.candidacy.id}
+          financementHorsPlateformeFeatureActive={
+            financementHorsPlateformeFeatureActive
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </>
   );
 }
