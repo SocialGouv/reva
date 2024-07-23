7f5c1b6ef18307787285eae9c12f5fecbb38a80b
Florian Bonniec <f.bonniec@me.com>
2024-07-23 12:06:36 +0200

    fix(reva-candidate): added missing trophy.png icon

--- packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/ProjectEndedTimelineElement/ProjectEndedTimelineElement.tsx
+++ packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/ProjectEndedTimelineElement/ProjectEndedTimelineElement.tsx
@@ -11,7 +11,7 @@ export const ProjectEndedTimelineElement = () => {
 
   return success ? (
     <div className="mt-8 flex flex-row gap-6 p-4 max-w-[600px] shadow-[0_6px_18px_0_rgba(0,0,18,0.16)]">
-      <Image width="56" height="56" src="/app/img/trophy.png" alt="Trophée" />
+      <Image width="56" height="56" src="/candidat/trophy.png" alt="Trophée" />
       <label className="text-xl font-bold">
         Félicitations, vous avez obtenu votre diplôme par la Validation des
         Acquis de l’Expérience (VAE) !
