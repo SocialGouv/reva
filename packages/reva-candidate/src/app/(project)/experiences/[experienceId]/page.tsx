8b5e4bdc861999cbeed7be36a5c0bf0b7662754a
Florian Bonniec <f.bonniec@me.com>
2024-07-25 11:25:52 +0200

    feat(candidate): removed 'optionnel' for 'Durée' in experiences

--- packages/reva-candidate/src/app/(project)/experiences/[experienceId]/page.tsx
+++ packages/reva-candidate/src/app/(project)/experiences/[experienceId]/page.tsx
@@ -76,7 +76,7 @@ export default async function UpdateExperience({
           <label htmlFor="duration" className="fr-label"></label>
 
           <Select
-            label="Durée (optionnel)"
+            label="Durée"
             hint="Pendant combien de temps avez-vous exercé ?"
             nativeSelectProps={{
               required: true,
