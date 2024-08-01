3715b4020d5d9b06bacc57f7d586aca9342e8acf
Cédric Soulas <soulascedric@gmail.com>
2024-08-01 11:59:27 +0200

    feat(candidate): add fr language to html document

--- packages/reva-candidate/src/app/layout.tsx
+++ packages/reva-candidate/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html {...getHtmlAttributes({ defaultColorScheme })}>
+    <html {...getHtmlAttributes({ defaultColorScheme })} lang="fr">
       <head>
         <StartDsfr />
         <DsfrHead />
