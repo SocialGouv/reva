5612f3001a92ab10b2a681ad63963d704b8ef833
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-08-07 16:46:15 +0200

    fix(api): getOrganism was throwing an error when organismId was undefined

--- packages/reva-api/modules/organism/features/getOrganism.ts
+++ packages/reva-api/modules/organism/features/getOrganism.ts
@@ -1,6 +1,14 @@
 import { prismaClient } from "../../../prisma/client";
 
-export const getOrganismById = async ({ organismId }: { organismId: string }) =>
-  prismaClient.organism.findUnique({
+export const getOrganismById = async ({
+  organismId,
+}: {
+  organismId: string;
+}) => {
+  if (!organismId) {
+    return null;
+  }
+  return prismaClient.organism.findUnique({
     where: { id: organismId },
   });
+};
