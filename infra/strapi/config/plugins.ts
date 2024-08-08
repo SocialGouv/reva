ab4bebdc6710e7fdf9d7d95c5c49b0df9dc2cd0c
Pierre A <pierre@avizou.eu>
2024-08-08 10:47:46 +0200

    feat(strapi):add and configure preview plugin

--- infra/strapi/config/plugins.ts
+++ infra/strapi/config/plugins.ts
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = ({ env }) => ({
   graphql: {
     config: {
       apolloServer: {
@@ -6,4 +6,70 @@ module.exports = {
       },
     },
   },
-};
+  "preview-button": {
+    config: {
+      contentTypes: [
+        {
+          uid: "api::article-d-aide.article-d-aide",
+          draft: {
+            alwaysVisible: true,
+            url: `${env("WEBSITE_URL")}/websiteapi/preview`,
+            query: {
+              type: "article-d-aide",
+              slug: "{slug}",
+              secret: env("STRAPI_PREVIEW_SECRET"),
+            },
+          },
+          published: {
+            url: `${env("WEBSITE_URL")}/savoir-plus/articles/{slug}`,
+          },
+        },
+        {
+          uid: "api::article-faq.article-faq",
+          draft: {
+            alwaysVisible: true,
+            url: `${env("WEBSITE_URL")}/websiteapi/preview`,
+            query: {
+              type: "article-faq",
+              secret: env("STRAPI_PREVIEW_SECRET"),
+            },
+          },
+          published: {
+            url: `${env("WEBSITE_URL")}/faq`,
+          },
+        },
+        {
+          uid: "api::region.region",
+          draft: {
+            alwaysVisible: true,
+            url: `${env("WEBSITE_URL")}/websiteapi/preview`,
+            query: {
+              type: "region",
+              slug: "{slug}",
+              secret: env("STRAPI_PREVIEW_SECRET"),
+            },
+          },
+          published: {
+            url: `${env("WEBSITE_URL")}/regions/{slug}`,
+          },
+        },
+        {
+          uid: "api::article-region.article-region",
+          draft: {
+            alwaysVisible: true,
+            url: `${env("WEBSITE_URL")}/websiteapi/preview`,
+            query: {
+              type: "article-region",
+              slug: "{slug}",
+              id: "{id}",
+              secret: env("STRAPI_PREVIEW_SECRET"),
+            },
+          },
+          published: {
+            url: `${env("WEBSITE_URL")}/regions`,
+          },
+        },
+      ],
+    },
+  },
+});
