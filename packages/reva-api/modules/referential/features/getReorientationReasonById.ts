b0fe220747321896839bc79d1b05d714fbfba445
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-08-09 10:17:48 +0200

    fix(api): fixed a bug on getReorientationReasonById when reorientationReasonId was null

--- packages/reva-api/modules/referential/features/getReorientationReasonById.ts
+++ packages/reva-api/modules/referential/features/getReorientationReasonById.ts
@@ -1,11 +1,14 @@
 import { prismaClient } from "../../../prisma/client";
-import { ReorientationReason } from "../referential.types";
 
-export const getReorientationReasonById = async (params: {
+export const getReorientationReasonById = async ({
+  reorientationReasonId,
+}: {
   reorientationReasonId: string;
-}): Promise<ReorientationReason | null> =>
-  prismaClient.reorientationReason.findUnique({
-    where: {
-      id: params.reorientationReasonId,
-    },
-  });
+}) =>
+  reorientationReasonId
+    ? prismaClient.reorientationReason.findUnique({
+        where: {
+          id: reorientationReasonId,
+        },
+      })
+    : null;
