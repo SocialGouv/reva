5575b20175871f8f8c46e88d171b0e9e418da4e3
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-08-14 15:06:37 +0200

    feat(website): new funding wording for candidates

--- packages/reva-website/src/components/home-page/CommentFinancerVotreParcours.tsx
+++ packages/reva-website/src/components/home-page/CommentFinancerVotreParcours.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import { useFeatureflipping } from "../feature-flipping/featureFlipping";
+
 const PolygonFinancer = ({
   bgUrl,
   label,
@@ -14,7 +16,7 @@ const PolygonFinancer = ({
   </div>
 );
 
-const CommentFinancerVotreParcours = () => (
+const CommentFinancerVotreParcoursFranceVae = () => (
   <section
     id="comment-financer-votre-parcours"
     className="w-full bg-[#1B1B35] text-white py-14 lg:py-[88px]"
@@ -61,5 +63,64 @@ const CommentFinancerVotreParcours = () => (
     </div>
   </section>
 );
+
+const CommentFinancerVotreParcoursHorsPlateforme = () => (
+  <section
+    id="comment-financer-votre-parcours"
+    className="w-full bg-[#1B1B35] text-white py-14 lg:py-[88px]"
+  >
+    <div className="flex flex-col lg:flex-row justify-center items-center w-full lg:gap-20 fr-container">
+      <div className="py-6 flex flex-col gap-14">
+        <h2 className="text-white my-0">Comment financer votre parcours ?</h2>
+        <p className="my-0 text-lg">
+          Actuellement, il existe plusieurs dispositifs (nationaux, régionaux,
+          privés…) pour financer intégralement ou partiellement votre parcours
+          VAE. Votre accompagnateur explorera les pistes de financement adaptées
+          à votre profil. L'objectif ? Que vous puissiez réaliser votre VAE dans
+          les meilleures conditions !
+        </p>
+        <p>
+          <Link
+            href="https://vae.gouv.fr/savoir-plus/articles/financer-son-accompagnement-vae/"
+            target="_blank"
+            className="text-[#8585F6]"
+          >
+            Quels sont les dispositifs qui financent un parcours VAE ?
+          </Link>
+        </p>
+      </div>
+      <div className="hidden lg:flex flex-col items-center relative">
+        <div className="relative top-12">
+          <PolygonFinancer
+            bgUrl="bg-[url('/home-page/polygon-financer/polygon-financer-orange.svg')]"
+            label="Mon Compte Formation"
+          />
+        </div>
+        <div className="flex gap-2">
+          <PolygonFinancer
+            bgUrl="bg-[url('/home-page/polygon-financer/polygon-financer-blue.svg')]"
+            label="OPCO"
+          />
+          <PolygonFinancer
+            bgUrl="bg-[url('/home-page/polygon-financer/polygon-financer-pink.svg')]"
+            label="Aides nationales et régionales"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+const CommentFinancerVotreParcours = () => {
+  const { isFeatureActive, status } = useFeatureflipping();
+  if (status !== "INITIALIZED") {
+    return null;
+  }
+  return isFeatureActive("AFFICHAGE_TYPES_FINANCEMENT_CANDIDATURE") ? (
+    <CommentFinancerVotreParcoursHorsPlateforme />
+  ) : (
+    <CommentFinancerVotreParcoursFranceVae />
+  );
+};
 
 export default CommentFinancerVotreParcours;
