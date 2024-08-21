3c5a1aef039327be8e31fd8e61e32b7aa8a15ae1
Pierre A <pierre@avizou.eu>
2024-08-21 17:14:37 +0200

    feat(admin-react):add one optional hierarchy level to breadcrumbs

--- packages/reva-admin-react/src/app/(admin)/certification-authority-structures/[certificationAuthorityStructureId]/_components/certification-authority-structure-breadcrumb/CertificationAuthorityStructureBreadcrumb.tsx
+++ packages/reva-admin-react/src/app/(admin)/certification-authority-structures/[certificationAuthorityStructureId]/_components/certification-authority-structure-breadcrumb/CertificationAuthorityStructureBreadcrumb.tsx
@@ -1,32 +1,55 @@
 import { Breadcrumb } from "@codegouvfr/react-dsfr/Breadcrumb";
 
+type BreadcrumbsProps = {
+  certificationAuthorityStructureId: string;
+  certificationAuthorityStructureLabel: string;
+  certificationAuthorityId?: never;
+  certificationAuthoritylabel?: never;
+  pageLabel: string;
+} | {
+  certificationAuthorityStructureId: string;
+  certificationAuthorityStructureLabel: string;
+  certificationAuthorityId: string;
+  certificationAuthoritylabel: string;
+  pageLabel: string;
+}
+
 export const CertificationAuthorityStructureBreadcrumb = ({
   certificationAuthorityStructureId,
   certificationAuthorityStructureLabel,
+  certificationAuthorityId,
+  certificationAuthoritylabel,
   pageLabel,
-}: {
-  certificationAuthorityStructureId: string;
-  certificationAuthorityStructureLabel: string;
-  pageLabel: string;
-}) => (
-  <Breadcrumb
-    currentPageLabel={pageLabel}
-    homeLinkProps={{
-      href: `/`,
-    }}
-    segments={[
-      {
-        label: "Annuaire certificateurs",
-        linkProps: {
-          href: `/certification-authority-structures/`,
-        },
+}: BreadcrumbsProps) => {
+  const segments = [
+    {
+      label: "Annuaire certificateurs",
+      linkProps: {
+        href: `/certification-authority-structures/`,
       },
-      {
-        label: certificationAuthorityStructureLabel,
-        linkProps: {
-          href: `/certification-authority-structures/${certificationAuthorityStructureId}/`,
-        },
+    },
+    {
+      label: certificationAuthorityStructureLabel,
+      linkProps: {
+        href: `/certification-authority-structures/${certificationAuthorityStructureId}/`,
       },
-    ]}
-  />
-);
+    },
+  ];
+  if (certificationAuthorityId && certificationAuthoritylabel) {
+    segments.push({
+      label: certificationAuthoritylabel,
+      linkProps: {
+        href: `/certification-authority-structures/${certificationAuthorityStructureId}/certificateurs-administrateurs/${certificationAuthorityId}/`,
+      },
+    });
+  }
+  return (
+    <Breadcrumb
+      currentPageLabel={pageLabel}
+      homeLinkProps={{
+        href: `/`,
+      }}
+      segments={segments}
+    />
+  );
+};
