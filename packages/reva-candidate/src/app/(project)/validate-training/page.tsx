3b3e23ab02847a4f1db3e8904e50c85a8c460db0
Cédric Soulas <soulascedric@gmail.com>
2024-08-22 14:32:48 +0200

    feat(candidate): add a new condition to validate training and refactor options

--- packages/reva-candidate/src/app/(project)/validate-training/page.tsx
+++ packages/reva-candidate/src/app/(project)/validate-training/page.tsx
@@ -13,14 +13,17 @@ import { useCandidacy } from "@/components/candidacy/candidacy.context";
 import { graphqlErrorToast } from "@/components/toast/toast";
 import { GraphQLError } from "graphql";
 
+type Condition =
+  | "conditionOne"
+  | "conditionTwo"
+  | "conditionThree"
+  | "conditionFour"
+  | "conditionFive";
+
 type PageAction = {
   type: "changeCondition";
   payload: {
-    condition:
-      | "conditionOne"
-      | "conditionTwo"
-      | "conditionThree"
-      | "conditionFour";
+    condition: Condition;
     checked: boolean;
   };
 };
@@ -30,6 +33,7 @@ interface PageState {
   conditionTwoChecked: boolean;
   conditionThreeChecked: boolean;
   conditionFourChecked: boolean;
+  conditionFiveChecked: boolean;
   allConditionsChecked: boolean;
   candidacyFundedByFranceVae: boolean;
 }
@@ -51,17 +55,23 @@ const pageReducer = (state: PageState, action: PageAction) => {
         case "conditionFour":
           newState.conditionFourChecked = action.payload.checked;
           break;
+        case "conditionFive":
+          newState.conditionFiveChecked = action.payload.checked;
+          break;
       }
       newState.allConditionsChecked = newState.candidacyFundedByFranceVae
         ? newState.conditionOneChecked &&
           newState.conditionTwoChecked &&
           newState.conditionThreeChecked &&
-          newState.conditionFourChecked
+          newState.conditionFourChecked &&
+          newState.conditionFiveChecked
         : newState.conditionOneChecked &&
           newState.conditionTwoChecked &&
-          newState.conditionFourChecked;
+          newState.conditionFourChecked &&
+          newState.conditionFiveChecked;
     }
   }
+  console.log(newState);
   return newState;
 };
 
@@ -77,6 +87,7 @@ export default function ValidateTraining() {
     conditionTwoChecked: false,
     conditionThreeChecked: false,
     conditionFourChecked: false,
+    conditionFiveChecked: false,
     allConditionsChecked: false,
     candidacyFundedByFranceVae: candidacy.financeModule !== "hors_plateforme",
   });
@@ -105,17 +116,91 @@ export default function ValidateTraining() {
     }
   };
 
+  const checkboxOption = ({
+    title,
+    condition,
+  }: {
+    title: string;
+    condition: Condition;
+  }) => ({
+    label: title,
+    nativeInputProps: {
+      checked: pageState[`${condition}Checked`],
+      onChange: (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.checked);
+        pageDispatch({
+          type: "changeCondition",
+          payload: {
+            condition,
+            checked: e.target.checked,
+          },
+        });
+      },
+    },
+  });
+
+  const trainingOptions: { title: string; condition: Condition }[] = [
+    {
+      title:
+        "J’ai bien compris qu’il s’agissait des étapes et prestations nécessaires pour atteindre mon objectif de diplôme.",
+      condition: "conditionOne",
+    },
+    {
+      title:
+        "Je m’engage à suivre ce parcours ou informer mon organisme d’accompagnement de tout abandon dans les 48h.",
+      condition: "conditionTwo",
+    },
+    ...(candidacy.financeModule !== "hors_plateforme"
+      ? [
+          {
+            title:
+              "J’ai bien compris que mon accord allait déclencher une demande de prise en charge financière de mon parcours.",
+            condition: "conditionThree" as Condition,
+          },
+        ]
+      : []),
+    {
+      title:
+        "J’accepte que les résultats de mon étude personnalisée ainsi que les résultats de ma session de jury me soient transmis ainsi qu’à mon accompagnateur.",
+      condition: "conditionFour",
+    },
+    {
+      title:
+        "Je certifie que je ne prépare pas ce diplôme en formation initiale durant l’année de mon parcours VAE.",
+      condition: "conditionFive",
+    },
+  ];
+
+  const TrainingValidationForm = () => (
+    <>
+      <Checkbox
+        data-test="accept-conditions-checkbox-group"
+        className="mt-10"
+        legend="Conditions générales"
+        options={trainingOptions.map((option) => checkboxOption(option))}
+      />
+      <Button
+        data-test="submit-training-program-button"
+        className="mt-6 justify-center w-[100%]  md:w-fit"
+        nativeButtonProps={{
+          onClick: onSubmit,
+        }}
+        disabled={!pageState.allConditionsChecked}
+      >
+        Valider votre parcours
+      </Button>
+    </>
+  );
+
   return (
     <PageLayout className="max-w-2xl" title="Votre parcours" displayBackToHome>
       <h2 className="mt-6 mb-4">
         {isTrainingConfirmed ? "Votre parcours" : "Valider votre parcours"}
       </h2>
-
       <p>
         Suite à votre rendez-vous de faisabilité avec votre Architecte de
         Parcours, veuillez valider la proposition de parcours suivante :
       </p>
-
       <ul
         className="text-dsfrGray-700 list-square list-inside"
         data-test="general-informations"
@@ -127,7 +212,6 @@ export default function ValidateTraining() {
         <li>Accompagnement collectif: {collectiveHourCount || 0}h</li>
         <li>Formation: {additionalHourCount || 0}h</li>
       </ul>
-
       {mandatoryTrainings?.length ? (
         <TrainingSection
           title="Formations obligatoires"
@@ -142,7 +226,6 @@ export default function ValidateTraining() {
           </ul>
         </TrainingSection>
       ) : null}
-
       {basicSkills?.length ? (
         <TrainingSection
           title="Savoirs de base"
@@ -157,7 +240,6 @@ export default function ValidateTraining() {
           </ul>
         </TrainingSection>
       ) : null}
-
       {certificateSkills && (
         <TrainingSection
           title="Bloc de compétences métiers"
@@ -166,101 +248,13 @@ export default function ValidateTraining() {
           <p>{certificateSkills}</p>
         </TrainingSection>
       )}
-
       {otherTraining && (
         <TrainingSection title="Autre" data-test="other-training-section">
           <p>{otherTraining}</p>
         </TrainingSection>
       )}
 
-      <Checkbox
-        data-test="accept-conditions-checkbox-group"
-        className="mt-10"
-        legend="Conditions générales"
-        options={[
-          {
-            label:
-              "J’ai bien compris qu’il s’agissait des étapes et prestations nécessaires pour atteindre mon objectif de diplôme",
-            nativeInputProps: {
-              disabled: isTrainingConfirmed,
-              defaultChecked: isTrainingConfirmed,
-              onChange: (e) =>
-                pageDispatch({
-                  type: "changeCondition",
-                  payload: {
-                    condition: "conditionOne",
-                    checked: e.target.checked,
-                  },
-                }),
-            },
-          },
-          {
-            label:
-              "Je m’engage à suivre ce parcours ou informer mon organisme d’accompagnement de tout abandon dans les 48h",
-            nativeInputProps: {
-              disabled: isTrainingConfirmed,
-              defaultChecked: isTrainingConfirmed,
-              onChange: (e) =>
-                pageDispatch({
-                  type: "changeCondition",
-                  payload: {
-                    condition: "conditionTwo",
-                    checked: e.target.checked,
-                  },
-                }),
-            },
-          },
-          ...(candidacy.financeModule !== "hors_plateforme"
-            ? [
-                {
-                  label:
-                    "J’ai bien compris que mon accord allait déclencher une demande de prise en charge financière de mon parcours",
-                  nativeInputProps: {
-                    disabled: isTrainingConfirmed,
-                    defaultChecked: isTrainingConfirmed,
-                    onChange: (e: ChangeEvent<HTMLInputElement>) =>
-                      pageDispatch({
-                        type: "changeCondition",
-                        payload: {
-                          condition: "conditionThree",
-                          checked: e.target.checked,
-                        },
-                      }),
-                  },
-                },
-              ]
-            : []),
-          {
-            label:
-              "J’accepte que les résultats de mon étude personnalisée ainsi que les résultats de ma session de jury me soient transmis ainsi qu’à mon accompagnateur.",
-            nativeInputProps: {
-              disabled: isTrainingConfirmed,
-              defaultChecked: isTrainingConfirmed,
-              onChange: (e) =>
-                pageDispatch({
-                  type: "changeCondition",
-                  payload: {
-                    condition: "conditionFour",
-                    checked: e.target.checked,
-                  },
-                }),
-            },
-          },
-        ]}
-      />
-
-      {!isTrainingConfirmed && (
-        <Button
-          data-test="submit-training-program-button"
-          className="mt-6 justify-center w-[100%]  md:w-fit"
-          nativeButtonProps={{
-            onClick: onSubmit,
-          }}
-          disabled={!pageState.allConditionsChecked}
-        >
-          Valider votre parcours
-        </Button>
-      )}
+      {!isTrainingConfirmed && <TrainingValidationForm />}
     </PageLayout>
   );
 }
