380f6bd3e64e10424c1c6ccf58c23b6b19df499c
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-08-29 15:36:32 +0200

    feat(admin): removed status badge and disabling rules on remote and onsite cards in the agencies settings v3 page

--- packages/reva-admin-react/src/app/(aap)/agencies-settings/page.tsx
+++ packages/reva-admin-react/src/app/(aap)/agencies-settings/page.tsx
@@ -16,10 +16,7 @@ const AgenciesSettingsPage = () => {
   const isGeneralInformationCompleted =
     maisonMereAAP?.statutValidationInformationsJuridiquesMaisonMereAAP ===
     "A_JOUR";
-  const isRemoteCompleted = !!organism?.isRemote;
-  const isOnSiteCompleted = !!organism?.isOnSite;
-  const isCollaboratorsEditable =
-    isGeneralInformationCompleted && (isRemoteCompleted || isOnSiteCompleted);
+  const isCollaboratorsEditable = isGeneralInformationCompleted;
   return (
     <div className="flex flex-col w-full">
       <h1>Paramètres</h1>
@@ -39,21 +36,15 @@ const AgenciesSettingsPage = () => {
         {(isGestionnaireMaisonMereAAP || organism?.isRemote === true) && (
           <EnhancedSectionCard
             title="Accompagnement à distance"
-            status={isRemoteCompleted ? "COMPLETED" : "TO_COMPLETE"}
-            isEditable={!isOnSiteCompleted}
             buttonOnClickHref="/agencies-settings/remote"
             titleIconClass="fr-icon-headphone-fill"
-            disabled={isOnSiteCompleted}
           />
         )}
         {isGestionnaireMaisonMereAAP && (
           <EnhancedSectionCard
             title="Accompagnement en présentiel"
-            status={isOnSiteCompleted ? "COMPLETED" : "TO_COMPLETE"}
-            isEditable={isRemoteCompleted}
             buttonOnClickHref="/agencies-settings/on-site"
             titleIconClass="fr-icon-home-4-fill"
-            disabled={!isRemoteCompleted}
           >
             <p className="md:w-4/5">
               Vous avez des collaborateurs qui font des accompagnements en
@@ -65,7 +56,6 @@ const AgenciesSettingsPage = () => {
         {!isGestionnaireMaisonMereAAP && organism?.isRemote === false && (
           <EnhancedSectionCard
             title="Accompagnement en présentiel"
-            status={isOnSiteCompleted ? "COMPLETED" : "TO_COMPLETE"}
             isEditable={false}
             buttonOnClickHref="/agencies-settings/on-site"
             titleIconClass="fr-icon-home-4-fill"
@@ -76,42 +66,43 @@ const AgenciesSettingsPage = () => {
           </EnhancedSectionCard>
         )}
 
-         {isGestionnaireMaisonMereAAP ? (
+        {isGestionnaireMaisonMereAAP ? (
           <EnhancedSectionCard
-          title="Comptes collaborateurs"
-          isEditable={isCollaboratorsEditable}
-          disabled={!isCollaboratorsEditable}
-          buttonOnClickHref="/agencies-settings/collaborators"
-          titleIconClass="fr-icon-team-fill"
-          CustomBadge={<div />}
-          status="TO_COMPLETE"
-          customButtonTitle="Ajouter"
-        >
-          <p className="md:w-4/5">
-            Vous avez besoin de collaborer à plusieurs sur la plateforme ?
-            Ajoutez des comptes collaborateurs pour que vos collaborateurs
-            puissent avoir accès à leurs candidatures.
-          </p>
-          {!isCollaboratorsEditable && (
-            <SmallNotice>
-              Vous pourrez ajouter des comptes collaborateurs une fois que vous
-              aurez complété les paramètres précédents.
-            </SmallNotice>
-          )}
-        </EnhancedSectionCard>
-         ) : (
+            title="Comptes collaborateurs"
+            isEditable={isCollaboratorsEditable}
+            disabled={!isCollaboratorsEditable}
+            buttonOnClickHref="/agencies-settings/collaborators"
+            titleIconClass="fr-icon-team-fill"
+            CustomBadge={<div />}
+            status="TO_COMPLETE"
+            customButtonTitle="Ajouter"
+          >
+            <p className="md:w-4/5">
+              Vous avez besoin de collaborer à plusieurs sur la plateforme ?
+              Ajoutez des comptes collaborateurs pour que vos collaborateurs
+              puissent avoir accès à leurs candidatures.
+            </p>
+            {!isCollaboratorsEditable && (
+              <SmallNotice>
+                Vous pourrez ajouter des comptes collaborateurs une fois que
+                vous aurez complété les paramètres précédents.
+              </SmallNotice>
+            )}
+          </EnhancedSectionCard>
+        ) : (
           <EnhancedSectionCard
-          title="Informations de connexion"
-          isEditable={false}
-          titleIconClass="fr-icon-team-fill"
-          CustomBadge={<div />}
-          status="COMPLETED"
-        >
-          <p className="md:w-4/5">
-            Ici les informations de connexion du collaborateur actuellement connecté
-          </p>
-        </EnhancedSectionCard>
-         )}
+            title="Informations de connexion"
+            isEditable={false}
+            titleIconClass="fr-icon-team-fill"
+            CustomBadge={<div />}
+            status="COMPLETED"
+          >
+            <p className="md:w-4/5">
+              Ici les informations de connexion du collaborateur actuellement
+              connecté
+            </p>
+          </EnhancedSectionCard>
+        )}
       </div>
     </div>
   );
