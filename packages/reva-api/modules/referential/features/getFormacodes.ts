771accde9b1ca6b7fb926a0985a53b868e8b5541
Florian Bonniec <f.bonniec@me.com>
2024-09-09 15:18:01 +0200

    fix(api): improved getFormacodes feature

--- packages/reva-api/modules/referential/features/getFormacodes.ts
+++ packages/reva-api/modules/referential/features/getFormacodes.ts
@@ -6,23 +6,14 @@ type Formacode = {
   parentCode?: string;
 };
 
-let formacodes: Formacode[];
-
-async function loadFormacodes() {
+export async function getFormacodes(): Promise<Formacode[]> {
   const codes = await prismaClient.formacode.findMany();
-  formacodes = codes.map((formacode) => ({
+
+  const formacodes: Formacode[] = codes.map((formacode) => ({
     code: formacode.code,
     label: formacode.label,
     parentCode: formacode.parentCode || undefined,
   }));
-}
-
-loadFormacodes();
-
-export function getFormacodes(): Formacode[] {
-  if (!formacodes) {
-    throw new Error("formacodes has not been load");
-  }
 
   return formacodes;
 }
