4de606b6a435cb81251cd98e9d92416635742584
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-09-10 15:39:52 +0200

    fix(candidate): stubbed missing keycloak call in cypress tests and added a mock tokens cookie

--- packages/reva-candidate/cypress/support/commands.js
+++ packages/reva-candidate/cypress/support/commands.js
@@ -12,9 +12,23 @@ Cypress.Commands.add("auth", () => {
   cy.intercept("POST", "**/realms/reva-app/protocol/openid-connect/token", {
     fixture: "auth/candidate-token.json",
   });
+
+  cy.intercept("GET", "**/realms/reva-app/protocol/openid-connect/auth*", {
+    headers: {
+      Location: `${
+        Cypress.config().baseUrl
+      }silent-check-sso.html#error=login_required&state=e4d54c2f-0eaa-490a-bce2-3fbad7ee5780&iss=https%3A%2F%2Fauth.reva.incubateur.net%2Frealms%2Freva-app`,
+    },
+    statusCode: 302,
+  });
 });
 
 Cypress.Commands.add("login", (config = { token: "abc" }, options) => {
+  cy.setCookie(
+    "tokens",
+    "%7B%22accessToken%22%3A%22eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c%22%2C%22idToken%22%3A%22eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c%22%2C%22refreshToken%22%3A%22eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c%22%7D", //mock token
+  );
+
   cy.auth();
   if (config.token) {
     cy.visit(`/login?token=${config.token}`, options);
