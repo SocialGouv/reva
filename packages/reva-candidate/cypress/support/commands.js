27b3a9de6cb351b561d5b0a0a89fba9871c4aab1
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-09-10 14:28:51 +0200

    chore(candidate): stub code for cypress tests auth

--- packages/reva-candidate/cypress/support/commands.js
+++ packages/reva-candidate/cypress/support/commands.js
@@ -1,3 +1,24 @@
+Cypress.Commands.add("auth", () => {
+  cy.intercept(
+    "**/realms/reva-app/protocol/openid-connect/3p-cookies/step1.html",
+    {
+      fixture: "auth/step1.html",
+    },
+  );
+  cy.intercept("GET", "**/candidate/silent-check-sso.html", {
+    fixture: "auth/silent-check-sso.html",
+  });
+
+  cy.intercept("POST", "**/realms/reva-app/protocol/openid-connect/token", {
+    fixture: "auth/candidate-token.json",
+  });
+});
+
 Cypress.Commands.add("login", (config = { token: "abc" }, options) => {
-  cy.visit(`/login/?token=${config.token}`, options);
+  cy.auth();
+  if (config.token) {
+    cy.visit(`/login?token=${config.token}`, options);
+  } else {
+    cy.visit(`/login`, options);
+  }
 });
