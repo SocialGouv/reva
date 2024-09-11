f80c4c151c47b4a1a665f5104daa094f0e771058
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-09-11 13:16:38 +0200

    chore(candidate): fixed candidacy submission cypress tests

--- packages/reva-candidate/cypress/e2e/submission.cy.js
+++ packages/reva-candidate/cypress/e2e/submission.cy.js
@@ -3,14 +3,18 @@ import { stubMutation, stubQuery } from "../utils/graphql";
 context("Submission", () => {
   it("log on a submitted project", function () {
     cy.intercept("POST", "/api/graphql", (req) => {
-      stubQuery(req, "getDepartments", "departments.json");
-      stubMutation(req, "candidate_login", "candidate2-submitted.json");
-      stubQuery(req, "getReferential", "referential.json");
+      stubMutation(req, "candidate_login", "candidate_login.json");
+      stubQuery(
+        req,
+        "candidate_getCandidateWithCandidacy",
+        "candidate2-submitted.json",
+      );
+
       stubQuery(req, "activeFeaturesForConnectedUser", "features.json");
     });
     cy.login();
     cy.wait("@candidate_login");
-    cy.wait("@getReferential");
+    cy.wait("@candidate_getCandidateWithCandidacy");
     cy.wait("@activeFeaturesForConnectedUser");
 
     cy.get('[data-test="project-submitted-label"');
@@ -18,14 +22,18 @@ context("Submission", () => {
 
   it("log on a project taken over", function () {
     cy.intercept("POST", "/api/graphql", (req) => {
-      stubQuery(req, "getDepartments", "departments.json");
-      stubMutation(req, "candidate_login", "candidate2-taken-over.json");
-      stubQuery(req, "getReferential", "referential.json");
+      stubMutation(req, "candidate_login", "candidate_login.json");
+      stubQuery(
+        req,
+        "candidate_getCandidateWithCandidacy",
+        "candidate2-taken-over",
+      );
+
       stubQuery(req, "activeFeaturesForConnectedUser", "features.json");
     });
     cy.login();
     cy.wait("@candidate_login");
-    cy.wait("@getReferential");
+    cy.wait("@candidate_getCandidateWithCandidacy");
     cy.wait("@activeFeaturesForConnectedUser");
 
     cy.get('[data-test="project-submitted-label"');
