f1979c810d52f2077d1a0906c9905bfbbd4f95d4
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-09-11 11:03:12 +0200

    chore(canidate): fixed experiences cypress test

--- packages/reva-candidate/cypress/e2e/experiences.cy.js
+++ packages/reva-candidate/cypress/e2e/experiences.cy.js
@@ -9,20 +9,20 @@ import { stubMutation, stubQuery } from "../utils/graphql";
 context("Experiences", () => {
   it("add and edit an experience", function () {
     cy.intercept("POST", "/api/graphql", (req) => {
-      stubQuery(req, "getDepartments", "departments.json");
-      stubMutation(
+      stubMutation(req, "candidate_login", "candidate_login.json");
+      stubQuery(
         req,
-        "candidate_login",
+        "candidate_getCandidateWithCandidacy",
         "candidate1-certification-and-goals-selected.json",
       );
-      stubQuery(req, "getReferential", "referential.json");
+
       stubQuery(req, "add_experience", "added-experience1.json");
       stubQuery(req, "update_experience", "updated-experience2.json");
       stubQuery(req, "activeFeaturesForConnectedUser", "features.json");
     });
     cy.login();
     cy.wait("@candidate_login");
-    cy.wait("@getReferential");
+    cy.wait("@candidate_getCandidateWithCandidacy");
     cy.wait("@activeFeaturesForConnectedUser");
 
     cy.get('[data-test="timeline-add-experience"]').click();
@@ -31,8 +31,17 @@ context("Experiences", () => {
     cy.get("[name='duration']").select("betweenOneAndThreeYears");
     cy.get("textarea[name='description']").type(experienceDescription1);
 
+    cy.intercept("POST", "/api/graphql", (req) => {
+      stubQuery(
+        req,
+        "candidate_getCandidateWithCandidacy",
+        "candidate1-certification-and-goals-selected-with-1-experience.json",
+      );
+    });
+
     cy.get('[data-test="project-experience-add"]').click();
     cy.wait("@add_experience");
+    cy.wait("@candidate_getCandidateWithCandidacy");
 
     cy.get('[data-test="timeline-experiences-list"] > li')
       .eq(0)
@@ -54,7 +63,19 @@ context("Experiences", () => {
     cy.get("textarea[name='description']").type(
       `{selectAll}${experienceDescription2}`,
     );
-    cy.get('[data-test="project-experience-save"]').click();
+
+    cy.intercept("POST", "/api/graphql", (req) => {
+      stubQuery(
+        req,
+        "candidate_getCandidateWithCandidacy",
+        "candidate1-certification-and-goals-selected-with-experiences-updated.json",
+      );
+    });
+
+    cy.get('[data-test="project-experience-update"]').click();
+
+    cy.wait("@update_experience");
+    cy.wait("@candidate_getCandidateWithCandidacy");
 
     cy.get('[data-test="timeline-experiences-list"] > li')
       .eq(0)
