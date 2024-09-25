d0359a841096af73538b161efb9a10fa321ba2c1
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-09-25 15:14:00 +0200

    feat(api): added a TYPE_ACCOMPAGNEMENT_CANDIDAT_FEATURE

---
+++ packages/reva-api/prisma/migrations/20240925125125_add_feature_type_accompagnement_candidat/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    "features" ("key", "label")
+VALUES
+    (
+        'TYPE_ACCOMPAGNEMENT_CANDIDAT',
+        'Choix du type d''accompagnement pour le candat (accompagné ou autonome)'
+    );
