c8d911317e4163a123731096c22d818ab8345ff3
ThomasDos <dosanjos.thomas@gmail.com>
2024-09-25 15:06:05 +0200

    chore(candidate): add missing title and meta description to candidate head

--- packages/reva-candidate/src/components/dsfr/DsfrHead.tsx
+++ packages/reva-candidate/src/components/dsfr/DsfrHead.tsx
@@ -1,7 +1,7 @@
-import { getAssetUrl } from "@codegouvfr/react-dsfr/tools/getAssetUrl";
 import AppleTouchIcon from "@codegouvfr/react-dsfr/dsfr/favicon/apple-touch-icon.png";
-import FaviconSvg from "@codegouvfr/react-dsfr/dsfr/favicon/favicon.svg";
 import FaviconIco from "@codegouvfr/react-dsfr/dsfr/favicon/favicon.ico";
+import FaviconSvg from "@codegouvfr/react-dsfr/dsfr/favicon/favicon.svg";
+import { getAssetUrl } from "@codegouvfr/react-dsfr/tools/getAssetUrl";
 
 export function DsfrHead() {
   return (
@@ -12,6 +12,11 @@ export function DsfrHead() {
         rel="shortcut icon"
         href={getAssetUrl(FaviconIco)}
         type="image/x-icon"
+      />
+      <title>France VAE | Bienvenue sur le portail de la VAE</title>
+      <meta
+        name="description"
+        content="Découvrez la version beta du portail officiel du service public de la Validation des Acquis de L'Expérience."
       />
     </>
   );
