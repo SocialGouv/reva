e47c0e5ced9f8af5e20613c67dadeaff35b2626d
ThomasDos <dosanjos.thomas@gmail.com>
2024-09-27 15:45:13 +0200

    chore(candidate): hide foreign languages' label if undefined

--- packages/reva-candidate/src/components/legacy/organisms/DffSummary/components/CertificationSection.tsx
+++ packages/reva-candidate/src/components/legacy/organisms/DffSummary/components/CertificationSection.tsx
@@ -64,18 +64,22 @@ export default function CertificationSection({
           <p className="font-medium">{option}</p>
         </>
       )}
-      <div className="flex gap-2 mb-4">
-        <div className="flex flex-col flex-1">
-          <p className="mb-0">Langue vivante 1 :</p>
-          <p className="mb-0 font-medium">{firstForeignLanguage}</p>
+      {(firstForeignLanguage || secondForeignLanguage) && (
+        <div className="flex gap-2 mb-4">
+          {firstForeignLanguage && (
+            <div className="flex flex-col flex-1">
+              <p className="mb-0">Langue vivante 1 :</p>
+              <p className="mb-0 font-medium">{firstForeignLanguage}</p>
+            </div>
+          )}
+          {secondForeignLanguage && (
+            <div className="flex flex-col flex-1">
+              <p className="mb-0">Langue vivante 2 :</p>
+              <p className="mb-0 font-medium">{secondForeignLanguage}</p>
+            </div>
+          )}
         </div>
-        {secondForeignLanguage && (
-          <div className="flex flex-col flex-1">
-            <p className="mb-0">Langue vivante 2 :</p>
-            <p className="mb-0 font-medium">{secondForeignLanguage}</p>
-          </div>
-        )}
-      </div>
+      )}
       <CallOut>
         {isCertificationPartial
           ? "Un ou plusieurs bloc(s) de compétences visé(s)"
