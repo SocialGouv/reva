3204efd993a7963f7ce3812478e5664ad5860b1a
Cédric Soulas <soulascedric@gmail.com>
2024-10-02 15:09:30 +0200

    attempt to convert search maison mere aap to raw sql (unfinished)

--- packages/reva-api/modules/organism/features/getMaisonMereAAPs.ts
+++ packages/reva-api/modules/organism/features/getMaisonMereAAPs.ts
@@ -3,69 +3,8 @@ import {
   Prisma,
   type StatutValidationInformationsJuridiquesMaisonMereAAP,
 } from "@prisma/client";
-
 import { prismaClient } from "../../../prisma/client";
 import { processPaginationInfo } from "../../shared/list/pagination";
-
-export const buildMaisonMereFilters: (
-  searchFilter: string,
-) => Prisma.MaisonMereAAPWhereInput = (searchFilter) => {
-  const containsFilter = (field: string) => ({
-    [field]: { contains: searchFilter, mode: "insensitive" },
-  });
-
-  const accountContainsFilter = [
-    containsFilter("firstname"),
-    containsFilter("lastname"),
-    containsFilter("email"),
-  ];
-
-  const filtersAccount: Prisma.MaisonMereAAPWhereInput = {
-    gestionnaire: {
-      OR: accountContainsFilter,
-    },
-  };
-
-  const filtersLegalInformation: Prisma.MaisonMereAAPWhereInput = {
-    maisonMereAAPLegalInformationDocuments: {
-      OR: [
-        containsFilter("managerFirstname"),
-        containsFilter("managerLastname"),
-      ],
-    },
-  };
-
-  const filterOrganisms: Prisma.MaisonMereAAPWhereInput = {
-    organismes: {
-      some: {
-        OR: [containsFilter("contactAdministrativeEmail")],
-      },
-    },
-  };
-
-  const filterOrganismAccounts: Prisma.MaisonMereAAPWhereInput = {
-    organismes: {
-      some: {
-        accounts: {
-          some: {
-            OR: accountContainsFilter,
-          },
-        },
-      },
-    },
-  };
-
-  return {
-    OR: [
-      containsFilter("raisonSociale"),
-      containsFilter("siret"),
-      filtersAccount,
-      filtersLegalInformation,
-      filterOrganisms,
-      filterOrganismAccounts,
-    ],
-  };
-};
 
 export const getMaisonMereAAPs = async ({
   limit = 10,
@@ -78,62 +17,98 @@ export const getMaisonMereAAPs = async ({
   searchFilter?: string;
   legalValidationStatus?: StatutValidationInformationsJuridiquesMaisonMereAAP;
 }): Promise<PaginatedListResult<MaisonMereAAP>> => {
-  const orderBy: Prisma.MaisonMereAAPOrderByWithRelationInput[] =
-    legalValidationStatus === "EN_ATTENTE_DE_VERIFICATION"
-      ? [
-          { maisonMereAAPLegalInformationDocuments: { createdAt: "desc" } },
-          { raisonSociale: "asc" },
-        ]
-      : [{ createdAt: "desc" }, { raisonSociale: "asc" }];
-
-  const queryMaisonMereAAPs: Prisma.MaisonMereAAPFindManyArgs = {
-    orderBy,
-    take: limit,
-    skip: offset,
-  };
-
-  const queryCount: Prisma.MaisonMereAAPCountArgs = {};
-
-  if (searchFilter) {
-    const words = searchFilter.split(/\s+/);
-    const filters = { AND: words.map(buildMaisonMereFilters) };
-
-    queryMaisonMereAAPs.where = {
-      ...queryMaisonMereAAPs.where,
-      ...filters,
-    };
-
-    queryCount.where = {
-      ...queryCount.where,
-      ...filters,
-    };
-  }
+  const whereClauses: string[] = [];
+  const whereParams: any[] = [];
 
   if (legalValidationStatus) {
-    queryMaisonMereAAPs.where = {
-      ...queryMaisonMereAAPs.where,
-      AND: [
-        {
-          statutValidationInformationsJuridiquesMaisonMereAAP:
-            legalValidationStatus,
-        },
-      ],
-    };
-
-    queryCount.where = {
-      ...queryCount.where,
-      AND: [
-        {
-          statutValidationInformationsJuridiquesMaisonMereAAP:
-            legalValidationStatus,
-        },
-      ],
-    };
+    whereParams.push(legalValidationStatus);
+    whereClauses.push(
+      `maison_mere_aap.statut_validation_informations_juridiques_maison_mere_aap = $${whereParams.length}`,
+    );
   }
 
-  const maisonMereAAPs =
-    await prismaClient.maisonMereAAP.findMany(queryMaisonMereAAPs);
-  const count = await prismaClient.maisonMereAAP.count(queryCount);
+  if (searchFilter) {
+    const words = searchFilter.trim().split(/\s+/);
+    words.forEach((word) => {
+      const conditions: string[] = [];
+      const paramValue = `%${word}%`;
+
+      const fields = {
+        maison_mere_aap: ["raison_sociale", "siret"],
+        account: ["firstname", "lastname", "email"],
+        maison_mere_aap_legal_information_documents: [
+          "manager_firstname",
+          "manager_lastname",
+        ],
+        organism: ["contact_administrative_email"],
+        // TODO:
+        //organism > account: ["firstname", "lastname", "email"],
+      };
+
+      for (const [table, columns] of Object.entries(fields)) {
+        columns.forEach((column) => {
+          whereParams.push(paramValue);
+          conditions.push(
+            `unaccent(${table}."${column}") ILIKE unaccent($${whereParams.length})`,
+          );
+        });
+      }
+
+      whereClauses.push(`(${conditions.join(" OR ")})`);
+    });
+  }
+
+  const whereClause =
+    whereClauses.length > 0 ? `WHERE ${whereClauses.join(" AND ")}` : "";
+
+  let orderByClause = "";
+  if (legalValidationStatus === "EN_ATTENTE_DE_VERIFICATION") {
+    orderByClause =
+      "ORDER BY maison_mere_aap_legal_information_documents.created_at DESC, maison_mere_aap.raison_sociale ASC";
+  } else {
+    orderByClause =
+      "ORDER BY maison_mere_aap.created_at DESC, maison_mere_aap.raison_sociale ASC";
+  }
+
+  const mainQueryParams = [...whereParams, limit, offset];
+  const limitParamIndex = whereParams.length + 1;
+  const offsetParamIndex = whereParams.length + 2;
+
+  // TODO LEFT JOIN account > organism
+  const sql = Prisma.sql`
+      SELECT maison_mere_aap.*
+      FROM maison_mere_aap
+               LEFT JOIN account ON maison_mere_aap.gestionnaire_account_id = account.id
+               LEFT JOIN maison_mere_aap_legal_information_documents
+                         ON maison_mere_aap_legal_information_documents.maison_mere_aap_id = maison_mere_aap.id
+               LEFT JOIN organism ON organism."maison_mere_aap_id" = maison_mere_aap.id ON account.organism_id = organism.id ${whereClause}
+          ${orderByClause}
+      LIMIT $${limitParamIndex}
+    OFFSET $${offsetParamIndex}
+  `;
+
+  const maisonMereAAPs = await prismaClient.$queryRaw<MaisonMereAAP[]>(
+    sql,
+    ...mainQueryParams,
+  );
+
+  // TODO LEFT JOIN account > organism
+  const countSql = Prisma.sql`
+      SELECT COUNT(DISTINCT maison_mere_aap.id) as total_count
+      FROM maison_mere_aap
+               LEFT JOIN account ON maison_mere_aap.gestionnaire_account_id = account.id
+               LEFT JOIN maison_mere_aap_legal_information_documents
+                         ON maison_mere_aap_legal_information_documents.maison_mere_aap_id = maison_mere_aap.id
+               LEFT JOIN organism ON organism.maison_mere_aap_id = maison_mere_aap.id
+          ${whereClause}
+  `;
+
+  const countResult = await prismaClient.$queryRaw<{ total_count: number }[]>(
+    countSql,
+    ...whereParams,
+  );
+  const count = countResult[0]?.total_count ?? 0;
+
   return {
     rows: maisonMereAAPs,
     info: processPaginationInfo({
