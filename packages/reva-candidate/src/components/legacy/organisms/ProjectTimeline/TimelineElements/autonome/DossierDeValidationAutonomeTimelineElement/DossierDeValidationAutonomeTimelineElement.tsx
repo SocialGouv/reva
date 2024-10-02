b054cf0b5604b6f5369569756487c5368299b9ff
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-10-02 13:56:05 +0200

    feat(candidate): added a readyForJuryEstimatedAt message in the dossier de validation autonome timeline element

--- packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/DossierDeValidationAutonomeTimelineElement/DossierDeValidationAutonomeTimelineElement.tsx
+++ packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/DossierDeValidationAutonomeTimelineElement/DossierDeValidationAutonomeTimelineElement.tsx
@@ -1,14 +1,46 @@
-import { useCandidacy } from "@/components/candidacy/candidacy.context";
+import { useGraphQlClient } from "@/components/graphql/graphql-client/GraphqlClient";
 import {
   TimelineElement,
   TimeLineElementStatus,
 } from "@/components/legacy/molecules/Timeline/Timeline";
+import { graphql } from "@/graphql/generated";
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import { Notice } from "@codegouvfr/react-dsfr/Notice";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
 import { useRouter } from "next/navigation";
 
+const getCandidateQuery = graphql(`
+  query getCandidateWithCandidacyForDossierDeValidationAutonomeTimelineElement {
+    candidate_getCandidateWithCandidacy {
+      id
+      candidacy {
+        id
+        readyForJuryEstimatedAt
+        status
+      }
+    }
+  }
+`);
+
 export const DossierDeValidationAutonomeTimelineElement = () => {
-  const { candidacy } = useCandidacy();
   const router = useRouter();
+  const { graphqlClient } = useGraphQlClient();
+
+  const { data: getCandidateResponse } = useQuery({
+    queryKey: [
+      "candidate",
+      "getCandidateWithCandidacyForDossierDeValidationAutonomeTimelineElement",
+    ],
+    queryFn: () => graphqlClient.request(getCandidateQuery),
+  });
+
+  const candidacy =
+    getCandidateResponse?.candidate_getCandidateWithCandidacy?.candidacy;
+
+  if (!candidacy) {
+    return null;
+  }
 
   let status: TimeLineElementStatus = "disabled";
 
@@ -25,6 +57,24 @@ export const DossierDeValidationAutonomeTimelineElement = () => {
       data-test="dossier-de-validation-autonome-timeline-element"
       description="Ce dossier permet de justifier de vos expériences et compétences lors de votre passage devant le jury."
     >
+      {status == "active" && !!candidacy.readyForJuryEstimatedAt && (
+        <Notice
+          title={
+            <span className=" text-dsfrGray-500 italic font-normal">
+              <p className="inline text-sm">
+                Vous avez renseigné une date de dépôt prévisionnelle, le{" "}
+                {format(candidacy.readyForJuryEstimatedAt, "dd/MM/yyyy")}.
+              </p>
+              <p className="ml-8 text-sm">
+                Assurez-vous de bien transmettre votre dossier de validation à
+                votre certificateur.
+              </p>
+            </span>
+          }
+          className="pt-0 bg-transparent  [&_.fr-container]:pl-0"
+          classes={{ title: "text-dsfrGray-500" }}
+        />
+      )}
       <Button
         data-test="dossier-de-validation-autonome-timeline-element-update-button"
         priority={status === "active" ? "primary" : "secondary"}
