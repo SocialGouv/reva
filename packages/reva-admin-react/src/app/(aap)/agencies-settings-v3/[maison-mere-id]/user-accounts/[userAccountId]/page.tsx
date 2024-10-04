b3fd46796b6c38e9ed394da03e73688f7412111b
ThomasDos <dosanjos.thomas@gmail.com>
2024-10-04 11:07:23 +0200

    fix(admin): display the view of HeadAgencyUserAccount if isAdmin

--- packages/reva-admin-react/src/app/(aap)/agencies-settings-v3/[maison-mere-id]/user-accounts/[userAccountId]/page.tsx
+++ packages/reva-admin-react/src/app/(aap)/agencies-settings-v3/[maison-mere-id]/user-accounts/[userAccountId]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { useAuth } from "@/components/auth/auth";
-import HeadAgencyUserAccount from "../_components/head-agency-user-account/HeadAgencyUserAccount";
-import AgencyUserAccount from "../_components/agency-user-account/AgencyUserAccount";
 import { Suspense } from "react";
+import AgencyUserAccount from "../_components/agency-user-account/AgencyUserAccount";
+import HeadAgencyUserAccount from "../_components/head-agency-user-account/HeadAgencyUserAccount";
 
 const UpdateUserAccountPage = () => {
-  const { isGestionnaireMaisonMereAAP } = useAuth();
-  return isGestionnaireMaisonMereAAP ? (
+  const { isGestionnaireMaisonMereAAP, isAdmin } = useAuth();
+  return isGestionnaireMaisonMereAAP || isAdmin ? (
     <HeadAgencyUserAccount />
   ) : (
     <Suspense fallback={<p>Chargement...</p>}>
