d746d8c67465bcb248295e84ab28ebfc02bcd50f
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-10-11 15:14:56 +0200

    fix(api): only admin or manager profiles can access a funding request or a payment request from a candidacy

--- packages/reva-api/modules/finance/unifvae/security.ts
+++ packages/reva-api/modules/finance/unifvae/security.ts
@@ -1,7 +1,13 @@
-import { isAdminOrCandidacyCompanion } from "../../shared/security/presets";
+import {
+  isAdminOrCandidacyCompanion,
+  isAdminOrManager,
+} from "../../shared/security/presets";
 
 export const resolversSecurityMap = {
   "Mutation.candidacy_createFundingRequestUnifvae": isAdminOrCandidacyCompanion,
   "Mutation.candidacy_createOrUpdatePaymentRequestUnifvae":
     isAdminOrCandidacyCompanion,
+
+  "Candidacy.fundingRequestUnifvae": isAdminOrManager,
+  "Candidacy.paymentRequestUnifvae": isAdminOrManager,
 };
