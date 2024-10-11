bc760c1f943670ebaee3d8947683a15ae9de67aa
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-10-11 10:16:42 +0200

    feat(website): added typeAccompagnement to candidate registration form

--- packages/reva-website/src/pages/inscription-candidat/index.tsx
+++ packages/reva-website/src/pages/inscription-candidat/index.tsx
@@ -16,8 +16,10 @@ import { GRAPHQL_API_URL } from "@/config/config";
 import { graphql } from "@/graphql/generated";
 import { Certification } from "@/graphql/generated/graphql";
 import { isUUID } from "@/utils";
-import Alert from "@codegouvfr/react-dsfr/Alert";
-import Notice from "@codegouvfr/react-dsfr/Notice";
+import { Alert } from "@codegouvfr/react-dsfr/Alert";
+import CallOut from "@codegouvfr/react-dsfr/CallOut";
+import { Notice } from "@codegouvfr/react-dsfr/Notice";
+import { RadioButtons } from "@codegouvfr/react-dsfr/RadioButtons";
 import request from "graphql-request";
 import Head from "next/head";
 import Link from "next/link";
@@ -67,15 +69,26 @@ const OrientationCandidatPage = () => {
     "id" | "label" | "codeRncp" | "typeDiplome"
   > | null>(null);
 
+  const [typeAccompagnement, setTypeAccompagnement] = useState<
+    "ACCOMPAGNE" | "AUTONOME" | null
+  >(null);
+
   const defaultAutocompleteLabel = certification
     ? undefined
     : (searchText as string | undefined);
 
   const handleFormSubmit = async (form: CandidateRegistrationFormSchema) => {
+    const actualTypeAccomagnement = isFeatureActive(
+      "TYPE_ACCOMPAGNEMENT_CANDIDAT",
+    )
+      ? typeAccompagnement
+      : "AUTONOME";
     await request(GRAPHQL_API_URL, askForRegistrationMutation, {
       candidate: {
         ...form,
-        typeAccompagnement: "ACCOMPAGNE",
+        typeAccompagnement: actualTypeAccomagnement as
+          | "AUTONOME"
+          | "ACCOMPAGNE",
         certificationId: certificationId as string,
       },
     });
@@ -132,7 +145,14 @@ const OrientationCandidatPage = () => {
           <p className="text-dsfrGray-mentionGrey mb-8">
             Tous les champs sont obligatoires
           </p>
-          <div className="flex flex-col ml-0 gap-4">
+          {isFeatureActive("TYPE_ACCOMPAGNEMENT_CANDIDAT") && (
+            <p className="mb-12">
+              Recherchez le diplôme que vous aimeriez obtenir en réalisant un
+              parcours VAE. Vous avez la possibilité de réaliser ce diplôme avec
+              un accompagnateur ou seul, en toute autonomie.
+            </p>
+          )}
+          <div className="flex flex-col ml-0 gap-12">
             <fieldset className="mb-4 max-w-lg">
               <CertificateAutocompleteDsfr
                 defaultLabel={defaultAutocompleteLabel}
@@ -192,61 +212,114 @@ const OrientationCandidatPage = () => {
                     />
                   )}
                 </div>
-                <h2 className="text-2xl font-bold mt-4 mb-0">
-                  Quel candidat êtes vous ?
-                </h2>
-                <div className="flex flex-col gap-4 lg:flex-row lg:gap-6">
-                  <span className="max-w-lg basis-1/2">
-                    <CandidateTypologySelect
-                      candidateTypology={candidateTypology}
-                      onChange={setCandidateTypology}
-                    />
-                  </span>
-                  {invalidTypology && (
-                    <Notice
-                      data-testid="candidate-typology-error-panel"
-                      className="basis-1/2"
-                      title={
-                        <span>
-                          Votre situation ne vous permet pas de réaliser un
-                          parcours VAE sur{" "}
-                          <a href="https://vae.gouv.fr/" target="_blank">
-                            vae.gouv.fr
-                          </a>
-                          . Pour savoir si d’autres options existent, vous
-                          pouvez contacter dès à présent un{" "}
-                          <a
-                            href="https://vae.gouv.fr/savoir-plus/articles/liste-prc/"
-                            target="_blank"
-                          >
-                            point relais conseil
-                          </a>{" "}
-                          ou un{" "}
-                          <a
-                            href="https://mon-cep.org/#trouver"
-                            target="_blank"
-                          >
-                            conseiller en évolution professionnelle
-                          </a>
-                          .
-                        </span>
-                      }
-                    />
-                  )}
-                  {validTypology && candidacyCreationDisabled && (
-                    <Alert
-                      severity="warning"
-                      data-testid="candidate-typology-error-panel"
-                      className="basis-1/2"
-                      title={
-                        <p className="font-normal">
-                          Le dépôt de nouvelles candidatures est temporairement
-                          indisponible. Nous vous remercions de votre patience
-                          et nous excusons pour tout désagrément.
-                        </p>
-                      }
-                    />
-                  )}
+                {isFeatureActive("TYPE_ACCOMPAGNEMENT_CANDIDAT") && (
+                  <div>
+                    <h2>Modalités de parcours</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                      <RadioButtons
+                        className="inline"
+                        legend="Que souhaitez-vous faire pour ce parcours ? "
+                        options={[
+                          {
+                            label:
+                              "Je souhaite réaliser ma VAE avec un accompagnateur",
+                            nativeInputProps: {
+                              value: "ACCOMPAGNE",
+                              onChange: () =>
+                                setTypeAccompagnement("ACCOMPAGNE"),
+                            },
+                          },
+                          {
+                            label: "Je souhaite réaliser ma VAE en autonomie",
+                            nativeInputProps: {
+                              value: "AUTONOME",
+                              onChange: () => setTypeAccompagnement("AUTONOME"),
+                            },
+                          },
+                        ]}
+                      />
+                      <CallOut
+                        title="À quoi sert un accompagnateur ?"
+                        classes={{ title: "pb-2" }}
+                      >
+                        C’est un expert de la VAE qui vous aide à chaque grande
+                        étape de votre parcours : rédaction du dossier de
+                        faisabilité, communication avec le certificateur,
+                        préparation au passage devant le jury, etc.
+                        <br />
+                        <br />
+                        <strong>Bon à savoir :</strong> ces accompagnements
+                        peuvent être en partie financés par votre{" "}
+                        <Link
+                          href="https://www.moncompteformation.gouv.fr/espace-public/consulter-mes-droits-formation"
+                          target="_blank"
+                        >
+                          Compte Personnel de Formation
+                        </Link>
+                        . En revanche, si vous faites ce parcours en autonomie,
+                        les frais de jury seront à votre charge.
+                      </CallOut>
+                    </div>
+                  </div>
+                )}
+                <div className="flex flex-col">
+                  <h2 className="text-2xl font-bold mt-4 mb-0">
+                    Quel candidat êtes vous ?
+                  </h2>
+                  <div className="flex flex-col gap-4 lg:flex-row lg:gap-6">
+                    <span className="max-w-lg basis-1/2">
+                      <CandidateTypologySelect
+                        candidateTypology={candidateTypology}
+                        onChange={setCandidateTypology}
+                      />
+                    </span>
+                    {invalidTypology && (
+                      <Notice
+                        data-testid="candidate-typology-error-panel"
+                        className="basis-1/2"
+                        title={
+                          <span>
+                            Votre situation ne vous permet pas de réaliser un
+                            parcours VAE sur{" "}
+                            <a href="https://vae.gouv.fr/" target="_blank">
+                              vae.gouv.fr
+                            </a>
+                            . Pour savoir si d’autres options existent, vous
+                            pouvez contacter dès à présent un{" "}
+                            <a
+                              href="https://vae.gouv.fr/savoir-plus/articles/liste-prc/"
+                              target="_blank"
+                            >
+                              point relais conseil
+                            </a>{" "}
+                            ou un{" "}
+                            <a
+                              href="https://mon-cep.org/#trouver"
+                              target="_blank"
+                            >
+                              conseiller en évolution professionnelle
+                            </a>
+                            .
+                          </span>
+                        }
+                      />
+                    )}
+                    {validTypology && candidacyCreationDisabled && (
+                      <Alert
+                        severity="warning"
+                        data-testid="candidate-typology-error-panel"
+                        className="basis-1/2"
+                        title={
+                          <p className="font-normal">
+                            Le dépôt de nouvelles candidatures est
+                            temporairement indisponible. Nous vous remercions de
+                            votre patience et nous excusons pour tout
+                            désagrément.
+                          </p>
+                        }
+                      />
+                    )}
+                  </div>
                 </div>
               </>
             )}
