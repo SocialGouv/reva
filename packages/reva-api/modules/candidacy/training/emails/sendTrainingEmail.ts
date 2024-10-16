361befdf0544cc608e8c955432b3da90305c58b7
Cédric Soulas <soulascedric@gmail.com>
2024-10-16 15:07:48 +0200

    feat(api): update training email to improve wording and remove funding part

--- packages/reva-api/modules/candidacy/training/emails/sendTrainingEmail.ts
+++ packages/reva-api/modules/candidacy/training/emails/sendTrainingEmail.ts
@@ -7,27 +7,30 @@ export const sendTrainingEmail = async (email: string, token: string) => {
     mjml2html(
       templateMail({
         content: `
-          Vous vous êtes engagé·e dans une démarche de VAE.
+          Bonjour,
           <br />
           <br />
-          Après échanges avec votre Architecte Accompagnateur de Parcours, vous avez défini le parcours le plus adapté à votre situation et à votre projet.
+          Vous avez entrepris une démarche de VAE et, après avoir échangé avec votre accompagnateur, le parcours le plus adapté à vos besoins a été défini !
           <br />
           <br />
-          Il est désormais prêt.
+          Nous avons le plaisir de vous informer qu’il est maintenant prêt.
           <br />
           <br />
-          Nous vous invitons à le valider au plus vite. Il vous suffit de cliquer sur le bouton ci-dessous.
-          <br />
-          <br />
-          Ce lien est valable 4 jours. 
+          Nous vous invitons à le valider rapidement en cliquant sur le bouton ci-dessous. Ce lien sera actif pendant 4 jours.
         `,
         labelCTA: "Accéder à mon parcours pédagogique",
         url,
         bottomLine: `
-          Cela permettra à votre architecte accompagnateur de parcours de transmettre votre dossier au certificateur et de faire la demande de financement de votre projet.
+          Une fois validé, votre accompagnateur pourra transmettre votre dossier au certificateur afin d’étudier votre recevabilité. 
           <br />
           <br />
-          Nos équipes ont élaboré <a href="https://scribehow.com/shared/Parcours_candidat__vp9k4YzATvmheao9kAoKjw#6062f89d">un guide en ligne pour vous aider dans cette démarche</a>.
+          Pour vous accompagner dans cette étape, nous avons mis à votre disposition <a href="https://scribehow.com/shared/Validation_du_parcours_candidat__QHFNcjJ4T56E3cSYEphpYQ">un guide en ligne</a>.
+          <br />
+          <br />
+          N’hésitez pas à nous contacter si vous avez la moindre question.
+          <br />
+          <br />
+          Bien à vous,
           <br />
           <br />
           L'équipe France VAE
@@ -41,7 +44,7 @@ export const sendTrainingEmail = async (email: string, token: string) => {
     token,
     action: "login",
     htmlContent,
-    subject: "Votre parcours pédagogique est prêt pour validation",
+    subject: "Vous devez valider votre parcours pédagogique",
     app: "candidate",
   });
 };
