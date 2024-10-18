f57583431a93ea1042d17bce3530280de02580b9
ThomasDos <dosanjos.thomas@gmail.com>
2024-10-18 12:25:07 +0200

    chore(api): update wording in sendDFFNotificationToCandidateEmail

--- packages/reva-api/modules/feasibility/dematerialized-feasibility-file/emails/sendDFFNotificationToCandidateEmail.ts
+++ packages/reva-api/modules/feasibility/dematerialized-feasibility-file/emails/sendDFFNotificationToCandidateEmail.ts
@@ -11,12 +11,13 @@ export const sendDFFNotificationToCandidateEmail = ({
       templateMail({
         content: `
         <p>Bonjour,</p>
-        <p>Votre accompagnateur a terminé de remplir votre dossier de faisabilité. Connectez-vous à votre espace, prenez le temps de le lire avec soin, relevez les éventuels oublis ou modifications à faire, et envoyez-lui une attestation sur l’honneur pour valider votre dossier.</p>
+        <p>Votre accompagnateur a terminé de remplir votre dossier de faisabilité.</p>
+        <p>Connectez-vous à votre espace, prenez le temps de le lire avec soin, transmettez-lui les éventuels oublis ou modifications à faire, et, lorsque tout est bon pour vous, envoyez-lui une attestation sur l’honneur pour valider votre dossier !</p>
         <p>Pour vous aider, vous trouverez un modèle d’attestation sur l’honneur à remplir dans le résumé de votre candidature.</p>
         <p>1. Téléchargez le document ;</p>
         <p>2. Remplissez-le avec les informations appropriées ;</p>
         <p>3. Signez-le puis transmettez-le directement depuis votre espace candidat.</p>
-        <p>Vous pouvez retrouver votre dossier de faisabilité ainsi que le modèle de l’attestation sur l’honneur dans votre espace France VAE, dans la rubrique “Dossier de faisabilité” :</p>
+        <p>Vous pouvez retrouver votre dossier de faisabilité ainsi que le modèle de l’attestation sur l’honneur dans votre espace France VAE, dans la rubrique “Recevabilité” :</p>
         `,
         labelCTA: "Se connecter",
         url,
