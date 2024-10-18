d555c997a4b86a235416fd8af050ada11738098d
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-10-18 14:55:52 +0200

    feat(api): updated organism resolver access to check for ressource ownership instead of just roles

--- packages/reva-api/modules/organism/organism.security.ts
+++ packages/reva-api/modules/organism/organism.security.ts
@@ -1,6 +1,8 @@
 import { hasRole, whenHasRole } from "../shared/security/middlewares";
+import { whenHasRoleButNotOthers } from "../shared/security/middlewares/whenHasRoleButNotOthers";
 import { isAdmin } from "../shared/security/presets";
 import { isGestionnaireOfMaisonMereAAP } from "./security/isGestionnaireOfMaisonMereAAP.security";
+import { isOwnerOfOrganism } from "./security/isOwnerOfOrganism";
 
 export const resolversSecurityMap = {
   // cf https://the-guild.dev/graphql/tools/docs/resolvers-composition#supported-path-matcher-format
@@ -54,18 +56,42 @@ export const resolversSecurityMap = {
 
   "Organism.accounts": [
     hasRole(["admin", "gestion_maison_mere_aap", "manage_candidacy"]),
+    whenHasRole("gestion_maison_mere_aap", isGestionnaireOfMaisonMereAAP),
+    whenHasRoleButNotOthers(
+      "manage_candidacy",
+      ["gestion_maison_mere_aap"],
+      isOwnerOfOrganism,
+    ),
   ],
 
   "Organism.maisonMereAAP": [
     hasRole(["admin", "gestion_maison_mere_aap", "manage_candidacy"]),
+    whenHasRole("gestion_maison_mere_aap", isGestionnaireOfMaisonMereAAP),
+    whenHasRoleButNotOthers(
+      "manage_candidacy",
+      ["gestion_maison_mere_aap"],
+      isOwnerOfOrganism,
+    ),
   ],
 
   "MaisonMereAAP.gestionnaire": [
     hasRole(["admin", "gestion_maison_mere_aap", "manage_candidacy"]),
+    whenHasRole("gestion_maison_mere_aap", isGestionnaireOfMaisonMereAAP),
+    whenHasRoleButNotOthers(
+      "manage_candidacy",
+      ["gestion_maison_mere_aap"],
+      isOwnerOfOrganism,
+    ),
   ],
 
   "MaisonMereAAP.organisms": [
     hasRole(["admin", "gestion_maison_mere_aap", "manage_candidacy"]),
+    whenHasRole("gestion_maison_mere_aap", isGestionnaireOfMaisonMereAAP),
+    whenHasRoleButNotOthers(
+      "manage_candidacy",
+      ["gestion_maison_mere_aap"],
+      isOwnerOfOrganism,
+    ),
   ],
 
   "MaisonMereAAP.legalInformationDocuments": isAdmin,
