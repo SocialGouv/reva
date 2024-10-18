75d0a682fa9ea5223005d0f0bf997976353b5051
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-10-18 14:55:22 +0200

    feat(api): added a new security preset: isOwnerOfOrganism

---
+++ packages/reva-api/modules/organism/security/isOwnerOfOrganism.ts
@@ -0,0 +1,26 @@
+import { IFieldResolver, MercuriusContext } from "mercurius";
+import { isUserOwnerOfOrganism } from "../features/isUserOwnerOfOrganism";
+
+export const isOwnerOfOrganism =
+  (next: IFieldResolver<unknown>) =>
+  async (
+    root: any,
+    args: Record<string, any>,
+    context: MercuriusContext,
+    info: any,
+  ) => {
+    if (
+      !(await isUserOwnerOfOrganism({
+        userRoles: context.auth.userInfo.realm_access?.roles || [],
+        organismId:
+          args.organismId ||
+          args.data?.organismId ||
+          root.organismId ||
+          root.id,
+        userKeycloakId: context.auth.userInfo.sub,
+      }))
+    ) {
+      throw new Error("Vous n'êtes pas autorisé à accéder à cet organisme");
+    }
+    return next(root, args, context, info);
+  };
