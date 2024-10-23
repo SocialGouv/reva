5fc3f4f8c117b167b9995ffc1eb96b5867b3a495
Pierre A <pierre@avizou.eu>
2024-10-23 16:02:52 +0200

    feat(api):add feature flip to load Crisp widget in admin backoffice

---
+++ packages/reva-api/prisma/migrations/20241023153849_add_feature_show_crisp_in_admin/migration.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    "features" ("key", "label", "is_active")
+VALUES
+    (
+        'SHOW_CRISP_IN_ADMIN',
+        'Afficher Crisp dans l''interface d''administration AAP / Certificateur',
+        FALSE
+    );
