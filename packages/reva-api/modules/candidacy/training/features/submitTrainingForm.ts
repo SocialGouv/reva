ddffb680f0863948f7e966cc69d44be880e27a19
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-10-28 14:55:43 +0100

    chore(api): merged updateTrainingInformations into submitTrainingForm

--- packages/reva-api/modules/candidacy/training/features/submitTrainingForm.ts
+++ packages/reva-api/modules/candidacy/training/features/submitTrainingForm.ts
@@ -6,11 +6,11 @@ import {
 import { generateJwt } from "../../../candidate/auth.helper";
 import { sendTrainingEmail } from "../emails";
 import { existsCandidacyHavingHadStatus } from "./existsCandidacyHavingHadStatus";
-import { updateTrainingInformations } from "./updateTrainingInformations";
 import { updateCandidacyStatus } from "../../features/updateCandidacyStatus";
 import { getCandidateById } from "../../../candidate/features/getCandidateById";
 import { getCandidacy } from "../../features/getCandidacy";
 import { CANDIDACY_FINANCING_METHOD_OTHER_SOURCE_ID } from "../../../referential/referential.types";
+import { prismaClient } from "../../../../prisma/client";
 
 export const submitTraining = async ({
   candidacyId,
@@ -122,3 +122,91 @@ export const submitTraining = async ({
 
   return candidacy;
 };
+
+const updateTrainingInformations = async (params: {
+  candidacyId: string;
+  training: {
+    basicSkillIds: string[];
+    mandatoryTrainingIds: string[];
+    certificateSkills: string;
+    otherTraining: string;
+    individualHourCount: number;
+    collectiveHourCount: number;
+    additionalHourCount: number;
+    isCertificationPartial: boolean;
+    candidacyFinancingMethodIds: string[];
+    candidacyFinancingMethodOtherSourceText?: string;
+    estimatedCost?: number;
+  };
+}) =>
+  prismaClient.$transaction(async (tx) => {
+    await tx.basicSkillOnCandidacies.deleteMany({
+      where: {
+        candidacyId: params.candidacyId,
+      },
+    });
+    await tx.basicSkillOnCandidacies.createMany({
+      data: params.training.basicSkillIds.map((id) => ({
+        candidacyId: params.candidacyId,
+        basicSkillId: id,
+      })),
+    });
+    await tx.trainingOnCandidacies.deleteMany({
+      where: {
+        candidacyId: params.candidacyId,
+      },
+    });
+    await tx.trainingOnCandidacies.createMany({
+      data: params.training.mandatoryTrainingIds.map((id) => ({
+        candidacyId: params.candidacyId,
+        trainingId: id,
+      })),
+    });
+
+    await tx.candidacyOnCandidacyFinancingMethod.deleteMany({
+      where: {
+        candidacyId: params.candidacyId,
+      },
+    });
+    await tx.candidacyOnCandidacyFinancingMethod.createMany({
+      data: params.training.candidacyFinancingMethodIds.map((id) => ({
+        candidacyId: params.candidacyId,
+        candidacyFinancingMethodId: id,
+      })),
+    });
+
+    if (
+      params.training.candidacyFinancingMethodIds.find(
+        (id) => id === CANDIDACY_FINANCING_METHOD_OTHER_SOURCE_ID,
+      )
+    ) {
+      await tx.candidacyOnCandidacyFinancingMethod.update({
+        where: {
+          candidacyId_candidacyFinancingMethodId: {
+            candidacyId: params.candidacyId,
+            candidacyFinancingMethodId:
+              CANDIDACY_FINANCING_METHOD_OTHER_SOURCE_ID,
+          },
+        },
+        data: {
+          additionalInformation:
+            params.training.candidacyFinancingMethodOtherSourceText,
+        },
+      });
+    }
+
+    await tx.candidacy.update({
+      where: {
+        id: params.candidacyId,
+      },
+      data: {
+        certificateSkills: params.training.certificateSkills,
+        otherTraining: params.training.otherTraining,
+        individualHourCount: params.training.individualHourCount,
+        collectiveHourCount: params.training.collectiveHourCount,
+        additionalHourCount: params.training.additionalHourCount,
+        isCertificationPartial: params.training.isCertificationPartial,
+        estimatedCost: params.training.estimatedCost,
+      },
+    });
+  });
