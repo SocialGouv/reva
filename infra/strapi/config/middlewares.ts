42657c22dc8e48a76324ea15cd64a33f07498807
Pierre A <pierre@avizou.eu>
2024-10-29 15:07:55 +0100

    chore(strapi):update CSP for latest ckeditor plugin

--- infra/strapi/config/middlewares.ts
+++ infra/strapi/config/middlewares.ts
@@ -1,12 +1,23 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  // 'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "script-src": ["https://cdn.ckeditor.com"],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
