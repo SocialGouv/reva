145bf81e422c40057330dd4842b1dbcb678222e7
ThomasDos <dosanjos.thomas@gmail.com>
2024-10-31 15:26:01 +0100

    test(admin): add E2E tests for dematerialization eligibility workflow

---
+++ packages/reva-admin-react/cypress/e2e/candidacy/feasibility/dematerialized-feasibility-file/dematerialized-feasibility-file-eligibility.cy.ts
@@ -0,0 +1,250 @@
+import { stubQuery } from "../../../../utils/graphql";
+import {
+  DEFAULT_FEASIBILITY_FILE,
+  DF_FORMATED_DATE_6_MONTHS_AGO,
+  DF_FORMATED_DATE_6_MONTHS_FROM_NOW,
+} from "./dff-mocks";
+
+function visitFeasibilityEligibility(feasibility = DEFAULT_FEASIBILITY_FILE) {
+  cy.fixture("candidacy/candidacy-dff.json").then((candidacy) => {
+    cy.intercept("POST", "/api/graphql", (req) => {
+      stubQuery(
+        req,
+        "activeFeaturesForConnectedUser",
+        "features/active-features.json",
+      );
+      stubQuery(
+        req,
+        "getMaisonMereCGUQuery",
+        "account/head-agency-cgu-accepted.json",
+      );
+      stubQuery(
+        req,
+        "getOrganismForAAPVisibilityCheck",
+        "visibility/organism.json",
+      );
+      stubQuery(req, "getAccountInfo", "account/head-agency-info.json");
+      candidacy.data.getCandidacyById.feasibility = feasibility;
+      stubQuery(req, "getCandidacyByIdForAAPFeasibilityPage", candidacy);
+
+      stubQuery(
+        req,
+        "getCandidacyMenuAndCandidateInfos",
+        "candidacy/candidacy-menu-dff.json",
+      );
+    });
+  });
+
+  cy.collaborateur(
+    "/candidacies/57bf364b-8c8b-4ff4-889b-66917e26d7d0/feasibility-aap/eligibility",
+  );
+}
+
+describe("Dematerialized Feasibility File Eligibility Page", () => {
+  context("Initial form state", () => {
+    it("should have disabled form buttons by default", function () {
+      visitFeasibilityEligibility();
+
+      cy.get('[data-test="eligibility-form-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("button").should("be.disabled");
+        });
+    });
+  });
+
+  context("First-time eligibility request", () => {
+    it("should disable date and time fields when TOTAL eligibility is selected", function () {
+      visitFeasibilityEligibility();
+
+      cy.get('[data-test="eligibility-form-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("button").should("be.disabled");
+        });
+
+      cy.get('[data-test="eligibility-select"]')
+        .children("select")
+        .select("TOTAL");
+
+      cy.get('[data-test="eligibility-valid-until-input"]')
+        .should("exist")
+        .within(() => {
+          cy.get("input").should("be.disabled");
+        });
+
+      cy.get('[data-test="eligibility-time-enough-radio-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("input").should("be.disabled");
+        });
+
+      cy.get('[data-test="eligibility-form-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("button").should("not.be.disabled");
+        });
+    });
+  });
+
+  context("Existing candidate eligibility", () => {
+    it("should handle PARTIAL eligibility with future date validation", function () {
+      visitFeasibilityEligibility();
+
+      cy.get('[data-test="eligibility-form-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("button").should("be.disabled");
+        });
+
+      cy.get('[data-test="eligibility-select"]')
+        .children("select")
+        .select("PARTIAL");
+
+      cy.get('[data-test="eligibility-valid-until-input"]')
+        .should("exist")
+        .within(() => {
+          cy.get("input")
+            .should("not.be.disabled")
+            .clear()
+            .type(DF_FORMATED_DATE_6_MONTHS_FROM_NOW);
+        });
+
+      cy.get('[data-test="eligibility-time-enough-radio-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("input")
+            .should("not.be.disabled")
+            .first()
+            .check({ force: true });
+        });
+
+      cy.get('[data-test="eligibility-form-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("button").should("not.be.disabled");
+        });
+    });
+
+    it("should show error message when submitting PARTIAL eligibility with past date", function () {
+      visitFeasibilityEligibility();
+
+      cy.get('[data-test="eligibility-form-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("button").should("be.disabled");
+        });
+
+      cy.get('[data-test="eligibility-select"]')
+        .children("select")
+        .select("PARTIAL");
+
+      cy.get('[data-test="eligibility-valid-until-input"]')
+        .should("exist")
+        .within(() => {
+          cy.get("input")
+            .should("not.be.disabled")
+            .clear()
+            .type(DF_FORMATED_DATE_6_MONTHS_AGO);
+        });
+
+      cy.get('[data-test="eligibility-time-enough-radio-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("input")
+            .should("not.be.disabled")
+            .first()
+            .check({ force: true });
+        });
+
+      cy.get('[data-test="eligibility-valid-until-input"]').within(() => {
+        cy.get('[class*="fr-error-text"]').should("not.exist");
+      });
+
+      cy.get('[data-test="eligibility-form-buttons"]')
+        .should("exist")
+        .within(() => {
+          cy.get("button").should("not.be.disabled");
+          cy.get("button").last().click();
+        });
+
+      cy.get('[data-test="eligibility-valid-until-input"]').within(() => {
+        cy.get('[class*="fr-error-text"]').should("exist");
+      });
+    });
+
+    context("RNCP code changes", () => {
+      it("should disable date and time fields when PARTIAL_WITH_CHANGE eligibility is selected", function () {
+        visitFeasibilityEligibility();
+
+        cy.get('[data-test="eligibility-form-buttons"]')
+          .should("exist")
+          .within(() => {
+            cy.get("button").should("be.disabled");
+          });
+
+        cy.get('[data-test="eligibility-select"]')
+          .children("select")
+          .select("PARTIAL_WITH_CHANGE");
+
+        cy.get('[data-test="eligibility-valid-until-input"]')
+          .should("exist")
+          .within(() => {
+            cy.get("input").should("be.disabled");
+          });
+
+        cy.get('[data-test="eligibility-time-enough-radio-buttons"]')
+          .should("exist")
+          .within(() => {
+            cy.get("input").should("be.disabled");
+          });
+
+        cy.get('[data-test="eligibility-form-buttons"]')
+          .should("exist")
+          .within(() => {
+            cy.get("button").should("not.be.disabled");
+          });
+      });
+    });
+
+    context("No RNCP code changes", () => {
+      it("should handle PARTIAL_WITHOUT_CHANGE eligibility with future date validation", function () {
+        visitFeasibilityEligibility();
+
+        cy.get('[data-test="eligibility-form-buttons"]')
+          .should("exist")
+          .within(() => {
+            cy.get("button").should("be.disabled");
+          });
+
+        cy.get('[data-test="eligibility-select"]')
+          .children("select")
+          .select("PARTIAL_WITHOUT_CHANGE");
+
+        cy.get('[data-test="eligibility-valid-until-input"]')
+          .should("exist")
+          .within(() => {
+            cy.get("input")
+              .should("not.be.disabled")
+              .clear()
+              .type(DF_FORMATED_DATE_6_MONTHS_FROM_NOW);
+          });
+
+        cy.get('[data-test="eligibility-time-enough-radio-buttons"]')
+          .should("exist")
+          .within(() => {
+            cy.get("input")
+              .should("not.be.disabled")
+              .first()
+              .check({ force: true });
+          });
+
+        cy.get('[data-test="eligibility-form-buttons"]')
+          .should("exist")
+          .within(() => {
+            cy.get("button").should("not.be.disabled");
+          });
+      });
+    });
+  });
+});
