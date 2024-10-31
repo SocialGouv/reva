fde5c8765103ebc859d7c32c1770832c86a10b41
Pierre A <pierre@avizou.eu>
2024-10-31 11:50:31 +0100

    feat(admin):load CGU from Strapi

--- packages/reva-admin-react/codegen.ts
+++ packages/reva-admin-react/codegen.ts
@@ -1,8 +1,16 @@
+import path from "path";
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.join(process.cwd(), "..", "..", ".env") });
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "../reva-api/modules/generated-graphql-schema.graphql",
+  schema: [
+    "../reva-api/modules/generated-graphql-schema.graphql",
+    process.env.NEXT_PUBLIC_WEBSITE_STRAPI_BASE_URL + "/graphql" ||
+      "http://localhost:1337/graphql",
+  ],
   documents: ["src/**/*.tsx", "src/**/*.ts"],
   generates: {
     "src/graphql/generated/": {
---
+++ packages/reva-admin-react/src/app/(aap)/cgu/_components/CguForm.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { IgnoreCguModalContent } from "@/app/(aap)/cgu/_components/IgnoreCguModalContent";
+import { graphqlErrorToast } from "@/components/toast/toast";
+import { Button } from "@codegouvfr/react-dsfr/Button";
+import Checkbox from "@codegouvfr/react-dsfr/Checkbox";
+import { createModal } from "@codegouvfr/react-dsfr/Modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useAppCgu } from "../page.hooks";
+
+const zodSchema = z.object({
+  cguAcceptance: z.literal<boolean>(true),
+});
+
+type CguFormSchema = z.infer<typeof zodSchema>;
+
+const modalIgnoreCgu = createModal({
+  id: "modal-ignore-cgu",
+  isOpenedByDefault: false,
+});
+
+export function CguForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<CguFormSchema>({
+    resolver: zodResolver(zodSchema),
+  });
+
+  const router = useRouter();
+
+  const { acceptMaisonMereCgu } = useAppCgu();
+
+  const queryClient = useQueryClient();
+
+  const submitCgu = async () => {
+    try {
+      await acceptMaisonMereCgu.mutateAsync();
+      queryClient.invalidateQueries({
+        queryKey: ["getMaisonMereCGUQuery"],
+      });
+      router.push("/candidacies");
+    } catch (e) {
+      graphqlErrorToast(e);
+    }
+  };
+
+  return (
+    <>
+      {/* <h1>Conditions générales d'utilisation</h1> */}
+      <form onSubmit={handleSubmit(submitCgu)}>
+        <fieldset>
+          <hr className="mt-6 mb-8" />
+          <p className="text-light-text-mention-grey text-xs leading-5 -mt-6">
+            Sauf mention contraire “(optionnel)” dans le label, tous les champs
+            sont obligatoires.
+          </p>
+          <Checkbox
+            options={[
+              {
+                label:
+                  "J’accepte les nouvelles conditions générales d’utilisation.",
+                nativeInputProps: {
+                  ...register("cguAcceptance"),
+                },
+              },
+            ]}
+          />
+        </fieldset>
+        <div className="flex gap-x-2 justify-end">
+          <Button
+            type="button"
+            priority="tertiary no outline"
+            onClick={modalIgnoreCgu.open}
+          >
+            Ignorer les CGU
+          </Button>
+          <Button type="submit" disabled={!isValid}>
+            Valider les CGU
+          </Button>
+        </div>
+      </form>
+      <modalIgnoreCgu.Component
+        size="large"
+        iconId="fr-icon-arrow-right-line"
+        title=" Que se passe-t-il si vous ignorez les nouvelles CGU ?"
+        buttons={[
+          {
+            linkProps: { href: "/candidacies" },
+            children: "Ignorer les CGU",
+          },
+
+          {
+            children: "Relire les CGU",
+          },
+        ]}
+      >
+        <IgnoreCguModalContent />
+      </modalIgnoreCgu.Component>
+    </>
+  );
+}
