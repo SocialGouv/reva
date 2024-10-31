1262619dbe7f062ab2a25dd479210e76bf9467b5
ThomasDos <dosanjos.thomas@gmail.com>
2024-10-31 16:10:38 +0100

    test(admin): add dematerialization certification mock data

--- packages/reva-admin-react/cypress/e2e/candidacy/feasibility/dematerialized-feasibility-file/dff-mocks.ts
+++ packages/reva-admin-react/cypress/e2e/candidacy/feasibility/dematerialized-feasibility-file/dff-mocks.ts
@@ -103,3 +103,55 @@ export const DF_FORMATED_DATE_6_MONTHS_AGO = format(
   addMonths(new Date(), -6),
   "yyyy-MM-dd",
 );
+
+export const DF_CERTIFICATION = {
+  label: "CAP Accompagnant éducatif petite enfance - AEPE",
+  codeRncp: "38565",
+  competenceBlocs: [
+    {
+      id: "4c06558e-8e3e-4559-882e-321607a6b4e1",
+      code: "RNCP38565BC01",
+      label: "Accompagner le développement du jeune enfant",
+    },
+    {
+      id: "17aed396-0a61-40b4-8acc-f58f14fbccf6",
+      code: "RNCP38565BC02",
+      label: "Exercer son activité en accueil collectif",
+    },
+    {
+      id: "0999cee6-651b-4595-95ee-23954d93c782",
+      code: "RNCP38565BC03",
+      label: "Exercer son activité en accueil individuel",
+    },
+    {
+      id: "31af3506-c0e7-46ae-9482-8a4f50dffa61",
+      code: "RNCP38565BC04",
+      label: "Français et histoire-géographie-enseignement moral et civique",
+    },
+    {
+      id: "6fc1fac7-8ad6-4487-bafd-8a56497c3061",
+      code: "RNCP38565BC05",
+      label: "Mathématiques et physique-chimie",
+    },
+    {
+      id: "da87f1d6-3fef-421a-bf93-fb1cec1a0627",
+      code: "RNCP38565BC06",
+      label: "Éducation physique et sportive",
+    },
+    {
+      id: "64b617c6-2b6f-49b9-8241-a120600c1618",
+      code: "RNCP38565BC07",
+      label: " Prévention-santé-environnement",
+    },
+    {
+      id: "7a93b361-c5f9-4e32-8905-b713255611bb",
+      code: "RNCP38565BC08",
+      label: "Langue vivante (bloc facultatif)",
+    },
+    {
+      id: "3852bb78-4a07-4624-8c6f-6d18b769fc12",
+      code: "RNCP38565BC09",
+      label: "Mobilité (bloc facultatif)",
+    },
+  ],
+};
