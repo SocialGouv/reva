64c62c18d41369047d553a5584dff75c0253f9d9
ThomasDos <dosanjos.thomas@gmail.com>
2024-11-13 16:01:50 +0100

    test(api): remove unused fixture in skillAndTraining

--- packages/reva-api/test/fixtures/skillAndTraining.ts
+++ packages/reva-api/test/fixtures/skillAndTraining.ts
@@ -1,3 +1,2 @@
 export const basicSkill1Label = "Communication en français";
 export const basicSkill2Label = "Usage et communication numérique";
-export const training1Label = "Equipier de Première Intervention";
