62cf5da88548a82e15474dc6b9e7b983ddb06086
ThomasDos <dosanjos.thomas@gmail.com>
2024-11-22 16:45:19 +0100

    test(api): add optional args to createReorientationReasonHelper

--- packages/reva-api/test/helpers/entities/create-reorientation-reason-helper.ts
+++ packages/reva-api/test/helpers/entities/create-reorientation-reason-helper.ts
@@ -1,9 +1,13 @@
 import { faker } from "@faker-js/faker/locale/fr";
 import { prismaClient } from "../../../prisma/client";
+import { ReorientationReason } from "@prisma/client";
 
-export const createReorientationReasonHelper = async () =>
+export const createReorientationReasonHelper = async (
+  reorientationReasonArgs?: Partial<ReorientationReason>,
+) =>
   prismaClient.reorientationReason.create({
     data: {
       label: faker.lorem.sentence(),
+      ...reorientationReasonArgs,
     },
   });
