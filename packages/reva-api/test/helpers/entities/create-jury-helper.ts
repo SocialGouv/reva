fe2e5566dbf25ecacebd257a6ce6868555de0c94
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-11-28 10:34:42 +0100

    feat(api): added a create jury test helper

---
+++ packages/reva-api/test/helpers/entities/create-jury-helper.ts
@@ -0,0 +1,32 @@
+import { Jury } from "@prisma/client";
+import { prismaClient } from "../../../prisma/client";
+import { createCandidacyHelper } from "./create-candidacy-helper";
+import { createCertificationAuthorityHelper } from "./create-certification-authority-helper";
+
+export const createJuryHelper = async (args?: Partial<Jury>) => {
+  //either take the candidacy whose id is passed in args or create a new one
+  const candidacy = args?.candidacyId
+    ? await prismaClient.candidacy.findUnique({
+        where: { id: args.candidacyId },
+      })
+    : await createCandidacyHelper();
+
+  if (!candidacy) {
+    throw new Error("candidacy not found");
+  }
+
+  const certificationAuthority = await createCertificationAuthorityHelper();
+
+  return prismaClient.jury.create({
+    data: {
+      isActive: true,
+      candidacyId: candidacy.id,
+      certificationAuthorityId: certificationAuthority.id,
+      dateOfSession: new Date(),
+      ...args,
+    },
+    include: {
+      candidacy: true,
+    },
+  });
+};
