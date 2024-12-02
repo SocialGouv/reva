81d33f780ab5cd223b51a1fb31412c777296fd1b
Pierre A <pierre@avizou.eu>
2024-12-02 16:35:25 +0100

    test(api):add create dossier de validation helper

---
+++ packages/reva-api/test/helpers/entities/create-dossier-de-validation-helper.ts
@@ -0,0 +1,34 @@
+import { DossierDeValidation } from "@prisma/client";
+import { prismaClient } from "../../../prisma/client";
+import { createCandidacyHelper } from "./create-candidacy-helper";
+import { createFileHelper } from "./create-file-helper";
+import { createCertificationAuthorityHelper } from "./create-certification-authority-helper";
+
+export const createDossierDeValidationHelper = async (
+  dossierDeValidationArgs?: Partial<DossierDeValidation>,
+) => {
+  const candidacy = await createCandidacyHelper();
+  const file = await createFileHelper();
+
+  return prismaClient.dossierDeValidation.create({
+    data: {
+      candidacyId: dossierDeValidationArgs?.candidacyId ?? candidacy.id,
+      decision: dossierDeValidationArgs?.decision ?? "PENDING",
+      certificationAuthorityId:
+        dossierDeValidationArgs?.certificationAuthorityId ??
+        (await createCertificationAuthorityHelper()).id,
+      dossierDeValidationFileId: file.id,
+      ...dossierDeValidationArgs,
+    },
+    include: {
+      candidacy: {
+        include: {
+          organism: { include: { accounts: true } },
+          candidate: true,
+          department: true,
+          certification: true,
+        },
+      },
+    },
+  });
+};
