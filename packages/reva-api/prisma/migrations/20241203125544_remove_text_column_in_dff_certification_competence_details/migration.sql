3437ef4b693f2e5026b9396ca304c4b339b2242b
ThomasDos <dosanjos.thomas@gmail.com>
2024-12-03 13:58:17 +0100

    chore(api): remove text column in dff_certification_competence_details table

---
+++ packages/reva-api/prisma/migrations/20241203125544_remove_text_column_in_dff_certification_competence_details/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `text` on the `dff_certification_competence_details` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "dff_certification_competence_details" DROP COLUMN "text";
