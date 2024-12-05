50d4f2f0ad4d1379992c8c4b99c67661e8a0165f
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-12-05 15:49:12 +0100

    feat(admin): added update competence bloc page to certification registry manager update certification page

---
+++ packages/reva-admin-react/cypress/e2e/responsable-certifications/certifications/update-certification-competence-bloc-page/update-certification-competence-bloc-page.cy.ts
@@ -0,0 +1,163 @@
+import { stubMutation, stubQuery } from "../../../../utils/graphql";
+import certificationCBBPBoucher1 from "./fixtures/certification-competence-bloc-bp-boucher-1.json";
+import updateCertificationBlocMutationResponse from "./fixtures/update-competence-bloc-bp-boucher-mutation-response.json";
+
+function interceptCertificationCompetenceBloc() {
+  cy.intercept("POST", "/api/graphql", (req) => {
+    stubQuery(
+      req,
+      "activeFeaturesForConnectedUser",
+      "features/active-features.json",
+    );
+    stubQuery(
+      req,
+      "getMaisonMereCGUQuery",
+      "account/gestionnaire-cgu-accepted.json",
+    );
+    stubQuery(
+      req,
+      "getCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+      certificationCBBPBoucher1,
+    );
+  });
+}
+
+function interceptUpdateCertificationCompetenceBlocMutation() {
+  cy.intercept("POST", "/api/graphql", (req) => {
+    stubMutation(
+      req,
+      "updateCertificationCompetenceBlocForCertificationRegistryManagerUpdateCertificationCompetenceBlocPage",
+      updateCertificationBlocMutationResponse,
+    );
+  });
+}
+
+function interceptDeleteCertificationCompetenceBlocMutation() {
+  cy.intercept("POST", "/api/graphql", (req) => {
+    stubMutation(
+      req,
+      "deleteCertificationCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+      updateCertificationBlocMutationResponse,
+    );
+  });
+}
+
+context("when i access the update certification page ", () => {
+  it("display the page with a correct title", function () {
+    interceptCertificationCompetenceBloc();
+
+    cy.admin(
+      "http://localhost:3003/admin2/responsable-certifications/certifications/bf78b4d6-f6ac-4c8f-9e6b-d6c6ae9e891b/bloc-competence/008a6fab-55ad-4412-ab17-56bc4b8e2fd0/",
+    );
+    cy.wait("@activeFeaturesForConnectedUser");
+    cy.wait("@getMaisonMereCGUQuery");
+    cy.wait(
+      "@getCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+    );
+
+    cy.get(
+      '[data-test="certification-registry-manager-update-certification-competence-bloc-page"]',
+    )
+      .children("h1")
+      .should(
+        "have.text",
+        "RNCP37310BC01 - Préparation, présentation, décoration et vente en boucherie",
+      );
+  });
+
+  it("dont let me submit the form if no edit has been made", function () {
+    interceptCertificationCompetenceBloc();
+    cy.admin(
+      "http://localhost:3003/admin2/responsable-certifications/certifications/bf78b4d6-f6ac-4c8f-9e6b-d6c6ae9e891b/bloc-competence/008a6fab-55ad-4412-ab17-56bc4b8e2fd0/",
+    );
+    cy.wait("@activeFeaturesForConnectedUser");
+    cy.wait("@getMaisonMereCGUQuery");
+    cy.wait(
+      "@getCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+    );
+
+    cy.get("button").contains("Enregistrer").should("be.disabled");
+  });
+
+  it("let me update a competence bloc and submit the form", function () {
+    interceptCertificationCompetenceBloc();
+    interceptUpdateCertificationCompetenceBlocMutation();
+    cy.admin(
+      "http://localhost:3003/admin2/responsable-certifications/certifications/bf78b4d6-f6ac-4c8f-9e6b-d6c6ae9e891b/bloc-competence/008a6fab-55ad-4412-ab17-56bc4b8e2fd0/",
+    );
+    cy.wait("@activeFeaturesForConnectedUser");
+    cy.wait("@getMaisonMereCGUQuery");
+    cy.wait(
+      "@getCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+    );
+
+    cy.get('[data-test="competence-bloc-label-input"] input')
+      .clear()
+      .type("updated competence bloc label");
+
+    cy.get("button").contains("Enregistrer").click();
+    cy.wait(
+      "@updateCertificationCompetenceBlocForCertificationRegistryManagerUpdateCertificationCompetenceBlocPage",
+    );
+  });
+
+  it("let me add a new competence to the competence bloc", function () {
+    interceptCertificationCompetenceBloc();
+    cy.admin(
+      "http://localhost:3003/admin2/responsable-certifications/certifications/bf78b4d6-f6ac-4c8f-9e6b-d6c6ae9e891b/bloc-competence/008a6fab-55ad-4412-ab17-56bc4b8e2fd0/",
+    );
+    cy.wait("@activeFeaturesForConnectedUser");
+    cy.wait("@getMaisonMereCGUQuery");
+    cy.wait(
+      "@getCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+    );
+
+    cy.get('[data-test="competence-list"] input').should("have.length", 4);
+
+    cy.get('[data-test="add-competence-button"]').click();
+
+    cy.get('[data-test="competence-list"] input').should("have.length", 5);
+  });
+
+  it("let me delete a competence from the competence bloc", function () {
+    interceptCertificationCompetenceBloc();
+    cy.admin(
+      "http://localhost:3003/admin2/responsable-certifications/certifications/bf78b4d6-f6ac-4c8f-9e6b-d6c6ae9e891b/bloc-competence/008a6fab-55ad-4412-ab17-56bc4b8e2fd0/",
+    );
+    cy.wait("@activeFeaturesForConnectedUser");
+    cy.wait("@getMaisonMereCGUQuery");
+    cy.wait(
+      "@getCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+    );
+
+    cy.get('[data-test="competence-list"] input').should("have.length", 4);
+
+    cy.get('[data-test="delete-competence-button"]').eq(1).click();
+
+    cy.get('[data-test="competence-list"] input').should("have.length", 3);
+  });
+
+  it("let me delete a competence bloc", function () {
+    interceptCertificationCompetenceBloc();
+    interceptDeleteCertificationCompetenceBlocMutation();
+    cy.admin(
+      "http://localhost:3003/admin2/responsable-certifications/certifications/bf78b4d6-f6ac-4c8f-9e6b-d6c6ae9e891b/bloc-competence/008a6fab-55ad-4412-ab17-56bc4b8e2fd0/",
+    );
+    cy.wait("@activeFeaturesForConnectedUser");
+    cy.wait("@getMaisonMereCGUQuery");
+    cy.wait(
+      "@getCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+    );
+
+    cy.get('[data-test="delete-competence-bloc-button"]').click();
+
+    cy.wait(
+      "@deleteCertificationCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage",
+    );
+
+    cy.url().should(
+      "eq",
+      "http://localhost:3003/admin2/responsable-certifications/certifications/bf78b4d6-f6ac-4c8f-9e6b-d6c6ae9e891b/",
+    );
+  });
+});
---
+++ packages/reva-admin-react/src/app/responsable-certifications/certifications/[certificationId]/bloc-competence/[certificationCompetenceBlocId]/updateCompetenceBloc.hook.ts
@@ -0,0 +1,102 @@
+import { useGraphQlClient } from "@/components/graphql/graphql-client/GraphqlClient";
+import { graphql } from "@/graphql/generated";
+import { UpdateCompetenceBlocInput } from "@/graphql/generated/graphql";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+const getCompetenceBlocQuery = graphql(`
+  query getCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage(
+    $certificationCompetenceBlocId: ID!
+  ) {
+    getCertificationCompetenceBloc(
+      certificationCompetenceBlocId: $certificationCompetenceBlocId
+    ) {
+      id
+      label
+      code
+      FCCompetences
+      competences {
+        id
+        label
+      }
+      certification {
+        id
+        label
+        codeRncp
+      }
+    }
+  }
+`);
+
+const updateCertificationCompetenceBlocMutation = graphql(`
+  mutation updateCertificationCompetenceBlocForCertificationRegistryManagerUpdateCertificationCompetenceBlocPage(
+    $input: UpdateCompetenceBlocInput!
+  ) {
+    referential_updateCertificationCompetenceBloc(input: $input) {
+      id
+    }
+  }
+`);
+
+const deleteCertificationCompetenceBlocMutation = graphql(`
+  mutation deleteCertificationCompetenceBlocForCertificationRegistryManagerUpdateCompetenceBlocPage(
+    $certificationCompetenceBlocId: String!
+  ) {
+    referential_deleteCertificationCompetenceBloc(
+      certificationCompetenceBlocId: $certificationCompetenceBlocId
+    ) {
+      id
+    }
+  }
+`);
+
+export const useUpdateCompetenceBlocPage = ({
+  certificationCompetenceBlocId,
+}: {
+  certificationCompetenceBlocId: string;
+}) => {
+  const { graphqlClient } = useGraphQlClient();
+  const queryClient = useQueryClient();
+
+  const {
+    data: getCompetenceBlocResponse,
+    status: getCompetenceBlocQueryStatus,
+  } = useQuery({
+    queryKey: [
+      certificationCompetenceBlocId,
+      "competenceBlocs",
+      "getCompetenceBlocForUpdateCompetenceBlocPage",
+    ],
+    queryFn: () =>
+      graphqlClient.request(getCompetenceBlocQuery, {
+        certificationCompetenceBlocId,
+      }),
+  });
+
+  const updateCertificationCompetenceBloc = useMutation({
+    mutationFn: (input: UpdateCompetenceBlocInput) =>
+      graphqlClient.request(updateCertificationCompetenceBlocMutation, {
+        input: { id: certificationCompetenceBlocId, ...input },
+      }),
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: [certificationCompetenceBlocId],
+      }),
+  });
+
+  const deleteCertificationCompetenceBloc = useMutation({
+    mutationFn: () =>
+      graphqlClient.request(deleteCertificationCompetenceBlocMutation, {
+        certificationCompetenceBlocId,
+      }),
+  });
+
+  const competenceBloc =
+    getCompetenceBlocResponse?.getCertificationCompetenceBloc;
+
+  return {
+    competenceBloc,
+    getCompetenceBlocQueryStatus,
+    updateCertificationCompetenceBloc,
+    deleteCertificationCompetenceBloc,
+  };
+};
