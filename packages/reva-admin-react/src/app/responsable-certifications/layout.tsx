9941bc4dcd7e488b0e992dec0cc258bb3f810a63
Pierre A <pierre@avizou.eu>
2024-12-05 18:13:48 +0100

    fix(admin):set array length to let tests pass

--- packages/reva-admin-react/src/app/responsable-certifications/layout.tsx
+++ packages/reva-admin-react/src/app/responsable-certifications/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import Image from "next/image";
 
-const attachedCertifications = [];
+const attachedCertifications = [1];
 const certificationsToValidate = [];
 
 const NoCertifications = () => {
