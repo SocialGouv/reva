e1114be0414bc9245ef027aca8e6a578fe0130bc
ThomasDos <dosanjos.thomas@gmail.com>
2024-12-06 09:01:10 +0100

    feat(api,candidate): expose and retrieve lastActivityDate for Candidacy

--- packages/reva-candidate/src/components/candidacy/candidacy.context.tsx
+++ packages/reva-candidate/src/components/candidacy/candidacy.context.tsx
@@ -51,6 +51,7 @@ const GET_CANDIDATE_WITH_CANDIDACY = graphql(`
         typeAccompagnement
         status
         firstAppointmentOccuredAt
+        lastActivityDate
         candidacyDropOut {
           createdAt
         }
