9d8db5fb8e95a524206efa6f42634c7c9d95bce7
ThomasDos <dosanjos.thomas@gmail.com>
2024-12-18 08:34:32 +0100

    feat(api): enhance getCandidacyIsCaduque function to include feasibility check and refine caduque logic

--- packages/reva-api/modules/candidacy/features/getCandidacyIsCaduque.ts
+++ packages/reva-api/modules/candidacy/features/getCandidacyIsCaduque.ts
@@ -1,4 +1,5 @@
 import { addDays, isBefore } from "date-fns";
+import { prismaClient } from "../../../prisma/client";
 import { getCandidacyById } from "./getCandidacyById";
 
 const CADUQUITE_THRESHOLD_DAYS = 183;
@@ -8,16 +9,41 @@ export const getCandidacyIsCaduque = async ({
 }: {
   candidacyId: string;
 }): Promise<boolean> => {
-  const candidacy = await getCandidacyById({ candidacyId });
+  const candidacy = await getCandidacyById({
+    candidacyId,
+  });
 
   if (!candidacy?.lastActivityDate) {
     return false;
   }
+
+  const feasibility = await prismaClient.feasibility.findFirst({
+    where: {
+      candidacyId,
+      isActive: true,
+    },
+  });
 
   const sixMonthsFromLastActivity = addDays(
     candidacy.lastActivityDate,
     CADUQUITE_THRESHOLD_DAYS,
   );
 
-  return isBefore(sixMonthsFromLastActivity, new Date());
+  const lastActiveStatus = candidacy?.status;
+  const isLastActiveStatusValidForActualisationBanner =
+    lastActiveStatus === "DOSSIER_FAISABILITE_RECEVABLE" ||
+    lastActiveStatus === "DOSSIER_DE_VALIDATION_SIGNALE";
+
+  const lastActivityHasNotBeenUpdatedInSixMonths = isBefore(
+    sixMonthsFromLastActivity,
+    new Date(),
+  );
+
+  const isFeasibilityDecisionValid = feasibility?.decision === "ADMISSIBLE";
+
+  return (
+    isLastActiveStatusValidForActualisationBanner &&
+    lastActivityHasNotBeenUpdatedInSixMonths &&
+    isFeasibilityDecisionValid
+  );
 };
