e49c2a803de8b2781193420aa943350eba8380cf
ThomasDos <dosanjos.thomas@gmail.com>
2024-12-18 12:11:25 +0100

    refactor(candidate): SelfServiceFeasibilityFileTimelineElement to use custom hook for timeline info management
    - Introduced `useGetSelfServiceFeasibilityFileTimelineInfo` hook to encapsulate logic for determining status, badge, and notice based on candidacy feasibility.
    - Simplified `SelfServiceFeasibilityFileTimelineElement` component by removing redundant logic and utilizing the new hook for cleaner code structure.

--- packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/SelfServiceFeasibilityFileTimelineElement/SelfServiceFeasibilityFileTimelineElement.tsx
+++ packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/SelfServiceFeasibilityFileTimelineElement/SelfServiceFeasibilityFileTimelineElement.tsx
@@ -1,105 +1,23 @@
-import {
-  TimelineElement,
-  TimeLineElementStatus,
-} from "@/components/legacy/molecules/Timeline/Timeline";
-
-import { useCandidacy } from "@/components/candidacy/candidacy.context";
+import { TimelineElement } from "@/components/legacy/molecules/Timeline/Timeline";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import { useRouter } from "next/navigation";
-import Badge from "@codegouvfr/react-dsfr/Badge";
-import { TimelineNotice } from "@/components/timeline-notice/TimelineNotice";
+import { useGetSelfServiceFeasibilityFileTimelineInfo } from "./useGetSelfServiceFeasibilityFileTimelineInfo";
 
 export const SelfServiceFeasibilityFileTimelineElement = () => {
-  const { candidacy } = useCandidacy();
+  const { status, badge, notice } =
+    useGetSelfServiceFeasibilityFileTimelineInfo();
+
   const router = useRouter();
-
-  let status: TimeLineElementStatus = "disabled";
-
-  const canComplete =
-    (!candidacy.feasibility ||
-      candidacy.feasibility.decision == "INCOMPLETE") &&
-    candidacy.certification;
-
-  if (canComplete) {
-    status = "editable";
-  }
-
-  if (candidacy.status !== "PROJET" && !canComplete) {
-    status = "readonly";
-  }
-
-  let badge = null;
-
-  if (canComplete) {
-    badge = (
-      <Badge
-        severity="warning"
-        data-test="feasibility-timeline-element-complete-badge"
-      >
-        À compléter
-      </Badge>
-    );
-  } else if (candidacy.feasibility?.decision === "PENDING") {
-    badge = (
-      <Badge
-        severity="info"
-        data-test="feasibility-timeline-element-pending-badge"
-      >
-        Envoyé
-      </Badge>
-    );
-  } else if (candidacy.feasibility?.decision === "ADMISSIBLE") {
-    badge = (
-      <Badge
-        severity="success"
-        data-test="feasibility-timeline-element-admissible-badge"
-      >
-        Recevable
-      </Badge>
-    );
-  } else if (candidacy.feasibility?.decision === "REJECTED") {
-    badge = (
-      <Badge
-        severity="error"
-        data-test="feasibility-timeline-element-rejected-badge"
-      >
-        Non-recevable
-      </Badge>
-    );
-  }
 
   return (
     <TimelineElement
       title="Dossier de faisabilité"
-      description="Un document important pour résumer vos expériences et tenter d’obtenir votre recevabilité !"
+      description="Un document important pour résumer vos expériences et tenter d'obtenir votre recevabilité !"
       status={status}
       badge={badge}
       data-test="feasibility-timeline-element"
     >
-      {candidacy.feasibility?.decision === "PENDING" && (
-        <TimelineNotice
-          icon="fr-icon-time-fill"
-          text="Votre dossier est étudié par votre certificateur. En cas d’erreur ou d’oubli, contactez-le pour pouvoir le modifier dans les plus brefs délais. Vous recevrez une réponse dans un délai de 2 mois. S’il est considéré comme incomplet, vous devrez le modifier et le renvoyer."
-        />
-      )}
-      {candidacy.feasibility?.decision === "INCOMPLETE" && (
-        <TimelineNotice
-          icon="fr-icon-info-fill"
-          text="Selon le certificateur, votre dossier est incomplet. Cliquez sur “Compléter” pour consulter ses remarques et rajouter le ou les éléments manquants avant de renvoyer votre dossier de faisabilité."
-        />
-      )}
-      {candidacy.feasibility?.decision === "REJECTED" && (
-        <TimelineNotice
-          icon="fr-icon-info-fill"
-          text="Votre certificateur a estimé que votre dossier n'est pas recevable pour la suite de votre VAE."
-        />
-      )}
-      {candidacy.feasibility?.decision === "ADMISSIBLE" && (
-        <TimelineNotice
-          icon="fr-icon-info-fill"
-          text="Félicitations, votre dossier est recevable ! Vous pouvez poursuivre votre parcours VAE."
-        />
-      )}
+      {notice}
       {status !== "readonly" ? (
         <Button
           data-test="feasibility-timeline-element-update-button"
---
+++ packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/SelfServiceFeasibilityFileTimelineElement/useGetSelfServiceFeasibilityFileTimelineInfo.tsx
@@ -0,0 +1,116 @@
+import { useCandidacy } from "@/components/candidacy/candidacy.context";
+import { TimeLineElementStatus } from "@/components/legacy/molecules/Timeline/Timeline";
+import { TimelineNotice } from "@/components/timeline-notice/TimelineNotice";
+import Badge from "@codegouvfr/react-dsfr/Badge";
+import { ReactNode } from "react";
+
+interface GetSelfServiceFeasibilityFileTimelineInfo {
+  status: TimeLineElementStatus;
+  badge: JSX.Element | null;
+  notice: ReactNode;
+}
+
+const ToCompleteBadge = (
+  <Badge
+    severity="warning"
+    data-test="feasibility-timeline-element-complete-badge"
+  >
+    À compléter
+  </Badge>
+);
+
+export function useGetSelfServiceFeasibilityFileTimelineInfo(): GetSelfServiceFeasibilityFileTimelineInfo {
+  const { candidacy, feasibility } = useCandidacy();
+
+  const ADMISSIBLE = feasibility?.decision === "ADMISSIBLE";
+  const PENDING = feasibility?.decision === "PENDING";
+  const INCOMPLETE = feasibility?.decision === "INCOMPLETE";
+  const REJECTED = feasibility?.decision === "REJECTED";
+
+  if (candidacy.status === "PROJET" && !feasibility) {
+    return {
+      status: "editable",
+      badge: ToCompleteBadge,
+      notice: null,
+    };
+  }
+
+  if (PENDING) {
+    return {
+      status: "readonly",
+      badge: (
+        <Badge
+          severity="info"
+          data-test="feasibility-timeline-element-pending-badge"
+        >
+          Envoyé
+        </Badge>
+      ),
+      notice: (
+        <TimelineNotice
+          icon="fr-icon-time-fill"
+          text="Votre dossier est étudié par votre certificateur. En cas d'erreur ou d'oubli, contactez-le pour pouvoir le modifier dans les plus brefs délais. Vous recevrez une réponse dans un délai de 2 mois. S'il est considéré comme incomplet, vous devrez le modifier et le renvoyer."
+        />
+      ),
+    };
+  }
+
+  if (INCOMPLETE) {
+    return {
+      status: "editable",
+      badge: ToCompleteBadge,
+      notice: (
+        <TimelineNotice
+          icon="fr-icon-info-fill"
+          text="Selon le certificateur, votre dossier est incomplet. Cliquez sur « Compléter » pour consulter ses remarques et rajouter le ou les éléments manquants avant de renvoyer votre dossier de faisabilité."
+        />
+      ),
+    };
+  }
+
+  if (ADMISSIBLE) {
+    return {
+      status: "readonly",
+      badge: (
+        <Badge
+          severity="success"
+          data-test="feasibility-timeline-element-admissible-badge"
+        >
+          Recevable
+        </Badge>
+      ),
+      notice: (
+        <TimelineNotice
+          icon="fr-icon-info-fill"
+          text="Félicitations, votre dossier est recevable ! Vous pouvez poursuivre votre parcours VAE."
+        />
+      ),
+    };
+  }
+
+  if (REJECTED) {
+    return {
+      status: "readonly",
+      badge: (
+        <Badge
+          severity="error"
+          data-test="feasibility-timeline-element-rejected-badge"
+        >
+          Non-recevable
+        </Badge>
+      ),
+      notice: (
+        <TimelineNotice
+          icon="fr-icon-info-fill"
+          text="Votre certificateur a estimé que votre dossier n'est pas recevable pour la suite de votre VAE."
+        />
+      ),
+    };
+  }
+
+  return {
+    status: "readonly",
+    badge: null,
+    notice: null,
+  };
+}
