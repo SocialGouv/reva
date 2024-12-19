f6ef9c0b3c8733da2680018fe3a2db457272a8f2
ThomasDos <dosanjos.thomas@gmail.com>
2024-12-19 12:32:59 +0100

    feat(candidate): add in SelfServiceFeasibilityFileTimelineElement caducite cases and button on candidacy statuses

--- packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/SelfServiceFeasibilityFileTimelineElement/SelfServiceFeasibilityFileTimelineElement.tsx
+++ packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/SelfServiceFeasibilityFileTimelineElement/SelfServiceFeasibilityFileTimelineElement.tsx
@@ -1,13 +1,9 @@
 import { TimelineElement } from "@/components/legacy/molecules/Timeline/Timeline";
-import { Button } from "@codegouvfr/react-dsfr/Button";
-import { useRouter } from "next/navigation";
 import { useGetSelfServiceFeasibilityFileTimelineInfo } from "./useGetSelfServiceFeasibilityFileTimelineInfo";
 
 export const SelfServiceFeasibilityFileTimelineElement = () => {
-  const { status, badge, notice } =
+  const { status, badge, notice, button } =
     useGetSelfServiceFeasibilityFileTimelineInfo();
-
-  const router = useRouter();
 
   return (
     <TimelineElement
@@ -18,28 +14,7 @@ export const SelfServiceFeasibilityFileTimelineElement = () => {
       data-test="feasibility-timeline-element"
     >
       {notice}
-      {status !== "readonly" ? (
-        <Button
-          data-test="feasibility-timeline-element-update-button"
-          priority="primary"
-          onClick={() => {
-            router.push("/feasibility");
-          }}
-          disabled={status === "disabled"}
-        >
-          Compléter
-        </Button>
-      ) : (
-        <Button
-          data-test="feasibility-timeline-element-review-button"
-          priority="secondary"
-          onClick={() => {
-            router.push("/feasibility");
-          }}
-        >
-          Consulter
-        </Button>
-      )}
+      {button}
     </TimelineElement>
   );
 };
--- packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/SelfServiceFeasibilityFileTimelineElement/useGetSelfServiceFeasibilityFileTimelineInfo.tsx
+++ packages/reva-candidate/src/components/legacy/organisms/ProjectTimeline/TimelineElements/autonome/SelfServiceFeasibilityFileTimelineElement/useGetSelfServiceFeasibilityFileTimelineInfo.tsx
@@ -1,14 +1,28 @@
 import { useCandidacy } from "@/components/candidacy/candidacy.context";
+import { useFeatureFlipping } from "@/components/feature-flipping/featureFlipping";
 import { TimeLineElementStatus } from "@/components/legacy/molecules/Timeline/Timeline";
 import { TimelineNotice } from "@/components/timeline-notice/TimelineNotice";
 import Badge from "@codegouvfr/react-dsfr/Badge";
+import Button from "@codegouvfr/react-dsfr/Button";
+import { format } from "date-fns";
+import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
 
 interface GetSelfServiceFeasibilityFileTimelineInfo {
   status: TimeLineElementStatus;
   badge: JSX.Element | null;
   notice: ReactNode;
+  button: ReactNode;
 }
+
+const NonRecevableBadge = (
+  <Badge
+    severity="warning"
+    data-test="feasibility-timeline-element-non-recevable-badge"
+  >
+    Non recevable
+  </Badge>
+);
 
 const ToCompleteBadge = (
   <Badge
@@ -21,19 +35,104 @@ const ToCompleteBadge = (
 
 export function useGetSelfServiceFeasibilityFileTimelineInfo(): GetSelfServiceFeasibilityFileTimelineInfo {
   const { candidacy, feasibility } = useCandidacy();
+  const { isFeatureActive } = useFeatureFlipping();
+  const candidacyActualisationFeatureIsActive = isFeatureActive(
+    "candidacy_actualisation",
+  );
+  const router = useRouter();
 
   const ADMISSIBLE = feasibility?.decision === "ADMISSIBLE";
   const PENDING = feasibility?.decision === "PENDING";
   const INCOMPLETE = feasibility?.decision === "INCOMPLETE";
   const REJECTED = feasibility?.decision === "REJECTED";
   const hasCertification = !!candidacy.certification;
+  const isCaduque = candidacy.isCaduque;
+
+  const pendingContestationCaducite =
+    candidacy.candidacyContestationsCaducite?.find(
+      (c) =>
+        c?.certificationAuthorityContestationDecision === "DECISION_PENDING",
+    );
+
+  const hasConfirmedCaducite = candidacy.candidacyContestationsCaducite?.some(
+    (contestation) =>
+      contestation?.certificationAuthorityContestationDecision ===
+      "CADUCITE_CONFIRMED",
+  );
+
+  const ReviewButton = (
+    <Button
+      data-test="feasibility-timeline-element-review-button"
+      priority="secondary"
+      onClick={() => {
+        router.push("/feasibility");
+      }}
+    >
+      Consulter
+    </Button>
+  );
+
+  const CompleteButton = (
+    <Button
+      data-test="feasibility-timeline-element-update-button"
+      priority="primary"
+      onClick={() => {
+        router.push("/feasibility");
+      }}
+    >
+      Compléter
+    </Button>
+  );
 
   switch (true) {
+    case hasConfirmedCaducite:
+      return {
+        status: "active",
+        badge: NonRecevableBadge,
+        notice: (
+          <TimelineNotice
+            icon="fr-icon-info-fill"
+            text="Après étude de votre contestation, le certificateur a décidé que votre recevabilité n'était plus valable. Cela signifie que votre parcours VAE s'arrête ici."
+          />
+        ),
+        button: ReviewButton,
+      };
+
+    case !!pendingContestationCaducite:
+      return {
+        status: "active",
+        badge: NonRecevableBadge,
+        notice: (
+          <TimelineNotice
+            icon="fr-icon-time-fill"
+            text={`Votre contestation a été faite le ${format(
+              pendingContestationCaducite.contestationSentAt,
+              "dd/MM/yyyy",
+            )}. Elle a été envoyée à votre certificateur qui y répondra dans les meilleurs délais.`}
+          />
+        ),
+        button: ReviewButton,
+      };
+
+    case isCaduque && candidacyActualisationFeatureIsActive:
+      return {
+        status: "active",
+        badge: NonRecevableBadge,
+        notice: (
+          <TimelineNotice
+            icon="fr-icon-info-fill"
+            text="Parce que vous ne vous êtes pas actualisé à temps, votre recevabilité est désormais caduque. Cela signifie que votre parcours VAE s'arrête ici. Vous pouvez contester cette décision en cliquant sur le bouton “Contester”."
+          />
+        ),
+        button: ReviewButton,
+      };
+
     case candidacy.status === "PROJET" && !feasibility && hasCertification:
       return {
         status: "editable",
         badge: ToCompleteBadge,
         notice: null,
+        button: CompleteButton,
       };
 
     case PENDING:
@@ -53,6 +152,7 @@ export function useGetSelfServiceFeasibilityFileTimelineInfo(): GetSelfServiceFe
             text="Votre dossier est étudié par votre certificateur. En cas d'erreur ou d'oubli, contactez-le pour pouvoir le modifier dans les plus brefs délais. Vous recevrez une réponse dans un délai de 2 mois. S'il est considéré comme incomplet, vous devrez le modifier et le renvoyer."
           />
         ),
+        button: ReviewButton,
       };
 
     case INCOMPLETE:
@@ -65,6 +165,7 @@ export function useGetSelfServiceFeasibilityFileTimelineInfo(): GetSelfServiceFe
             text="Selon le certificateur, votre dossier est incomplet. Cliquez sur « Compléter » pour consulter ses remarques et rajouter le ou les éléments manquants avant de renvoyer votre dossier de faisabilité."
           />
         ),
+        button: CompleteButton,
       };
 
     case ADMISSIBLE:
@@ -84,6 +185,7 @@ export function useGetSelfServiceFeasibilityFileTimelineInfo(): GetSelfServiceFe
             text="Félicitations, votre dossier est recevable ! Vous pouvez poursuivre votre parcours VAE."
           />
         ),
+        button: ReviewButton,
       };
 
     case REJECTED:
@@ -103,6 +205,7 @@ export function useGetSelfServiceFeasibilityFileTimelineInfo(): GetSelfServiceFe
             text="Votre certificateur a estimé que votre dossier n'est pas recevable pour la suite de votre VAE."
           />
         ),
+        button: ReviewButton,
       };
 
     default:
@@ -110,6 +213,7 @@ export function useGetSelfServiceFeasibilityFileTimelineInfo(): GetSelfServiceFe
         status: "readonly",
         badge: null,
         notice: null,
+        button: null,
       };
   }
 }
