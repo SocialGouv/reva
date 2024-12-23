fe2d3b38eca600b37c021fcb167afc2874f7a9c2
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-12-23 10:39:27 +0100

    feat(api): added CANDIDACY_DROP_OUT_CANDIDATE_CONFIRMATION_FEATURE feature

---
+++ packages/reva-api/prisma/migrations/20241223093803_add_candidacy_drop_out_candidate_confirmation_feature/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    "features" ("key", "label")
+VALUES
+    (
+        'CANDIDACY_DROP_OUT_CANDIDATE_CONFIRMATION_FEATURE',
+        'Confirmation de l''abandon par le candidat'
+    );
