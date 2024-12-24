213b1d1e91da3ce0af8b3ae92f9ac2d9e2d2600b
Alexandre Garbe <alexandre.garbe@gmail.com>
2024-12-24 13:33:25 +0100

    feat(api): added a createFormaCodeHelper test helper

---
+++ packages/reva-api/test/helpers/entities/create-formacode-helper.ts
@@ -0,0 +1,15 @@
+import { faker } from "@faker-js/faker";
+import { Prisma } from "@prisma/client";
+import { prismaClient } from "../../../prisma/client";
+
+export const createFormaCodeHelper = async (
+  formaCodeArgs?: Partial<Prisma.FormacodeCreateInput>,
+) =>
+  prismaClient.formacode.create({
+    data: {
+      code: faker.lorem.word(),
+      label: faker.lorem.word(),
+      type: "DOMAIN",
+      ...formaCodeArgs,
+    },
+  });
