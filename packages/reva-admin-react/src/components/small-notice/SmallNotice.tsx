e63d6a9b0af1438f1a37bdf577900027157a72cf
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-01-03 11:00:04 +0100

    feat(admin): added the data-test attribute to the SmallNotice component

--- packages/reva-admin-react/src/components/small-notice/SmallNotice.tsx
+++ packages/reva-admin-react/src/components/small-notice/SmallNotice.tsx
@@ -3,12 +3,15 @@ import { ReactNode } from "react";
 export const SmallNotice = ({
   className = "",
   children,
+  "data-test": dataTest,
 }: {
   className?: string;
   children: ReactNode;
+  "data-test"?: string;
 }) => (
   <div
     className={`text-blue-light-text-default-info flex items-start ${className}`}
+    data-test={dataTest || ""}
   >
     <span className="fr-icon--sm fr-icon-info-fill mr-2 -mt-[1px]" />
     <p className="text-sm mb-0">{children}</p>
