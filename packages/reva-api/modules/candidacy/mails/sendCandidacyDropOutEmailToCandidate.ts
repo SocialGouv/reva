a1bbc31afaac8c482a9ea852a528363da73b514c
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-01-07 15:32:47 +0100

    feat(api): updated candidate dropout email

--- packages/reva-api/modules/candidacy/mails/sendCandidacyDropOutEmailToCandidate.ts
+++ packages/reva-api/modules/candidacy/mails/sendCandidacyDropOutEmailToCandidate.ts
@@ -1,23 +1,30 @@
 import mjml2html from "mjml";
 
-import { sendGenericEmail, templateMail } from "../../shared/email";
+import { sendEmailWithLink, templateMail } from "../../shared/email";
+import { generateJwt } from "../../candidate/auth.helper";
 
 export const sendCandidacyDropOutEmailToCandidate = async (email: string) => {
-  const htmlContent = mjml2html(
-    templateMail({
-      headlineDsfr: `<div>Votre accompagnateur a déclaré votre abandon de candidature</div>`,
-      content: `
-      <p>Nous vous informons que dans le cadre de votre parcours VAE, votre conseiller a placé votre candidature en abandon.</p>
-      <p>Si vous souhaitez contester cette décision veuillez nous contacter rapidement sur le mail <a href="mailto:support@vae.gouv.fr">support@vae.gouv.fr</a>.</p>
-      <p>Sans retour de votre part, l’abandon sera effectif dans un délai de 15 jours à compter de la réception de ce message.</p>
-      <p>L’équipe France VAE.</p>
+  const htmlContent = (url: string) =>
+    mjml2html(
+      templateMail({
+        content: `<p>Nous vous informons que dans le cadre de votre parcours VAE, votre conseiller a placé votre candidature en abandon.</p>
+        <p>Vous avez la possibilité de confirmer cette décision ou de la contester afin de poursuivre votre parcours. 
+        Sans action de votre part dans un délai de 6 mois, votre parcours sera automatiquement abandonné.</p>
         `,
-    }),
-  );
+        labelCTA: "Accéder à mon espace",
+        url,
+        bottomLine: `<p>L’équipe France VAE.</p>`,
+      }),
+    );
 
-  return sendGenericEmail({
+  const token = generateJwt({ email, action: "login" }, 1 * 60 * 60 * 24 * 4);
+
+  return sendEmailWithLink({
     to: { email },
-    htmlContent: htmlContent.html,
+    token,
+    action: "login",
+    htmlContent,
     subject: "Votre accompagnateur a déclaré votre abandon de candidature",
+    app: "candidate",
   });
 };
