9e5e79088df3141664182fb5a4bb02eeb399366c
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-01-14 15:05:36 +0100

    feat(admin): added an HelpBubble component

---
+++ packages/reva-admin-react/src/components/help-bubble/HelpBubble.tsx
@@ -0,0 +1,79 @@
+import { useState, useCallback } from "react";
+import helpIcon from "./assets/help.svg";
+import closeIcon from "./assets/close.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+const helpLinks = [
+  {
+    label: "Nouvelles fonctionnalités",
+    url: "https://www.notion.so/fabnummas/Nouveaut-s-produit-42e539695d68436abe32fcf4b146c192?pvs=4",
+  },
+  {
+    label: "Espace documentaire",
+    url: "https://www.notion.so/fabnummas/f697c4fa5fcf42d49d85428b5e0b40c5?v=9f55d3b4b2e54bd19d390ebe6febe3ea",
+  },
+  {
+    label: "Cahier des charges AAP",
+    url: "https://fabnummas.notion.site/Cahier-des-charges-ea8790303ab447cfb25b5c11c26b0d67",
+  },
+  {
+    label: "En savoir plus sur la VAE",
+    url: "https://vae.gouv.fr/savoir-plus/",
+  },
+  {
+    label: "FAQ AAP",
+    url: "https://vae.gouv.fr/faq/",
+  },
+  {
+    label: "Calendrier des webinaires",
+    url: "https://fabnummas.notion.site/Calendrier-des-webinaires-b93fb857a9ab458e934802ebbc605073",
+  },
+];
+
+export const HelpBubble = ({ className }: { className?: string }) => {
+  const [open, setOpen] = useState(false);
+  const toggle = useCallback(() => setOpen(!open), [open]);
+
+  return (
+    <div className={`flex flex-col ${className || ""}`}>
+      {open ? (
+        <div className="flex flex-col items-start w-[320px] bg-white rounded-2xl p-6 pt-4 shadow-[0px_6px_18px_0px_rgba(0,0,18,0.16)]">
+          <h1 className="text-xl font-bold mb-0">Notre aide en ligne</h1>
+          <ul className="list-none pl-0">
+            {helpLinks.map(({ label, url }) => (
+              <li key={url}>
+                <Link target="_blank" className="fr-link" href={url}>
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
+
+      <ToggleButton open={open} onClick={toggle} className="ml-auto mt-5" />
+    </div>
+  );
+};
+
+const ToggleButton = ({
+  open,
+  className,
+  onClick,
+}: {
+  open: boolean;
+  className?: string;
+  onClick?: () => void;
+}) => (
+  <button
+    className={`w-[54px] h-[54px] md:w-[60px] md:h-[60px] flex items-center justify-center bg-white shadow-[0px_6px_18px_0px_rgba(0,0,18,0.16)] rounded-full ${className || ""}`}
+    onClick={onClick}
+  >
+    {open ? (
+      <Image src={closeIcon} alt="fermer" />
+    ) : (
+      <Image src={helpIcon} alt="aide" />
+    )}
+  </button>
+);
