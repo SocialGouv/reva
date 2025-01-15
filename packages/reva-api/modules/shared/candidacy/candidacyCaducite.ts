5cce5e2a526cee6b1c86e25d453cca46f77f918b
ThomasDos <dosanjos.thomas@gmail.com>
2025-01-15 15:48:25 +0100

    feat(api): add ACTUALISATION_THRESHOLD_DAYS constant to candidacy module

--- packages/reva-api/modules/shared/candidacy/candidacyCaducite.ts
+++ packages/reva-api/modules/shared/candidacy/candidacyCaducite.ts
@@ -2,6 +2,8 @@ import { CandidacyStatusStep } from "@prisma/client";
 
 export const CADUCITE_THRESHOLD_DAYS = 183;
 
+export const ACTUALISATION_THRESHOLD_DAYS = 166;
+
 export const CADUCITE_VALID_STATUSES = [
   "DOSSIER_FAISABILITE_RECEVABLE",
   "DOSSIER_DE_VALIDATION_SIGNALE",
