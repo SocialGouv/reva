ea179fa9362e3bf51f6b963c3d4bc20dc7d5bee8
ThomasDos <dosanjos.thomas@gmail.com>
2025-01-21 11:31:43 +0100

    feat(api): enhance updateContestationCertificationAuthorityDecision to include email notifications for confirmed and invalidated candidacy contestation decisions

--- packages/reva-api/modules/candidacy/candidacy-contestation-caducite/features/updateContestationCertificationAuthorityDecision.ts
+++ packages/reva-api/modules/candidacy/candidacy-contestation-caducite/features/updateContestationCertificationAuthorityDecision.ts
@@ -1,6 +1,9 @@
 import { prismaClient } from "../../../../prisma/client";
 import { logCandidacyAuditEvent } from "../../../candidacy-log/features/logCandidacyAuditEvent";
 import { UpdateCandidacyContestationCaduciteInput } from "../candidacy-contestation-caducite.types";
+import { sendCandidacyContestationCaduciteConfirmedEmailToAap } from "../emails/sendCandidacyContestationCaduciteConfirmedEmailToAap";
+import { sendCandidacyContestationCaduciteConfirmedEmailToCandidate } from "../emails/sendCandidacyContestationCaduciteConfirmedEmailToCandidate";
+import { sendCandidacyContestationCaduciteInvalidatedEmailToAap } from "../emails/sendCandidacyContestationCaduciteInvalidatedEmailToAap";
 
 export const updateContestationCertificationAuthorityDecision = async ({
   input,
@@ -17,6 +20,14 @@ export const updateContestationCertificationAuthorityDecision = async ({
         candidacyId,
         certificationAuthorityContestationDecision: "DECISION_PENDING",
       },
+      include: {
+        candidacy: {
+          include: {
+            candidate: true,
+            organism: true,
+          },
+        },
+      },
     });
 
   if (!candidacyContestationCaducite) {
@@ -25,8 +36,14 @@ export const updateContestationCertificationAuthorityDecision = async ({
     );
   }
 
+  if (!candidacyContestationCaducite.candidacy) {
+    throw new Error("La candidature n'existe pas");
+  }
+
   const decisionIsInvalidated =
     certificationAuthorityContestationDecision === "CADUCITE_INVALIDATED";
+  const decisionIsConfirmed =
+    certificationAuthorityContestationDecision === "CADUCITE_CONFIRMED";
 
   const updatedCandidacyContestationCaducite =
     await prismaClient.candidacyContestationCaducite.update({
@@ -38,6 +55,38 @@ export const updateContestationCertificationAuthorityDecision = async ({
         }),
       },
     });
+
+  const candidateFullName = `${candidacyContestationCaducite.candidacy.candidate?.firstname} ${candidacyContestationCaducite.candidacy.candidate?.lastname}`;
+  const candidateEmail = candidacyContestationCaducite.candidacy.candidate
+    ?.email as string;
+  const aapEmail =
+    candidacyContestationCaducite.candidacy.organism?.emailContact ||
+    candidacyContestationCaducite.candidacy.organism
+      ?.contactAdministrativeEmail;
+
+  const aapLabel =
+    candidacyContestationCaducite.candidacy.organism?.nomPublic ||
+    candidacyContestationCaducite.candidacy.organism?.label;
+
+  if (decisionIsConfirmed) {
+    await sendCandidacyContestationCaduciteConfirmedEmailToCandidate({
+      candidateFullName,
+      candidateEmail,
+    });
+    await sendCandidacyContestationCaduciteConfirmedEmailToAap({
+      aapLabel: aapLabel || "",
+      aapEmail: aapEmail || "",
+      candidateFullName,
+    });
+  }
+
+  if (decisionIsInvalidated) {
+    await sendCandidacyContestationCaduciteInvalidatedEmailToAap({
+      candidateFullName,
+      aapLabel: aapLabel || "",
+      aapEmail: aapEmail || "",
+    });
+  }
 
   await logCandidacyAuditEvent({
     candidacyId,
