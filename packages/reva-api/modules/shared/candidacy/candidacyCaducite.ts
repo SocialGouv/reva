b2847630ead07d5ec7d1fb97c865f2c3229b8993
ThomasDos <dosanjos.thomas@gmail.com>
2025-01-22 11:44:57 +0100

    feat(api): add new query structure for candidacy caducity with enhanced status validation

--- packages/reva-api/modules/shared/candidacy/candidacyCaducite.ts
+++ packages/reva-api/modules/shared/candidacy/candidacyCaducite.ts
@@ -8,5 +8,42 @@ export const CADUCITE_VALID_STATUSES = [
   "DOSSIER_FAISABILITE_RECEVABLE",
   "DOSSIER_DE_VALIDATION_SIGNALE",
   "DEMANDE_FINANCEMENT_ENVOYE",
+] as CandidacyStatusStep[];
+
+export const CADUCITE_VALID_STATUSES_WITH_INCOMPLETE_DOSSIER_DE_VALIDATION = [
   "DEMANDE_PAIEMENT_ENVOYEE",
 ] as CandidacyStatusStep[];
+
+export const WHERE_CLAUSE_CANDIDACY_CADUQUE_AND_ACTUALISATION = {
+  OR: [
+    {
+      AND: [
+        {
+          status: {
+            in: CADUCITE_VALID_STATUSES_WITH_INCOMPLETE_DOSSIER_DE_VALIDATION,
+          },
+        },
+        {
+          dossierDeValidation: {
+            some: {
+              isActive: true,
+              decision: "INCOMPLETE" as const,
+            },
+          },
+        },
+      ],
+    },
+    {
+      status: {
+        in: CADUCITE_VALID_STATUSES,
+      },
+    },
+  ],
+  Feasibility: {
+    some: {
+      isActive: true,
+      decision: "ADMISSIBLE" as const,
+    },
+  },
+  candidacyDropOut: null,
+};
