e8ec053b46f77a5a067080c46fc68daac69f846f
Cédric Soulas <soulascedric@gmail.com>
2025-01-22 16:16:54 +0100

    test(api): add tests for jury results

--- packages/reva-api/modules/jury/features/updateResultOfJury.test.ts
+++ packages/reva-api/modules/jury/features/updateResultOfJury.test.ts
@@ -1,6 +1,3 @@
-/**
- * @jest-environment ./test/fastify-test-env.ts
- */
 import { clearDatabase } from "../../../test/jestClearDatabaseBeforeEachTestFile";
 import { prismaClient } from "../../../prisma/client";
 import { injectGraphql } from "../../../test/helpers/graphql-helper";
@@ -9,11 +6,21 @@ import { createJuryHelper } from "../../../test/helpers/entities/create-jury-hel
 import { startOfYesterday } from "date-fns";
 import { createCandidacyHelper } from "../../../test/helpers/entities/create-candidacy-helper";
 import { createFeasibilityUploadedPdfHelper } from "../../../test/helpers/entities/create-feasibility-uploaded-pdf-helper";
+import * as SendJuryResultCandidateEmailModule from "../emails/sendJuryResultCandidateEmail";
+import * as SendJuryResultAAPEmailModule from "../emails/sendJuryResultAAPEmail";
+import { buildApp } from "../../../infra/server/app";
+import keycloakPluginMock from "../../../test/mocks/keycloak-plugin.mock";
 
 const yesterday = startOfYesterday();
 
+beforeAll(async () => {
+  const app = await buildApp({ keycloakPluginMock });
+  (global as any).fastify = app;
+});
+
 afterEach(async () => {
   await clearDatabase();
+  await jest.clearAllMocks();
 });
 
 const readyForJuryEstimatedAt = yesterday;
@@ -35,6 +42,34 @@ async function createJuryAndDependenciesHelper() {
   });
 }
 
+async function graphqlUpdateJuryResult({
+  juryId,
+  result,
+}: {
+  juryId: string;
+  result: string;
+}) {
+  return injectGraphql({
+    fastify: (global as any).fastify,
+    authorization: authorizationHeaderForUser({
+      role: "admin",
+      keycloakId: "3c6d4571-da18-49a3-90e5-cc83ae7446bf",
+    }),
+    payload: {
+      requestType: "mutation",
+      endpoint: "jury_updateResult",
+      arguments: {
+        juryId,
+        input: {
+          result,
+        },
+      },
+      enumFields: ["result"],
+      returnFields: "{ id }",
+    },
+  });
+}
+
 const failedJuryResults = [
   "PARTIAL_SUCCESS_OF_FULL_CERTIFICATION",
   "PARTIAL_SUCCESS_OF_PARTIAL_CERTIFICATION",
@@ -47,24 +82,9 @@ failedJuryResults.forEach((failedResult) => {
   test(`should reset ready jury estimated date when submitting failed jury result ${failedResult}`, async () => {
     const jury = await createJuryAndDependenciesHelper();
 
-    await injectGraphql({
-      fastify: (global as any).fastify,
-      authorization: authorizationHeaderForUser({
-        role: "admin",
-        keycloakId: "3c6d4571-da18-49a3-90e5-cc83ae7446bf",
-      }),
-      payload: {
-        requestType: "mutation",
-        endpoint: "jury_updateResult",
-        arguments: {
-          juryId: jury.id,
-          input: {
-            result: failedResult,
-          },
-        },
-        enumFields: ["result"],
-        returnFields: "{id}",
-      },
+    await graphqlUpdateJuryResult({
+      juryId: jury.id,
+      result: failedResult,
     });
 
     const candidacyUpdated = await prismaClient.candidacy.findUnique({
@@ -86,27 +106,12 @@ const successfulJuryResults = [
 ];
 
 successfulJuryResults.forEach((successfulResult) => {
-  test("should keep ready jury estimated date when submitting full jury result", async () => {
+  test(`should keep ready jury estimated date when submitting full jury result ${successfulResult}`, async () => {
     const jury = await createJuryAndDependenciesHelper();
 
-    await injectGraphql({
-      fastify: (global as any).fastify,
-      authorization: authorizationHeaderForUser({
-        role: "admin",
-        keycloakId: "3c6d4571-da18-49a3-90e5-cc83ae7446bf",
-      }),
-      payload: {
-        requestType: "mutation",
-        endpoint: "jury_updateResult",
-        arguments: {
-          juryId: jury.id,
-          input: {
-            result: successfulResult,
-          },
-        },
-        enumFields: ["result"],
-        returnFields: "{id}",
-      },
+    await graphqlUpdateJuryResult({
+      juryId: jury.id,
+      result: successfulResult,
     });
 
     const candidacyUpdated = await prismaClient.candidacy.findUnique({
@@ -122,4 +127,77 @@ successfulJuryResults.forEach((successfulResult) => {
       readyForJuryEstimatedAt,
     );
   });
+});
+
+test("should save jury result without errors", async () => {
+  const jury = await createJuryAndDependenciesHelper();
+
+  const resp = await graphqlUpdateJuryResult({
+    juryId: jury.id,
+    result: "FULL_SUCCESS_OF_FULL_CERTIFICATION",
+  });
+
+  const juryUpdated = await prismaClient.jury.findUnique({
+    where: { id: jury.id },
+  });
+
+  expect(resp.statusCode).toEqual(200);
+  expect(resp.json()).not.toHaveProperty("errors");
+  expect(juryUpdated?.result).toEqual("FULL_SUCCESS_OF_FULL_CERTIFICATION");
+});
+
+test("should send jury result to candidate", async () => {
+  const sendJuryResultCandidateEmailSpy = jest.spyOn(
+    SendJuryResultCandidateEmailModule,
+    "sendJuryResultCandidateEmail",
+  );
+
+  const jury = await createJuryAndDependenciesHelper();
+
+  await graphqlUpdateJuryResult({
+    juryId: jury.id,
+    result: "FULL_SUCCESS_OF_FULL_CERTIFICATION",
+  });
+
+  expect(sendJuryResultCandidateEmailSpy).toHaveBeenCalledWith({
+    email: jury.candidacy.candidate?.email,
+  });
+  expect(await sendJuryResultCandidateEmailSpy.mock.results[0].value).toBe(
+    `email sent to ${jury.candidacy.candidate?.email}`,
+  );
+});
+
+test("should send jury result to organism", async () => {
+  const sendJuryResultAAPEmailSpy = jest.spyOn(
+    SendJuryResultAAPEmailModule,
+    "sendJuryResultAAPEmail",
+  );
+
+  const jury = await createJuryAndDependenciesHelper();
+
+  if (!jury.candidacy.organismId) {
+    throw new Error("OrganismId is not defined");
+  }
+
+  const organism = await prismaClient.organism.findUnique({
+    where: { id: jury.candidacy.organismId },
+  });
+
+  if (!organism) {
+    throw new Error("No organism found");
+  }
+
+  await graphqlUpdateJuryResult({
+    juryId: jury.id,
+    result: "FULL_SUCCESS_OF_FULL_CERTIFICATION",
+  });
+
+  expect(sendJuryResultAAPEmailSpy).toHaveBeenCalledWith({
+    candidacyId: jury.candidacy.id,
+    email: organism?.contactAdministrativeEmail,
+    candidateFullName: `${jury.candidacy.candidate?.firstname} ${jury.candidacy.candidate?.lastname}`,
+  });
+  expect(await sendJuryResultAAPEmailSpy.mock.results[0].value).toBe(
+    `email sent to ${organism.contactAdministrativeEmail}`,
+  );
 });
