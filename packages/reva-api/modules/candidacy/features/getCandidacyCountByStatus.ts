2e151e11307a5d65fe031119942d5f2bd4ca2bc2
ThomasDos <dosanjos.thomas@gmail.com>
2025-01-22 15:03:42 +0100

    refactor(api): move raw sql where clause query in candidacyCaducite

--- packages/reva-api/modules/candidacy/features/getCandidacyCountByStatus.ts
+++ packages/reva-api/modules/candidacy/features/getCandidacyCountByStatus.ts
@@ -1,10 +1,6 @@
 import { Prisma } from "@prisma/client";
 import { prismaClient } from "../../../prisma/client";
-import {
-  CADUCITE_THRESHOLD_DAYS,
-  CADUCITE_VALID_STATUSES,
-  CADUCITE_VALID_STATUSES_WITH_INCOMPLETE_DOSSIER_DE_VALIDATION,
-} from "../../shared/candidacy/candidacyCaducite";
+import { WHERE_CLAUSE_RAW_CANDIDACY_CADUQUE_AND_ACTUALISATION } from "../../shared/candidacy/candidacyCaducite";
 import {
   CandidacyStatusFilter,
   candidacyStatusFilters,
@@ -204,32 +200,7 @@ const getSQLSelectSumClauseFromStatusFilter = (
         `candidacy.status in ('DOSSIER_FAISABILITE_ENVOYE','DOSSIER_FAISABILITE_COMPLET') and candidacyDropOut.candidacy_id is null`,
       );
     case "CADUQUE":
-      return getSumClause(
-        `(
-          (
-            candidacy.status in (${CADUCITE_VALID_STATUSES_WITH_INCOMPLETE_DOSSIER_DE_VALIDATION.map(
-              (status) => `'${status}'`,
-            ).join(", ")})
-            and exists (
-              select 1 from "dossier_de_validation" ddv
-              where ddv.candidacy_id = candidacy.id
-              and ddv.is_active = true
-              and ddv.decision = 'INCOMPLETE'
-            )
-          )
-          or candidacy.status in (${CADUCITE_VALID_STATUSES.map(
-            (status) => `'${status}'`,
-          ).join(", ")})
-        )
-        and exists (
-          select 1 from "feasibility" f
-          where f.candidacy_id = candidacy.id
-          and f.is_active = true
-          and f.decision = 'ADMISSIBLE'
-        )
-        and candidacyDropOut.candidacy_id is null
-        and DATE_PART('day', NOW() - candidacy.last_activity_date) > ${CADUCITE_THRESHOLD_DAYS}`,
-      );
+      return getSumClause(WHERE_CLAUSE_RAW_CANDIDACY_CADUQUE_AND_ACTUALISATION);
   }
 };
 
--- packages/reva-api/modules/shared/candidacy/candidacyCaducite.ts
+++ packages/reva-api/modules/shared/candidacy/candidacyCaducite.ts
@@ -4,15 +4,15 @@ export const CADUCITE_THRESHOLD_DAYS = 183;
 
 export const ACTUALISATION_THRESHOLD_DAYS = 166;
 
-export const CADUCITE_VALID_STATUSES = [
+const CADUCITE_VALID_STATUSES = [
   "DOSSIER_FAISABILITE_RECEVABLE",
   "DOSSIER_DE_VALIDATION_SIGNALE",
   "DEMANDE_FINANCEMENT_ENVOYE",
-] as CandidacyStatusStep[];
+] satisfies CandidacyStatusStep[];
 
-export const CADUCITE_VALID_STATUSES_WITH_INCOMPLETE_DOSSIER_DE_VALIDATION = [
+const CADUCITE_VALID_STATUSES_WITH_INCOMPLETE_DOSSIER_DE_VALIDATION = [
   "DEMANDE_PAIEMENT_ENVOYEE",
-] as CandidacyStatusStep[];
+] satisfies CandidacyStatusStep[];
 
 export const WHERE_CLAUSE_CANDIDACY_CADUQUE_AND_ACTUALISATION = {
   OR: [
@@ -47,3 +47,28 @@ export const WHERE_CLAUSE_CANDIDACY_CADUQUE_AND_ACTUALISATION = {
   },
   candidacyDropOut: null,
 };
+
+export const WHERE_CLAUSE_RAW_CANDIDACY_CADUQUE_AND_ACTUALISATION = `(
+          (
+            candidacy.status in (${CADUCITE_VALID_STATUSES_WITH_INCOMPLETE_DOSSIER_DE_VALIDATION.map(
+              (status) => `'${status}'`,
+            ).join(", ")})
+            and exists (
+              select 1 from "dossier_de_validation" ddv
+              where ddv.candidacy_id = candidacy.id
+              and ddv.is_active = true
+              and ddv.decision = 'INCOMPLETE'
+            )
+          )
+          or candidacy.status in (${CADUCITE_VALID_STATUSES.map(
+            (status) => `'${status}'`,
+          ).join(", ")})
+        )
+        and exists (
+          select 1 from "feasibility" f
+          where f.candidacy_id = candidacy.id
+          and f.is_active = true
+          and f.decision = 'ADMISSIBLE'
+        )
+        and candidacyDropOut.candidacy_id is null
+        and DATE_PART('day', NOW() - candidacy.last_activity_date) > ${CADUCITE_THRESHOLD_DAYS}`;
