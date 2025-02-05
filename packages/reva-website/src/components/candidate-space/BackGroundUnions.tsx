d6f54c962106ed09656c75f04ab6c626c232b7e3
Pierre A <pierre@avizou.eu>
2025-02-05 17:02:51 +0100

    feat(website):allow background component to render only a signe polygon

--- packages/reva-website/src/components/candidate-space/BackGroundUnions.tsx
+++ packages/reva-website/src/components/candidate-space/BackGroundUnions.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const BackGroundUnions = () => (
+const BackGroundUnions = ({ double = true }: { double?: boolean }) => (
   <div className="absolute -top-8 -z-10 w-full">
     <div className="hidden lg:block">
       <Image
@@ -11,15 +11,17 @@ const BackGroundUnions = () => (
         alt="rayon rose en fond d'écran"
       />
     </div>
-    <div className="top-[1190px] hidden lg:block">
-      <Image
-        src="/candidate-space/unions-background/union-background2.svg"
-        width={3000}
-        height={1074}
-        style={{ width: "100%" }}
-        alt="rayon rose en fond d'écran"
-      />
-    </div>
+    {double && (
+      <div className="top-[1190px] hidden lg:block">
+        <Image
+          src="/candidate-space/unions-background/union-background2.svg"
+          width={3000}
+          height={1074}
+          style={{ width: "100%" }}
+          alt="rayon rose en fond d'écran"
+        />
+      </div>
+    )}
   </div>
 );
 
