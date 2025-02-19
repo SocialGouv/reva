02c70c3134ead72aa0bc4138ef02e0bb369ac1e1
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-02-19 16:22:34 +0100

    feat(api): candidate dropout email can now use brevo email templates

--- packages/reva-api/modules/candidacy/emails/sendCandidacyDropOutEmailToCandidate.ts
+++ packages/reva-api/modules/candidacy/emails/sendCandidacyDropOutEmailToCandidate.ts
@@ -1,30 +1,47 @@
 import mjml2html from "mjml";
 
-import { sendEmailWithLink, templateMail } from "../../shared/email";
+import {
+  sendEmailUsingTemplate,
+  sendEmailWithLink,
+  templateMail,
+} from "../../shared/email";
 import { generateJwt } from "../../candidate/auth.helper";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
 export const sendCandidacyDropOutEmailToCandidate = async (email: string) => {
-  const htmlContent = (url: string) =>
-    mjml2html(
-      templateMail({
-        content: `<p>Nous vous informons que dans le cadre de votre parcours VAE, votre conseiller a placé votre candidature en abandon.</p>
-        <p>Vous avez la possibilité de confirmer cette décision ou de la contester afin de poursuivre votre parcours. 
-        Sans action de votre part dans un délai de 6 mois, votre parcours sera automatiquement abandonné.</p>
-        `,
-        labelCTA: "Accéder à mon espace",
-        url,
-        bottomLine: `<p>L’équipe France VAE.</p>`,
-      }),
-    );
-
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES",
+  });
   const token = generateJwt({ email, action: "login" }, 1 * 60 * 60 * 24 * 4);
 
-  return sendEmailWithLink({
-    to: { email },
-    token,
-    action: "login",
-    htmlContent,
-    subject: "Votre accompagnateur a déclaré votre abandon de candidature",
-    app: "candidate",
-  });
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 491,
+      params: {
+        candidateLoginUrl: `${process.env.BASE_URL}/candidat/login?token=${token}`,
+      },
+    });
+  } else {
+    const htmlContent = (url: string) =>
+      mjml2html(
+        templateMail({
+          content: `<p>Nous vous informons que dans le cadre de votre parcours VAE, votre conseiller a placé votre candidature en abandon.</p>
+          <p>Vous avez la possibilité de confirmer cette décision ou de la contester afin de poursuivre votre parcours. 
+          Sans action de votre part dans un délai de 6 mois, votre parcours sera automatiquement abandonné.</p>
+          `,
+          labelCTA: "Accéder à mon espace",
+          url,
+          bottomLine: `<p>L’équipe France VAE.</p>`,
+        }),
+      );
+    return sendEmailWithLink({
+      to: { email },
+      token,
+      action: "login",
+      htmlContent,
+      subject: "Votre accompagnateur a déclaré votre abandon de candidature",
+      app: "candidate",
+    });
+  }
 };
