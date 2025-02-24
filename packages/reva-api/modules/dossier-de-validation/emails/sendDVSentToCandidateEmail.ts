5e38d63a12d9a26d9dcbeefe8a7881c23e8e65b9
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-02-24 15:51:08 +0100

    feat(api): linked candidate dv  sent confirmation email to its brevo template

--- packages/reva-api/modules/dossier-de-validation/emails/sendDVSentToCandidateEmail.ts
+++ packages/reva-api/modules/dossier-de-validation/emails/sendDVSentToCandidateEmail.ts
@@ -1,26 +1,46 @@
 import mjml2html from "mjml";
 
-import { sendGenericEmail, templateMail } from "../../shared/email";
+import {
+  sendEmailUsingTemplate,
+  sendGenericEmail,
+  templateMail,
+} from "../../shared/email";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
-export const sendDVSentToCandidateEmail = ({ email }: { email: string }) => {
-  const htmlContent = mjml2html(
-    templateMail({
-      content: `
+export const sendDVSentToCandidateEmail = async ({
+  email,
+}: {
+  email: string;
+}) => {
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_CANDIDATE_EMAILS",
+  });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 512,
+    });
+  } else {
+    const htmlContent = mjml2html(
+      templateMail({
+        content: `
           <p>Nous vous confirmons que votre dossier de validation a bien été déposé et qu’il sera prochainement consulté par les membres de jury.</p>
           <p>Vous recevrez une convocation environ 15 jours avant votre date de jury.</p>
           <p>Il est prévu que le jury se tienne sous 3 mois, néanmoins certains délais organisationnels peuvent prolonger ce délai.</p>
           <p>Le cas échéant, vous pouvez contacter votre accompagnateur afin qu’il fasse une relance auprès du service concerné.</p>
           `,
-      bottomLine: `
+        bottomLine: `
           <p>Cordialement,</p>
           <p>L'équipe France VAE</p>
           `,
-    }),
-  ).html;
+      }),
+    ).html;
 
-  return sendGenericEmail({
-    to: { email },
-    htmlContent,
-    subject: "Votre dossier de validation a été transmis",
-  });
+    return sendGenericEmail({
+      to: { email },
+      htmlContent,
+      subject: "Votre dossier de validation a été transmis",
+    });
+  }
 };
