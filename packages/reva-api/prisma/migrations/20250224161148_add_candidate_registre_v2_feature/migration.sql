64a027bfd76a5071098cc840a3f61992b4b5f83f
Cédric Soulas <soulascedric@gmail.com>
2025-02-24 17:16:54 +0100

    feat(api): add feature flag for new candidate registration

---
+++ packages/reva-api/prisma/migrations/20250224161148_add_candidate_registre_v2_feature/migration.sql
@@ -0,0 +1,3 @@
+insert into features (key, label, description, is_active)
+values ('CANDIDATE_REGISTRATION_V2', 'Nouveau parcours d''inscription candidat',
+        'La candidat s''inscrit via un formulaire en deux étapes', false);
