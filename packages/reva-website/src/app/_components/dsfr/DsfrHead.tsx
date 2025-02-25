ef9e0fd4c17f99b02f3d33db3ce3fc6404c83f1c
Pierre A <pierre@avizou.eu>
2025-02-25 16:00:01 +0100

    feat(website):use Next Link component with DSFR

--- packages/reva-website/src/app/_components/dsfr/DsfrHead.tsx
+++ packages/reva-website/src/app/_components/dsfr/DsfrHead.tsx
@@ -2,16 +2,16 @@ import { getAssetUrl } from "@codegouvfr/react-dsfr/tools/getAssetUrl";
 import AppleTouchIcon from "@codegouvfr/react-dsfr/dsfr/favicon/apple-touch-icon.png";
 import FaviconSvg from "@codegouvfr/react-dsfr/dsfr/favicon/favicon.svg";
 import FaviconIco from "@codegouvfr/react-dsfr/dsfr/favicon/favicon.ico";
-// import { setLink } from "@codegouvfr/react-dsfr/link";
-// import Link from "next/link";
-// import { useMemo } from "react";
+import { setLink } from "@codegouvfr/react-dsfr/link";
+import Link from "next/link";
+import { useMemo } from "react";
 
 export function DsfrHead() {
-  // useMemo(() => {
-  //   if (Link !== undefined) {
-  //     setLink({ Link });
-  //   }
-  // }, []);
+  useMemo(() => {
+    if (Link !== undefined) {
+      setLink({ Link });
+    }
+  }, []);
   return (
     <>
       <link rel="apple-touch-icon" href={getAssetUrl(AppleTouchIcon)} />
