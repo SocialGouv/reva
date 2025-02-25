e737b3ae52725887724a12a5ec785f85a97cf3a5
ThomasDos <dosanjos.thomas@gmail.com>
2025-02-25 16:35:11 +0100

    feat(candidate): add autocomplete address component with address search functionality

---
+++ packages/reva-candidate/src/components/autocomplete-address/AutocompleteAddress.tsx
@@ -0,0 +1,150 @@
+import Input from "@codegouvfr/react-dsfr/Input";
+import { DetailedHTMLProps, InputHTMLAttributes, useState } from "react";
+import {
+  AddressOption,
+  useAutocompleteAddress,
+} from "./useAutocompleteAddress.hook";
+
+export const AutocompleteAddress = ({
+  onOptionSelection,
+  className,
+  nativeInputProps,
+  state,
+  stateRelatedMessage,
+}: {
+  onOptionSelection: (selectedOption: AddressOption) => void;
+  className?: string;
+  nativeInputProps?:
+    | DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+    | undefined;
+  state?: "error" | "success" | "info" | "default";
+  stateRelatedMessage?: string;
+}) => {
+  const [searchText, setSearchText] = useState("");
+  const { data: options = [], status } = useAutocompleteAddress({
+    search: searchText,
+  });
+
+  const [selectedOption, setSelectedOption] = useState<AddressOption | null>(
+    null,
+  );
+
+  const [displayOptions, setDisplayOptions] = useState(true);
+
+  const updateSearchText = async (newSearchText: string) => {
+    setSearchText(newSearchText);
+  };
+
+  const handleKeyDownOnOptions = (e: React.KeyboardEvent) => {
+    switch (e.key) {
+      case "Enter":
+        e.preventDefault();
+        handleSubmit();
+        break;
+      case "Escape":
+        setDisplayOptions(false);
+        break;
+      case "ArrowDown":
+        e.preventDefault();
+        if (options.length) {
+          const index = options.findIndex(
+            (option) => option.label === selectedOption?.label,
+          );
+          const nextOption = options[index + 1] || options[0];
+          setSelectedOption(nextOption);
+        }
+        break;
+      case "ArrowUp":
+        e.preventDefault();
+        if (options.length) {
+          const index = options.findIndex(
+            (option) => option.label === selectedOption?.label,
+          );
+          const nextOption = options[index - 1] || options[options.length - 1];
+          setSelectedOption(nextOption);
+        }
+        break;
+      case "Tab":
+        setDisplayOptions(false);
+        break;
+    }
+  };
+
+  const handleOptionSelection = (newSelectedOption: AddressOption) => {
+    setSelectedOption(newSelectedOption);
+    onOptionSelection?.(newSelectedOption);
+    setSearchText(newSelectedOption.label);
+  };
+
+  const handleSubmit = () => {
+    if (!selectedOption) {
+      return;
+    }
+
+    handleOptionSelection(selectedOption);
+    setDisplayOptions(false);
+  };
+
+  return (
+    <div data-testid="autocomplete" className={`relative ${className}`}>
+      <Input
+        nativeInputProps={{
+          ...nativeInputProps,
+          onKeyDown: handleKeyDownOnOptions,
+          onChange: (event) => {
+            updateSearchText(event.target.value);
+
+            setDisplayOptions(true);
+          },
+          value: searchText,
+          onBlur: () => {
+            setTimeout(() => setDisplayOptions(false), 200);
+          },
+          onFocus: () => setDisplayOptions(true),
+          onClick: () => setDisplayOptions(true),
+        }}
+        state={state}
+        stateRelatedMessage={stateRelatedMessage}
+        label="Adresse complète"
+        data-testid="autocomplete-input"
+        iconId="fr-icon-map-pin-2-fill"
+      />
+      {status === "success" && displayOptions && options.length > 0 && (
+        <div
+          data-testid="autocomplete-options"
+          className="absolute z-10 max-h-[500px] list-none overflow-y-auto top-[75px] whitespace-normal w-full bg-white border-[1px] border-gray-300 px-4 py-2 shadow-[0px_2px_6px_0px_rgba(0,0,18,0.16)]"
+        >
+          {options.map((option) => {
+            const isSelected = selectedOption?.label === option.label;
+            return (
+              <div
+                key={option.label}
+                onClick={() => handleOptionSelection(option)}
+                className={`whitespace-normal cursor-pointer select-none py-2  ${
+                  isSelected ? "bg-dsfrGray-contrast" : ""
+                }`}
+                onMouseOver={() => setSelectedOption(option)}
+              >
+                {option.label}
+              </div>
+            );
+          })}
+        </div>
+      )}
+
+      {status === "error" && (
+        <div
+          data-testid="autocomplete-options"
+          className="absolute z-10 max-h-[500px] list-none overflow-y-auto top-[75px] whitespace-normal w-full bg-white border-[1px] border-gray-300 px-4 py-2 shadow-[0px_2px_6px_0px_rgba(0,0,18,0.16)]"
+        >
+          <div
+            key="empty-label"
+            className="whitespace-normal cursor-default select-none py"
+          >
+            Une erreur est survenue lors de la recherche
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ packages/reva-candidate/src/components/autocomplete-address/useAutocompleteAddress.hook.ts
@@ -0,0 +1,80 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+export interface AddressOption {
+  label: string;
+  street: string;
+  zip: string;
+  city: string;
+}
+
+type AutocompleteAddressResponse = {
+  type: string;
+  version: string;
+  features: Array<{
+    type: string;
+    geometry: {
+      type: string;
+      coordinates: [number, number];
+    };
+    properties: {
+      label: string;
+      score: number;
+      housenumber?: string;
+      id: string;
+      banId?: string;
+      name: string;
+      postcode: string;
+      citycode: string;
+      x: number;
+      y: number;
+      city: string;
+      district?: string;
+      context: string;
+      type: string;
+      importance: number;
+      street: string;
+    };
+  }>;
+  attribution: string;
+  licence: string;
+  query: string;
+  limit: number;
+};
+
+export function useAutocompleteAddress({ search }: { search?: string }) {
+  const [debouncedSearch, setDebouncedSearch] = useState(search);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearch(search);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [search]);
+
+  return useQuery<AddressOption[]>({
+    queryKey: ["autocomplete-address", debouncedSearch],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://api-adresse.data.gouv.fr/search/?q=${debouncedSearch}&limit=10`,
+      );
+      const data: AutocompleteAddressResponse = await response.json();
+      if (!data.features.length) {
+        return [];
+      }
+      const result = data.features.map((feature) => {
+        return {
+          label: feature.properties.label,
+          street: feature.properties.name,
+          zip: feature.properties.postcode,
+          city: feature.properties.city,
+        };
+      });
+      return result;
+    },
+    enabled: !!debouncedSearch,
+  });
+}
