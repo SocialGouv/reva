1de530add753e5f5c1059a3fd589ca50798dc65a
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-02-25 14:43:23 +0100

    feat(api): linked candidarte dropout confirmation email to its brevo template

--- packages/reva-api/modules/candidacy/emails/sendCandidacyDropOutConfirmedEmailToCandidate.ts
+++ packages/reva-api/modules/candidacy/emails/sendCandidacyDropOutConfirmedEmailToCandidate.ts
@@ -1,6 +1,11 @@
 import mjml2html from "mjml";
 
-import { sendGenericEmail, templateMail } from "../../shared/email";
+import {
+  sendEmailUsingTemplate,
+  sendGenericEmail,
+  templateMail,
+} from "../../shared/email";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
 export const sendCandidacyDropOutConfirmedEmailToCandidate = async ({
   candidateEmail,
@@ -9,9 +14,22 @@ export const sendCandidacyDropOutConfirmedEmailToCandidate = async ({
   candidateEmail: string;
   candidateFullName: string;
 }) => {
-  const htmlContent = mjml2html(
-    templateMail({
-      content: `
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_CANDIDATE_EMAILS",
+  });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email: candidateEmail },
+      templateId: 518,
+      params: {
+        candidateFullName,
+      },
+    });
+  } else {
+    const htmlContent = mjml2html(
+      templateMail({
+        content: `
       <div>Bonjour ${candidateFullName},</div>
       <p>Vous avez récemment accepté la décision de votre accompagnateur concernant l’abandon de votre parcours VAE. 
       Nous vous informons que ce choix a bien été enregistré et transmis à l’accompagnateur par mail.</p>
@@ -19,12 +37,13 @@ export const sendCandidacyDropOutConfirmedEmailToCandidate = async ({
       <p>Cordialement,</p>
       <p>L’équipe France VAE.</p>
         `,
-    }),
-  );
+      }),
+    );
 
-  return sendGenericEmail({
-    to: { email: candidateEmail },
-    htmlContent: htmlContent.html,
-    subject: "Confirmation de votre abandon de parcours VAE",
-  });
+    return sendGenericEmail({
+      to: { email: candidateEmail },
+      htmlContent: htmlContent.html,
+      subject: "Confirmation de votre abandon de parcours VAE",
+    });
+  }
 };
