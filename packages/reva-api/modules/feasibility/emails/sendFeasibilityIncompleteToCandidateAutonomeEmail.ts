0716118715f5c6a0c78c605e1128b4eccb738bb2
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-02-25 10:48:45 +0100

    feat(api): linked autonomous candidate feasibility incomplete email to its brevo template

--- packages/reva-api/modules/feasibility/emails/sendFeasibilityIncompleteToCandidateAutonomeEmail.ts
+++ packages/reva-api/modules/feasibility/emails/sendFeasibilityIncompleteToCandidateAutonomeEmail.ts
@@ -2,10 +2,12 @@ import mjml2html from "mjml";
 
 import {
   formatFreeText,
+  sendEmailUsingTemplate,
   sendGenericEmail,
   templateMail,
 } from "../../shared/email";
 import { logger } from "../../shared/logger";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
 export const sendFeasibilityIncompleteToCandidateAutonomeEmail = async ({
   email,
@@ -18,17 +20,32 @@ export const sendFeasibilityIncompleteToCandidateAutonomeEmail = async ({
   certificationAuthorityLabel: string;
   certificationName: string;
 }) => {
-  const commentInfo = comment
-    ? `
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_CANDIDATE_EMAILS",
+  });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 514,
+      params: {
+        certificationAuthorityLabel,
+        certificationName,
+        comment: comment,
+      },
+    });
+  } else {
+    const commentInfo = comment
+      ? `
         <br/>
         <p><strong>Voici les remarques faites par le certificateur :</strong></p>
         <p><em>${formatFreeText(comment)}</em></p>
         `
-    : "";
+      : "";
 
-  const htmlContent = mjml2html(
-    templateMail({
-      content: `
+    const htmlContent = mjml2html(
+      templateMail({
+        content: `
         <p>Bonjour,</p>
         <p>Vous trouverez ci-dessous la décision de recevabilité du certificateur ${certificationAuthorityLabel} concernant votre dossier de faisabilité pour la certification ${certificationName}.</p>
         <p>Votre dossier a été jugé incomplet par le certificateur.</p>
@@ -39,20 +56,21 @@ export const sendFeasibilityIncompleteToCandidateAutonomeEmail = async ({
         <p>Cordialement,</p>
         <p>L’équipe France VAE</p>
       `,
-    }),
-  );
+      }),
+    );
 
-  if (htmlContent.errors.length > 0) {
-    const errorMessage = htmlContent.errors
-      .map((e) => e.formattedMessage)
-      .join("\n");
-    logger.error(errorMessage);
-    throw new Error(errorMessage);
+    if (htmlContent.errors.length > 0) {
+      const errorMessage = htmlContent.errors
+        .map((e) => e.formattedMessage)
+        .join("\n");
+      logger.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    return sendGenericEmail({
+      to: { email },
+      htmlContent: htmlContent.html,
+      subject: "Votre dossier de faisabilité VAE est incomplet",
+    });
   }
-
-  return sendGenericEmail({
-    to: { email },
-    htmlContent: htmlContent.html,
-    subject: "Votre dossier de faisabilité VAE est incomplet",
-  });
 };
