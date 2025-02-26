b93ba321c04a560f16bb4694e65d570e73674ede
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-02-26 12:12:53 +0100

    feat(api): linked candidate jury scheduled  email to its brevo template

--- packages/reva-api/modules/jury/emails/sendJuryScheduledCandidateEmail.ts
+++ packages/reva-api/modules/jury/emails/sendJuryScheduledCandidateEmail.ts
@@ -1,7 +1,13 @@
 import mjml2html from "mjml";
 
-import { sendEmailWithLink, templateMail } from "../../shared/email";
+import {
+  sendEmailUsingTemplate,
+  sendEmailWithLink,
+  templateMail,
+} from "../../shared/email";
 import { UploadedFile } from "../../shared/file";
+import { getCandidateAppUrl } from "../../candidate/utils/candidate.url.helpers";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
 export const sendJuryScheduledCandidateEmail = async ({
   email,
@@ -10,26 +16,6 @@ export const sendJuryScheduledCandidateEmail = async ({
   email: string;
   convocationFile?: UploadedFile;
 }) => {
-  const htmlContent = (url: string) =>
-    mjml2html(
-      templateMail({
-        content: `
-      <p>Bonjour,</p>
-      <p>La date de votre passage devant un jury VAE a été planifiée et est disponible dans votre espace candidat.</p>
-    `,
-        labelCTA: "Accéder à mon espace",
-        bottomLine: `
-      <p>${
-        convocationFile
-          ? "Vous trouverez toutes les informations importantes dans la convocation officielle disponible en pièce jointe de cet e-mail."
-          : "Le certificateur vous fera parvenir la convocation officielle par courrier. Vous y trouverez toutes les informations importantes et tous les détails nécessaires."
-      }</p>
-      <p>L'équipe France VAE.</p>
-        `,
-        url,
-      }),
-    );
-
   const attachment = convocationFile
     ? [
         {
@@ -39,11 +25,47 @@ export const sendJuryScheduledCandidateEmail = async ({
       ]
     : undefined;
 
-  return sendEmailWithLink({
-    to: { email },
-    htmlContent,
-    subject: "Convocation de passage devant un jury VAE",
-    attachment,
-    app: "candidate",
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_CANDIDATE_EMAILS",
   });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 523,
+      params: {
+        candidateAppUrl: getCandidateAppUrl(),
+        convocationFilePresent: !!convocationFile,
+      },
+      attachment,
+    });
+  } else {
+    const htmlContent = (url: string) =>
+      mjml2html(
+        templateMail({
+          content: `
+      <p>Bonjour,</p>
+      <p>La date de votre passage devant un jury VAE a été planifiée et est disponible dans votre espace candidat.</p>
+    `,
+          labelCTA: "Accéder à mon espace",
+          bottomLine: `
+      <p>${
+        convocationFile
+          ? "Vous trouverez toutes les informations importantes dans la convocation officielle disponible en pièce jointe de cet e-mail."
+          : "Le certificateur vous fera parvenir la convocation officielle par courrier. Vous y trouverez toutes les informations importantes et tous les détails nécessaires."
+      }</p>
+      <p>L'équipe France VAE.</p>
+        `,
+          url,
+        }),
+      );
+
+    return sendEmailWithLink({
+      to: { email },
+      htmlContent,
+      subject: "Convocation de passage devant un jury VAE",
+      attachment,
+      app: "candidate",
+    });
+  }
 };
