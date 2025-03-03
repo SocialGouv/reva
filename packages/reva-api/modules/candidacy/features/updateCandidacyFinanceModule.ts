4916ce96f7ff5540639e0e9310a987960d2ceb77
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-03-03 16:07:58 +0100

    feat(api): updating a candidacy finance module to "hors_plateforme" when its status is either DEMANDE_FINANCEMENT_ENVOYE or DEMANDE_PAIEMENT_ENVOYEE now roll back the candidacy to the previous status

--- packages/reva-api/modules/candidacy/features/updateCandidacyFinanceModule.ts
+++ packages/reva-api/modules/candidacy/features/updateCandidacyFinanceModule.ts
@@ -4,6 +4,8 @@ import {
   CandidacyAuditLogUserInfo,
   logCandidacyAuditEvent,
 } from "../../candidacy-log/features/logCandidacyAuditEvent";
+import { getCandidacy } from "./getCandidacy";
+import { updateCandidacyStatus } from "./updateCandidacyStatus";
 
 export const updateCandidacyFinanceModule = async ({
   candidacyId,
@@ -14,6 +16,21 @@ export const updateCandidacyFinanceModule = async ({
   financeModule: FinanceModule;
   userInfo: CandidacyAuditLogUserInfo;
 }) => {
+  //if we are updating the finance module to hors_plateforme and the current status is either DEMANDE_PAIEMENT_ENVOYEE or DEMANDE_FINANCEMENT_ENVOYE
+  // we need to rollback the candidacy to the previous status since those two statuses don't make sense with an hors_plateforme candidacy
+  if (financeModule === "hors_plateforme") {
+    const candidacy = await getCandidacy({ candidacyId });
+    if (!candidacy) {
+      throw new Error(`Candidature non trouvée`);
+    }
+    if (
+      candidacy.status === "DEMANDE_PAIEMENT_ENVOYEE" ||
+      candidacy.status === "DEMANDE_FINANCEMENT_ENVOYE"
+    ) {
+      await rollbackToPreviousStatus({ candidacyId });
+    }
+  }
+
   const result = await prismaClient.candidacy.update({
     where: { id: candidacyId },
     data: { financeModule },
@@ -27,4 +44,38 @@ export const updateCandidacyFinanceModule = async ({
   });
 
   return result;
+};
+
+const rollbackToPreviousStatus = async ({
+  candidacyId,
+}: {
+  candidacyId: string;
+}) => {
+  const candidacyWithSortedStatus = await prismaClient.candidacy.findUnique({
+    where: { id: candidacyId },
+    include: { candidacyStatuses: { orderBy: { createdAt: "asc" } } },
+  });
+
+  if (!candidacyWithSortedStatus) {
+    throw new Error(`Candidature non trouvée`);
+  }
+
+  const currentStatus = candidacyWithSortedStatus.status;
+  const currentStatusIndex =
+    candidacyWithSortedStatus.candidacyStatuses.findIndex(
+      (status) => status.status === currentStatus && status.isActive,
+    );
+
+  if (currentStatusIndex < 1) {
+    throw new Error(
+      `Impossible de revenir en arrière dans les statuts de la candidature`,
+    );
+  }
+  const previousStatus =
+    candidacyWithSortedStatus.candidacyStatuses[currentStatusIndex - 1];
+
+  await updateCandidacyStatus({
+    candidacyId,
+    status: previousStatus.status,
+  });
 };
