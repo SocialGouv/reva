61c0a9dff18cdcd3a2e1bc9d155a852f975c5d1d
Cédric Soulas <soulascedric@gmail.com>
2025-03-06 14:33:28 +0100

    feat(api): add feature flag use Brevo templates for emails sent to maisons meres and agences

---
+++ packages/reva-api/prisma/migrations/20250306132618_add_brevo_mail_template_feature_flag_for_organisms/migration.sql
@@ -0,0 +1,7 @@
+insert into features (key, label, description, is_active)
+values ('USE_BREVO_EMAIL_TEMPLATES_FOR_ORGANISM_EMAILS',
+        'Utiliser les templates définis dans Brevo pour envoyer des emails aux maisons mères et aux agences',
+        'Les emails envoyés aux maisons mères et aux agences utiliseront les templates définis sur Brevo plutôt que les messages définis dans le code',
+        false);
+
+
