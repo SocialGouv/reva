4346cda2677d026822739a2bab6bbfbcd259ab14
ThomasDos <dosanjos.thomas@gmail.com>
2025-03-07 11:52:38 +0100

    refactor(candidate): simplify dashboard candidacy query and hook

--- packages/reva-candidate/src/components/dashboard/dashboard.hooks.ts
+++ packages/reva-candidate/src/components/dashboard/dashboard.hooks.ts
@@ -1,39 +1,19 @@
 import { useGraphQlClient } from "@/components/graphql/graphql-client/GraphqlClient";
 import { graphql } from "@/graphql/generated";
-import { candidateCanEditCandidacy } from "@/utils/candidateCanEditCandidacy.util";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 const GET_CANDIDATE_WITH_CANDIDACY = graphql(`
   query candidate_getCandidateWithCandidacyForDashboard {
     candidate_getCandidateWithCandidacy {
-      phone
-      email
-      birthDepartment {
-        id
-      }
-      birthCity
-      city
-      zip
-      street
-      birthdate
-      firstname
-      lastname
-      gender
-      nationality
       candidacy {
-        id
-        typeAccompagnement
         status
-        lastActivityDate
-        isCaduque
+        typeAccompagnement
         firstAppointmentOccuredAt
         candidacyStatuses {
           status
         }
         candidacyDropOut {
-          createdAt
           dropOutConfirmedByCandidate
-          proofReceivedByAdmin
         }
         certification {
           id
@@ -59,38 +39,8 @@ const GET_CANDIDATE_WITH_CANDIDACY = graphql(`
           adresseCodePostal
           adresseVille
         }
-        activeDossierDeValidation {
-          id
-          decision
-          decisionComment
-          decisionSentAt
-        }
-        jury {
-          id
-          dateOfSession
-          timeOfSession
-          timeSpecified
-        }
-        feasibilityFormat
         feasibility {
           id
-          feasibilityFileSentAt
-          decision
-          decisionComment
-          decisionSentAt
-          dematerializedFeasibilityFile {
-            id
-            sentToCandidateAt
-            isReadyToBeSentToCandidate
-            candidateConfirmationAt
-            aapDecision
-            aapDecisionComment
-            candidateDecisionComment
-          }
-        }
-        candidacyContestationsCaducite {
-          contestationSentAt
-          certificationAuthorityContestationDecision
         }
       }
     }
@@ -112,7 +62,7 @@ const useCandidateWithCandidacy = () => {
 
 export const useCandidacyForDashboard = () => {
   const {
-    candidateWithCandidacy: { data, refetch },
+    candidateWithCandidacy: { data },
   } = useCandidateWithCandidacy();
 
   const candidate = data?.candidate_getCandidateWithCandidacy;
@@ -121,33 +71,13 @@ export const useCandidacyForDashboard = () => {
 
   const candidacyStatus = candidacy?.status;
 
-  const isCurrentlySubmitted = candidacyStatus === "PARCOURS_ENVOYE";
-
-  const isTrainingConfirmed =
-    candidacy?.candidacyStatuses.findIndex(
-      (status) => status.status == "PARCOURS_CONFIRME",
-    ) != -1 && !isCurrentlySubmitted;
-
-  const canEditCandidacy = candidateCanEditCandidacy({
-    candidacyStatus,
-    candidacyDropOut: !!candidacy?.candidacyDropOut,
-  });
-
   const candidacyAlreadySubmitted = candidacyStatus !== "PROJET";
 
   const feasibility = candidacy?.feasibility;
-  const dematerializedFeasibilityFile =
-    feasibility?.dematerializedFeasibilityFile;
 
   return {
-    refetch,
-    candidate,
     candidacy,
-    isTrainingConfirmed,
-    canEditCandidacy,
     candidacyAlreadySubmitted,
-    candidacyStatus,
     feasibility,
-    dematerializedFeasibilityFile,
   };
 };
