d2f19b7c3e63966ac5a0090d7a381f8408350643
Cédric Soulas <soulascedric@gmail.com>
2025-03-10 15:46:29 +0100

    feat(api): use brevo template for two legal info emails

--- packages/reva-api/modules/organism/emails/sendLegalInformationDocumentsDecisionEmail.ts
+++ packages/reva-api/modules/organism/emails/sendLegalInformationDocumentsDecisionEmail.ts
@@ -1,13 +1,14 @@
 import mjml2html from "mjml";
 
 import {
+  getBackofficeUrl,
+  sendEmailUsingTemplate,
   sendEmailWithLink,
   sendGenericEmail,
   templateMail,
 } from "../../shared/email";
 import { logger } from "../../shared/logger";
-
-// const baseUrl = process.env.APP_BASE_URL || "https://vae.gouv.fr";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
 export const sendLegalInformationDocumentsApprovalEmail = async ({
   email,
@@ -16,9 +17,22 @@ export const sendLegalInformationDocumentsApprovalEmail = async ({
   email: string;
   managerName: string;
 }) => {
-  const htmlContent = mjml2html(
-    templateMail({
-      content: `
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_ORGANISM_EMAILS",
+  });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 531,
+      params: {
+        managerName,
+      },
+    });
+  } else {
+    const htmlContent = mjml2html(
+      templateMail({
+        content: `
       <p>Bonjour ${managerName},</p>
       <p>Nous vous informons que vos pièces justificatives ont été validées. Votre compte est désormais à jour.</p>
       <p>Besoin d’aide ? Vous pouvez nous contacter à l’adresse support@vae.gouv.fr.</p>
@@ -27,22 +41,23 @@ export const sendLegalInformationDocumentsApprovalEmail = async ({
       
       <p>L’équipe France VAE</p>
     `,
-    }),
-  );
+      }),
+    );
 
-  if (htmlContent.errors.length > 0) {
-    const errorMessage = htmlContent.errors
-      .map((e) => e.formattedMessage)
-      .join("\n");
-    logger.error(errorMessage);
-    throw new Error(errorMessage);
+    if (htmlContent.errors.length > 0) {
+      const errorMessage = htmlContent.errors
+        .map((e) => e.formattedMessage)
+        .join("\n");
+      logger.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    return sendGenericEmail({
+      to: { email },
+      htmlContent: htmlContent.html,
+      subject: "Vos pièces justificatives sont validées",
+    });
   }
-
-  return sendGenericEmail({
-    to: { email },
-    htmlContent: htmlContent.html,
-    subject: "Vos pièces justificatives sont validées",
-  });
 };
 
 export const sendLegalInformationDocumentsUpdateNeededEmail = async ({
@@ -54,39 +69,55 @@ export const sendLegalInformationDocumentsUpdateNeededEmail = async ({
   managerName: string;
   aapComment: string;
 }) => {
-  return sendEmailWithLink({
-    app: "admin",
-    action: "agencies-settings-v3",
-    to: { email },
-    htmlContent: (url: string) => {
-      const htmlContent = mjml2html(
-        templateMail({
-          content: `
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_ORGANISM_EMAILS",
+  });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 532,
+      params: {
+        managerName,
+        aapComment,
+        backofficeUrl: getBackofficeUrl({ path: "/" }),
+      },
+    });
+  } else {
+    return sendEmailWithLink({
+      app: "admin",
+      action: "agencies-settings-v3",
+      to: { email },
+      htmlContent: (url: string) => {
+        const htmlContent = mjml2html(
+          templateMail({
+            content: `
           <p>Bonjour ${managerName},</p>
           <p>Nous vous remercions pour l’envoi de vos documents. </p>
           <p>Après examen de l’administrateur VAE, votre compte ne peut être vérifié pour les raisons suivantes : </p>
           <p style="border-left: 3px solid #ccc; padding-left: 15px;">${aapComment}</p>
           <p>Rendez-vous dans votre espace professionnel afin de renvoyer vos documents : </p>
         `,
-          labelCTA: "Accéder à mon compte",
-          url,
-          bottomLine: `
+            labelCTA: "Accéder à mon compte",
+            url,
+            bottomLine: `
           <p>Besoin d’aide ? Vous pouvez nous contacter à l’adresse support@vae.gouv.fr.</p>
 
           <p>Cordialement,</p>
           
           <p>L’équipe France VAE</p>`,
-        }),
-      );
-      if (htmlContent.errors.length > 0) {
-        const errorMessage = htmlContent.errors
-          .map((e) => e.formattedMessage)
-          .join("\n");
-        logger.error(errorMessage);
-        throw new Error(errorMessage);
-      }
-      return htmlContent;
-    },
-    subject: "Une action est attendue de votre part dans votre espace",
-  });
+          }),
+        );
+        if (htmlContent.errors.length > 0) {
+          const errorMessage = htmlContent.errors
+            .map((e) => e.formattedMessage)
+            .join("\n");
+          logger.error(errorMessage);
+          throw new Error(errorMessage);
+        }
+        return htmlContent;
+      },
+      subject: "Une action est attendue de votre part dans votre espace",
+    });
+  }
 };
