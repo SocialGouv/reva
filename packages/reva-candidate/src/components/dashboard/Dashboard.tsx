a5b791a46148ff494b64ef151cc1d1bb4bfe7740
Pierre A <pierre@avizou.eu>
2025-03-12 17:22:23 +0100

    feat(candidate):change banner text depending on candidacy status

--- packages/reva-candidate/src/components/dashboard/Dashboard.tsx
+++ packages/reva-candidate/src/components/dashboard/Dashboard.tsx
@@ -67,18 +67,11 @@ const Dashboard = () => {
         />
         <div className="pt-8 mt-[-120px] lg:mt-0 lg:p-0 text-justify">
           <p className="my-0 pl-8">
-            {canSubmitCandidacy ? (
-              <>
-                Votre candidature est correctement complétée ? Vous pouvez
-                l’envoyer sans plus tarder !{" "}
-              </>
-            ) : (
-              <>
-                Pour envoyer votre candidature, vous devez avoir complété, vos
-                informations dans <b>“Mon profil”</b> et toutes les catégories
-                de la section <b>“Compléter ma candidature”</b>.
-              </>
-            )}
+            <BannerContent
+              candidacy={candidacy}
+              canSubmitCandidacy={canSubmitCandidacy}
+              candidacyAlreadySubmitted={candidacyAlreadySubmitted}
+            />
           </p>
         </div>
       </div>
@@ -414,5 +407,75 @@ const SentToSendBadge = ({ isComplete }: { isComplete: boolean }) => (
     {isComplete ? "Envoyée" : "à envoyer"}
   </Badge>
 );
+
+const BannerContent = ({
+  candidacy,
+  candidacyAlreadySubmitted,
+  canSubmitCandidacy,
+}: {
+  candidacy: {
+    status: CandidacyStatusStep;
+    firstAppointmentOccuredAt?: number | null;
+    organism?: {
+      label: string;
+      nomPublic?: string | null;
+    } | null;
+  };
+  candidacyAlreadySubmitted: boolean;
+  canSubmitCandidacy: boolean;
+}) => {
+  if (candidacyAlreadySubmitted && !candidacy.firstAppointmentOccuredAt) {
+    return (
+      <>
+        Votre accompagnateur vous enverra prochainement un date de rendez-vous
+        pour parler de votre projet. <br /> Si vous n’avez toujours pas eu de
+        retour 10 jours après l’envoi de votre candidatures, contactez-le
+        directement par mail.
+      </>
+    );
+  } else if (
+    candidacyAlreadySubmitted &&
+    !!candidacy.firstAppointmentOccuredAt &&
+    isAfter(candidacy.firstAppointmentOccuredAt, new Date())
+  ) {
+    return (
+      <>
+        <b>Information importante :</b> un rendez-vous est prévu le{" "}
+        {format(candidacy.firstAppointmentOccuredAt, "dd/MM/yyyy, à HH'h'mm")}{" "}
+        avec{" "}
+        <em>{candidacy.organism?.nomPublic || candidacy.organism?.label}</em>.
+        Consultez la section “Mes prochains rendez-vous” pour en savoir plus.{" "}
+      </>
+    );
+  } else if (
+    candidacyAlreadySubmitted &&
+    !!candidacy.firstAppointmentOccuredAt &&
+    isBefore(candidacy.firstAppointmentOccuredAt, new Date())
+  ) {
+    return (
+      <>
+        Votre accompagnateur est en train de construire votre parcours France
+        VAE (heures d'accompagnement, formations prévues...).
+        <br />
+        Vous le découvrirez très prochainement !
+      </>
+    );
+  } else if (canSubmitCandidacy) {
+    return (
+      <>
+        Votre candidature est correctement complétée ? Vous pouvez l’envoyer
+        sans plus tarder !
+      </>
+    );
+  } else {
+    return (
+      <>
+        Pour envoyer votre candidature, vous devez avoir complété, vos
+        informations dans <b>“Mon profil”</b> et toutes les catégories de la
+        section <b>“Compléter ma candidature”</b>.
+      </>
+    );
+  }
+};
 
 export default Dashboard;
