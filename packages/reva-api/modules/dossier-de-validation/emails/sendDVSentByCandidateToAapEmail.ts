9b227dad03c8bb46c7805774ab97f9ab0605099c
Cédric Soulas <soulascedric@gmail.com>
2025-03-13 17:01:28 +0100

    feat(api): use brevo template for dv notification sent to aap

--- packages/reva-api/modules/dossier-de-validation/emails/sendDVSentByCandidateToAapEmail.ts
+++ packages/reva-api/modules/dossier-de-validation/emails/sendDVSentByCandidateToAapEmail.ts
@@ -1,8 +1,14 @@
 import mjml2html from "mjml";
 
-import { sendEmailWithLink, templateMail } from "../../shared/email";
+import {
+  getBackofficeUrl,
+  sendEmailUsingTemplate,
+  sendEmailWithLink,
+  templateMail,
+} from "../../shared/email";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
-export const sendDVSentByCandidateToAapEmail = ({
+export const sendDVSentByCandidateToAapEmail = async ({
   email,
   candidacyId,
   aapName,
@@ -13,6 +19,24 @@ export const sendDVSentByCandidateToAapEmail = ({
   aapName: string;
   candidateName: string;
 }) => {
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_ORGANISM_EMAILS",
+  });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 553,
+      params: {
+        backofficeUrl: getBackofficeUrl({
+          path: `/candidacies/${candidacyId}/dossier-de-validation-aap`,
+        }),
+        aapName,
+        candidateName,
+      },
+    });
+  }
+
   const htmlContent = (url: string) =>
     mjml2html(
       templateMail({
