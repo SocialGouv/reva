8f1be29b55bb425379e8b6fed8be7d996f433257
Cédric Soulas <soulascedric@gmail.com>
2025-03-18 20:57:41 +0100

    feat(api): use brevo template for invalid subscription email

--- packages/reva-api/modules/subscription/emails/rejection.ts
+++ packages/reva-api/modules/subscription/emails/rejection.ts
@@ -1,7 +1,8 @@
 import mjml2html from "mjml";
 
-import { sendGenericEmail } from "../../shared/email";
+import { sendEmailUsingTemplate, sendGenericEmail } from "../../shared/email";
 import { template } from "./template";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
 export const sendRejectionEmail = async ({
   email,
@@ -10,6 +11,18 @@ export const sendRejectionEmail = async ({
   email: string;
   reason: string;
 }) => {
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_ORGANISM_EMAILS",
+  });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 561,
+      params: { reason },
+    });
+  }
+
   const htmlContent = mjml2html(
     template({
       headline: "Bonjour,",
