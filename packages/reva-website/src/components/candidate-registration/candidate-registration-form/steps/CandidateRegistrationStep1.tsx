50bf65c88cfaaf71121cc30b4f6291b2c20224e9
Cédric Soulas <soulascedric@gmail.com>
2025-03-20 17:10:48 +0100

    feat(website): improve wording on candidate registration step 1

--- packages/reva-website/src/components/candidate-registration/candidate-registration-form/steps/CandidateRegistrationStep1.tsx
+++ packages/reva-website/src/components/candidate-registration/candidate-registration-form/steps/CandidateRegistrationStep1.tsx
@@ -23,7 +23,19 @@ export const CandidateRegistrationStep1 = ({
           disabled={!isAapAvailable}
           enlargeLinkOrButton
           title={"Avec un accompagnateur"}
-          desc="Vous serez accompagné par un expert de la VAE qui vous aidera à chaque grande étape de votre parcours VAE. Une présence de A à Z, utile pour réaliser sereinement votre parcours."
+          desc={
+            <>
+              <p>
+                Vous serez accompagné par un expert de la VAE qui vous aidera à
+                chaque grande étape de votre parcours VAE. Une présence de A à
+                Z, utile pour réaliser sereinement votre parcours.
+              </p>
+              <p className="mt-6 font-semibold">
+                Cette accompagnement peut être financé par votre Compte
+                Personnel de Formation.
+              </p>
+            </>
+          }
           buttonProps={{
             onClick: () => {
               onSubmit("ACCOMPAGNE");
@@ -37,7 +49,19 @@ export const CandidateRegistrationStep1 = ({
           data-testid="tile-autonome"
           enlargeLinkOrButton
           title="En autonomie"
-          desc="Vous réaliserez toutes les démarches et grandes étapes d'un parcours VAE en toute autonomie. Pour vous aider, nos articles de blog, tutos et webinaires seront là."
+          desc={
+            <>
+              <p>
+                Vous réaliserez toutes les démarches et grandes étapes d’un
+                parcours VAE seul. Pour vous aider, nos articles et tutoriels
+                sont à votre disposition.
+              </p>
+              <p className="mt-6 font-semibold">
+                Les frais de passage devant le jury et les formations
+                complémentaires seront entièrement à votre charge.
+              </p>
+            </>
+          }
           buttonProps={{
             onClick: () => {
               onSubmit("AUTONOME");
