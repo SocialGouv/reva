5c054d50dcc84e35dfdf30d2d140ae9e0148065e
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-03-21 09:45:57 +0100

    feat(api): added missing departments to the department table

---
+++ packages/reva-api/prisma/migrations/20250321084424_add_missing_departments/migration.sql
@@ -0,0 +1,27 @@
+INSERT INTO
+    department (id, label, code, elligible_vae)
+VALUES
+    (
+        'bfcd22bc-ca82-4ee9-ad44-863b3a0842b4',
+        'Nouvelle-Calédonie',
+        '988',
+        false
+    ),
+    (
+        '9a5340b8-7c18-4b4c-839e-6d50d1f451c9',
+        'NouvSaint-Barthélémy',
+        '977',
+        false
+    ),
+    (
+        '5a1bdcde-58bb-4b4a-82e0-e8e0bda8ea0e',
+        'Wallis-et-Futuna',
+        '986',
+        false
+    ),
+    (
+        'a7762455-4a19-4827-95c8-4e4c2799688d',
+        'Polynésie française',
+        '987',
+        false
+    );
