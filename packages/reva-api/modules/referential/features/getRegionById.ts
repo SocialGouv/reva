0bde04dbcafc981a25e65d2f86370008aad1e7cd
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-03-21 11:49:31 +0100

    fix(api): fixed an error where getRegionById was crashing with a null id param

--- packages/reva-api/modules/referential/features/getRegionById.ts
+++ packages/reva-api/modules/referential/features/getRegionById.ts
@@ -2,5 +2,11 @@ import { Region } from "@prisma/client";
 
 import { prismaClient } from "../../../prisma/client";
 
-export const getRegionById = ({ id }: { id: string }): Promise<Region | null> =>
-  prismaClient.region.findUnique({ where: { id } });
+export const getRegionById = ({
+  id,
+}: {
+  id?: string;
+}): Promise<Region | null> =>
+  id
+    ? prismaClient.region.findUnique({ where: { id } })
+    : Promise.resolve(null);
