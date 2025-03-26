4c4240a3c7405486b06edea376660a7ab17081bf
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-03-26 17:11:24 +0100

    feat(api): added UPDATE_CANDIDATE_CONTACT_DETAILS feature

---
+++ packages/reva-api/prisma/migrations/20250326160902_add_update_candidate_contact_details_feature/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    FEATURES ("key", "label")
+values
+    (
+        'UPDATE_CANDIDATE_CONTACT_DETAILS',
+        'Nouvelle carte de modification des coordonnées du candidats dans la page de résumé de la candidature'
+    );
