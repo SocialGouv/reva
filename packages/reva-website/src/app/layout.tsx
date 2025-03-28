552f4a8f7c5da05a4bc846a11b53e5f72680b4de
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-03-28 10:50:09 +0100

    chore(website): react-dsfr version bump

--- packages/reva-website/src/app/layout.tsx
+++ packages/reva-website/src/app/layout.tsx
@@ -2,23 +2,27 @@ import "@/styles/globals.css";
 import "@/styles/dsfr-theme-tac.min.css";
 import "@/styles/dsfr-theme-tac-extra.css";
 import "@/styles/ckeditor5-content-styles.css";
-import { DsfrProvider } from "@codegouvfr/react-dsfr/next-appdir/DsfrProvider";
-import { getHtmlAttributes } from "@codegouvfr/react-dsfr/next-appdir/getHtmlAttributes";
-import { StartDsfr } from "./StartDsfr";
+import { DsfrProvider } from "../dsfr-bootstrap";
+import { StartDsfrOnHydration } from "../dsfr-bootstrap";
 import { tarteaucitronScript } from "@/components/script/TarteaucitronScript";
 import Script from "next/script";
 import { DsfrHead } from "./_components/dsfr/DsfrHead";
-
-const defaultColorScheme = "light";
 
 export default function RootLayout({ children }: { children: JSX.Element }) {
   const matomoBaseUrl = process.env.NEXT_PUBLIC_MATOMO_URL;
   const matomoContainerName = process.env.NEXT_PUBLIC_MATOMO_CONTAINER_NAME;
   const lang = "fr";
+
   return (
-    <html {...getHtmlAttributes({ defaultColorScheme, lang })}>
+    //should use  <html {...getHtmlAttributes({ lang })} ... but calling getHtmlAttributes clashes with tailwind css overrides for unknown reasons
+    <html
+      lang="fr"
+      data-fr-scheme="light"
+      data-fr-theme="light"
+      data-fr-js="true"
+    >
       <head>
-        <StartDsfr />
+        <StartDsfrOnHydration />
         <DsfrHead />
       </head>
       <body>
