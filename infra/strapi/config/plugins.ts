2f6464bcad0f535dd4cd6c744be190abc92f1f43
Pierre A <pierre@avizou.eu>
2025-03-31 16:20:10 +0200

    feat(strapi):preview URL to app router for regions and region articles

--- infra/strapi/config/plugins.ts
+++ infra/strapi/config/plugins.ts
@@ -42,7 +42,7 @@ module.exports = ({ env }) => ({
           uid: "api::region.region",
           draft: {
             alwaysVisible: true,
-            url: `${env("WEBSITE_URL")}/websiteapi/preview`,
+            url: `${env("WEBSITE_URL")}/websiteapi/app/preview`,
             query: {
               type: "region",
               slug: "{slug}",
@@ -57,11 +57,11 @@ module.exports = ({ env }) => ({
           uid: "api::article-region.article-region",
           draft: {
             alwaysVisible: true,
-            url: `${env("WEBSITE_URL")}/websiteapi/preview`,
+            url: `${env("WEBSITE_URL")}/websiteapi/app/preview`,
             query: {
               type: "article-region",
               slug: "{slug}",
-              id: "{id}",
+              id: "{documentId}",
               secret: env("STRAPI_PREVIEW_SECRET"),
             },
           },
