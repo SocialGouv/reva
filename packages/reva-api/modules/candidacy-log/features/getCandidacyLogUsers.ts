fb24e4abde2ffb5e77f1e1c4f46819be88818db9
Florian Bonniec <f.bonniec@me.com>
2025-04-01 16:25:02 +0200

    fix(api): set default user with keycloakId and email in CandidacyLog.user resolver if user not found

--- packages/reva-api/modules/candidacy-log/features/getCandidacyLogUsers.ts
+++ packages/reva-api/modules/candidacy-log/features/getCandidacyLogUsers.ts
@@ -5,10 +5,13 @@ export const getCandidacyLogUsers = async ({
   userKeycloakIdAndProfiles,
 }: {
   userKeycloakIdAndProfiles: {
+    userEmail: string;
     userKeycloakId: string;
     userProfile: CandidacyLogUserProfile;
   }[];
-}): Promise<{ keycloakId: string; firstname: string; lastname: string }[]> => {
+}): Promise<
+  { keycloakId: string; email: string; firstname: string; lastname: string }[]
+> => {
   const candidateKeycloakIds = userKeycloakIdAndProfiles
     .filter((u) => u.userProfile === "CANDIDAT")
     .map((u) => u.userKeycloakId);
@@ -25,16 +28,38 @@ export const getCandidacyLogUsers = async ({
     where: { keycloakId: { in: otherKeycloakIds } },
   });
 
-  return [
-    ...candidates.map((c) => ({
-      keycloakId: c.keycloakId,
-      firstname: c.firstname,
-      lastname: c.lastname,
-    })),
-    ...others.map((o) => ({
-      keycloakId: o.keycloakId,
-      firstname: o.firstname || "",
-      lastname: o.lastname || "",
-    })),
-  ];
+  const users = userKeycloakIdAndProfiles.map((user) => {
+    if (user.userProfile == "CANDIDAT") {
+      const candidate = candidates.find(
+        (c) => c.keycloakId == user.userKeycloakId,
+      );
+      if (candidate) {
+        return {
+          keycloakId: candidate.keycloakId,
+          email: candidate.email,
+          firstname: candidate.firstname || "",
+          lastname: candidate.lastname || "",
+        };
+      }
+    } else {
+      const account = others.find((a) => a.keycloakId == user.userKeycloakId);
+      if (account) {
+        return {
+          keycloakId: account.keycloakId,
+          email: account.email,
+          firstname: account.firstname || "",
+          lastname: account.lastname || "",
+        };
+      }
+    }
+
+    return {
+      keycloakId: user.userKeycloakId,
+      email: user.userEmail,
+      firstname: "",
+      lastname: "",
+    };
+  });
+
+  return users;
 };
