cea3fcdb4943f33639b7499f26dd9d90a58c79a7
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-01 17:17:07 +0200

    feat(candidate): update jury session tile condition to check session date

--- packages/reva-candidate/src/app/_components/dashboard/tiles/AppointmentTiles.tsx
+++ packages/reva-candidate/src/app/_components/dashboard/tiles/AppointmentTiles.tsx
@@ -1,9 +1,9 @@
 import { isAfter } from "date-fns";
 import { CandidacyUseCandidateForDashboard } from "../dashboard.hooks";
+import { JurySessionTile } from "./JurySessionTile";
 import { NoRendezVousTile } from "./NoRendezVousTile";
 import { ReadyForJuryTile } from "./ReadyForJuryTile";
 import { RendezVousPedagogiqueTile } from "./RendezVousPedagogiqueTile";
-import { JurySessionTile } from "./JurySessionTile";
 
 export const AppointmentTiles = ({
   candidacy,
@@ -33,9 +33,10 @@ export const AppointmentTiles = ({
     );
   }
 
-  if (candidacy.jury) {
+  if (candidacy.jury && isAfter(candidacy.jury.dateOfSession, new Date())) {
     appointments.push(<JurySessionTile jury={candidacy.jury} />);
   }
+
   if (appointments.length === 0) {
     appointments.push(<NoRendezVousTile />);
   }
