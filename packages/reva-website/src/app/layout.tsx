d9a8f74706af8ba1297f430a85c1eb774d045b29
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-04-03 12:04:33 +0200

    feat(website): added QueryClientProvidet to app router main layout

--- packages/reva-website/src/app/layout.tsx
+++ packages/reva-website/src/app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import "@/styles/globals.css";
 import "@/styles/dsfr-theme-tac.min.css";
 import "@/styles/dsfr-theme-tac-extra.css";
@@ -7,11 +8,13 @@ import { StartDsfrOnHydration } from "../dsfr-bootstrap";
 import { tarteaucitronScript } from "@/components/script/TarteaucitronScript";
 import Script from "next/script";
 import { DsfrHead } from "./_components/dsfr/DsfrHead";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function RootLayout({ children }: { children: JSX.Element }) {
   const matomoBaseUrl = process.env.NEXT_PUBLIC_MATOMO_URL;
   const matomoContainerName = process.env.NEXT_PUBLIC_MATOMO_CONTAINER_NAME;
   const lang = "fr";
+  const queryClient = new QueryClient();
 
   return (
     //should use  <html {...getHtmlAttributes({ lang })} ... but calling getHtmlAttributes clashes with tailwind css overrides for unknown reasons
@@ -26,7 +29,11 @@ export default function RootLayout({ children }: { children: JSX.Element }) {
         <DsfrHead />
       </head>
       <body>
-        <DsfrProvider lang={lang}>{children}</DsfrProvider>
+        <DsfrProvider lang={lang}>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </DsfrProvider>
         {matomoBaseUrl && matomoContainerName && (
           <Script strategy="beforeInteractive" id="tarteaucitron-wrapper">
             {tarteaucitronScript({
