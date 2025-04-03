f4fa9de715d256e1e69d2f8e720886108798e938
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-03 15:11:54 +0200

    feat(candidate): display last activity date in candidacy timeline

--- packages/reva-candidate/src/app/page.tsx
+++ packages/reva-candidate/src/app/page.tsx
@@ -7,9 +7,9 @@ import { ProjectTimeline } from "@/components/legacy/organisms/ProjectTimeline/P
 
 import { useCandidacy } from "@/components/candidacy/candidacy.context";
 import { useFeatureFlipping } from "@/components/feature-flipping/featureFlipping";
+import { format } from "date-fns";
 import { CandidacyBanner } from "./_components/CandidacyBanner";
 import Dashboard from "./_components/dashboard/Dashboard";
-
 export default function Home() {
   const { candidate } = useCandidacy();
   const { isFeatureActive } = useFeatureFlipping();
@@ -37,6 +37,12 @@ export default function Home() {
             lastname={candidate.lastname}
           />
           <CandidacyBanner />
+          {candidate.candidacy.lastActivityDate && (
+            <div className="text-sm text-dsfrGray-500">
+              Dernière actualisation <strong>enregistrée</strong> le{" "}
+              {format(candidate.candidacy.lastActivityDate, "dd/MM/yyyy")}.
+            </div>
+          )}
           <ProjectTimeline className="mt-8" />
         </PageLayout>
       )}
