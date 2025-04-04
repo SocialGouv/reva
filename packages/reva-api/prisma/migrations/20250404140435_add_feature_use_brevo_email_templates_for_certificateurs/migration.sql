1284a6b5c274f6a186e01a21f067c415cf66d850
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-04 17:14:18 +0200

    feat(api): add feature brevo emails certificateurs

---
+++ packages/reva-api/prisma/migrations/20250404140435_add_feature_use_brevo_email_templates_for_certificateurs/migration.sql
@@ -0,0 +1,5 @@
+insert into features (key, label, description, is_active)
+values ('USE_BREVO_EMAIL_TEMPLATES_FOR_CERTIFICATEURS',
+        'Utiliser les templates définis dans Brevo pour envoyer des emails aux certificateurs',
+        'Les emails envoyés aux certificateurs utiliseront les templates définis sur Brevo plutôt que les messages définis dans le code',
+        false);
