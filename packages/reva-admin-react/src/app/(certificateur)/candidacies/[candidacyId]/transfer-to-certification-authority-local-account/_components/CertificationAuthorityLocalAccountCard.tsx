40eadf5b3e817bce63f05e24477cf6e6197edd28
Florian Bonniec <f.bonniec@me.com>
2025-04-09 10:14:40 +0200

    fix(admin): display lastname and firstname as label in CertificationAuthorityLocalAccountCard component

--- packages/reva-admin-react/src/app/(certificateur)/candidacies/[candidacyId]/transfer-to-certification-authority-local-account/_components/CertificationAuthorityLocalAccountCard.tsx
+++ packages/reva-admin-react/src/app/(certificateur)/candidacies/[candidacyId]/transfer-to-certification-authority-local-account/_components/CertificationAuthorityLocalAccountCard.tsx
@@ -7,15 +7,25 @@ export const CertificationAuthorityLocalAccountCard = ({
 }: {
   certificationAuthorityLocalAccount: CertificationAuthorityLocalAccount;
   buttonComponent?: ReactNode;
-}) => (
-  <div className="flex flex-col justify-between p-6 border-[1px] border-[#DDD]">
-    <div>
-      <h6>{certificationAuthorityLocalAccount.account.firstname}</h6>
-      <p>
-        <span className="fr-icon-mail-line mr-2" />
-        {certificationAuthorityLocalAccount.account.email}
-      </p>
+}) => {
+  const { firstname, lastname, email } =
+    certificationAuthorityLocalAccount.account;
+
+  let label = lastname;
+  if (firstname) {
+    label = label ? `${label} ${firstname}` : label;
+  }
+
+  return (
+    <div className="flex flex-col justify-between p-6 border-[1px] border-[#DDD]">
+      <div>
+        <h6>{label}</h6>
+        <p>
+          <span className="fr-icon-mail-line mr-2" />
+          {email}
+        </p>
+      </div>
+      {buttonComponent}
     </div>
-    {buttonComponent}
-  </div>
-);
+  );
+};
