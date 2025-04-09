bca37bb44fb1868cc39dc95cdfedecde6cb7c616
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-09 16:46:07 +0200

    fix(api): update base URL in backoffice URL helper to default to localhost

--- packages/reva-api/modules/shared/email/backoffice.url.helpers.ts
+++ packages/reva-api/modules/shared/email/backoffice.url.helpers.ts
@@ -1,5 +1,5 @@
 export const getBackofficeUrl = ({ path }: { path: string }) => {
-  const baseUrl = process.env.BASE_URL || "";
+  const baseUrl = process.env.BASE_URL || "http://localhost";
   const pagePath = path.startsWith("/") ? path : `/${path}`;
   const adminPath = `/admin2${pagePath}`;
   const url = new URL(adminPath, baseUrl);
