dcaa6a907c32ddc1fcff5bbfd497fdd40c6457bc
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-09 16:17:55 +0200

    feat(api): integrate Brevo email template for sendCandidacyTransferToNewCertificationAuthorityEmail

--- packages/reva-api/modules/certification-authority/emails/sendCandidacyTransferToNewCertificationAuthority.ts
+++ packages/reva-api/modules/certification-authority/emails/sendCandidacyTransferToNewCertificationAuthority.ts
@@ -1,8 +1,14 @@
 import mjml2html from "mjml";
 
-import { sendEmailWithLink, templateMail } from "../../shared/email";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
+import {
+  getBackofficeUrl,
+  sendEmailUsingTemplate,
+  sendEmailWithLink,
+  templateMail,
+} from "../../shared/email";
 
-export const sendCandidacyTransferToNewCertificationAuthorityEmail = ({
+export const sendCandidacyTransferToNewCertificationAuthorityEmail = async ({
   email,
   previousCertificationAuthorityName,
   newCertificationAuthorityName,
@@ -17,29 +23,50 @@ export const sendCandidacyTransferToNewCertificationAuthorityEmail = ({
   transferReason: string;
   candidacyId: string;
 }) => {
-  const htmlContent = (url: string) =>
-    mjml2html(
-      templateMail({
-        content: `
+  const feasibilityUrl = getBackofficeUrl({
+    path: `/candidacies/${candidacyId}/feasibility`,
+  });
+
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_CERTIFICATEURS",
+  });
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 571,
+      params: {
+        feasibilityUrl,
+        previousCertificationAuthorityName,
+        newCertificationAuthorityName,
+        candidateName,
+        transferReason,
+      },
+    });
+  } else {
+    const htmlContent = (url: string) =>
+      mjml2html(
+        templateMail({
+          content: `
           <p>Bonjour ${newCertificationAuthorityName},</p>
           <p>Le service ${previousCertificationAuthorityName} vous a transféré la candidature de ${candidateName} pour la raison suivante : ${transferReason}.</p>
           <p>Vous êtes désormais le nouveau certificateur en charge de ce dossier. Pour consulter la candidature, cliquez sur le bouton ci-dessous :</p>
          `,
-        url,
-        labelCTA: "Accéder à la candidature",
-        bottomLine: `
+          url,
+          labelCTA: "Accéder à la candidature",
+          bottomLine: `
         <p>Nous restons disponibles si vous avez la moindre question.</p>
         <p>Cordialement,</p>
         <p>L'équipe France VAE</p>
         `,
-      }),
-    );
+        }),
+      );
 
-  return sendEmailWithLink({
-    to: { email },
-    htmlContent,
-    subject: "Une candidature vous a été transférée",
-    customUrl: `/candidacies/${candidacyId}/feasibility/`,
-    app: "admin",
-  });
+    return sendEmailWithLink({
+      to: { email },
+      htmlContent,
+      subject: "Une candidature vous a été transférée",
+      customUrl: `/candidacies/${candidacyId}/feasibility/`,
+      app: "admin",
+    });
+  }
 };
