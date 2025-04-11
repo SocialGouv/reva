61cdecd481c8bb11518c731ba01a8b320f64ab48
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-11 11:21:44 +0200

    chore(candidate): remove console log from keyboardValidationHelper

--- packages/reva-candidate/src/utils/keyboardValidationHelper.ts
+++ packages/reva-candidate/src/utils/keyboardValidationHelper.ts
@@ -1,6 +1,5 @@
 export const onKeyboardValidation =
   (handler?: () => void) => (e: React.KeyboardEvent<HTMLElement>) => {
-    console.log("keyUp", e);
     if (handler && (e.code === "Enter" || e.code === "Space")) {
       handler();
     }
