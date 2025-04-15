20a60b94cac7d559621fbb3103c93886fd633719
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-04-15 14:30:58 +0200

    feat(api): added CANDIDACY_FINANCING_METHOD_AMOUNT feature

---
+++ packages/reva-api/prisma/migrations/20250415094828_add_candidacy_financing_method_amount/migration.sql
@@ -0,0 +1,7 @@
+insert into
+    features (key, label)
+values
+    (
+        'CANDIDACY_FINANCING_METHOD_AMOUNT',
+        'Saisie et affichage des montants des méthodes de financement de la candidature'
+    );
