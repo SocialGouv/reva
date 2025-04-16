4099a0c334ebfdcd4d97292aafe5d79b4a3cd8b6
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-16 14:11:01 +0200

    feat(api): add CGU feature for certificateurs in migration SQL

---
+++ packages/reva-api/prisma/migrations/20250416115424_add_feature_cgu_certificateur/migration.sql
@@ -0,0 +1,5 @@
+insert into features (key, label, description)
+values ('CGU_CERTIFICATEUR',
+        'CGU pour les certificateurs',
+        'Les CGUs pour les certificateurs devront être acceptées pour pouvoir accéder à la plateforme'
+        );
