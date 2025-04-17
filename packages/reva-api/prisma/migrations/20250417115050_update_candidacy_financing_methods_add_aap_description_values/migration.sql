98c0066c7923ec5a8fbb327eb2b36dde8c0edff4
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-04-17 13:59:23 +0200

    feat(api): added candidacy_financing_method table aap_description values

---
+++ packages/reva-api/prisma/migrations/20250417115050_update_candidacy_financing_methods_add_aap_description_values/migration.sql
@@ -0,0 +1,17 @@
+update candidacy_financing_method
+set
+    aap_description = 'Dans le cadre d’un financement via Mon Compte Formation, le devis doit être validé avant l’étude de faisabilité pour qu’elle puisse être prise en charge. '
+where
+    id = 'a32212f0-5727-41fe-86b2-ffeb71944d79';
+
+update candidacy_financing_method
+set
+    aap_description = 'Si le financement mobilisé par le candidat doit être sollicité après la notification de recevabilité, vous pouvez renseigner un montant prévisionnel.'
+where
+    id = 'ba03ba33-05ea-4dfb-b064-d7aa84057c0d';
+
+update candidacy_financing_method
+set
+    aap_description = 'Si le financement mobilisé par le candidat doit être sollicité après la notification de recevabilité, vous pouvez renseigner un montant prévisionnel.'
+where
+    id = '39c1bc88-5975-40d9-85de-01c8cfa35d04';
