0912544d591bafd766a78054793e5cfa6acaac0f
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-22 14:05:27 +0200

    chore(admin): update unused props in cypress.config.ts

--- packages/reva-admin-react/cypress.config.ts
+++ packages/reva-admin-react/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 1000,
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3003/admin2",
