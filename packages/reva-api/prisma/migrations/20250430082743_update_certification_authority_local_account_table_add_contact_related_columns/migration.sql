17744060aadb00472a6e0eada2eeb3df8877c745
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-04-30 10:28:11 +0200

    feat(api): added contact related information to certification_authority_local_account table

---
+++ packages/reva-api/prisma/migrations/20250430082743_update_certification_authority_local_account_table_add_contact_related_columns/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "certification_authority_local_account" ADD COLUMN     "contact_email" VARCHAR(200),
+ADD COLUMN     "contact_full_name" VARCHAR(200),
+ADD COLUMN     "contact_phone" VARCHAR(25);
