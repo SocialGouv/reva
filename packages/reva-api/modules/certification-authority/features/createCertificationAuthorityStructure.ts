ef1c21b7816a115f6e3f999f44aca29decf93ea9
ThomasDos <dosanjos.thomas@gmail.com>
2025-04-30 14:33:05 +0200

    feat(api): implement createCertificationAuthorityStructure function to handle creation of certification authority structures with validation

---
+++ packages/reva-api/modules/certification-authority/features/createCertificationAuthorityStructure.ts
@@ -0,0 +1,21 @@
+import { prismaClient } from "../../../prisma/client";
+
+export const createCertificationAuthorityStructure = async (label: string) => {
+  const certificationAuthorityStructure =
+    await prismaClient.certificationAuthorityStructure.findFirst({
+      where: {
+        label: {
+          equals: label,
+          mode: "insensitive",
+        },
+      },
+    });
+
+  if (certificationAuthorityStructure) {
+    throw new Error("Cette structure certificatrice existe déjà");
+  }
+
+  return prismaClient.certificationAuthorityStructure.create({
+    data: { label },
+  });
+};
