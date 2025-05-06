543f29469954a1406bbe011d94571db37c4877cc
Pierre A <pierre@avizou.eu>
2025-05-06 17:31:53 +0200

    tests(admin):e2e tests for certification authority general information page

---
+++ packages/reva-admin-react/cypress/e2e/(certificateur)/settings/update-general-information/certification-authority-settings-update-general-information.cy.ts
@@ -0,0 +1,100 @@
+import { stubQuery } from "../../../../utils/graphql";
+import certificationAuthorityFixture from "./fixtures/certification-authority-info.json";
+
+function interceptUpdateGeneralInformation() {
+  cy.intercept("POST", "/api/graphql", (req) => {
+    stubQuery(
+      req,
+      "activeFeaturesForConnectedUser",
+      "features/active-features.json",
+    );
+    stubQuery(req, "getOrganismForAAPVisibilityCheck", "visibility/admin.json");
+    stubQuery(
+      req,
+      "getMaisonMereCGUQuery",
+      "account/gestionnaire-cgu-accepted.json",
+    );
+    stubQuery(
+      req,
+      "getCertificationAuthorityGeneralInfoForEditPage",
+      certificationAuthorityFixture,
+    );
+  });
+}
+
+context("main page", () => {
+  context("when i access the update general information page ", () => {
+    it("display the page with a correct title", () => {
+      interceptUpdateGeneralInformation();
+
+      cy.certificateur(
+        "/certification-authorities/settings/informations-generales",
+      );
+      cy.wait("@activeFeaturesForConnectedUser");
+      cy.wait("@getOrganismForAAPVisibilityCheck");
+      cy.wait("@getMaisonMereCGUQuery");
+      cy.wait("@getCertificationAuthorityGeneralInfoForEditPage");
+
+      cy.get(
+        '[data-test="certification-authority-general-info-page-title"]',
+      ).should("have.text", "Informations générales");
+    });
+
+    it("display the correct form default values", () => {
+      interceptUpdateGeneralInformation();
+
+      cy.certificateur(
+        "/certification-authorities/settings/informations-generales",
+      );
+      cy.wait("@activeFeaturesForConnectedUser");
+      cy.wait("@getOrganismForAAPVisibilityCheck");
+      cy.wait("@getMaisonMereCGUQuery");
+      cy.wait("@getCertificationAuthorityGeneralInfoForEditPage");
+
+      cy.get('[data-test="certification-authority-label"] input').should(
+        "have.value",
+        "certification authority label",
+      );
+
+      cy.get(
+        '[data-test="certification-authority-account-lastname"] input',
+      ).should("have.value", "doe admin");
+
+      cy.get(
+        '[data-test="certification-authority-account-firstname"] input',
+      ).should("have.value", "jane admin");
+
+      cy.get(
+        '[data-test="certification-authority-account-email"] input',
+      ).should("have.value", "monemaildeconnexion@example.com");
+
+      cy.get(
+        '[data-test="certification-authority-contact-full-name"] input',
+      ).should("have.value", "jane doe");
+
+      cy.get(
+        '[data-test="certification-authority-contact-email"] input',
+      ).should("have.value", "monemail@example.com");
+
+      cy.get(
+        '[data-test="certification-authority-contact-phone"] input',
+      ).should("have.value", "0101010101");
+    });
+
+    it("do not let me click on the submit button if there is no changes", () => {
+      interceptUpdateGeneralInformation();
+
+      cy.certificateur(
+        "/certification-authorities/settings/informations-generales",
+      );
+      cy.wait("@activeFeaturesForConnectedUser");
+      cy.wait("@getOrganismForAAPVisibilityCheck");
+      cy.wait("@getMaisonMereCGUQuery");
+      cy.wait("@getCertificationAuthorityGeneralInfoForEditPage");
+
+      cy.get('[data-test="certification-authority-submit-button"]').should(
+        "be.disabled",
+      );
+    });
+  });
+});
