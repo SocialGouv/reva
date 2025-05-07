53b9897d8d486007a70a76884d67b9a20d46ba5d
ThomasDos <dosanjos.thomas@gmail.com>
2025-05-07 15:45:22 +0200

    test(candidate): enable submission context in Cypress tests

--- packages/reva-candidate/cypress/e2e/submission.cy.js
+++ packages/reva-candidate/cypress/e2e/submission.cy.js
@@ -1,6 +1,6 @@
 import { stubQuery } from "../utils/graphql";
 
-context.skip("Submission", () => {
+context("Submission", () => {
   it("log on a submitted project", function () {
     cy.intercept("POST", "/api/graphql", (req) => {
       stubQuery(
@@ -9,14 +9,22 @@ context.skip("Submission", () => {
         "candidate2-submitted.json",
       );
 
+      stubQuery(
+        req,
+        "candidate_getCandidateWithCandidacyForDashboard",
+        "candidate2-submitted.json",
+      );
+
       stubQuery(req, "activeFeaturesForConnectedUser", "features.json");
     });
     cy.login();
 
     cy.wait("@candidate_getCandidateWithCandidacy");
-    cy.wait("@activeFeaturesForConnectedUser");
+    cy.wait("@candidate_getCandidateWithCandidacyForDashboard");
 
-    cy.get('[data-test="project-submitted-label"');
+    cy.get('[data-test="submit-candidacy-tile"] button').should(
+      "not.be.disabled",
+    );
   });
 
   it("log on a project taken over", function () {
@@ -27,13 +35,21 @@ context.skip("Submission", () => {
         "candidate2-taken-over.json",
       );
 
+      stubQuery(
+        req,
+        "candidate_getCandidateWithCandidacyForDashboard",
+        "candidate2-submitted.json",
+      );
+
       stubQuery(req, "activeFeaturesForConnectedUser", "features.json");
     });
     cy.login();
 
     cy.wait("@candidate_getCandidateWithCandidacy");
-    cy.wait("@activeFeaturesForConnectedUser");
+    cy.wait("@candidate_getCandidateWithCandidacyForDashboard");
 
-    cy.get('[data-test="project-submitted-label"');
+    cy.get('[data-test="submit-candidacy-tile"] button').should(
+      "not.be.disabled",
+    );
   });
 });
