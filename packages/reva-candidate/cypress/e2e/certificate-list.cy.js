f4873ae9c28687d8f16dbf612942b477b1e51043
ThomasDos <dosanjos.thomas@gmail.com>
2025-05-07 14:34:01 +0200

    test(candidate): update certificate list test to visit set-certification page

--- packages/reva-candidate/cypress/e2e/certificate-list.cy.js
+++ packages/reva-candidate/cypress/e2e/certificate-list.cy.js
@@ -11,14 +11,11 @@ context("Certificate list", () => {
     cy.login();
 
     cy.wait("@candidate_getCandidateWithCandidacy");
-    cy.wait("@activeFeaturesForConnectedUser");
+    cy.visit("/set-certification");
+    cy.wait("@searchCertificationsForCandidate");
   });
 
-  it.skip("should show only 2 certifications", function () {
-    cy.get('[data-test="project-home-set-certification"]').click();
-
-    cy.wait("@searchCertificationsForCandidate");
-
+  it("should show only 2 certifications", function () {
     cy.get('[data-test="results"]').children("li").should("have.length", 2);
 
     cy.get('[data-test="results"]')
