a94d2db789b33f1af3da80978767f53e52b54cb3
ThomasDos <dosanjos.thomas@gmail.com>
2025-05-09 08:57:02 +0200

    test(candidate): refactor candidacy dropout decision tests to improve structure

--- packages/reva-candidate/cypress/e2e/candidacy-dropout/candidacy-dropout-decision.cy.ts
+++ packages/reva-candidate/cypress/e2e/candidacy-dropout/candidacy-dropout-decision.cy.ts
@@ -3,6 +3,16 @@ import candidateDropOut from "./fixtures/candidate-dropped-out.json";
 
 function interceptCandidacy() {
   cy.intercept("POST", "/api/graphql", (req) => {
+    const candidate = {
+      ...candidateDropOut.data.candidate_getCandidateWithCandidacy,
+      candidacy: {
+        ...candidateDropOut.data.candidate_getCandidateWithCandidacy.candidacy,
+        candidacyDropOut: {
+          createdAt: "2021-09-01T00:00:00Z",
+          dropOutConfirmedByCandidate: true,
+        },
+      },
+    };
     stubMutation(
       req,
       "updateCandidateCandidacyDropoutDecision",
@@ -10,17 +20,12 @@ function interceptCandidacy() {
     );
     stubQuery(req, "candidate_getCandidateWithCandidacy", {
       data: {
-        candidate_getCandidateWithCandidacy: {
-          ...candidateDropOut.data.candidate_getCandidateWithCandidacy,
-          candidacy: {
-            ...candidateDropOut.data.candidate_getCandidateWithCandidacy
-              .candidacy,
-            candidacyDropOut: {
-              createdAt: "2021-09-01T00:00:00Z",
-              dropOutConfirmedByCandidate: true,
-            },
-          },
-        },
+        candidate_getCandidateWithCandidacy: candidate,
+      },
+    });
+    stubQuery(req, "candidate_getCandidateWithCandidacyForDashboard", {
+      data: {
+        candidate_getCandidateWithCandidacy: candidate,
       },
     });
     stubQuery(req, "getCandidacyForDropOutDecisionPage", {
@@ -36,26 +41,20 @@ function interceptCandidacy() {
   });
 }
 
-context.skip("Candidacy dropout decision page", () => {
-  it("should let me access the page", function () {
+context("Candidacy dropout decision page", () => {
+  beforeEach(() => {
     interceptCandidacy();
     cy.login();
-
     cy.wait("@candidate_getCandidateWithCandidacy");
-    cy.wait("@activeFeaturesForConnectedUser");
     cy.visit("/candidacy-dropout-decision/");
     cy.wait("@getCandidacyForDropOutDecisionPage");
+  });
+
+  it("should let me access the page", function () {
     cy.get('[data-test="candidacy-dropout-decision-page"]').should("exist");
     cy.get("h1").should("contain.text", "Abandon du parcours VAE");
   });
   it("should let me validate my drop out and lead me to the confirmation page", function () {
-    interceptCandidacy();
-    cy.login();
-
-    cy.wait("@candidate_getCandidateWithCandidacy");
-    cy.wait("@activeFeaturesForConnectedUser");
-    cy.visit("/candidacy-dropout-decision/");
-    cy.wait("@getCandidacyForDropOutDecisionPage");
     cy.get(".drop-out-confirmation-radio-button~label").click();
     cy.get("button[type=submit]").click();
     cy.wait("@updateCandidateCandidacyDropoutDecision");
@@ -66,13 +65,6 @@ context.skip("Candidacy dropout decision page", () => {
     );
   });
   it("should let me cancel my drop out and redirect me to the homepage", function () {
-    interceptCandidacy();
-    cy.login();
-
-    cy.wait("@candidate_getCandidateWithCandidacy");
-    cy.wait("@activeFeaturesForConnectedUser");
-    cy.visit("/candidacy-dropout-decision/");
-    cy.wait("@getCandidacyForDropOutDecisionPage");
     cy.get(".drop-out-cancelation-radio-button~label").click();
     cy.get("button[type=submit]").click();
     cy.wait("@updateCandidateCandidacyDropoutDecision");
