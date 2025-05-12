940177b0d09dcf0dcc87f5e62839f5287093ca61
Cédric Soulas <soulascedric@gmail.com>
2025-05-12 15:18:20 +0200

    feat(api): migrate candidacy transfer email to brevo template

--- packages/reva-api/modules/certification-authority/emails/sendCandidacyTransferToCandidate.ts
+++ packages/reva-api/modules/certification-authority/emails/sendCandidacyTransferToCandidate.ts
@@ -1,6 +1,4 @@
-import mjml2html from "mjml";
-
-import { sendGenericEmail, templateMail } from "../../shared/email";
+import { sendEmailUsingTemplate } from "../../shared/email";
 
 export const sendCandidacyTransferToCandidate = async ({
   email,
@@ -9,27 +7,11 @@ export const sendCandidacyTransferToCandidate = async ({
   email: string;
   newCertificationAuthorityName: string;
 }) => {
-  const htmlContent = mjml2html(
-    templateMail({
-      content: `
-      <p>Bonjour,</p>
-      <p>Nous vous informons que votre candidature a été transférée au gestionnaire de candidatures ${newCertificationAuthorityName}, à l'initiative du certificateur.</p>
-      <p>Il sera ainsi le destinataire des différents documents liés à votre parcours (dossier de faisabilité, dossier de validation) et vous communiquera, via la plateforme, les informations liées à votre passage devant le jury.</p>
-      
-      <p>Nous vous souhaitons une bonne poursuite de parcours. </p>
-      
-      <p>Besoin d’aide ? Vous pouvez nous contacter à l’adresse support@vae.gouv.fr.</p>
-
-      <p>Cordialement,</p>
-      
-      <p>L’équipe France VAE</p>
-        `,
-    }),
-  );
-
-  return sendGenericEmail({
+  return sendEmailUsingTemplate({
     to: { email },
-    htmlContent: htmlContent.html,
-    subject: "Votre candidature a été transférée à un autre certificateur",
+    templateId: 596,
+    params: {
+      newCertificationAuthorityName,
+    },
   });
 };
