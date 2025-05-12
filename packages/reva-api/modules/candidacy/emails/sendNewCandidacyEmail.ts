3bdb404efa67e9d9856eab1f90e0b333789caba2
Cédric Soulas <soulascedric@gmail.com>
2025-05-12 14:33:24 +0200

    feat: migrate new candidacy email to brevo template

--- packages/reva-api/modules/candidacy/emails/sendNewCandidacyEmail.ts
+++ packages/reva-api/modules/candidacy/emails/sendNewCandidacyEmail.ts
@@ -1,14 +1,36 @@
 import mjml2html from "mjml";
 
-import { sendEmailWithLink, templateMail } from "../../shared/email";
+import {
+  getBackofficeUrl,
+  sendEmailUsingTemplate,
+  sendEmailWithLink,
+  templateMail,
+} from "../../shared/email";
+import { isFeatureActiveForUser } from "../../feature-flipping/feature-flipping.features";
 
-export const sendNewCandidacyEmail = ({
+export const sendNewCandidacyEmail = async ({
   email,
   candidacyId,
 }: {
   email: string;
   candidacyId: string;
 }) => {
+  const useBrevoTemplate = await isFeatureActiveForUser({
+    feature: "USE_BREVO_EMAIL_TEMPLATES_FOR_ORGANISM_EMAILS",
+  });
+
+  if (useBrevoTemplate) {
+    return sendEmailUsingTemplate({
+      to: { email },
+      templateId: 533,
+      params: {
+        backofficeUrl: getBackofficeUrl({
+          path: `/candidacies/${candidacyId}/summary/`,
+        }),
+      },
+    });
+  }
+
   const htmlContent = (url: string) =>
     mjml2html(
       templateMail({
--- packages/reva-api/modules/candidacy/emails/sendNewOrganismCandidateNewCandidacyEmail.ts
+++ packages/reva-api/modules/candidacy/emails/sendNewOrganismCandidateNewCandidacyEmail.ts
@@ -29,26 +29,26 @@ export const sendNewOrganismCandidateNewCandidacyEmail = async ({
         }),
       },
     });
-  } else {
-    const htmlContent = (url: string) =>
-      mjml2html(
-        templateMail({
-          content: `
+  }
+
+  const htmlContent = (url: string) =>
+    mjml2html(
+      templateMail({
+        content: `
         <p>Bonjour,</p>
         <p>Une nouvelle candidature VAE est disponible dans votre espace de travail. Connectez-vous pour y accéder.</p>
           `,
-          url,
-          labelCTA: "Accéder à mon espace",
-          bottomLine: "L'équipe France VAE",
-        }),
-      );
+        url,
+        labelCTA: "Accéder à mon espace",
+        bottomLine: "L'équipe France VAE",
+      }),
+    );
 
-    return sendEmailWithLink({
-      to: { email },
-      htmlContent,
-      subject: "Une nouvelle candidature est arrivée",
-      customUrl: `/candidacies/${candidacyId}/summary/`,
-      app: "admin",
-    });
-  }
+  return sendEmailWithLink({
+    to: { email },
+    htmlContent,
+    subject: "Une nouvelle candidature est arrivée",
+    customUrl: `/candidacies/${candidacyId}/summary/`,
+    app: "admin",
+  });
 };
