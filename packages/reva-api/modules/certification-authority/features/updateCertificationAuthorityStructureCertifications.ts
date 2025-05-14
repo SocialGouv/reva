a6330f2fd4a40aab3a6b1af42522b4bf3994e3f0
ThomasDos <dosanjos.thomas@gmail.com>
2025-05-14 10:46:30 +0200

    chore(api): add comments to updateCertificationAuthorityStructureCertifications function

--- packages/reva-api/modules/certification-authority/features/updateCertificationAuthorityStructureCertifications.ts
+++ packages/reva-api/modules/certification-authority/features/updateCertificationAuthorityStructureCertifications.ts
@@ -1,5 +1,8 @@
 import { prismaClient } from "../../../prisma/client";
 
+// Cette fonction met à jour les liaisons entre une structure certificatrice et ses certifications.
+// Attention : l'opération de mise à jour remplace toutes les liaisons existantes par les nouvelles.
+// Si une certification est déjà liée à une autre structure certificatrice, cette liaison sera supprimée.
 export const updateCertificationAuthorityStructureCertifications = ({
   certificationAuthorityStructureId,
   certificationIds,
