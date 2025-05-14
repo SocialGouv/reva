383061bb0ddeaf4bfeb4e777ac1b34d2e60c7c17
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-05-14 11:16:25 +0200

    chore(admin): moved admin certification authority test to another folder

--- packages/reva-admin-react/cypress/e2e/admin/annuaire/certification-authority/certification-authority.cy.ts
+++ packages/reva-admin-react/cypress/e2e/admin/annuaire/certification-authority/certification-authority.cy.ts
@@ -1,4 +1,4 @@
-import { stubQuery } from "../../../../../utils/graphql";
+import { stubQuery } from "../../../../utils/graphql";
 import certificationAuthorityFixture from "./fixtures/certification-authority.json";
 import activeFeaturesFixture from "./fixtures/active-features.json";
 
