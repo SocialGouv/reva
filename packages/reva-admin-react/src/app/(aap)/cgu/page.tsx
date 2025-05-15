e7d6f6f9ab2656213056f292c13ab4cf3baefa3c
Pierre A <pierre@avizou.eu>
2025-05-15 14:30:43 +0200

    chore(admin):remove CGU_FROM_STRAPI feature flag

--- packages/reva-admin-react/src/app/(aap)/cgu/page.tsx
+++ packages/reva-admin-react/src/app/(aap)/cgu/page.tsx
@@ -1,35 +1,22 @@
 "use client";
 import { Cgu } from "@/components/cgu/Cgu";
-import { useFeatureflipping } from "@/components/feature-flipping/featureFlipping";
 import { CguForm } from "./_components/CguForm";
-import { HardCodedCgu } from "./_components/HardCodedCGU";
 import { useAppCgu } from "./page.hooks";
 
 export default function CguPage() {
   const { cguResponse } = useAppCgu();
-  const { isFeatureActive } = useFeatureflipping();
-  const loadFromStrapi = isFeatureActive("CGU_FROM_STRAPI");
-  if (!cguResponse && loadFromStrapi) {
+  if (!cguResponse) {
     return <div>Loading...</div>;
   }
   return (
     <div>
       <h1>Conditions générales d'utilisation</h1>
-      {loadFromStrapi ? (
-        <>
-          <Cgu
-            cguHtml={cguResponse?.contenu ?? ""}
-            chapo={cguResponse?.chapo ?? ""}
-            updatedAt={cguResponse?.dateDeMiseAJour}
-          />
-          <CguForm />
-        </>
-      ) : (
-        <>
-          <HardCodedCgu />
-          <CguForm />
-        </>
-      )}
+      <Cgu
+        cguHtml={cguResponse?.contenu ?? ""}
+        chapo={cguResponse?.chapo ?? ""}
+        updatedAt={cguResponse?.dateDeMiseAJour}
+      />
+      <CguForm />
     </div>
   );
 }
