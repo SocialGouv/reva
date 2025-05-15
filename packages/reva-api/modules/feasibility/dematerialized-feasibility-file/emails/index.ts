4176253af14245864dee5bef54deb10738b223c8
Cédric Soulas <soulascedric@gmail.com>
2025-05-15 11:08:41 +0200

    chore(api): remove sendDFFNotificationToCertificationAuthorityEmail in favor of common sendNewFeasibilitySubmittedEmail

--- packages/reva-api/modules/feasibility/dematerialized-feasibility-file/emails/index.ts
+++ packages/reva-api/modules/feasibility/dematerialized-feasibility-file/emails/index.ts
@@ -1,4 +1,3 @@
 export * from "./sendDFFNotificationToCandidateEmail";
-export * from "./sendDFFNotificationToCertificationAuthorityEmail";
 export * from "./sendFeasibilityConfirmedByCandidateWithoutSwornAttestmentToAAP.email";
 export * from "./sendFeasibilityConfirmedByCandidateWithSwornAttestmentToAAP.email";
