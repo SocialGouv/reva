bc29c1fd655322e7591e26c16d2d42399c8ffc43
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-05-19 11:25:42 +0200

    feat(api): added REMOVE_FUNDING_AND_PAYMENT_REQUESTS_FROM_CANDIDACY_STATUSES feature flag

---
+++ packages/reva-api/prisma/migrations/20250519092229_add_remove_funding_and_payment_requests_from_candidacy_statuses_feature_flag/migration.sql
@@ -0,0 +1,7 @@
+insert into
+    features (key, label)
+values
+    (
+        'REMOVE_FUNDING_AND_PAYMENT_REQUESTS_FROM_CANDIDACY_STATUSES',
+        'Retire les demandes de financement et de paiement des statuts de candidature'
+    )
