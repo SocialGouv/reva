37aac77d03d096676853b3f50264b6c1b5682d02
Pierre A <pierre@avizou.eu>
2025-05-26 15:48:17 +0200

    chore(website):migrate PRC list page to app router

---
+++ packages/reva-website/src/app/savoir-plus/articles/liste-prc/_components/PrcList.tsx
@@ -0,0 +1,109 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { SearchFilterBar } from "@/components/search-filter-bar/SearchFilterBar";
+import { useMemo } from "react";
+
+export const PrcList = ({
+  prcs,
+}: {
+  prcs: ({
+    documentId: string;
+    nom: string;
+    email: string;
+    adresse: string;
+    mandataire?: string | null;
+    region?: string | null;
+    telephone?: string | null;
+    departement?: {
+      __typename?: "Departement";
+      nom: string;
+      code: string;
+    } | null;
+  } | null)[];
+}) => {
+  const searchParams = useSearchParams();
+  const searchFilter = searchParams?.get("search") || "";
+  const displayedPrcs = useMemo(() => {
+    if (searchFilter) {
+      const subFilter = searchFilter.substring(0, 2);
+      return prcs?.filter((prc) =>
+        prc?.departement?.code?.startsWith(subFilter),
+      );
+    } else {
+      return prcs;
+    }
+  }, [searchFilter, prcs]);
+  return (
+    <>
+      <div className="py-8 px-10 shadow-lifted mb-12 border-b-fvaeOrange border-b-4">
+        <p className="text-[32px] font-bold">
+          Recherchez un conseiller proche de chez vous
+        </p>
+        <SearchFilterBar
+          big
+          placeholder="Votre code postal"
+          searchFilter={searchFilter}
+          resultCount={displayedPrcs.length}
+          onSearchFilterChange={(filter) => {
+            const queryParams = new URLSearchParams(searchParams || "");
+            if (filter) {
+              queryParams.set("search", filter);
+            } else {
+              queryParams.delete("search");
+            }
+
+            // Use native pushState to avoid re-rendering the server-component and refetching from Strapi the PRC list we already have
+            window.history.pushState(null, "", `?${queryParams.toString()}`);
+          }}
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {displayedPrcs?.map((prc) => (
+          <div key={prc?.documentId} className="flex flex-col border p-6 gap-2">
+            <h1 className="text-2xl font-bold mb-4">{prc?.nom}</h1>
+            <div>
+              <span
+                className="fr-icon-home-4-line fr-icon--sm mr-2"
+                aria-hidden="true"
+                aria-label="Adresse"
+                title="Adresse"
+              ></span>
+              {prc?.adresse}
+              <br />
+              {prc?.departement?.nom} ({prc?.departement?.code})
+            </div>
+            <div>
+              <span
+                className="fr-icon-mail-line fr-icon--sm mr-2"
+                aria-hidden="true"
+                aria-label="Email"
+                title="Email"
+              ></span>
+              {prc?.email}
+            </div>
+            {prc?.mandataire && (
+              <div>
+                <span
+                  className="fr-icon-team-line fr-icon--sm mr-2"
+                  aria-hidden="true"
+                  aria-label="Mandataire"
+                  title="Mandataire"
+                ></span>
+                {prc?.mandataire}
+              </div>
+            )}
+            <div>
+              <span
+                className="fr-icon-phone-line fr-icon--sm mr-2"
+                aria-hidden="true"
+                aria-label="Téléphone"
+                title="Téléphone"
+              ></span>
+              {prc?.telephone}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
---
+++ packages/reva-website/src/app/savoir-plus/articles/liste-prc/page.tsx
@@ -0,0 +1,96 @@
+import { MainLayout } from "@/app/_components/layout/main-layout/MainLayout";
+import { STRAPI_GRAPHQL_API_URL } from "@/config/config";
+import { graphql } from "@/graphql/generated";
+import request from "graphql-request";
+import { Metadata } from "next/types";
+import { PrcList } from "./_components/PrcList";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const articleResponse = await getArticleDAide("liste-prc", false);
+  const article = articleResponse?.articleDAides[0];
+  return {
+    title: `France VAE | ${article?.titre}`,
+    description: article?.description,
+    keywords: ["Gouvernement", "France", "VAE", "France VAE"],
+    openGraph: {
+      title: `France VAE | ${article?.titre}`,
+      url: `https://vae.gouv.fr/savoir-plus/articles/${article?.slug}`,
+      images: [article?.vignette.url ?? ""],
+      description: article?.description ?? "",
+    },
+  };
+}
+
+const ListePrcPage = async () => {
+  const articleResponse = await getArticleDAide("liste-prc", false);
+  const prcsResponse = await getPRCs();
+  const prcs = prcsResponse?.prcs;
+  const article = articleResponse?.articleDAides[0];
+
+  if (!article || !prcs) return null;
+  return (
+    <>
+      <MainLayout>
+        {
+          <div className="flex flex-col sm:flex-row w-full gap-8 sm:gap-16 fr-container p-32 pt-16">
+            <div>
+              <h1 className="font-bold mb-12">{article.titre}</h1>
+              <PrcList prcs={prcs} />
+            </div>
+          </div>
+        }
+      </MainLayout>
+    </>
+  );
+};
+
+const articleQuery = graphql(`
+  query getArticleDAide(
+    $filters: ArticleDAideFiltersInput!
+    $publicationState: PublicationStatus!
+  ) {
+    articleDAides(filters: $filters, status: $publicationState) {
+      documentId
+      titre
+      slug
+      vignette {
+        url
+        alternativeText
+      }
+      contenu
+      description
+    }
+  }
+`);
+
+const getArticleDAide = async (slug: string, preview = false) => {
+  const articles = await request(STRAPI_GRAPHQL_API_URL, articleQuery, {
+    filters: { slug: { eq: slug } },
+    publicationState: preview ? "DRAFT" : "PUBLISHED",
+  });
+  return articles;
+};
+
+const getPrcsQuery = graphql(`
+  query getPRCs {
+    prcs(pagination: { page: 1, pageSize: 1000 }) {
+      documentId
+      nom
+      email
+      adresse
+      mandataire
+      region
+      telephone
+      departement {
+        nom
+        code
+      }
+    }
+  }
+`);
+
+const getPRCs = async () => {
+  return request(STRAPI_GRAPHQL_API_URL, getPrcsQuery);
+};
+
+export default ListePrcPage;
