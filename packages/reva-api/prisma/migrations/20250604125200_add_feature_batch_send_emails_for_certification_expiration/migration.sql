99a3a4e8d78d86771874fe68372b039ba2e54597
ThomasDos <dosanjos.thomas@gmail.com>
2025-06-04 14:57:22 +0200

    feat(api): add batch send emails feature for certification expiration

---
+++ packages/reva-api/prisma/migrations/20250604125200_add_feature_batch_send_emails_for_certification_expiration/migration.sql
@@ -0,0 +1,2 @@
+insert into "features" ("key", "label", "is_active")
+values ('batch.send-emails-for-certification-expiration', 'Active l''envoi des emails de notification d''expiration de certification', false);
