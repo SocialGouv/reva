ed201d8bb8ba876df4bdc5c31dbdf8f29254f18f
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-06-04 16:34:25 +0200

    feat(api): added a createFundingRequestUnifvaeHelper test helper

---
+++ packages/reva-api/test/helpers/entities/create-funding-request-unifvae-helper.ts
@@ -0,0 +1,36 @@
+import { faker } from "@faker-js/faker/.";
+import { prismaClient } from "../../../prisma/client";
+import { Prisma } from "@prisma/client";
+import { createCandidacyHelper } from "./create-candidacy-helper";
+
+export const createFundingRequestUnifvaeHelper = async (
+  args?: Partial<Prisma.FundingRequestUnifvaeUncheckedCreateInput>,
+) => {
+  let candidacyId = args?.candidacyId;
+  if (!candidacyId) {
+    const candidacy = await createCandidacyHelper();
+    candidacyId = candidacy.id;
+  }
+
+  return prismaClient.fundingRequestUnifvae.create({
+    data: {
+      candidacyId,
+      numAction: faker.string.uuid(),
+      individualHourCount: 0,
+      individualCost: 0,
+      collectiveHourCount: 0,
+      collectiveCost: 0,
+      basicSkillsCost: 0,
+      basicSkillsHourCount: 0,
+      mandatoryTrainingsHourCount: 0,
+      mandatoryTrainingsCost: 0,
+      certificateSkillsHourCount: 0,
+      certificateSkillsCost: 0,
+      otherTrainingCost: 0,
+      otherTrainingHourCount: 0,
+      certificateSkills: "",
+      otherTraining: "",
+      ...args,
+    },
+  });
+};
