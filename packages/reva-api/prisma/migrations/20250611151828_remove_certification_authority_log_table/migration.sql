4afe5f09ee0f8a2485a1bb5ba677310dea3fa531
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-06-12 10:02:37 +0200

    fix(api): only drop certification_authority_log tables and constraints if they exists

--- packages/reva-api/prisma/migrations/20250611151828_remove_certification_authority_log_table/migration.sql
+++ packages/reva-api/prisma/migrations/20250611151828_remove_certification_authority_log_table/migration.sql
@@ -1,17 +1,19 @@
 /*
-  Warnings:
+Warnings:
 
-  - You are about to drop the `certification_authority_log` table. If the table is not empty, all the data it contains will be lost.
+- You are about to drop the `certification_authority_log` table. If the table is not empty, all the data it contains will be lost.
 
-*/
+ */
 -- DropForeignKey
-ALTER TABLE "certification_authority_log" DROP CONSTRAINT "certification_authority_log_certification_authority_id_fkey";
+ALTER TABLE "certification_authority_log"
+DROP CONSTRAINT IF EXISTS "certification_authority_log_certification_authority_id_fkey";
 
 -- DropForeignKey
-ALTER TABLE "certification_authority_log" DROP CONSTRAINT "certification_authority_log_certification_authority_local__fkey";
+ALTER TABLE "certification_authority_log"
+DROP CONSTRAINT IF EXISTS "certification_authority_log_certification_authority_local__fkey";
 
 -- DropTable
-DROP TABLE "certification_authority_log";
+DROP TABLE IF EXISTS "certification_authority_log";
 
 -- DropEnum
-DROP TYPE "CertificationAuthorityLogUserProfile";
+DROP TYPE IF EXISTS "CertificationAuthorityLogUserProfile";
