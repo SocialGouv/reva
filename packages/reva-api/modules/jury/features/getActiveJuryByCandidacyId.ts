9725481c490983b97f189b2178162041d7068c1b
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-06-16 11:20:01 +0200

    fix(api): candidacy -> jury resolver was creating n+1 requests

--- packages/reva-api/modules/jury/features/getActiveJuryByCandidacyId.ts
+++ packages/reva-api/modules/jury/features/getActiveJuryByCandidacyId.ts
@@ -1,10 +1,22 @@
 import { prismaClient } from "../../../prisma/client";
 
-export const getActivejuryByCandidacyId = ({
+export const getActivejuryByCandidacyId = async ({
   candidacyId,
 }: {
   candidacyId: string;
-}) =>
-  prismaClient.jury.findFirst({
-    where: { isActive: true, candidacyId },
+}) => {
+  //Prisma graphql optimization. We start with the candidacy since we can use a unique index
+  //It allows prisma to automatically batch the queries in the case of a n+1 graphql query (all the active jurys of many candidacies)
+  const candidacy = await prismaClient.candidacy.findUnique({
+    where: { id: candidacyId },
+    include: {
+      Jury: {
+        where: { isActive: true },
+      },
+    },
   });
+
+  const activeJury = candidacy?.Jury[0];
+
+  return activeJury;
+};
