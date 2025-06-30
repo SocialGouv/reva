3eebefd357939ffd64ff4cd919597e783a62a856
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-06-30 15:27:46 +0200

    feat(reva-vae-collective): added react-dsfr package

--- packages/reva-vae-collective/next.config.ts
+++ packages/reva-vae-collective/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.woff2$/,
+      type: "asset/resource",
+    });
+    return config;
+  },
 };
 
+// eslint-disable-next-line import/no-unused-modules
 export default nextConfig;
--- packages/reva-vae-collective/src/app/layout.tsx
+++ packages/reva-vae-collective/src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {
+  getHtmlAttributes,
+  DsfrHead,
+} from "../dsfr-bootstrap/server-only-index";
+import { DsfrProvider, StartDsfrOnHydration } from "../dsfr-bootstrap";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html {...getHtmlAttributes({ lang: "fr" })}>
+      <head>
+        <StartDsfrOnHydration />
+        <DsfrHead />
+      </head>
+      <body>
+        <DsfrProvider lang="fr">{children}</DsfrProvider>
       </body>
     </html>
   );
