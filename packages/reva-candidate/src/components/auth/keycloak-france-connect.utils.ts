bfbfe6f91583bb4a855c162960005144a365b163
Cédric Soulas <soulascedric@gmail.com>
2025-07-02 17:07:02 +0200

    feat(candidate): build FranceConnect poc URL from config var

--- packages/reva-candidate/src/components/auth/keycloak-france-connect.utils.ts
+++ packages/reva-candidate/src/components/auth/keycloak-france-connect.utils.ts
@@ -1,29 +1,24 @@
-export const getFranceConnectLoginUrl = () => {
-  const baseApiUrl =
-    process.env.NEXT_PUBLIC_API_GRAPHQL?.replace("/api/graphql", "") ||
-    "http://localhost:8080";
-  const keycloakUrl =
-    process.env.NEXT_PUBLIC_KEYCLOAK_URL || "http://localhost:8888";
+import { KEYCLOAK_URL, REST_API_URL } from "@/config/config";
 
+export const getFranceConnectLoginUrl = () => {
   /**
    * Il s'agit d'un POC pour tester la récupération
    * de données du bac à sable France Connect.
    * À terme, l'URL sera générée coté serveur.
    * Ce code ne doit pas être utilisé en production.
    */
-  if (baseApiUrl.includes(".gouv.fr")) {
+  if (REST_API_URL.includes(".gouv.fr")) {
     return "#";
   }
 
   const url = new URL(
-    `/auth/realms/reva-app/protocol/openid-connect/auth`,
-    keycloakUrl,
+    `${KEYCLOAK_URL}/realms/reva-app/protocol/openid-connect/auth`,
   );
 
   url.searchParams.set("client_id", "reva-app");
   url.searchParams.set(
     "redirect_uri",
-    `${baseApiUrl}/api/account/franceconnect/callback`,
+    `${REST_API_URL}/account/franceconnect/callback`,
   );
   url.searchParams.set("response_type", "code");
   url.searchParams.set("scope", "openid");
