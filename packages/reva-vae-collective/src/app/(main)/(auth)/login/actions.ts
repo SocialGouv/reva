bc688e64e9f05511bbfcd360bd474f132473f802
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-08 17:48:25 +0200

    fix(vae-collective): added missing clientApp parameter to account_loginWithCredentials mutation

--- packages/reva-vae-collective/src/app/(main)/(auth)/login/actions.ts
+++ packages/reva-vae-collective/src/app/(main)/(auth)/login/actions.ts
@@ -14,7 +14,11 @@ export const login = async (formData: FormData) => {
     await client.mutation(
       gql`
         mutation Login($email: String!, $password: String!) {
-          account_loginWithCredentials(email: $email, password: $password) {
+          account_loginWithCredentials(
+            email: $email
+            password: $password
+            clientApp: REVA_VAE_COLLECTIVE
+          ) {
             tokens {
               accessToken
               refreshToken
