470b98a44bf7081120f1fef2a0e4b56899d4234b
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-10 15:54:22 +0200

    fix(vae-collective): removed unused helper

--- packages/reva-vae-collective/src/helpers/graphql/throw-urql-errors/throwUrqlErrors.ts
+++
@@ -1,8 +0,0 @@
-import { OperationResult } from "@urql/core";
-
-export const throwUrqlErrors = (urqlResult: OperationResult) => {
-  if (urqlResult.error) {
-    throw new Error(urqlResult.error?.message);
-  }
-  return urqlResult;
-};
