877da6ce22ac627960e0f1866e6b98274c306813
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-10 14:10:17 +0200

    fix(vae-collective): only show logout button if the user is authenticated

--- packages/reva-vae-collective/src/components/header/Header.tsx
+++ packages/reva-vae-collective/src/components/header/Header.tsx
@@ -4,7 +4,8 @@ import { Header as DsfrHeader } from "@codegouvfr/react-dsfr/Header";
 import { useKeycloakContext } from "../auth/keycloakContext";
 
 export const Header = () => {
-  const { logout } = useKeycloakContext();
+  const { logout, authenticated } = useKeycloakContext();
+
   return (
     <DsfrHeader
       brandTop={
@@ -18,16 +19,20 @@ export const Header = () => {
         href: "/../",
         title: "Accueil - France VAE",
       }}
-      quickAccessItems={[
-        {
-          buttonProps: {
-            onClick: () => logout(),
-            className: "!text-sm !px-3 !py-1 !mb-4 !mx-1",
-          },
-          iconId: "ri-logout-box-r-line",
-          text: "Se déconnecter",
-        },
-      ]}
+      quickAccessItems={
+        authenticated
+          ? [
+              {
+                buttonProps: {
+                  onClick: () => logout(),
+                  className: "!text-sm !px-3 !py-1 !mb-4 !mx-1",
+                },
+                iconId: "ri-logout-box-r-line",
+                text: "Se déconnecter",
+              },
+            ]
+          : []
+      }
       classes={{ operator: "min-w-[9.0625rem] min-h-[90px]" }}
     />
   );
