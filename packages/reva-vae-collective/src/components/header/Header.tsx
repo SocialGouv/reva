dec32e82c349d0c3fff3962a2e15b0f6936700be
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-11 13:39:55 +0200

    feat(vae-collective): added "Espace documentaire" to header

--- packages/reva-vae-collective/src/components/header/Header.tsx
+++ packages/reva-vae-collective/src/components/header/Header.tsx
@@ -2,9 +2,14 @@
 
 import { Header as DsfrHeader } from "@codegouvfr/react-dsfr/Header";
 import { useKeycloakContext } from "../auth/keycloakContext";
+import { ComponentProps } from "react";
 
 export const Header = () => {
   const { logout, authenticated } = useKeycloakContext();
+
+  type QuickAccessItem = NonNullable<
+    ComponentProps<typeof DsfrHeader>["quickAccessItems"]
+  >[number];
 
   return (
     <DsfrHeader
@@ -16,21 +21,32 @@ export const Header = () => {
         </>
       }
       homeLinkProps={{
-        href: "/../",
+        href: "/",
         title: "Accueil - France VAE",
       }}
       quickAccessItems={
         authenticated
-          ? [
+          ? ([
+              {
+                linkProps: {
+                  href: "https://fabnummas.notion.site/Espace-documentaire-218653b7be07800f9981d837b2df6520",
+                  title: "Espace documentaire - France VAE",
+                },
+                text: "Espace documentaire",
+              },
               {
                 buttonProps: {
                   onClick: () => logout(),
                   className: "!text-sm !px-3 !py-1 !mb-4 !mx-1",
                 },
-                iconId: "ri-logout-box-r-line",
-                text: "Se déconnecter",
+                text: (
+                  <span>
+                    Se déconnecter
+                    <span className="ml-1.5 ri-logout-box-r-line fr-icon--sm" />
+                  </span>
+                ),
               },
-            ]
+            ] as QuickAccessItem[])
           : []
       }
       classes={{ operator: "min-w-[9.0625rem] min-h-[90px]" }}
