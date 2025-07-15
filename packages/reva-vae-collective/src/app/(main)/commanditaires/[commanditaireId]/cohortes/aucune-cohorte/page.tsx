04516157848123f07b508708fa124da7f8cc00b7
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-15 14:09:14 +0200

    fix(vae-collective): spelling mistake

--- packages/reva-vae-collective/src/app/(main)/commanditaires/[commanditaireId]/cohortes/aucune-cohorte/page.tsx
+++ packages/reva-vae-collective/src/app/(main)/commanditaires/[commanditaireId]/cohortes/aucune-cohorte/page.tsx
@@ -8,8 +8,8 @@ export default function AucuneCohortePage() {
       <div>
         <h1>Bienvenue dans votre espace France VAE</h1>
         <p className="text-xl">
-          Retrouvez ici, toutes les cohortes que vous avez créé et demandait un
-          accès direct pour consulter le pilotage de celles-ci.
+          Retrouvez ici toutes les cohortes que vous avez créées, et demandez
+          l'accès à votre tableau de bord de pilotage de vos cohortes.
         </p>
         <p className="text-xl">Commencez en créant votre première cohorte.</p>
         <Button
