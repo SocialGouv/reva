1d4551648356c542b88adb08128384e515f0296b
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-16 13:49:38 +0200

    feat(api): additional security on CohorteVaeCollective->certificationCohorteVaeCollectives resolver

--- packages/reva-api/modules/vae-collective/vae-collective.security.ts
+++ packages/reva-api/modules/vae-collective/vae-collective.security.ts
@@ -15,4 +15,7 @@ export const vaeCollectiveResolversSecurityMap = {
 
   "Mutation.vaeCollective_createCohorteVaeCollective":
     isAdminOrGestionnaireOfCommanditaireVaeCollective,
+
+  "CohorteVaeCollective.certificationCohorteVaeCollectives":
+    isAdminOrGestionnaireOfCommanditaireVaeCollective,
 };
