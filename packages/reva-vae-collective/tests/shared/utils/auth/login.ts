87ae95ec3030baed5b671d72991cff5fac4db724
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-17 14:58:38 +0200

    feat(vae-collective): added a playwright login helper

---
+++ packages/reva-vae-collective/tests/shared/utils/auth/login.ts
@@ -0,0 +1,65 @@
+import { Page } from "@playwright/test";
+
+import adminToken from "./tokens/adminToken.json";
+import gestionnaireVaeCollectiveToken from "./tokens/gestionnaireVaeCollectiveToken.json";
+
+export const login = async ({
+  page,
+  role,
+}: {
+  page: Page;
+  role: "admin" | "gestionnaireVaeCollective";
+}) => {
+  await page.route(
+    "**/auth/realms/reva/protocol/openid-connect/3p-cookies/step1.html",
+    async (route) => {
+      await route.fulfill({
+        path: "tests/shared/utils/auth/pages/step1.html",
+      });
+    },
+  );
+  await page.route(
+    "**/realms/reva/protocol/openid-connect/login-status-iframe.html",
+    async (route) => {
+      await route.fulfill({
+        path: "tests/shared/utils/auth/pages/status-iframe.html",
+      });
+    },
+  );
+  await page.route("**/vae-collective/silent-check-sso.html", async (route) => {
+    await route.fulfill({
+      path: `tests/shared/utils/auth/pages/silent-check-sso.html`,
+    });
+  });
+
+  await page.route(
+    "**/realms/reva/protocol/openid-connect/auth**",
+    async (route) => {
+      await route.fulfill({
+        path: `tests/shared/utils/auth/pages/mock-redirect-to-silent-check-sso.html`,
+      });
+    },
+  );
+
+  const tokens = role === "admin" ? adminToken : gestionnaireVaeCollectiveToken;
+
+  await page.route(
+    "**/realms/reva/protocol/openid-connect/token",
+    async (route) => {
+      await route.fulfill({
+        json: tokens,
+      });
+    },
+  );
+
+  const tokensForPostLoginUrl = {
+    accessToken: tokens.access_token,
+    refreshToken: tokens.refresh_token,
+  };
+
+  await page.goto(
+    `http://localhost:4005/vae-collective/post-login?tokens=${encodeURIComponent(
+      JSON.stringify(tokensForPostLoginUrl),
+    )}`,
+  );
+};
