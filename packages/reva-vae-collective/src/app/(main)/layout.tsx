7b57596d31cc367c7dc061d817fbbc502a4da07c
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-21 13:52:51 +0200

    fix(vae-collective): removed some top margin in global layout

--- packages/reva-vae-collective/src/app/(main)/layout.tsx
+++ packages/reva-vae-collective/src/app/(main)/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <main role="main" id="content" className="flex flex-col flex-1">
         <div>
           <div
-            className={`md:mt-4 pt-4 md:pt-8 md:pb-8 fr-grid-row mb-12 fr-container`}
+            className={`pt-4 md:pt-8 md:pb-8 fr-grid-row mb-12 fr-container`}
           >
             <AuthGuard>{children}</AuthGuard>
           </div>
