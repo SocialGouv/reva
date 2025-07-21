3c5abcc1f4e9317257c155c11cac11294740b8c5
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-21 14:05:00 +0200

    feat(vae-collective): added tests to the cohorte details page

---
+++ packages/reva-vae-collective/tests/commanditaires/commanditaire/cohortes/cohorte/cohorte.spec.ts
@@ -0,0 +1,52 @@
+import {
+  test,
+  expect,
+  graphql,
+  HttpResponse,
+} from "next/experimental/testmode/playwright/msw";
+import { login } from "../../../../shared/utils/auth/login";
+const fvae = graphql.link("https://reva-api/api/graphql");
+
+test.use({
+  mswHandlers: [
+    [
+      fvae.query("getCohorteByIdForCohortePage", () => {
+        return HttpResponse.json({
+          data: {
+            vaeCollective_getCohorteVaeCollectiveById: {
+              id: "0eda2cbf-78ae-47af-9f28-34d05f972712",
+              nom: "macohorte",
+            },
+          },
+        });
+      }),
+    ],
+    { scope: "test" },
+  ],
+});
+
+test("it should render the cohorte detail page", async ({ page }) => {
+  await login({ page, role: "gestionnaireVaeCollective" });
+
+  await page.goto(
+    "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
+  );
+
+  await expect(page.getByRole("heading")).toHaveText("macohorte");
+});
+
+test("it should go back to the cohortes list page when i click on the back button", async ({
+  page,
+}) => {
+  await login({ page, role: "gestionnaireVaeCollective" });
+
+  await page.goto(
+    "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
+  );
+
+  await page.getByRole("link", { name: "Retour" }).click();
+
+  await expect(page).toHaveURL(
+    "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes",
+  );
+});
