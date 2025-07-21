e53e1f96be0e95f5a217daf65b399474dd3e3087
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-21 13:53:12 +0200

    feat(vae-collective): added a breadcrumb, description and a back button to the cohorte detail page

--- packages/reva-vae-collective/src/app/(main)/commanditaires/[commanditaireId]/cohortes/[cohorteVaeCollectiveId]/page.tsx
+++ packages/reva-vae-collective/src/app/(main)/commanditaires/[commanditaireId]/cohortes/[cohorteVaeCollectiveId]/page.tsx
@@ -1,5 +1,7 @@
 import { getAccessTokenFromCookie } from "@/helpers/auth/get-access-token-from-cookie/getAccessTokenFromCookie";
 import { client } from "@/helpers/graphql/urql-client/urqlClient";
+import { Breadcrumb } from "@codegouvfr/react-dsfr/Breadcrumb";
+import { Button } from "@codegouvfr/react-dsfr/Button";
 
 const getCohorteById = async (
   commanditaireVaeCollectiveId: string,
@@ -43,7 +45,32 @@ export default async function CohortePage({
 
   return (
     <div>
+      <Breadcrumb
+        className="mt-0 mb-4"
+        currentPageLabel={cohorte.nom}
+        segments={[
+          {
+            label: "Cohortes",
+            linkProps: {
+              href: `/vae-collective/commanditaires/${commanditaireId}/cohortes`,
+            },
+          },
+        ]}
+      />
       <h1>{cohorte.nom}</h1>
+      <p>
+        Paramétrez votre cohorte, afin de générer un lien unique à transmettre
+        aux candidats devant intégrer cette cohorte.
+      </p>
+      <Button
+        className="mt-12"
+        priority="secondary"
+        linkProps={{
+          href: `/vae-collective/commanditaires/${commanditaireId}/cohortes`,
+        }}
+      >
+        Retour
+      </Button>
     </div>
   );
 }
