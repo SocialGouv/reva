8cd8de3b3fce7a0884d541076ac169a937915979
Cédric Soulas <soulascedric@gmail.com>
2025-07-22 15:41:09 +0200

    feat(api): fix and improve LLToEarth generation script

--- packages/reva-api/scripts/generateLLToEarthFromZipCodeToAAP.ts
+++ packages/reva-api/scripts/generateLLToEarthFromZipCodeToAAP.ts
@@ -43,14 +43,28 @@ type SearchResponse = {
       },
     });
 
+    console.log(
+      `Found ${organisms.length} organisms without llToEarth coordinates.`,
+    );
+
     for (const organism of organisms) {
       const { adresseCodePostal } = organism;
 
       if (!adresseCodePostal) continue;
 
+      // add wait time to avoid rate limiting
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
       const res = await fetch(
-        `https://data.geopf.fr/geocodage/search?type=municipality&q=${adresseCodePostal}&limit=1"`,
+        `https://data.geopf.fr/geocodage/search?type=municipality&q=${adresseCodePostal}&limit=1`,
       );
+
+      if (!res.ok) {
+        console.error(
+          `Failed to fetch coordinates for zip code ${adresseCodePostal}: ${res.statusText}`,
+        );
+        continue;
+      }
 
       const { features }: SearchResponse = await res.json();
       if (!features.length) {
@@ -67,6 +81,7 @@ type SearchResponse = {
 
       const [{ ll_to_earth: llToEarth }]: { ll_to_earth: string }[] =
         await prismaClient.$queryRaw`SELECT CAST(ll_to_earth(${latitude}, ${longitude}) AS TEXT)`;
+
       await prismaClient.organism.update({
         where: { id: organism.id },
         data: {
