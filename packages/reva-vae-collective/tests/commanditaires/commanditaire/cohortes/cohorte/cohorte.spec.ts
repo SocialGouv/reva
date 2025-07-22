42af8f9efb58a746c6046d4d20d2816e5e1ec9a5
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-22 15:49:47 +0200

    feat(vae-collective): added tests on the delete cohorte button

--- packages/reva-vae-collective/tests/commanditaires/commanditaire/cohortes/cohorte/cohorte.spec.ts
+++ packages/reva-vae-collective/tests/commanditaires/commanditaire/cohortes/cohorte/cohorte.spec.ts
@@ -7,62 +7,160 @@ import {
 import { login } from "../../../../shared/utils/auth/login";
 const fvae = graphql.link("https://reva-api/api/graphql");
 
-test.use({
-  mswHandlers: [
-    [
-      fvae.query("getCohorteByIdForCohortePage", () => {
-        return HttpResponse.json({
-          data: {
-            vaeCollective_getCohorteVaeCollectiveById: {
-              id: "0eda2cbf-78ae-47af-9f28-34d05f972712",
-              nom: "macohorte",
+test.describe("global page behavior", () => {
+  test.use({
+    mswHandlers: [
+      [
+        fvae.query("getCohorteByIdForCohortePage", () => {
+          return HttpResponse.json({
+            data: {
+              vaeCollective_getCohorteVaeCollectiveById: {
+                id: "0eda2cbf-78ae-47af-9f28-34d05f972712",
+                nom: "macohorte",
+                status: "BROUILLON",
+              },
             },
-          },
-        });
-      }),
+          });
+        }),
+      ],
+      { scope: "test" },
     ],
-    { scope: "test" },
-  ],
+  });
+
+  test("it should render the cohorte detail page", async ({ page }) => {
+    await login({ page, role: "gestionnaireVaeCollective" });
+
+    await page.goto(
+      "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
+    );
+
+    await expect(page.getByRole("heading", { level: 1 })).toHaveText(
+      "macohorte",
+    );
+  });
+
+  test("it should go back to the cohortes list page when i click on the back button", async ({
+    page,
+  }) => {
+    await login({ page, role: "gestionnaireVaeCollective" });
+
+    await page.goto(
+      "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
+    );
+
+    await page.getByRole("link", { name: "Retour" }).click();
+
+    await expect(page).toHaveURL(
+      "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes",
+    );
+  });
+
+  test("it should lead me to the edit cohorte name page when i click on the edit cohorte name button", async ({
+    page,
+  }) => {
+    await login({ page, role: "gestionnaireVaeCollective" });
+
+    await page.goto(
+      "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
+    );
+
+    await page.getByRole("link", { name: "Modifier l’intitulé" }).click();
+
+    await expect(page).toHaveURL(
+      "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712/modifier-intitule",
+    );
+  });
 });
 
-test("it should render the cohorte detail page", async ({ page }) => {
-  await login({ page, role: "gestionnaireVaeCollective" });
+test.describe("delete cohorte button", () => {
+  test.describe("when the cohorte status is 'PUBLIE'", () => {
+    test.use({
+      mswHandlers: [
+        [
+          fvae.query("getCohorteByIdForCohortePage", () => {
+            return HttpResponse.json({
+              data: {
+                vaeCollective_getCohorteVaeCollectiveById: {
+                  id: "0eda2cbf-78ae-47af-9f28-34d05f972712",
+                  nom: "macohorte",
+                  status: "PUBLIE",
+                },
+              },
+            });
+          }),
+        ],
+        { scope: "test" },
+      ],
+    });
 
-  await page.goto(
-    "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
-  );
+    test("the delete button should be disabled", async ({ page }) => {
+      await login({ page, role: "gestionnaireVaeCollective" });
 
-  await expect(page.getByRole("heading", { level: 1 })).toHaveText("macohorte");
-});
+      await page.goto(
+        "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
+      );
 
-test("it should go back to the cohortes list page when i click on the back button", async ({
-  page,
-}) => {
-  await login({ page, role: "gestionnaireVaeCollective" });
+      await expect(
+        page.getByRole("button", { name: "Supprimer cette cohorte" }),
+      ).toBeDisabled();
+    });
+  });
 
-  await page.goto(
-    "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
-  );
+  test.describe("when the cohorte status is 'BROUILLON'", () => {
+    test.use({
+      mswHandlers: [
+        [
+          fvae.query("getCohorteByIdForCohortePage", () => {
+            return HttpResponse.json({
+              data: {
+                vaeCollective_getCohorteVaeCollectiveById: {
+                  id: "0eda2cbf-78ae-47af-9f28-34d05f972712",
+                  nom: "macohorte",
+                  status: "BROUILLON",
+                },
+              },
+            });
+          }),
+          fvae.mutation("deleteCohorte", () => {
+            return HttpResponse.json({
+              data: {
+                vaeCollective_deleteCohorte: null,
+              },
+            });
+          }),
+        ],
+        { scope: "test" },
+      ],
+    });
 
-  await page.getByRole("link", { name: "Retour" }).click();
+    test("the delete button should be enabled", async ({ page }) => {
+      await login({ page, role: "gestionnaireVaeCollective" });
 
-  await expect(page).toHaveURL(
-    "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes",
-  );
-});
+      await page.goto(
+        "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
+      );
 
-test("it should lead me to the edit cohorte name page when i click on the edit cohorte name button", async ({
-  page,
-}) => {
-  await login({ page, role: "gestionnaireVaeCollective" });
+      await expect(
+        page.getByRole("button", { name: "Supprimer cette cohorte" }),
+      ).toBeEnabled();
+    });
 
-  await page.goto(
-    "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
-  );
+    test("when i click on the delete button, a confirmation modal should be displayed", async ({
+      page,
+    }) => {
+      await login({ page, role: "gestionnaireVaeCollective" });
 
-  await page.getByRole("link", { name: "Modifier l’intitulé" }).click();
-
-  await expect(page).toHaveURL(
-    "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712/modifier-intitule",
-  );
+      await page.goto(
+        "/vae-collective/commanditaires/115c2693-b625-491b-8b91-c7b3875d86a0/cohortes/0eda2cbf-78ae-47af-9f28-34d05f972712",
+      );
+      await page
+        .getByRole("button", { name: "Supprimer cette cohorte" })
+        .click();
+      await expect(
+        page.getByRole("heading", {
+          name: "La suppression d’une cohorte est irreversible.",
+        }),
+      ).toBeVisible();
+    });
+  });
 });
