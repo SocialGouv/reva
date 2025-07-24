3b651b0b24be436b100c4846230c620b5ccd895d
Pierre A <pierre@avizou.eu>
2025-07-24 14:48:50 +0200

    fix(api):ingore unused exports from test helpers in eslint rule

--- packages/reva-api/eslint.config.mjs
+++ packages/reva-api/eslint.config.mjs
@@ -54,7 +54,7 @@ export default defineConfig([
         2,
         {
           unusedExports: true,
-          ignoreExports: ["eslint.config.mjs", "codegen.ts"],
+          ignoreExports: ["eslint.config.mjs", "codegen.ts", "test/helpers"],
         },
       ],
 
