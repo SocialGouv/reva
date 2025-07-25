842f9330bf0bb74fca2295d52178b5b1d323d8d1
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-25 14:13:47 +0200

    fix(vae-collective): fixed home link

--- packages/reva-vae-collective/src/components/header/Header.tsx
+++ packages/reva-vae-collective/src/components/header/Header.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
         </>
       }
       homeLinkProps={{
-        href: "/",
+        href: "/../",
         title: "Accueil - France VAE",
       }}
       operatorLogo={{
