d6047ff50d739613c28380e3777b523307b76968
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-07-30 14:08:40 +0200

    feat(api): added tests for vaeCollective_publishCohorteVAECollective mutation

---
+++ packages/reva-api/modules/vae-collective/vae-collective.publishCohorteVaeCollective.test.ts
@@ -0,0 +1,188 @@
+import { authorizationHeaderForUser } from "@/test/helpers/authorization-helper";
+import { createCertificationHelper } from "@/test/helpers/entities/create-certification-helper";
+import { createOrganismHelper } from "@/test/helpers/entities/create-organism-helper";
+import { createCohorteVaeCollectiveHelper } from "@/test/helpers/entities/create-vae-collective-helper";
+import { getGraphQLClient, getGraphQLError } from "@/test/jestGraphqlClient";
+
+import { graphql } from "../graphql/generated";
+
+const publishCohorteVAECollective = ({
+  userKeycloakId,
+  userRole,
+  commanditaireVaeCollectiveId,
+  cohorteVaeCollectiveId,
+}: {
+  userKeycloakId: string;
+  userRole: KeyCloakUserRole;
+  commanditaireVaeCollectiveId: string;
+  cohorteVaeCollectiveId: string;
+}) => {
+  const graphqlClient = getGraphQLClient({
+    headers: {
+      authorization: authorizationHeaderForUser({
+        role: userRole,
+        keycloakId: userKeycloakId,
+      }),
+    },
+  });
+
+  const publishCohorteVAECollectiveMutation = graphql(`
+    mutation publishCohorteVAECollective(
+      $commanditaireVaeCollectiveId: ID!
+      $cohorteVaeCollectiveId: ID!
+    ) {
+      vaeCollective_publishCohorteVAECollective(
+        commanditaireVaeCollectiveId: $commanditaireVaeCollectiveId
+        cohorteVaeCollectiveId: $cohorteVaeCollectiveId
+      ) {
+        id
+        codeInscription
+        status
+      }
+    }
+  `);
+
+  return graphqlClient.request(publishCohorteVAECollectiveMutation, {
+    commanditaireVaeCollectiveId,
+    cohorteVaeCollectiveId,
+  });
+};
+
+describe("publish a cohorte vae collective", () => {
+  test("should let me publish a valid cohorte vae collective and generate a valide code inscription", async () => {
+    const certification = await createCertificationHelper();
+
+    const organism = await createOrganismHelper();
+
+    const cohorteVaeCollective = await createCohorteVaeCollectiveHelper({
+      certificationCohorteVaeCollectives: {
+        create: {
+          certification: { connect: { id: certification.id } },
+          certificationCohorteVaeCollectiveOnOrganisms: {
+            create: { organism: { connect: { id: organism.id } } },
+          },
+        },
+      },
+    });
+
+    const userKeycloakId =
+      cohorteVaeCollective.commanditaireVaeCollective?.gestionnaire?.keycloakId;
+
+    if (!userKeycloakId) {
+      throw new Error("Compte gestionnaire non trouvé");
+    }
+
+    const response = await publishCohorteVAECollective({
+      userKeycloakId,
+      userRole: "manage_vae_collective",
+      commanditaireVaeCollectiveId:
+        cohorteVaeCollective.commanditaireVaeCollectiveId,
+      cohorteVaeCollectiveId: cohorteVaeCollective.id,
+    });
+
+    const publishedCohorteVAECollective =
+      response.vaeCollective_publishCohorteVAECollective;
+
+    expect(publishedCohorteVAECollective.status).toBe("PUBLIE");
+    expect(publishedCohorteVAECollective.codeInscription).toMatch(
+      /[abcdefghjklmnpqrstuvwxyz23456789]{8}/,
+    );
+  });
+
+  test("should not let me publish a cohorte vae collective if it is already published", async () => {
+    const cohorteVaeCollective = await createCohorteVaeCollectiveHelper({
+      status: "PUBLIE",
+    });
+
+    const userKeycloakId =
+      cohorteVaeCollective.commanditaireVaeCollective?.gestionnaire?.keycloakId;
+
+    if (!userKeycloakId) {
+      throw new Error("Compte gestionnaire non trouvé");
+    }
+
+    try {
+      await publishCohorteVAECollective({
+        userKeycloakId,
+        userRole: "manage_vae_collective",
+        commanditaireVaeCollectiveId:
+          cohorteVaeCollective.commanditaireVaeCollectiveId,
+        cohorteVaeCollectiveId: cohorteVaeCollective.id,
+      });
+
+      //should reach this line
+      expect(false).toBe(true);
+    } catch (e) {
+      const gqlError = getGraphQLError(e);
+      expect(gqlError).toEqual(
+        "Impossible de publier une cohorte si son statut n'est pas 'BROUILLON'",
+      );
+    }
+  });
+
+  test("should not let me publish a cohorte vae collective if it is not associated to a certification", async () => {
+    const cohorteVaeCollective = await createCohorteVaeCollectiveHelper();
+
+    const userKeycloakId =
+      cohorteVaeCollective.commanditaireVaeCollective?.gestionnaire?.keycloakId;
+
+    if (!userKeycloakId) {
+      throw new Error("Compte gestionnaire non trouvé");
+    }
+
+    try {
+      await publishCohorteVAECollective({
+        userKeycloakId,
+        userRole: "manage_vae_collective",
+        commanditaireVaeCollectiveId:
+          cohorteVaeCollective.commanditaireVaeCollectiveId,
+        cohorteVaeCollectiveId: cohorteVaeCollective.id,
+      });
+
+      //should reach this line
+      expect(false).toBe(true);
+    } catch (e) {
+      const gqlError = getGraphQLError(e);
+      expect(gqlError).toEqual(
+        "Impossible de publier une cohorte si elle n'a pas de certification associée",
+      );
+    }
+  });
+
+  test("should not let me publish a cohorte vae collective if it is not associated to an organism", async () => {
+    const certification = await createCertificationHelper();
+
+    const cohorteVaeCollective = await createCohorteVaeCollectiveHelper({
+      certificationCohorteVaeCollectives: {
+        create: {
+          certification: { connect: { id: certification.id } },
+        },
+      },
+    });
+
+    const userKeycloakId =
+      cohorteVaeCollective.commanditaireVaeCollective?.gestionnaire?.keycloakId;
+
+    if (!userKeycloakId) {
+      throw new Error("Compte gestionnaire non trouvé");
+    }
+
+    try {
+      await publishCohorteVAECollective({
+        userKeycloakId,
+        userRole: "manage_vae_collective",
+        commanditaireVaeCollectiveId:
+          cohorteVaeCollective.commanditaireVaeCollectiveId,
+        cohorteVaeCollectiveId: cohorteVaeCollective.id,
+      });
+
+      //should reach this line
+      expect(false).toBe(true);
+    } catch (e) {
+      const gqlError = getGraphQLError(e);
+      expect(gqlError).toEqual(
+        "Impossible de publier une cohorte si elle n'a pas d'aap associé",
+      );
+    }
+  });
+});
