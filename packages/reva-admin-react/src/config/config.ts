b68eeebba1527f399e22942c51ed90082679235c
ThomasDos <dosanjos.thomas@gmail.com>
2025-08-04 15:19:52 +0200

    refactor(admin): remove unused ADMIN_ELM_URL from config

--- packages/reva-admin-react/src/config/config.ts
+++ packages/reva-admin-react/src/config/config.ts
@@ -6,9 +6,6 @@ export const REST_API_URL =
   process.env.NEXT_PUBLIC_ADMIN_REACT_REST_API_URL ||
   "http://localhost:8080/api";
 
-export const ADMIN_ELM_URL =
-  process.env.NEXT_PUBLIC_ADMIN_ELM_URL || "http://localhost:3000/admin";
-
 export const KEYCLOAK_CLIENT_ID = process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID;
 export const KEYCLOAK_REALM = process.env.NEXT_PUBLIC_KEYCLOAK_REALM;
 export const KEYCLOAK_URL = process.env.NEXT_PUBLIC_KEYCLOAK_URL;
