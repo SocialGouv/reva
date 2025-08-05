d57315b2ff6455623fa86fe45522aff032771cd8
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-05 13:48:38 +0200

    feat(vae-collective): better layout for forgot password page

--- packages/reva-vae-collective/src/app/(main)/(auth)/forgot-password/page.tsx
+++ packages/reva-vae-collective/src/app/(main)/(auth)/forgot-password/page.tsx
@@ -8,43 +8,45 @@ import { sendForgotPasswordEmail } from "./actions";
 export default function ForgotPasswordPage() {
   const [state, action, pending] = useActionState(sendForgotPasswordEmail, {});
   return (
-    <div>
+    <div className="fr-container">
       <h1 className="mb-10">Récupération de mot de passe sur France VAE</h1>
-      <form className="flex flex-col gap-6 max-w-lg" action={action}>
-        <div className="flex flex-col gap-4">
-          <p className="mb-0">
-            Veuillez saisir l’adresse électronique associée à votre compte. Nous
-            vous enverrons plus d’informations pour réinitialiser votre mot de
-            passe.
-          </p>
+      <p className="mb-20">
+        Veuillez saisir l’adresse électronique associée à votre compte. Nous
+        vous enverrons plus d’informations pour réinitialiser votre mot de
+        passe.
+      </p>
 
-          <Input
-            hintText="Format attendu : nom@domaine.fr"
-            nativeInputProps={{
-              id: "email",
-              name: "email",
-              required: true,
-              type: "email",
-              autoComplete: "email",
-              spellCheck: "false",
-            }}
-            label="Email"
-            state={state.errors?.email ? "error" : "default"}
-            stateRelatedMessage={state.errors?.email?.message}
-          />
-        </div>
+      <div className="flex flex-col w-full mx-auto p-6 max-w-xl shadow-[0px_6px_18px_0px_rgba(0,0,18,0.16)]">
+        <form className="flex flex-col gap-6 max-w-lg" action={action}>
+          <div className="flex flex-col gap-4">
+            <Input
+              hintText="Format attendu : nom@domaine.fr"
+              nativeInputProps={{
+                id: "email",
+                name: "email",
+                required: true,
+                type: "email",
+                autoComplete: "email",
+                spellCheck: "false",
+              }}
+              label="Email"
+              state={state.errors?.email ? "error" : "default"}
+              stateRelatedMessage={state.errors?.email?.message}
+            />
+          </div>
 
-        <Button
-          type="submit"
-          className="w-full justify-center"
-          data-test="forgot-password-home-submit"
-          disabled={pending}
-        >
-          Envoyer la demande
-        </Button>
+          <Button
+            type="submit"
+            className="w-full justify-center"
+            data-test="forgot-password-home-submit"
+            disabled={pending}
+          >
+            Envoyer la demande
+          </Button>
 
-        <div className="w-full bg-dsfrGray-200 h-[1px]" />
-      </form>
+          <div className="w-full bg-dsfrGray-200 h-[1px]" />
+        </form>
+      </div>
     </div>
   );
 }
