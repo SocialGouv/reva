a2f8f369c3212e745f9ff3d28d7f9d88a864e127
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-05 14:41:40 +0200

    feat(vae-collective): added a "copied" message on cohorte details page registration code and url components

--- packages/reva-vae-collective/src/app/(main)/commanditaires/[commanditaireId]/cohortes/[cohorteVaeCollectiveId]/_components/registration-code-display/RegistrationCodeDisplay.tsx
+++ packages/reva-vae-collective/src/app/(main)/commanditaires/[commanditaireId]/cohortes/[cohorteVaeCollectiveId]/_components/registration-code-display/RegistrationCodeDisplay.tsx
@@ -1,25 +1,48 @@
 "use client";
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import { useEffect, useState } from "react";
 
 export const RegistrationCodeDisplay = ({
   registrationCode,
 }: {
   registrationCode: string;
-}) => (
-  <div
-    className="flex items-center min-h-[40px]"
-    data-testid="registration-code-display"
-  >
-    <div className="flex px-4 items-center font-bold w-[218px] h-full border">
-      {registrationCode}
-    </div>
-    <Button
-      className="h-full"
-      iconId="fr-icon-qr-code-line"
-      onClick={() => navigator.clipboard.writeText(registrationCode)}
-      priority="tertiary"
+}) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1000);
+    }
+  }, [isCopied]);
+
+  return (
+    <div
+      className="flex items-center min-h-[40px]"
+      data-testid="registration-code-display"
     >
-      Copier le code
-    </Button>
-  </div>
-);
+      <div className="flex px-4 items-center font-bold w-[218px] h-full border">
+        {registrationCode}
+      </div>
+      <Button
+        className="h-full min-w-[168px]"
+        iconId="fr-icon-qr-code-line"
+        onClick={() => {
+          navigator.clipboard.writeText(registrationCode);
+          setIsCopied(true);
+        }}
+        priority="tertiary"
+      >
+        {isCopied ? (
+          <>
+            Copié
+            <span className="ml-auto fr-icon-check-line fr-icon--sm" />
+          </>
+        ) : (
+          <>Copier le code</>
+        )}
+      </Button>
+    </div>
+  );
+};
--- packages/reva-vae-collective/src/app/(main)/commanditaires/[commanditaireId]/cohortes/[cohorteVaeCollectiveId]/_components/registration-url-display/RegistrationUrlDisplay.tsx
+++ packages/reva-vae-collective/src/app/(main)/commanditaires/[commanditaireId]/cohortes/[cohorteVaeCollectiveId]/_components/registration-url-display/RegistrationUrlDisplay.tsx
@@ -1,25 +1,49 @@
 "use client";
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import { useEffect, useState } from "react";
 
 export const RegistrationUrlDisplay = ({
   registrationUrl,
 }: {
   registrationUrl: string;
-}) => (
-  <div
-    className="flex items-center min-h-[40px]"
-    data-testid="registration-url-display"
-  >
-    <div className="flex px-4 items-center font-bold w-[600px] h-full border">
-      {registrationUrl}
-    </div>
-    <Button
-      className="h-full"
-      iconId="fr-icon-links-line"
-      onClick={() => navigator.clipboard.writeText(registrationUrl)}
-      priority="tertiary"
+}) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1000);
+    }
+  }, [isCopied]);
+
+  return (
+    <div
+      className="flex items-center min-h-[40px]"
+      data-testid="registration-url-display"
     >
-      Copier l’URL
-    </Button>
-  </div>
-);
+      <div className="flex px-4 items-center font-bold w-[600px] h-full border">
+        {registrationUrl}
+      </div>
+      <Button
+        className="h-full min-w-[151px]"
+        iconId="fr-icon-links-line"
+        onClick={() => {
+          navigator.clipboard.writeText(registrationUrl);
+          setIsCopied(true);
+        }}
+        priority="tertiary"
+        title={isCopied ? "Copié" : "Copier l’URL"}
+      >
+        {isCopied ? (
+          <>
+            Copié
+            <span className="ml-auto fr-icon-check-line fr-icon--sm" />
+          </>
+        ) : (
+          <>Copier l’URL</>
+        )}
+      </Button>
+    </div>
+  );
+};
