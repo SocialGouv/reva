cb971125ffca8eb5e3a119a454afc97822a7ea95
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-05 10:00:12 +0200

    feat(vae-collective): updated PasswordInput component to take optionals label and nativeInputProps as props

--- packages/reva-vae-collective/src/components/password-input/PasswordInput.tsx
+++ packages/reva-vae-collective/src/components/password-input/PasswordInput.tsx
@@ -5,11 +5,19 @@ import { Input } from "@codegouvfr/react-dsfr/Input";
 import { useState } from "react";
 
 export const PasswordInput = ({
+  label = "Mot de passe",
   state,
   stateRelatedMessage,
+  nativeInputProps = {
+    id: "password",
+    name: "password",
+    required: true,
+  },
 }: {
+  label?: string;
   state?: "error" | "default";
   stateRelatedMessage?: string;
+  nativeInputProps?: React.InputHTMLAttributes<HTMLInputElement>;
 }) => {
   const [revealPassword, setRevealPassword] = useState<boolean>(false);
 
@@ -17,17 +25,15 @@ export const PasswordInput = ({
     <Input
       className="mb-0"
       nativeInputProps={{
-        id: "password",
-        name: "password",
-        required: true,
         type: revealPassword ? "text" : "password",
         spellCheck: "false",
+        ...nativeInputProps,
       }}
       state={state}
       stateRelatedMessage={stateRelatedMessage}
       label={
         <div className="flex flex-row justify-between items-center overflow-hidden max-h-6">
-          Mot de passe
+          {label}
           <Checkbox
             small
             options={[
