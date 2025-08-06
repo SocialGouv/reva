20dead48032869d26f6b46589f250bcf0b0d0198
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-06 10:06:39 +0200

    feat(vae-collective): style changes on reset password confirmation page

--- packages/reva-vae-collective/src/app/(main)/(auth)/reset-password-confirmation/page.tsx
+++ packages/reva-vae-collective/src/app/(main)/(auth)/reset-password-confirmation/page.tsx
@@ -1,15 +1,18 @@
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import Image from "next/image";
+
+import checkPolygon from "./assets/check-polygon.svg";
 
 export default async function ResetPasswordConfirmation() {
   return (
-    <div className="fr-container flex flex-col items-center justify-center text-center p-6 pt-8">
-      <h1 className="text-3xl font-bold text-dsfrGray-800">
-        Votre mot de passe a bien été réinitialisé.
-      </h1>
-
-      <Button className="mt-6" linkProps={{ href: "/login" }}>
-        Retournez à la page d’accueil
-      </Button>
+    <div className="flex flex-col-reverse items-center md:flex-row md:justify-between gap-[50px]">
+      <div>
+        <h1>Votre mot de passe a bien été réinitialisé.</h1>
+        <Button className="mt-6" linkProps={{ href: "/login" }}>
+          Se connecter
+        </Button>
+      </div>
+      <Image src={checkPolygon} alt="icône application" />
     </div>
   );
 }
