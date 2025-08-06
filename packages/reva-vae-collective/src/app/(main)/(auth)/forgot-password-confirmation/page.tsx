21f6c25a2c15ecd37aec01f3d4681e7803f1a3cd
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-06 10:02:15 +0200

    feat(vae-collective): style changes on forget password confirmation page

--- packages/reva-vae-collective/src/app/(main)/(auth)/forgot-password-confirmation/page.tsx
+++ packages/reva-vae-collective/src/app/(main)/(auth)/forgot-password-confirmation/page.tsx
@@ -1,24 +1,25 @@
 "use client";
+import Image from "next/image";
+
+import mailPolygon from "./assets/mail-polygon.svg";
 
 export default function ForgotPasswordConfirmationPage() {
   return (
-    <div className="flex flex-col w-full items-center justify-center text-center p-6 pt-8">
-      <h1 className="text-3xl font-bold text-dsfrGray-800">
-        Un e-mail vous a été envoyé.
-      </h1>
-
-      <p className="text-lg font-bold">
-        Vous avez demandé la réinitialisation du mot de passe de votre compte
-        France VAE.
-      </p>
-      <p className="max-w-2xl">
-        Si vous avez déjà un compte chez France VAE, vous allez recevoir un
-        e-mail avec un lien pour réinitialiser votre mot de passe.
-      </p>
-      <p className="max-w-2xl">
-        Si vous ne trouvez pas notre e-mail, pensez à vérifier votre dossier de
-        courriers indésirables (spams).
-      </p>
+    <div className="flex flex-col-reverse items-center md:flex-row md:justify-between gap-[50px]">
+      <div>
+        <h1>Un e-mail vous a été envoyé.</h1>
+        <p className="text-xl leading-loose">
+          Vous avez demandé la réinitialisation du mot de passe de votre compte
+          France VAE.
+        </p>
+        <p className="text-sm">
+          Si vous avez déjà un compte chez France VAE, vous allez recevoir un
+          e-mail avec un lien pour réinitialiser votre mot de passe. <br />
+          Si vous ne trouvez pas notre e-mail, pensez à vérifier votre dossier
+          de courriers indésirables (spams).
+        </p>
+      </div>
+      <Image src={mailPolygon} alt="icône application" />
     </div>
   );
 }
