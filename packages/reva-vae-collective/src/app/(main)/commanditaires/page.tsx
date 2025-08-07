78102baf2f94c8477200b5d94c8418ae001269cd
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-07 11:39:30 +0200

    feat(vae-collective): added an fvae admin commanditaires page

--- packages/reva-vae-collective/src/app/(main)/commanditaires/page.tsx
+++ packages/reva-vae-collective/src/app/(main)/commanditaires/page.tsx
@@ -1,11 +1,103 @@
-import { redirect } from "next/navigation";
+import { Card } from "@codegouvfr/react-dsfr/Card";
+import { Pagination } from "@codegouvfr/react-dsfr/Pagination";
 
-export default async function CommanditairePage({
-  params,
+import { getAccessTokenFromCookie } from "@/helpers/auth/get-access-token-from-cookie/getAccessTokenFromCookie";
+import { throwUrqlErrors } from "@/helpers/graphql/throw-urql-errors/throwUrqlErrors";
+import { client } from "@/helpers/graphql/urql-client/urqlClient";
+
+import { graphql } from "@/graphql/generated";
+
+const RECORDS_PER_PAGE = 10;
+
+const loadCommanditaires = async ({
+  commanditairesPage = 1,
 }: {
-  params: Promise<{ commanditaireId: string }>;
-}) {
-  const { commanditaireId } = await params;
+  commanditairesPage?: number;
+}) => {
+  const accessToken = await getAccessTokenFromCookie();
 
-  redirect(`/commanditaires/${commanditaireId}/cohortes`);
+  const result = throwUrqlErrors(
+    await client.query(
+      graphql(`
+        query commanditaireVaeCollectivesForCommanditairesPage(
+          $offset: Int!
+          $limit: Int!
+        ) {
+          vaeCollective_commanditaireVaeCollectives(
+            offset: $offset
+            limit: $limit
+          ) {
+            info {
+              totalRows
+            }
+            rows {
+              id
+              raisonSociale
+            }
+          }
+        }
+      `),
+      {
+        offset: (commanditairesPage - 1) * RECORDS_PER_PAGE,
+        limit: RECORDS_PER_PAGE,
+      },
+      {
+        fetchOptions: { headers: { Authorization: `Bearer ${accessToken}` } },
+      },
+    ),
+  );
+  return result.data?.vaeCollective_commanditaireVaeCollectives;
+};
+
+export default async function CommanditairesPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const { page } = await searchParams;
+
+  const currentPage = page ? Number(page) : 1;
+
+  const commanditaires = await loadCommanditaires({
+    commanditairesPage: currentPage,
+  });
+
+  if (!commanditaires) {
+    throw new Error("Erreur lors du chargement des commanditaires");
+  }
+
+  return (
+    <div className="fr-container flex flex-col">
+      <h1 className="mb-12">Commanditaires</h1>
+
+      <ul className="flex flex-col gap-4 list-none px-0 my-0">
+        {commanditaires.rows.map((commanditaire) => {
+          return (
+            <li key={commanditaire.id}>
+              <Card
+                enlargeLink
+                size="small"
+                title={commanditaire.raisonSociale}
+                linkProps={{
+                  href: `/commanditaires/${commanditaire.id}/cohortes`,
+                }}
+              ></Card>
+            </li>
+          );
+        })}
+      </ul>
+
+      <Pagination
+        classes={{
+          root: "mt-12 ml-auto",
+        }}
+        showFirstLast={false}
+        defaultPage={currentPage}
+        count={Math.ceil(commanditaires.info.totalRows / RECORDS_PER_PAGE)}
+        getPageLinkProps={(page) => ({
+          href: `/commanditaires?page=${page}`,
+        })}
+      />
+    </div>
+  );
 }
