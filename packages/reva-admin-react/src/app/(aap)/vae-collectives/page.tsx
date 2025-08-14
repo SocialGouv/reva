e328481cf7c3b9998d53916cff53d16b2e60c72b
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-14 13:47:17 +0200

    feat(admin): added results, pagination and search bar to the vae-collectives page

--- packages/reva-admin-react/src/app/(aap)/vae-collectives/page.tsx
+++ packages/reva-admin-react/src/app/(aap)/vae-collectives/page.tsx
@@ -1,10 +1,28 @@
 "use client";
 import { SideMenu } from "@codegouvfr/react-dsfr/SideMenu";
+import { toDate } from "date-fns";
+import { useSearchParams } from "next/navigation";
+
+import { CandidacyCard } from "@/components/card/candidacy-card/CandidacyCard";
+import { SearchList } from "@/components/search/search-list/SearchList";
+
+import { CandidacyStatusFilter } from "@/graphql/generated/graphql";
 
 import { useVAECollectivesPage } from "./vaeCollectives.hook";
 
 export default function VAECollectivePage() {
-  const { cohortes } = useVAECollectivesPage();
+  const searchParams = useSearchParams();
+  const cohorte_id = searchParams.get("cohorte_id");
+  const status = searchParams.get("status") as CandidacyStatusFilter;
+  const page = searchParams.get("page") || "1";
+  const searchFilter = searchParams.get("search") || "";
+
+  const { cohortes, candidacies } = useVAECollectivesPage({
+    cohorteId: cohorte_id,
+    status: status,
+    page: parseInt(page),
+    searchFilter: searchFilter,
+  });
 
   if (!cohortes) {
     return null;
@@ -29,31 +47,31 @@ export default function VAECollectivePage() {
                 {
                   text: "Candidatures actives",
                   linkProps: {
-                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=ACTIVE_HORS_ABANDON`,
+                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=ACTIVE_HORS_ABANDON&search=${searchFilter}`,
                   },
                 },
                 {
                   text: "Candidatures en jury",
                   linkProps: {
-                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=JURY_HORS_ABANDON`,
+                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=JURY_HORS_ABANDON&search=${searchFilter}`,
                   },
                 },
                 {
                   text: "Candidatures non recevables",
                   linkProps: {
-                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=DOSSIER_FAISABILITE_NON_RECEVABLE_HORS_ABANDON`,
+                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=DOSSIER_FAISABILITE_NON_RECEVABLE_HORS_ABANDON&search=${searchFilter}`,
                   },
                 },
                 {
                   text: "Candidatures abandonnées",
                   linkProps: {
-                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=ABANDON`,
+                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=ABANDON&search=${searchFilter}`,
                   },
                 },
                 {
                   text: "Candidatures réorientées",
                   linkProps: {
-                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=REORIENTEE`,
+                    href: `/vae-collectives?cohorte_id=${cohorte.id}&status=REORIENTEE&search=${searchFilter}`,
                   },
                 },
               ],
@@ -66,6 +84,61 @@ export default function VAECollectivePage() {
             Retrouvez toutes les candidatures en VAE collective en fonction de
             leur état d’avancement.
           </p>
+
+          <SearchList
+            searchResultsPage={
+              candidacies || {
+                rows: [],
+                info: { totalRows: 0, totalPages: 0, currentPage: 0 },
+              }
+            }
+            searchFilter={searchFilter}
+          >
+            {(candidacy) => (
+              <CandidacyCard
+                key={candidacy.id}
+                candidacyId={candidacy.id}
+                candidateFirstname={candidacy.candidate?.firstname || ""}
+                candidateLastname={candidacy.candidate?.lastname || ""}
+                candidateGivenName={candidacy.candidate?.givenName || undefined}
+                certificationLabel={
+                  candidacy.certification
+                    ? `RNCP ${candidacy?.certification.codeRncp} : ${candidacy?.certification?.label}`
+                    : undefined
+                }
+                departmentCode={candidacy.candidate?.department?.code}
+                departmentLabel={candidacy.candidate?.department?.label}
+                organismLabel={
+                  candidacy.organism?.nomPublic || candidacy.organism?.label
+                }
+                organismModalitateAccompagnement={
+                  candidacy.organism?.modaliteAccompagnement
+                }
+                candidacySentAt={
+                  candidacy.candidacyStatuses.some(
+                    (s) => s.status === "VALIDATION",
+                  )
+                    ? toDate(
+                        candidacy.candidacyStatuses.find(
+                          (s) => s.status === "VALIDATION",
+                        )?.createdAt || 0,
+                      )
+                    : undefined
+                }
+                fundable={candidacy.financeModule !== "hors_plateforme"}
+                vaeCollective={!!candidacy.cohorteVaeCollective}
+                vaeCollectiveCommanditaireLabel={
+                  candidacy.cohorteVaeCollective?.commanditaireVaeCollective
+                    .raisonSociale
+                }
+                vaeCollectiveCohortLabel={candidacy.cohorteVaeCollective?.nom}
+                status={candidacy.status}
+                feasibility={candidacy.feasibility}
+                jury={candidacy.jury}
+                dropout={candidacy.candidacyDropOut}
+              />
+            )}
+          </SearchList>
         </div>
       </div>
     </div>
--- packages/reva-admin-react/src/app/(aap)/vae-collectives/vaeCollectives.hook.ts
+++ packages/reva-admin-react/src/app/(aap)/vae-collectives/vaeCollectives.hook.ts
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useGraphQlClient } from "@/components/graphql/graphql-client/GraphqlClient";
 
 import { graphql } from "@/graphql/generated";
+import { CandidacyStatusFilter } from "@/graphql/generated/graphql";
 
 const getCohortesQuery = graphql(`
   query getCohortesForVaeCollectivesPage {
@@ -13,7 +14,96 @@ const getCohortesQuery = graphql(`
   }
 `);
 
-export const useVAECollectivesPage = () => {
+const getCandidaciesQuery = graphql(`
+  query getCandidaciesForVaeCollectivesPage(
+    $cohorteId: ID!
+    $status: CandidacyStatusFilter
+    $limit: Int!
+    $offset: Int!
+    $searchFilter: String!
+  ) {
+    getCandidacies(
+      cohorteVaeCollectiveId: $cohorteId
+      statusFilter: $status
+      limit: $limit
+      offset: $offset
+      searchFilter: $searchFilter
+    ) {
+      rows {
+        id
+        candidate {
+          firstname
+          lastname
+          givenName
+          department {
+            label
+            code
+          }
+        }
+        certification {
+          label
+          codeRncp
+        }
+        organism {
+          label
+          nomPublic
+          modaliteAccompagnement
+        }
+        financeModule
+        candidacyStatuses {
+          status
+          createdAt
+        }
+        status
+        feasibility {
+          dematerializedFeasibilityFile {
+            sentToCandidateAt
+            isReadyToBeSentToCertificationAuthority
+            isReadyToBeSentToCandidate
+            candidateConfirmationAt
+            swornStatementFileId
+          }
+          decision
+          feasibilityFileSentAt
+        }
+        candidacyDropOut {
+          createdAt
+        }
+        jury {
+          dateOfSession
+          result
+        }
+        cohorteVaeCollective {
+          nom
+          nom
+          commanditaireVaeCollective {
+            raisonSociale
+          }
+        }
+      }
+      info {
+        currentPage
+        totalPages
+        totalRows
+      }
+    }
+  }
+`);
+
+export const useVAECollectivesPage = ({
+  cohorteId,
+  status,
+  page = 1,
+  searchFilter = "",
+}: {
+  cohorteId?: string | null;
+  status?: CandidacyStatusFilter | null;
+  page?: number;
+  searchFilter?: string;
+}) => {
+  const RECORDS_PER_PAGE = 10;
+  const offset = (page - 1) * RECORDS_PER_PAGE;
+
   const { graphqlClient } = useGraphQlClient();
 
   const { data: getCohortesResponse } = useQuery({
@@ -21,9 +111,30 @@ export const useVAECollectivesPage = () => {
     queryFn: () => graphqlClient.request(getCohortesQuery),
   });
 
+  const { data: getCandidaciesResponse } = useQuery({
+    queryKey: [
+      cohorteId,
+      status,
+      page,
+      searchFilter,
+      "getCandidaciesForVaeCollectivesPage",
+    ],
+    queryFn: () =>
+      graphqlClient.request(getCandidaciesQuery, {
+        cohorteId: cohorteId ?? "",
+        status: status ?? "ACTIVE_HORS_ABANDON",
+        offset,
+        limit: RECORDS_PER_PAGE,
+        searchFilter,
+      }),
+    enabled: !!cohorteId && !!status,
+  });
+
   const cohortes = getCohortesResponse?.cohortesVaeCollectivesForConnectedAap;
+  const candidacies = getCandidaciesResponse?.getCandidacies;
 
   return {
     cohortes,
+    candidacies,
   };
 };
