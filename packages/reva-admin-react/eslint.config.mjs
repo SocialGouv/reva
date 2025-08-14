8bbc1c13aea7623bf9a7aec5191f8983278c5801
ThomasDos <dosanjos.thomas@gmail.com>
2025-08-14 14:17:49 +0200

    chore(api,admin,candidate,interop,website): remove eslint-plugin-import from package.json and update package-lock.json

--- packages/reva-admin-react/eslint.config.mjs
+++ packages/reva-admin-react/eslint.config.mjs
@@ -1,10 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { fixupConfigRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
 import { defineConfig } from "eslint/config";
-import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -30,10 +29,6 @@ export default defineConfig([
         "plugin:prettier/recommended",
       ),
     ),
-
-    plugins: {
-      import: fixupPluginRules(importPlugin),
-    },
 
     rules: {
       "react/no-unescaped-entities": "off",
