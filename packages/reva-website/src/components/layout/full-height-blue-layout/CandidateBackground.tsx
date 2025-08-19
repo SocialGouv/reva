eab787983c57264d0be54b552e210c07e4316583
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-19 11:08:32 +0200

    fix(website): CandidateBackground now takes all available space inside a flex box

--- packages/reva-website/src/components/layout/full-height-blue-layout/CandidateBackground.tsx
+++ packages/reva-website/src/components/layout/full-height-blue-layout/CandidateBackground.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export const CandidateBackground = (props: { children?: ReactNode }) => (
-  <div className="xl:bg-candidate">
+  <div className="xl:bg-candidate flex-1">
     <div className="fr-container lg:shadow-lifted bg-white xl:my-8">
       {props.children}
     </div>
