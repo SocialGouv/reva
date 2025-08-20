610ead292474a39eec027c4e4fc9724ddf413d45
Florian Bonniec <f.bonniec@me.com>
2025-08-20 10:46:13 +0200

    feat(candidate): use server side generation of df demat

---
+++ packages/reva-candidate/src/components/legacy/organisms/DffSummary/components/PdfLink.tsx
@@ -0,0 +1,84 @@
+import { SyntheticEvent } from "react";
+
+import { useKeycloakContext } from "@/components/auth/keycloak.context";
+
+export const PdfLink = ({
+  url,
+  text,
+  title,
+  className,
+  fileName,
+}: {
+  url: string;
+  text: string;
+  title: string;
+  className?: string;
+  fileName?: string;
+}) => {
+  const { accessToken } = useKeycloakContext();
+
+  const getFileUrlFromBlob = async (): Promise<string | undefined> => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.responseType = "blob";
+      xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+
+      xhr.onload = async function (_error) {
+        if (this.status == 200) {
+          try {
+            const blob = new Blob([this.response], {
+              type: xhr.getResponseHeader("content-type") || "",
+            });
+            const url = window.URL.createObjectURL(blob);
+
+            resolve(url);
+
+            return;
+          } catch (error) {
+            console.error(error);
+          }
+
+          resolve(undefined);
+
+          return;
+        }
+        reject();
+      };
+
+      xhr.send();
+    });
+  };
+
+  const handleClick = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const url = await getFileUrlFromBlob();
+    const a = document.createElement("a");
+
+    document.body.appendChild(a);
+
+    a.href = url!;
+    a.target = "_blank";
+    a.download = fileName || "document_sans_nom";
+
+    a.click();
+
+    document.body.removeChild(a);
+
+    setTimeout(function () {
+      window.URL.revokeObjectURL(url!);
+    }, 1000);
+  };
+  return (
+    <div className="max-w-screen-md truncate">
+      <a
+        href="#"
+        title={title}
+        onClick={handleClick}
+        className={className || ""}
+      >
+        {text}
+      </a>
+    </div>
+  );
+};
