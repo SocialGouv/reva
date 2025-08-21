5c52c235786131ee281bbd42c4debef42dd5039d
ThomasDos <dosanjos.thomas@gmail.com>
2025-08-21 16:12:34 +0200

    chore(website): update legal mentions page content and structure

--- packages/reva-website/src/app/mentions-legales/page.tsx
+++ packages/reva-website/src/app/mentions-legales/page.tsx
@@ -15,19 +15,20 @@ const IndexMentionsLegalesPage = () => {
       </Head>
 
       <div className="w-full max-w-[1048px] mx-auto">
-        <MainTitle>Mentions légales</MainTitle>
-        <ContentSection id="editeur" title="Éditeur">
+        <MainTitle>Mentions légales – France VAE</MainTitle>
+        <ContentSection id="editeur" title="Éditeur de la plateforme">
           <SectionParagraph>
-            Ce site est édité par la Délégation générale à l’emploi et à la
-            formation professionnelle :
+            La plateforme « France VAE » est éditée par la Délégation générale à
+            l'emploi et à la formation professionnelle (DGEFP), située :
             <br />
-            DGEFP
             <br />
             14 avenue Duquesne
             <br />
-            75350 Paris 07 SP
+            75007 Paris
             <br />
-            Tel. : 01 444 38 33 10
+            France
+            <br />
+            Tel. : 01 44 38 38 38
           </SectionParagraph>
         </ContentSection>
         <ContentSection
@@ -35,64 +36,36 @@ const IndexMentionsLegalesPage = () => {
           title="Directeur de la publication"
         >
           <SectionParagraph>
-            Monsieur Benjamin Maurice, Délégué général à l’emploi et à la
-            formation professionnelle. 
+            Le directeur de publication est Monsieur Benjamin MAURICE, Délégué
+            général à l'emploi et à la formation professionnelle.
           </SectionParagraph>
         </ContentSection>
-        <ContentSection id="hebergement" title="Hébergement du site">
+        <ContentSection id="hebergement" title="Hébergement de la plateforme">
           <SectionParagraph>
-            Ce site est hébergé par :<br />
             Scalingo
             <br />
             15 avenue du Rhin
             <br />
             67100 Strasbourg
-          </SectionParagraph>
-        </ContentSection>
-        <ContentSection id="accessibilite" title="Accessibilité">
-          <SectionParagraph>
-            La conformité aux normes d’accessibilité numérique est une priorité,
-            en cours de mise en oeuvre et sera effective dans une prochaine
-            version de ce site.
-          </SectionParagraph>
-        </ContentSection>
-        <ContentSection
-          id="dysfonctionnement"
-          title="Signaler un dysfonctionnement"
-        >
-          <SectionParagraph>
-            Si vous rencontrez un défaut d’accessibilité vous empêchant
-            d’accéder à un contenu ou une fonctionnalité du site, merci de nous
-            en faire part à l'adresse{" "}
-            <a href="mailto:contact@vae.gouv.fr">contact@vae.gouv.fr</a>
-          </SectionParagraph>
-          <SectionParagraph>
-            Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en
-            droit de faire parvenir vos doléances ou une demande de saisine au
-            Défenseur des droits.
-          </SectionParagraph>
-          <SectionParagraph>
-            Pour en savoir plus sur la politique d’accessibilité numérique de
-            l’État:{" "}
-            <a href="http://references.modernisation.gouv.fr/accessibilite-numerique">
-              accessibilite.numerique.gouv.fr
-            </a>
+            <br />
+            France
           </SectionParagraph>
         </ContentSection>
         <ContentSection id="securite" title="Sécurité">
           <SectionParagraph>
             Le site est protégé par un certificat électronique, matérialisé pour
             la grande majorité des navigateurs par un cadenas. Cette protection
-            participe à la confidentialité des échanges. En aucun cas les
-            services associés à la plateforme ne seront à l'origine d'envoi de
-            courriels pour demander la saisie d'informations personnelles.
-          </SectionParagraph>
-        </ContentSection>
-        <ContentSection id="credit" title="Crédits et droits des visuels">
-          <SectionParagraph>
-            Les visuels générés par intelligence artificielle sur ce site
-            proviennent de Visual Electric et sont utilisés conformément aux
-            conditions de leur licence commerciale.
+            participe à la confidentialité des échanges.
+            <br />
+            <br />
+            En aucun cas les services associés à la plateforme ne seront à
+            l'origine d'envoi de courriels pour demander la saisie
+            d'informations personnelles. Pour plus d'information, veuillez-vous
+            rendre directement ici :{" "}
+            <a href="https://vae.gouv.fr/confidentialite/">
+              https://vae.gouv.fr/confidentialite/
+            </a>{" "}
+            .
           </SectionParagraph>
         </ContentSection>
       </div>
