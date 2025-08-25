42bca7ad0f8317f36ecc69c76f15233f2d830d1d
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-08-25 14:09:38 +0200

    feat(website): updated candidate page background red triangles

--- packages/reva-website/src/components/candidate-space/BackGroundUnions.tsx
+++ packages/reva-website/src/components/candidate-space/BackGroundUnions.tsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
 
 const BackGroundUnions = ({ double = true }: { double?: boolean }) => (
-  <div className="absolute -top-8 -z-10 w-full">
-    <div className="hidden lg:block">
+  <div className="absolute -z-10 w-full">
+    <div className="hidden lg:block overflow-hidden">
       <Image
         src="/candidate-space/unions-background/union-background1.svg"
-        width={3000}
-        height={1074}
-        style={{ width: "100%" }}
+        width={3576}
+        height={990}
+        style={{ width: "3576px", height: "990px" }}
+        className="object-none"
         alt="rayon rose en fond d'écran"
       />
     </div>
     {double && (
-      <div className="top-[1190px] hidden lg:block">
+      <div className="relative top-[90px] hidden lg:block overflow-hidden">
         <Image
           src="/candidate-space/unions-background/union-background2.svg"
-          width={3000}
-          height={1074}
-          style={{ width: "100%" }}
+          width={3576}
+          height={774}
+          style={{ width: "3576px", height: "774px" }}
+          className="object-none"
           alt="rayon rose en fond d'écran"
         />
       </div>
