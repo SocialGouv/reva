1ca36d43766e5fdc63c6166b2cbbe04e468d7db3
ThomasDos <dosanjos.thomas@gmail.com>
2025-09-01 16:23:46 +0200

    chore(candidate): remove unused getStrapiImageUrl utility function

--- packages/reva-candidate/src/utils/getStrapiImageUrl.util.ts
+++
@@ -1,8 +0,0 @@
-import { STRAPI_BASE_URL } from "@/config/config";
-
-export const getStrapiImageUrl = (strapiUrl: string | null | undefined) => {
-  if (!strapiUrl) return "";
-  return strapiUrl.startsWith("http")
-    ? strapiUrl
-    : `${STRAPI_BASE_URL}${strapiUrl}`;
-};
