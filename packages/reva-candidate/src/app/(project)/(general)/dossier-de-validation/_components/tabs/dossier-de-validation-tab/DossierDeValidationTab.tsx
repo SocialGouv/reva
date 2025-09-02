d862e1569173c1f6a5bca25047e08de14c47fd7d
Cédric Soulas <soulascedric@gmail.com>
2025-09-02 15:53:04 +0200

    feat(candidate): move incomplet dv alert and history above the resource sidebar

--- packages/reva-candidate/src/app/(project)/(general)/dossier-de-validation/_components/tabs/dossier-de-validation-tab/DossierDeValidationTab.tsx
+++ packages/reva-candidate/src/app/(project)/(general)/dossier-de-validation/_components/tabs/dossier-de-validation-tab/DossierDeValidationTab.tsx
@@ -104,181 +104,181 @@ export const DossierDeValidationTab = ({
   const [currentProblem, ...previousProblems] = dossierDeValidationProblems;
 
   return (
-    <div className="flex gap-6">
-      <main className="flex-1">
-        {dossierDeValidationIncomplete && currentProblem && (
-          <>
-            <Alert
-              data-test="dossier-de-validation-signale-alert"
-              className="mb-6"
-              severity="warning"
-              title={`Dossier de validation signalé par le certificateur le ${format(currentProblem.decisionSentAt, "dd/MM/yyyy")}`}
-              description={
-                <div className="flex flex-col gap-6">
-                  <p>
-                    Ce dossier de validation a été signalé comme comportant des
-                    erreurs par le certificateur.
-                  </p>
-                  <p>
-                    <span className="font-semibold">
-                      Motif du signalement :{" "}
-                    </span>
-                    {currentProblem.decisionComment}
-                  </p>
-                  <p className="font-semibold">
-                    Nous vous invitons à retourner dès que possible un dossier
-                    corrigé.
-                  </p>
-                </div>
-              }
-            />
-            {previousProblems.length > 0 && (
-              <div className="fr-accordions-group mb-8">
-                <Accordion label="Voir les anciens dossiers de validation">
-                  <ul className="flex flex-col gap-8 pl-0">
-                    {previousProblems.map((p, i) => (
-                      <GrayCard key={i}>
-                        <dl>
-                          <dt className="font-bold text-xl">
-                            Dossier signalé :
-                          </dt>
-                          <dd>
-                            Dossier signalé le{" "}
-                            {format(p.decisionSentAt, "dd/MM/yyyy")}
-                          </dd>
-                          <br />
-                          <dt className="font-bold text-xl">
-                            Motif du signalement :
-                          </dt>
-                          <dd>{p.decisionComment}</dd>
-                        </dl>
-                      </GrayCard>
-                    ))}
-                  </ul>
-                </Accordion>
+    <div className="flex flex-col">
+      {dossierDeValidationIncomplete && currentProblem && (
+        <>
+          <Alert
+            data-test="dossier-de-validation-signale-alert"
+            className="mb-6"
+            severity="warning"
+            title={`Dossier de validation signalé par le certificateur le ${format(currentProblem.decisionSentAt, "dd/MM/yyyy")}`}
+            description={
+              <div className="flex flex-col gap-6">
+                <p>
+                  Ce dossier de validation a été signalé comme comportant des
+                  erreurs par le certificateur.
+                </p>
+                <p>
+                  <span className="font-semibold">Motif du signalement : </span>
+                  {currentProblem.decisionComment}
+                </p>
+                <p className="font-semibold">
+                  Nous vous invitons à retourner dès que possible un dossier
+                  corrigé.
+                </p>
               </div>
-            )}
-          </>
-        )}
-
-        <h2>Déposer votre dossier de validation complété</h2>
-        <p className="text-lg">
-          Une fois votre dossier complété, déposez-le ici. Si des pièces
-          supplémentaires sont requises (ex : attestation de premiers secours),
-          elles seront également à envoyer ici en pièces jointes
-          supplémentaires.
-        </p>
-        <form
-          className="flex flex-col flex-1 mb-2 overflow-auto"
-          onSubmit={handleFormSubmit}
-          onReset={(e) => {
-            e.preventDefault();
-            handleReset();
-          }}
-        >
-          <div className="flex flex-col gap-6 mb-12">
-            <FancyUpload
-              className="dossier-de-validation-file-upload"
-              title="Joindre le dossier de validation"
-              description="Si vous souhaitez déposer une attestation de transmission / finalisation du DV : vous pouvez déposer cette pièce ici."
-              hint="Formats supportés : jpg, png, pdf avec un poids maximum de 15Mo"
-              nativeInputProps={{
-                ...register("dossierDeValidationFile"),
-                accept: ".jpg,.jpeg,.png,.pdf",
-              }}
-              state={errors.dossierDeValidationFile ? "error" : "default"}
-              stateRelatedMessage={errors.dossierDeValidationFile?.[0]?.message}
-            />
-            {dossierDeValidationOtherFilesFields.map((d, i) => (
-              <FancyUpload
-                key={d.id}
-                title="Joindre des pièces supplémentaires (optionnel)"
-                hint="Formats supportés : jpg, png, pdf avec un poids maximum de 15Mo"
-                nativeInputProps={{
-                  ...register(`dossierDeValidationOtherFiles.${i}`, {
-                    onChange: (e) => {
-                      if (!e.target.value) {
-                        removeDossierDeValidationOtherFiles(i);
-                      }
-                    },
-                  }),
-                  accept: ".jpg,.jpeg,.png,.pdf",
-                }}
-                onClickDelete={() => removeDossierDeValidationOtherFiles(i)}
-                state={
-                  errors.dossierDeValidationOtherFiles?.[i]
-                    ? "error"
-                    : "default"
-                }
-                stateRelatedMessage={
-                  errors.dossierDeValidationOtherFiles?.[i]?.message
-                }
-              />
-            ))}
-            <div>
-              <hr className="pb-3" />
-              <Button
-                priority="tertiary no outline"
-                iconId="fr-icon-add-line"
-                onClick={handleAddAdditionalFile}
-                type="button"
-              >
-                Ajouter une pièce jointe supplémentaire
-              </Button>
-            </div>
-          </div>
-          <Checkbox
-            className="mr-0"
-            legend="Avant de finaliser votre envoi"
-            data-test="dossier-de-validation-checkbox-group"
-            options={[
-              {
-                label:
-                  "J'ai bien vérifié que le dossier de validation était complet et lisible.",
-                nativeInputProps: {
-                  ...register("dossierDeValidationCheck1"),
-                },
-              },
-              {
-                label:
-                  "J'ai bien vérifié que toutes les pièces supplémentaires étaient lisibles",
-                nativeInputProps: {
-                  ...register("dossierDeValidationCheck2"),
-                },
-              },
-            ]}
-            small
-            state={
-              errors.dossierDeValidationCheck1 ||
-              errors.dossierDeValidationCheck2
-                ? "error"
-                : "default"
-            }
-            stateRelatedMessage={
-              errors.dossierDeValidationCheck1?.message ||
-              errors.dossierDeValidationCheck2?.message
             }
           />
-          <div className="flex justify-end gap-2">
-            <Button
-              priority="tertiary no outline"
-              type="reset"
-              disabled={isSubmitting}
-            >
-              Réinitialiser
-            </Button>
-            <Button
-              type="submit"
-              disabled={!isDirty || isSubmitting}
-              data-test="submit-dossier-de-validation-form-button"
-            >
-              Envoyer les documents
-            </Button>
-          </div>
-        </form>
-      </main>
+          {previousProblems.length > 0 && (
+            <div className="fr-accordions-group mb-6">
+              <Accordion label="Voir les anciens dossiers de validation">
+                <ul className="flex flex-col gap-8 pl-0">
+                  {previousProblems.map((p, i) => (
+                    <GrayCard key={i}>
+                      <dl>
+                        <dt className="font-bold text-xl">Dossier signalé :</dt>
+                        <dd>
+                          Dossier signalé le{" "}
+                          {format(p.decisionSentAt, "dd/MM/yyyy")}
+                        </dd>
+                        <br />
+                        <dt className="font-bold text-xl">
+                          Motif du signalement :
+                        </dt>
+                        <dd>{p.decisionComment}</dd>
+                      </dl>
+                    </GrayCard>
+                  ))}
+                </ul>
+              </Accordion>
+            </div>
+          )}
+        </>
+      )}
 
-      <ResourcesSection certification={certification} />
+      <div className="flex gap-6">
+        <main className="flex-1">
+          <h2>Déposer votre dossier de validation complété</h2>
+          <p className="text-lg">
+            Une fois votre dossier complété, déposez-le ici. Si des pièces
+            supplémentaires sont requises (ex : attestation de premiers
+            secours), elles seront également à envoyer ici en pièces jointes
+            supplémentaires.
+          </p>
+          <form
+            className="flex flex-col flex-1 mb-2 overflow-auto"
+            onSubmit={handleFormSubmit}
+            onReset={(e) => {
+              e.preventDefault();
+              handleReset();
+            }}
+          >
+            <div className="flex flex-col gap-6 mb-12">
+              <FancyUpload
+                className="dossier-de-validation-file-upload"
+                title="Joindre le dossier de validation"
+                description="Si vous souhaitez déposer une attestation de transmission / finalisation du DV : vous pouvez déposer cette pièce ici."
+                hint="Formats supportés : jpg, png, pdf avec un poids maximum de 15Mo"
+                nativeInputProps={{
+                  ...register("dossierDeValidationFile"),
+                  accept: ".jpg,.jpeg,.png,.pdf",
+                }}
+                state={errors.dossierDeValidationFile ? "error" : "default"}
+                stateRelatedMessage={
+                  errors.dossierDeValidationFile?.[0]?.message
+                }
+              />
+              {dossierDeValidationOtherFilesFields.map((d, i) => (
+                <FancyUpload
+                  key={d.id}
+                  title="Joindre des pièces supplémentaires (optionnel)"
+                  hint="Formats supportés : jpg, png, pdf avec un poids maximum de 15Mo"
+                  nativeInputProps={{
+                    ...register(`dossierDeValidationOtherFiles.${i}`, {
+                      onChange: (e) => {
+                        if (!e.target.value) {
+                          removeDossierDeValidationOtherFiles(i);
+                        }
+                      },
+                    }),
+                    accept: ".jpg,.jpeg,.png,.pdf",
+                  }}
+                  onClickDelete={() => removeDossierDeValidationOtherFiles(i)}
+                  state={
+                    errors.dossierDeValidationOtherFiles?.[i]
+                      ? "error"
+                      : "default"
+                  }
+                  stateRelatedMessage={
+                    errors.dossierDeValidationOtherFiles?.[i]?.message
+                  }
+                />
+              ))}
+              <div>
+                <hr className="pb-3" />
+                <Button
+                  priority="tertiary no outline"
+                  iconId="fr-icon-add-line"
+                  onClick={handleAddAdditionalFile}
+                  type="button"
+                >
+                  Ajouter une pièce jointe supplémentaire
+                </Button>
+              </div>
+            </div>
+            <Checkbox
+              className="mr-0"
+              legend="Avant de finaliser votre envoi"
+              data-test="dossier-de-validation-checkbox-group"
+              options={[
+                {
+                  label:
+                    "J'ai bien vérifié que le dossier de validation était complet et lisible.",
+                  nativeInputProps: {
+                    ...register("dossierDeValidationCheck1"),
+                  },
+                },
+                {
+                  label:
+                    "J'ai bien vérifié que toutes les pièces supplémentaires étaient lisibles",
+                  nativeInputProps: {
+                    ...register("dossierDeValidationCheck2"),
+                  },
+                },
+              ]}
+              small
+              state={
+                errors.dossierDeValidationCheck1 ||
+                errors.dossierDeValidationCheck2
+                  ? "error"
+                  : "default"
+              }
+              stateRelatedMessage={
+                errors.dossierDeValidationCheck1?.message ||
+                errors.dossierDeValidationCheck2?.message
+              }
+            />
+            <div className="flex justify-end gap-2">
+              <Button
+                priority="tertiary no outline"
+                type="reset"
+                disabled={isSubmitting}
+              >
+                Réinitialiser
+              </Button>
+              <Button
+                type="submit"
+                disabled={!isDirty || isSubmitting}
+                data-test="submit-dossier-de-validation-form-button"
+              >
+                Envoyer les documents
+              </Button>
+            </div>
+          </form>
+        </main>
+
+        <ResourcesSection certification={certification} />
+      </div>
     </div>
   );
 };
