bb531344d3552e0908863efc0a0c0a5420b1395a
Florian Bonniec <f.bonniec@me.com>
2025-09-19 09:50:02 +0200

    fix(api): fixed generated pdf by generateFeasibilityFileByCandidacyId feature

--- packages/reva-api/modules/shared/date/formatDateWithoutTimestamp.ts
+++ packages/reva-api/modules/shared/date/formatDateWithoutTimestamp.ts
@@ -1,4 +1,19 @@
-export const formatDateWithoutTimestamp = (date: Date) =>
-  date.toLocaleDateString("fr-FR", {
+type DateFormat = "dd/MM/yyyy" | "dd MMMM yyyy";
+
+export const formatDateWithoutTimestamp = (
+  date: Date,
+  format: DateFormat = "dd/MM/yyyy",
+) => {
+  if (format == "dd MMMM yyyy") {
+    return date.toLocaleDateString("fr-FR", {
+      timeZone: "Europe/Paris",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+  }
+
+  return date.toLocaleDateString("fr-FR", {
     timeZone: "Europe/Paris",
   });
+};
