e28dc0538a8e13e2bce636ee8044432ce85a9f77
Pierre A <pierre@avizou.eu>
2025-09-22 13:47:56 +0200

    feat(candidate):add new information about jury on certification page

--- packages/reva-candidate/src/components/certification-page/tabs/JuryTab.tsx
+++ packages/reva-candidate/src/components/certification-page/tabs/JuryTab.tsx
@@ -1,4 +1,5 @@
 import Tag from "@codegouvfr/react-dsfr/Tag";
+import Link from "next/link";
 
 import { CertificationJuryTypeOfModality } from "@/graphql/generated/graphql";
 
@@ -23,10 +24,10 @@ export const JuryTab = ({
       label="Mise en situation professionnelle"
       typeOfModality={juryTypeMiseEnSituationProfessionnelle}
     />
-    <h2 className="text-xl  mb-0">Frais de certification</h2>
+    <h2 className="text-xl  mb-0">Estimation des frais de certification</h2>
     {juryEstimatedCost && (
       <div className="flex flex-col items-start md:flex-row md:items-center gap-2">
-        <div className="w-[400px]">À prévoir pour passer l’épreuve</div>
+        <div className="w-[400px]">Montant à régler au certificateur</div>
         <Tag>
           <span className="fr-icon-money-euro-circle-fill fr-icon--sm mr-1" />
           {juryEstimatedCost}
@@ -34,19 +35,41 @@ export const JuryTab = ({
       </div>
     )}
 
+    <div>
+      <h3 className="text-base my-0">
+        Pour comprendre le rôle du certificateur :
+      </h3>
+      <Link
+        href="https://vae.gouv.fr/savoir-plus/articles/missions-organisme-certificateur/"
+        className="fr-link"
+        target="_blank"
+      >
+        Quelles sont les missions d’un organisme certificateur ?
+      </Link>
+    </div>
+
     {juryPlace ? (
-      <>
-        <h2 className="text-xl">Lieu de passage</h2>
-        <p>{juryPlace}</p>
-      </>
+      <div>
+        <h2 className="text-xl mb-4">Lieu de passage du jury</h2>
+        <p className="my-0">{juryPlace}</p>
+      </div>
     ) : (
       <div className="flex gap-2">
         <span className="fr-icon-info-fill text-dsfrGray-mentionGrey" />
-        <p className="text-dsfrGray-titleGrey italic">
+        <p className="text-dsfrGray-titleGrey italic mb-0">
           Il n’y a pas de lieu de passage renseigné pour le jury sur ce diplôme.
         </p>
       </div>
     )}
+    <div>
+      <Link
+        href="https://vae.gouv.fr/savoir-plus/articles/comment-se-deroule-un-jury-vae/"
+        className="fr-link"
+        target="_blank"
+      >
+        Comment se déroule un jury VAE ?
+      </Link>
+    </div>
   </div>
 );
 
