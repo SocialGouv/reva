03db42b6c531255027a72c39cb8207af0c9fd492
Pierre A <pierre@avizou.eu>
2025-09-24 16:42:52 +0200

    feat(admin):rework certification additional info summary card

--- packages/reva-admin-react/src/components/certifications/certification-additional-info-summary-card/CertificationAdditionalInfoSummaryCard.tsx
+++ packages/reva-admin-react/src/components/certifications/certification-additional-info-summary-card/CertificationAdditionalInfoSummaryCard.tsx
@@ -36,7 +36,7 @@ export const CertificationAdditionalInfoSummaryCard = ({
   <EnhancedSectionCard
     data-test="additional-info-summary-card"
     isEditable={isAdmin ? false : isEditable}
-    title="Ressources complémentaires"
+    title="Documentation"
     titleIconClass="fr-icon-info-fill"
     status={certificationAdditionalInfo ? "COMPLETED" : "TO_COMPLETE"}
     buttonOnClickHref={updateButtonHref}
@@ -44,7 +44,32 @@ export const CertificationAdditionalInfoSummaryCard = ({
     {certificationAdditionalInfo ? (
       <div className="flex flex-col gap-4" data-test="additional-info-content">
         <section className="flex flex-col gap-4">
-          <h3 className="mb-0">Documentation sur la certification :</h3>
+          <section className="flex flex-col gap-4">
+            <h3 className="mb-0">Documents essentiels :</h3>
+            {certificationAdditionalInfo.dossierDeValidationTemplate
+              ?.previewUrl && (
+              <FancyPreview
+                src={
+                  certificationAdditionalInfo.dossierDeValidationTemplate
+                    .previewUrl
+                }
+                title={"Trame du dossier de validation"}
+                name={
+                  certificationAdditionalInfo.dossierDeValidationTemplate.name
+                }
+                defaultDisplay={false}
+              />
+            )}
+            {certificationAdditionalInfo.dossierDeValidationLink && (
+              <Link
+                href={certificationAdditionalInfo.dossierDeValidationLink}
+                className="fr-link mr-auto"
+                target="_blank"
+              >
+                Trame du dossier de validation
+              </Link>
+            )}
+          </section>
           {certificationAdditionalInfo.linkToReferential && (
             <Link
               href={certificationAdditionalInfo.linkToReferential}
@@ -74,53 +99,7 @@ export const CertificationAdditionalInfoSummaryCard = ({
           )}
         </section>
         <section className="flex flex-col gap-4">
-          <h3 className="mb-0">Documentation sur le dossier de validation :</h3>
-          {certificationAdditionalInfo.dossierDeValidationTemplate
-            ?.previewUrl && (
-            <FancyPreview
-              src={
-                certificationAdditionalInfo.dossierDeValidationTemplate
-                  .previewUrl
-              }
-              title={"Trame du dossier de validation"}
-              name={
-                certificationAdditionalInfo.dossierDeValidationTemplate.name
-              }
-              defaultDisplay={false}
-            />
-          )}
-          {certificationAdditionalInfo.dossierDeValidationLink && (
-            <Link
-              href={certificationAdditionalInfo.dossierDeValidationLink}
-              className="fr-link mr-auto"
-              target="_blank"
-            >
-              Trame du dossier de validation
-            </Link>
-          )}
-        </section>
-        <section className="flex flex-col gap-4">
-          <h3 className="mb-0">Contact d’un expert de la certification :</h3>
-          {certificationAdditionalInfo.certificationExpertContactDetails && (
-            <p className="mb-0 font-bold">
-              {certificationAdditionalInfo.certificationExpertContactDetails}
-            </p>
-          )}
-          <div className="flex flex-row gap-4">
-            {certificationAdditionalInfo.certificationExpertContactPhone && (
-              <p className="mb-0">
-                {certificationAdditionalInfo.certificationExpertContactPhone}
-              </p>
-            )}
-            {certificationAdditionalInfo.certificationExpertContactEmail && (
-              <p className="mb-0">
-                {certificationAdditionalInfo.certificationExpertContactEmail}
-              </p>
-            )}
-          </div>
-        </section>
-        <section className="flex flex-col gap-4">
-          <h3 className="mb-0">Documentation libre :</h3>
+          <h3 className="mb-0">Documents complémentaires :</h3>
           {certificationAdditionalInfo.additionalDocuments.map(
             (document) =>
               document.previewUrl && (
@@ -136,16 +115,25 @@ export const CertificationAdditionalInfoSummaryCard = ({
         </section>
         <section className="flex flex-col gap-4">
           <h3 className="mb-0">Informations complémentaires :</h3>
-          <dl>
-            <dt>Ressources pour aider au parcours VAE :</dt>
-            <dd className="font-medium mb-4">
-              {certificationAdditionalInfo.usefulResources}
-            </dd>
-            <dt>Remarques à transmettre à l’AAP :</dt>
-            <dd className="font-medium mb-4">
-              {certificationAdditionalInfo.commentsForAAP}
-            </dd>
-          </dl>
+          <div>
+            {certificationAdditionalInfo.certificationExpertContactDetails && (
+              <p className="mb-2 font-bold">
+                {certificationAdditionalInfo.certificationExpertContactDetails}
+              </p>
+            )}
+            <div className="flex flex-row gap-4">
+              {certificationAdditionalInfo.certificationExpertContactPhone && (
+                <p className="mb-0">
+                  {certificationAdditionalInfo.certificationExpertContactPhone}
+                </p>
+              )}
+              {certificationAdditionalInfo.certificationExpertContactEmail && (
+                <p className="mb-0">
+                  {certificationAdditionalInfo.certificationExpertContactEmail}
+                </p>
+              )}
+            </div>
+          </div>
         </section>
       </div>
     ) : !isAdmin ? (
