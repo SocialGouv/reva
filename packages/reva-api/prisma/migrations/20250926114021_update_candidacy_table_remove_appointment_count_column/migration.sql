eab6cbdf1ac32fdb970a61d300984296efe229cc
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-09-26 13:45:32 +0200

    chore(api+candidate): removed appointmentCount column from the candidacy table

---
+++ packages/reva-api/prisma/migrations/20250926114021_update_candidacy_table_remove_appointment_count_column/migration.sql
@@ -0,0 +1,9 @@
+/*
+Warnings:
+
+- You are about to drop the column `appointment_count` on the `candidacy` table. All the data in the column will be lost.
+
+ */
+-- AlterTable
+ALTER TABLE "candidacy"
+DROP COLUMN "appointment_count";
