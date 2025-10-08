6f38bd49a054079ad4c4c1ddf5149b03eab8f1fc
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-10-08 10:15:53 +0200

    feat(api): past appointments cannot be updated

--- packages/reva-api/modules/appointment/appointment.test.ts
+++ packages/reva-api/modules/appointment/appointment.test.ts
@@ -3,6 +3,7 @@ import { AppointmentType } from "@prisma/client";
 import { authorizationHeaderForUser } from "@/test/helpers/authorization-helper";
 import { createAppointmentHelper } from "@/test/helpers/entities/create-appointment-helper";
 import { createCandidacyHelper } from "@/test/helpers/entities/create-candidacy-helper";
+import { createFeatureHelper } from "@/test/helpers/entities/create-feature-helper";
 import { getGraphQLClient, getGraphQLError } from "@/test/test-graphql-client";
 
 import { graphql } from "../graphql/generated";
@@ -14,6 +15,16 @@ const graphqlClient = getGraphQLClient({
       keycloakId: "1b0e7046-ca61-4259-b716-785f36ab79b2",
     }),
   },
+});
+
+beforeEach(async () => {
+  await createFeatureHelper({
+    args: {
+      key: "APPOINTMENTS",
+      label: "APPOINTMENTS",
+      isActive: true,
+    },
+  });
 });
 
 test("should get a candidacy appointments", async () => {
@@ -175,7 +186,7 @@ test("should not create an appointment and throw an error if there is already a 
   }
 });
 
-test("should update an appointment", async () => {
+test("should update an appointment when it is not past", async () => {
   const updateAppointment = graphql(`
     mutation updateAppointment($input: UpdateAppointmentInput!) {
       appointment_updateAppointment(input: $input) {
@@ -193,6 +204,7 @@ test("should update an appointment", async () => {
 
   const appointment = await createAppointmentHelper({
     candidacyId: candidacy.id,
+    date: new Date("2225-08-12"),
   });
 
   const res = await graphqlClient.request(updateAppointment, {
@@ -202,7 +214,7 @@ test("should update an appointment", async () => {
       title: "Updated test Appointment",
       description: "Updated test Description",
       location: "Updated test Location",
-      date: "2025-09-26",
+      date: "2225-09-26",
       duration: "TWO_HOURS",
     },
   });
@@ -211,10 +223,46 @@ test("should update an appointment", async () => {
     appointment_updateAppointment: {
       title: "Updated test Appointment",
       type: AppointmentType.RENDEZ_VOUS_PEDAGOGIQUE,
-      date: "2025-09-26",
+      date: "2225-09-26",
       description: "Updated test Description",
       location: "Updated test Location",
       duration: "TWO_HOURS",
     },
   });
+});
+
+test("should not update an appointment when it is past", async () => {
+  const updateAppointment = graphql(`
+    mutation updateAppointment($input: UpdateAppointmentInput!) {
+      appointment_updateAppointment(input: $input) {
+        title
+        type
+        date
+        description
+        location
+        duration
+      }
+    }
+  `);
+
+  const candidacy = await createCandidacyHelper();
+
+  const appointment = await createAppointmentHelper({
+    candidacyId: candidacy.id,
+    date: new Date("1999-08-12"),
+  });
+
+  expect(() =>
+    graphqlClient.request(updateAppointment, {
+      input: {
+        appointmentId: appointment.id,
+        candidacyId: candidacy.id,
+        title: "Updated test Appointment",
+        description: "Updated test Description",
+        location: "Updated test Location",
+        date: "2225-09-26",
+        duration: "TWO_HOURS",
+      },
+    }),
+  ).rejects.toThrowError("Impossible de modifier un rendez-vous passé");
 });
--- packages/reva-api/modules/appointment/features/updateAppointment.ts
+++ packages/reva-api/modules/appointment/features/updateAppointment.ts
@@ -2,9 +2,12 @@ import {
   CandidacyAuditLogUserInfo,
   logCandidacyAuditEvent,
 } from "@/modules/candidacy-log/features/logCandidacyAuditEvent";
+import { isFeatureActiveForUser } from "@/modules/feature-flipping/feature-flipping.features";
 import { prismaClient } from "@/prisma/client";
 
 import { UpdateAppointmentInput } from "../appointment.types";
+
+import { getAppointmentTemporalStatus } from "./getAppointmentTemporalStatus";
 
 export const updateAppointment = async ({
   input,
@@ -13,7 +16,28 @@ export const updateAppointment = async ({
   input: UpdateAppointmentInput;
   userInfo: CandidacyAuditLogUserInfo;
 }) => {
+  const appointmentsFeatureActive = await isFeatureActiveForUser({
+    userKeycloakId: userInfo.userKeycloakId,
+    feature: "APPOINTMENTS",
+  });
+
   const { appointmentId, ...rest } = input;
+
+  if (appointmentsFeatureActive) {
+    const oldAppointment = await prismaClient.appointment.findUnique({
+      where: { id: appointmentId },
+    });
+
+    if (!oldAppointment) {
+      throw new Error("Rendez-vous non trouvé");
+    }
+
+    if (
+      getAppointmentTemporalStatus({ date: oldAppointment.date }) === "PAST"
+    ) {
+      throw new Error("Impossible de modifier un rendez-vous passé");
+    }
+  }
 
   const result = await prismaClient.appointment.update({
     where: { id: input.appointmentId },
