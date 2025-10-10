40e61b810761b27e9d0499c546c6e0bcaa996d1a
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-10-10 15:21:06 +0200

    feat(api): removed appointment time field from database and graphql type

---
+++ packages/reva-api/prisma/migrations/20251010132036_update_appointment_table_drop_time_column/migration.sql
@@ -0,0 +1,9 @@
+/*
+Warnings:
+
+- You are about to drop the column `time` on the `appointment` table. All the data in the column will be lost.
+
+ */
+-- AlterTable
+ALTER TABLE "appointment"
+DROP COLUMN "time";
