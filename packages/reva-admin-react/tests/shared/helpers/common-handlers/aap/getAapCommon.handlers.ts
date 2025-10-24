d76c1daff0c6c76c6ed72cf79fe10f198c04b921
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-10-24 13:15:59 +0200

    feat(admin): added a helper in playwright tests for common aap handlers

---
+++ packages/reva-admin-react/tests/shared/helpers/common-handlers/aap/getAapCommon.handlers.ts
@@ -0,0 +1,69 @@
+import { graphql, Page } from "next/experimental/testmode/playwright/msw";
+
+import { graphQLResolver } from "../../network/msw";
+import { waitGraphQL } from "../../network/requests";
+
+const aapCommonWait = async (page: Page) => {
+  await Promise.all([
+    waitGraphQL(page, "activeFeaturesForConnectedUser"),
+    waitGraphQL(page, "getAccountInfo"),
+    waitGraphQL(page, "getMaisonMereCGUQuery"),
+    waitGraphQL(page, "getOrganismForAAPVisibilityCheck"),
+    waitGraphQL(page, "getCandidacyMenuAndCandidateInfos"),
+    waitGraphQL(page, "candidacy_canAccessCandidacy"),
+  ]);
+};
+
+export const getAAPCommonHandlers = () => {
+  const fvae = graphql.link("https://reva-api/api/graphql");
+
+  return {
+    aapCommonHandlers: [
+      fvae.query(
+        "activeFeaturesForConnectedUser",
+        graphQLResolver({
+          activeFeaturesForConnectedUser: [],
+        }),
+      ),
+      fvae.query(
+        "getAccountInfo",
+        graphQLResolver({
+          account_getAccountForConnectedUser: {
+            maisonMereAAP: null,
+          },
+        }),
+      ),
+      fvae.query(
+        "getMaisonMereCGUQuery",
+        graphQLResolver({
+          maisonMereAAP: {
+            id: "7b7539e7-a30c-4a6e-b13a-a82cdb6b4081",
+            cgu: {
+              version: 2,
+              acceptedAt: 1725001318488,
+              isLatestVersion: true,
+            },
+          },
+        }),
+      ),
+      fvae.query("getOrganismForAAPVisibilityCheck", graphQLResolver({})),
+      fvae.query(
+        "getCandidacyMenuAndCandidateInfos",
+        graphQLResolver({
+          getCandidacyMenuAndCandidateInfos: {
+            candidacyMenu_getCandidacyMenu: {
+              menuHeader: [],
+              mainMenu: [],
+              menuFooter: [],
+            },
+          },
+        }),
+      ),
+      fvae.query(
+        "candidacy_canAccessCandidacy",
+        graphQLResolver({ candidacy_canAccessCandidacy: true }),
+      ),
+    ],
+    aapCommonWait,
+  };
+};
