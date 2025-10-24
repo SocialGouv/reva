18a2db14b689cb50b904d5a94f20e39201702648
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-10-24 13:16:29 +0200

    feat(admin): added a simple appointments list playwright test

---
+++ packages/reva-admin-react/tests/(aap)/candidacies/candidacy/appointments/appointments.spec.ts
@@ -0,0 +1,88 @@
+import {
+  expect,
+  graphql,
+  test,
+} from "next/experimental/testmode/playwright/msw";
+
+import { login } from "../../../../shared/helpers/auth/login";
+import { getAAPCommonHandlers } from "../../../../shared/helpers/common-handlers/aap/getAapCommon.handlers";
+import { graphQLResolver } from "../../../../shared/helpers/network/msw";
+const fvae = graphql.link("https://reva-api/api/graphql");
+
+test.describe("appointments page", () => {
+  const { aapCommonHandlers, aapCommonWait } = getAAPCommonHandlers();
+
+  const candidacyAndUpcomingAppointmentsHandler = fvae.query(
+    "getCandidacyAndUpcomingAppointmentsForAppointmentsPage",
+    graphQLResolver({
+      getCandidacyById: {
+        id: "fb451fbc-3218-416d-9ac9-65b13432469f",
+        appointments: {
+          rows: [],
+          info: {
+            totalRows: 0,
+            currentPage: 1,
+            totalPages: 1,
+          },
+        },
+      },
+    }),
+  );
+
+  const candidacyAndPastAppointmentsHandler = fvae.query(
+    "getCandidacyAndPastAppointmentsForAppointmentsPage",
+    graphQLResolver({
+      getCandidacyById: {
+        id: "fb451fbc-3218-416d-9ac9-65b13432469f",
+        appointments: {
+          rows: [],
+          info: {
+            totalRows: 0,
+            currentPage: 1,
+            totalPages: 1,
+          },
+        },
+      },
+    }),
+  );
+
+  const rendezVousPedagogiqueForAppointmentsPageHandler = fvae.query(
+    "getRendezVousPedagogiqueForAppointmentsPage",
+    graphQLResolver({
+      getCandidacyById: {
+        id: "fb451fbc-3218-416d-9ac9-65b13432469f",
+        appointments: {
+          rows: [],
+          info: {
+            totalRows: 0,
+            currentPage: 1,
+            totalPages: 1,
+          },
+        },
+      },
+    }),
+  );
+
+  test.use({
+    mswHandlers: [
+      [
+        ...aapCommonHandlers,
+        candidacyAndUpcomingAppointmentsHandler,
+        candidacyAndPastAppointmentsHandler,
+        rendezVousPedagogiqueForAppointmentsPageHandler,
+      ],
+      { scope: "test" },
+    ],
+  });
+  test("shows appointments page title", async ({ page }) => {
+    await login({ role: "aap", page });
+    await page.goto(
+      "/admin2/candidacies/fb451fbc-3218-416d-9ac9-65b13432469f/appointments/",
+    );
+    await aapCommonWait(page);
+
+    expect(page.getByRole("heading", { level: 1 })).toHaveText(
+      "Gestion des rendez-vous",
+    );
+  });
+});
