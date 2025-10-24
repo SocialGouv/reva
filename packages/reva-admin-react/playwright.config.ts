3266ef6f5cbc9c4ddd993470a8bf5d7a4421206f
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-10-24 11:24:25 +0200

    feat(admin): updated playwright config to use next experimental integration

--- packages/reva-admin-react/playwright.config.ts
+++ packages/reva-admin-react/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from "next/experimental/testmode/playwright";
 
 /**
  * Read environment variables from file.
@@ -12,7 +12,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testMatch: "tests/**/*.spec.ts",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -38,7 +38,7 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
+    /*
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
@@ -47,7 +47,7 @@ export default defineConfig({
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
