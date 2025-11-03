96a7fe7b3ded771fc2afd0348fd561b609ae9d55
Florian Bonniec <f.bonniec@me.com>
2025-11-03 12:07:23 +0100

    feat(api): if certification is directly linked to domains, assign certification to all sub domains of domains

--- packages/reva-api/modules/referential/features/updateCertificationWithRncpFieldsAndSubDomains.ts
+++ packages/reva-api/modules/referential/features/updateCertificationWithRncpFieldsAndSubDomains.ts
@@ -1,14 +1,10 @@
-import { logger } from "@/modules/shared/logger/logger";
 import { prismaClient } from "@/prisma/client";
 
 import { RNCPReferential } from "../rncp/referential";
 import { getLevelFromRNCPCertification } from "../utils/rncp.helpers";
 
-import { Formacode, getFormacodes } from "./getFormacodes";
-
-type CertificationFormacode = Formacode & {
-  isMain: boolean;
-};
+import { getSubdomains } from "./getDomainsByFormacodes";
+import { getFormacodes } from "./getFormacodes";
 
 export const updateCertificationWithRncpFieldsAndSubDomains = async (params: {
   id: string;
@@ -64,38 +60,7 @@ export const updateCertificationWithRncpFieldsAndSubDomains = async (params: {
   // Link certification to sub domains
   const referential = await getFormacodes();
 
-  const subDomains: CertificationFormacode[] = [];
-
-  for (let index = 0; index < rncpCertification.FORMACODES.length; index++) {
-    const isMain = index == 0;
-
-    const rncpFormacode = rncpCertification.FORMACODES[index];
-    const formacode = getFormacodeByCode(rncpFormacode.CODE, referential);
-
-    if (!formacode) {
-      const error = new Error(
-        `Le formacode avec le code ${rncpFormacode.CODE} n'existe pas dans le référentiel RNCP`,
-      );
-      logger.error(error);
-
-      // stop here and continue with the next iteration
-      continue;
-    }
-
-    const parents = getParents(formacode, referential);
-    const subDomain = parents.find(
-      (formacode) => formacode.type == "SUB_DOMAIN",
-    );
-    if (
-      subDomain &&
-      subDomains.findIndex((domain) => domain.code == subDomain.code) == -1
-    ) {
-      subDomains.push({
-        ...subDomain,
-        isMain,
-      });
-    }
-  }
+  const subDomains = getSubdomains(rncpCertification.FORMACODES, referential);
 
   // Remove all links by certificationId
   await prismaClient.certificationOnFormacode.deleteMany({
@@ -110,28 +75,4 @@ export const updateCertificationWithRncpFieldsAndSubDomains = async (params: {
       isMain: subDomain.isMain,
     })),
   });
-};
-
-const getFormacodeByCode = (
-  code: string,
-  referential: Formacode[],
-): Formacode | undefined => {
-  return referential.find((formacode) => formacode.code == code);
-};
-
-const getParent = (child: Formacode, referential: Formacode[]) => {
-  return referential.find((formacode) => formacode.code == child.parentCode);
-};
-
-const getParents = (
-  formacode: Formacode,
-  referential: Formacode[],
-): Formacode[] => {
-  const parent = getParent(formacode, referential);
-
-  if (parent) {
-    return [...getParents(parent, referential), formacode];
-  }
-
-  return [formacode];
 };
