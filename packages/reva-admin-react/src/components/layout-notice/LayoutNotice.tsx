ce9110c8f9173f9a203db6721476ddc353e97f7d
ThomasDos <dosanjos.thomas@gmail.com>
2025-11-21 11:39:54 +0100

    refactor(admin): remove AAP visibility notice and related logic from LayoutNotice component

--- packages/reva-admin-react/src/components/layout-notice/LayoutNotice.tsx
+++ packages/reva-admin-react/src/components/layout-notice/LayoutNotice.tsx
@@ -3,26 +3,20 @@ import { usePathname } from "next/navigation";
 import { useAuth } from "../auth/auth";
 import { useKeycloakContext } from "../auth/keycloakContext";
 
-import {
-  AAPNotVisibleInSearchResultNotice,
-  useAAPVisibilityCheck,
-} from "./_components/AAPNotVisibleInSearchResultNotice";
 import { AapCgu } from "./_components/AppCgu.component";
 import { useAppCgu } from "./_components/AppCgu.hooks";
 
 export const LayoutNotice = () => {
   const { authenticated } = useKeycloakContext();
-  const { isGestionnaireMaisonMereAAP, isAdmin, isOrganism } = useAuth();
-  const { isVisibleInSearchResults, getOrganismisLoading } =
-    useAAPVisibilityCheck();
+  const { isGestionnaireMaisonMereAAP, isAdmin } = useAuth();
+
   const pathname = usePathname();
 
   const isCguPathname =
     pathname.startsWith("/information") || pathname.startsWith("/cgu");
 
   const { maisonMereCgu, getMaisonMereCGUisLoading } = useAppCgu();
-  const isLoading =
-    getMaisonMereCGUisLoading || getOrganismisLoading || status === "LOADING";
+  const isLoading = getMaisonMereCGUisLoading || status === "LOADING";
   const canSeeAapCgu =
     authenticated &&
     isGestionnaireMaisonMereAAP &&
@@ -30,22 +24,12 @@ export const LayoutNotice = () => {
     maisonMereCgu &&
     !maisonMereCgu.isLatestVersion;
 
-  const canSeeAAPNotVisibleInSearchResultNotice =
-    authenticated &&
-    !isGestionnaireMaisonMereAAP &&
-    isOrganism &&
-    !isVisibleInSearchResults;
-
   if (isLoading || isAdmin) {
     return null;
   }
 
   if (canSeeAapCgu) {
     return <AapCgu />;
-  }
-
-  if (canSeeAAPNotVisibleInSearchResultNotice) {
-    return <AAPNotVisibleInSearchResultNotice />;
   }
 
   return null;
