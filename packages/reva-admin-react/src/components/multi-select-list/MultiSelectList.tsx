9e091f00f45d29f811bcd389c9565a15c719504d
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-11-25 14:51:07 +0100

    feat(admin): added an onlyShowAddedItems switch to the MultiSelectList component

--- packages/reva-admin-react/src/components/multi-select-list/MultiSelectList.tsx
+++ packages/reva-admin-react/src/components/multi-select-list/MultiSelectList.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import Card, { CardProps } from "@codegouvfr/react-dsfr/Card";
+import ToggleSwitch from "@codegouvfr/react-dsfr/ToggleSwitch";
 
 import { Pagination } from "../pagination/Pagination";
 
@@ -11,13 +12,16 @@ type MultiSelectItemProps = Pick<
 };
 
 type MultiSelectListProps = {
+  className?: string;
   pageItems: MultiSelectItemProps[];
   selectedItemsIds: string[];
   onSelectionChange?: (args: { itemId: string; selected: boolean }) => void;
   currentPage: number;
   totalPages: number;
   basePaginationUrl: string;
-  className?: string;
+  onlyShowAddedItemsSwitchLabel?: string;
+  onlyShowAddedItems: boolean;
+  onOnlyShowAddedItemsSwitchChange?: (checked: boolean) => void;
 };
 
 export const MultiSelectList = ({
@@ -28,46 +32,59 @@ export const MultiSelectList = ({
   totalPages,
   basePaginationUrl,
   className = "",
+  onlyShowAddedItemsSwitchLabel = "Afficher uniquement les éléments ajoutés",
+  onlyShowAddedItems,
+  onOnlyShowAddedItemsSwitchChange,
 }: MultiSelectListProps) => (
-  <div className={`flex flex-col gap-4 ${className}`}>
-    {pageItems.map((item) => (
-      <Card
-        data-testid={`multi-select-list-item-${item.id}`}
-        key={item.id}
-        size="small"
-        {...item}
-        endDetail={
-          selectedItemsIds.includes(item.id) ? (
-            <Button
-              onClick={() =>
-                onSelectionChange?.({
-                  itemId: item.id,
-                  selected: false,
-                })
-              }
-            >
-              Retirer
-            </Button>
-          ) : (
-            <Button
-              onClick={() =>
-                onSelectionChange?.({
-                  itemId: item.id,
-                  selected: true,
-                })
-              }
-            >
-              Ajouter
-            </Button>
-          )
-        }
-      />
-    ))}
-    <Pagination
-      className="mx-auto"
-      totalPages={totalPages}
-      currentPage={currentPage}
-      baseHref={basePaginationUrl}
+  <div className={`flex flex-col md:flex-row gap-6 ${className}`}>
+    <ToggleSwitch
+      className="mb-auto"
+      inputTitle={onlyShowAddedItemsSwitchLabel}
+      label={onlyShowAddedItemsSwitchLabel}
+      labelPosition="left"
+      onChange={(checked) => onOnlyShowAddedItemsSwitchChange?.(checked)}
+      checked={onlyShowAddedItems}
     />
+    <div className="flex flex-col w-full gap-4 ">
+      {pageItems.map((item) => (
+        <Card
+          data-testid={`multi-select-list-item-${item.id}`}
+          key={item.id}
+          size="small"
+          {...item}
+          endDetail={
+            selectedItemsIds.includes(item.id) ? (
+              <Button
+                onClick={() =>
+                  onSelectionChange?.({
+                    itemId: item.id,
+                    selected: false,
+                  })
+                }
+              >
+                Retirer
+              </Button>
+            ) : (
+              <Button
+                onClick={() =>
+                  onSelectionChange?.({
+                    itemId: item.id,
+                    selected: true,
+                  })
+                }
+              >
+                Ajouter
+              </Button>
+            )
+          }
+        />
+      ))}
+      <Pagination
+        className="mx-auto"
+        totalPages={totalPages}
+        currentPage={currentPage}
+        baseHref={basePaginationUrl}
+      />
+    </div>
   </div>
 );
