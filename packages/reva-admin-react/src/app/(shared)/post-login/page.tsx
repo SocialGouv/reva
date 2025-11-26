af2e541159091b5f549968da4238ed2ccde07cc2
ThomasDos <dosanjos.thomas@gmail.com>
2025-11-26 17:44:55 +0100

    feat(admin): update post-login routing with certificateur annuaire feature flag

--- packages/reva-admin-react/src/app/(shared)/post-login/page.tsx
+++ packages/reva-admin-react/src/app/(shared)/post-login/page.tsx
@@ -3,22 +3,37 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 import { useAuth } from "@/components/auth/auth";
+import { useFeatureflipping } from "@/components/feature-flipping/featureFlipping";
 
 const PostLoginPage = () => {
   const router = useRouter();
+  const { isFeatureActive } = useFeatureflipping();
+  const isCertificateurCandidaciesAnnuaireFeatureActive = isFeatureActive(
+    "CERTIFICATEUR_CANDIDACIES_ANNUAIRE",
+  );
+
+  const certificationAuthorityDefaultPath =
+    isCertificateurCandidaciesAnnuaireFeatureActive
+      ? "/candidacies/annuaire"
+      : "/candidacies/feasibilities";
 
   const { isCertificationAuthority, isCertificationRegistryManager } =
     useAuth();
 
   useEffect(() => {
     if (isCertificationAuthority) {
-      router.replace("/candidacies/feasibilities");
+      router.replace(certificationAuthorityDefaultPath);
     } else if (isCertificationRegistryManager) {
       router.replace("/responsable-certifications");
     } else {
       router.replace("/candidacies");
     }
-  }, [isCertificationAuthority, isCertificationRegistryManager, router]);
+  }, [
+    isCertificationAuthority,
+    isCertificationRegistryManager,
+    router,
+    certificationAuthorityDefaultPath,
+  ]);
 
   return null;
 };
