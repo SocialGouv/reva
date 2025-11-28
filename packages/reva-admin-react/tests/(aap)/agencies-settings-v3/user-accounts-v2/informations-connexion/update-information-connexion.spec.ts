9e599d93eeee0fe72edad80089c090f88099fa08
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-11-28 14:16:12 +0100

    feat(admin): updated user-account v2  information connexion update page tests to handle the admin role

--- packages/reva-admin-react/tests/(aap)/agencies-settings-v3/user-accounts-v2/informations-connexion/update-information-connexion.spec.ts
+++ packages/reva-admin-react/tests/(aap)/agencies-settings-v3/user-accounts-v2/informations-connexion/update-information-connexion.spec.ts
@@ -63,131 +63,153 @@ test.describe("Settings update user account information connexion", () => {
       { scope: "test" },
     ],
   });
-  test.describe("as a Gestionnaire AAP", () => {
-    test.describe("when I access i access the update user account information connexion page", () => {
-      test("it shows the correct title and all the form fields are filled with the correct values", async ({
-        page,
-      }) => {
-        await login({ role: "aap", page });
-        await page.goto(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
-        );
-        await waitForPageQueries(page);
 
-        await expect(page.getByRole("heading", { level: 1 })).toHaveText(
-          "Informations de connexion",
-        );
-        await expect(
-          page.getByTestId("lastname-input").locator("input"),
-        ).toHaveValue("Doe");
-        await expect(
-          page.getByTestId("firstname-input").locator("input"),
-        ).toHaveValue("John");
-        await expect(
-          page.getByTestId("email-input").locator("input"),
-        ).toHaveValue("john.doe@example.com");
-      });
-
-      test("it let me go back to the user account page", async ({ page }) => {
-        await login({ role: "aap", page });
-        await page.goto(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
-        );
-        await waitForPageQueries(page);
-        await page
-          .getByRole("button", { name: "Retour à la page précédente" })
-          .click();
-
-        await expect(page).toHaveURL(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/",
-        );
-      });
-
-      test("it shows an error when i try to submit the form with empty fields", async ({
-        page,
-      }) => {
-        await login({ role: "aap", page });
-        await page.goto(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
-        );
-        await waitForPageQueries(page);
-
-        await page.getByTestId("lastname-input").locator("input").clear();
-        await page.getByTestId("firstname-input").locator("input").clear();
-        await page.getByTestId("email-input").locator("input").clear();
-
-        await page.getByRole("button", { name: "Enregistrer" }).click();
-
-        await expect(page.getByTestId("lastname-input")).toHaveClass(
-          /fr-input-group--error/,
-        );
-        await expect(page.getByTestId("firstname-input")).toHaveClass(
-          /fr-input-group--error/,
-        );
-        await expect(page.getByTestId("email-input")).toHaveClass(
-          /fr-input-group--error/,
-        );
-
-        await expect(page).toHaveURL(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
-        );
-      });
-
-      test("it let me submit a valid form", async ({ page }) => {
-        await login({ role: "aap", page });
-        await page.goto(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
-        );
-        await waitForPageQueries(page);
-
-        await page.getByTestId("lastname-input").locator("input").fill("Doe1");
-        await page
-          .getByTestId("firstname-input")
-          .locator("input")
-          .fill("John1");
-        await page
-          .getByTestId("email-input")
-          .locator("input")
-          .fill("john1.doe1@example.com");
-        await page.getByRole("button", { name: "Enregistrer" }).click();
-
-        const mutationPromise = waitGraphQL(
+  [
+    {
+      label: "As a Gestionnaire AAP",
+      role: "aap" as const,
+      rootBreadCrumbLabel: "Paramètres",
+      rootBreadCrumbUrl: "/admin2/agencies-settings-v3/",
+    },
+    {
+      label: "As an Admin",
+      role: "admin" as const,
+      rootBreadCrumbLabel: "Maison mère AAP",
+      rootBreadCrumbUrl:
+        "/admin2/maison-mere-aap/a8e32301-86b8-414b-8b55-af86d289adee/",
+    },
+  ].forEach(({ role, label, rootBreadCrumbLabel, rootBreadCrumbUrl }) => {
+    test.describe(label, () => {
+      test.describe("when I access i access the update user account information connexion page", () => {
+        test("it shows the correct title and all the form fields are filled with the correct values", async ({
           page,
-          "updateUserAccountForUpdateUserAccountPage",
-        );
+        }) => {
+          await login({ role, page });
+          await page.goto(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
+          );
+          await waitForPageQueries(page);
 
-        await page.getByRole("button", { name: "Enregistrer" }).click();
+          await expect(page.getByRole("heading", { level: 1 })).toHaveText(
+            "Informations de connexion",
+          );
+          await expect(
+            page.getByTestId("lastname-input").locator("input"),
+          ).toHaveValue("Doe");
+          await expect(
+            page.getByTestId("firstname-input").locator("input"),
+          ).toHaveValue("John");
+          await expect(
+            page.getByTestId("email-input").locator("input"),
+          ).toHaveValue("john.doe@example.com");
+        });
 
-        await mutationPromise;
+        test("it let me go back to the user account page", async ({ page }) => {
+          await login({ role, page });
+          await page.goto(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
+          );
+          await waitForPageQueries(page);
+          await page
+            .getByRole("button", { name: "Retour à la page précédente" })
+            .click();
 
-        await expect(page).toHaveURL(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
-        );
+          await expect(page).toHaveURL(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/",
+          );
+        });
+
+        test("it shows an error when i try to submit the form with empty fields", async ({
+          page,
+        }) => {
+          await login({ role, page });
+          await page.goto(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
+          );
+          await waitForPageQueries(page);
+
+          await page.getByTestId("lastname-input").locator("input").clear();
+          await page.getByTestId("firstname-input").locator("input").clear();
+          await page.getByTestId("email-input").locator("input").clear();
+
+          await page.getByRole("button", { name: "Enregistrer" }).click();
+
+          await expect(page.getByTestId("lastname-input")).toHaveClass(
+            /fr-input-group--error/,
+          );
+          await expect(page.getByTestId("firstname-input")).toHaveClass(
+            /fr-input-group--error/,
+          );
+          await expect(page.getByTestId("email-input")).toHaveClass(
+            /fr-input-group--error/,
+          );
+
+          await expect(page).toHaveURL(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
+          );
+        });
+
+        test("it let me submit a valid form", async ({ page }) => {
+          await login({ role, page });
+          await page.goto(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
+          );
+          await waitForPageQueries(page);
+
+          await page
+            .getByTestId("lastname-input")
+            .locator("input")
+            .fill("Doe1");
+          await page
+            .getByTestId("firstname-input")
+            .locator("input")
+            .fill("John1");
+          await page
+            .getByTestId("email-input")
+            .locator("input")
+            .fill("john1.doe1@example.com");
+          await page.getByRole("button", { name: "Enregistrer" }).click();
+
+          const mutationPromise = waitGraphQL(
+            page,
+            "updateUserAccountForUpdateUserAccountPage",
+          );
+
+          await page.getByRole("button", { name: "Enregistrer" }).click();
+
+          await mutationPromise;
+
+          await expect(page).toHaveURL(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
+          );
+        });
       });
-    });
-    test.describe("when I use the breadcrumb", () => {
-      test("it let me go back to the user account page", async ({ page }) => {
-        await login({ role: "aap", page });
-        await page.goto(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
-        );
-        await waitForPageQueries(page);
-        await page.getByRole("link", { name: "Doe John" }).click();
-        await expect(page).toHaveURL(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/",
-        );
-      });
-      test("it let me go back to the root settings page", async ({ page }) => {
-        await login({ role: "aap", page });
-        await page.goto(
-          "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
-        );
-        await waitForPageQueries(page);
-        await page
-          .locator(".fr-breadcrumb")
-          .getByRole("link", { name: "Paramètres" })
-          .click();
-        await expect(page).toHaveURL("/admin2/agencies-settings-v3/");
+      test.describe("when I use the breadcrumb", () => {
+        test("it let me go back to the user account page", async ({ page }) => {
+          await login({ role, page });
+          await page.goto(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
+          );
+          await waitForPageQueries(page);
+          await page.getByRole("link", { name: "Doe John" }).click();
+          await expect(page).toHaveURL(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/",
+          );
+        });
+        test("it let me go back to the root page of the breadcrumb", async ({
+          page,
+        }) => {
+          await login({ role, page });
+          await page.goto(
+            "/admin2/agencies-settings-v3/a8e32301-86b8-414b-8b55-af86d289adee/user-accounts-v2/account-1/informations-connexion/",
+          );
+          await waitForPageQueries(page);
+          await page
+            .locator(".fr-breadcrumb")
+            .getByRole("link", { name: rootBreadCrumbLabel })
+            .click();
+          await expect(page).toHaveURL(rootBreadCrumbUrl);
+        });
       });
     });
   });
