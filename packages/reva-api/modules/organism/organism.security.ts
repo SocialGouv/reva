012a3187d1b0b7b944f4b849252a5e917e168710
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-12-01 16:48:57 +0100

    feat(api): organism_getCompteCollaborateurById query in organism module is now accessible for the owner of the account in addition to the admin and gestionnaire maison mère aap

--- packages/reva-api/modules/organism/organism.security.ts
+++ packages/reva-api/modules/organism/organism.security.ts
@@ -2,6 +2,8 @@ import { hasRole, whenHasRole } from "@/modules/shared/security/middlewares";
 import { whenHasRoleButNotOthers } from "@/modules/shared/security/middlewares/whenHasRoleButNotOthers";
 import { isAdmin } from "@/modules/shared/security/presets";
 
+import { isOwnerOfAccount } from "../account/security/isOwnerOfAccount.security";
+
 import { isGestionnaireOfMaisonMereAAP } from "./security/isGestionnaireOfMaisonMereAAP.security";
 import { isOwnerOfOrganism } from "./security/isOwnerOfOrganism";
 import { isOwnerOrCanManageOrganism } from "./security/isOwnerOrCanManageOrganism.security";
@@ -25,6 +27,16 @@ const isAdminOrGestionnaireVaeCollective = [
   hasRole(["admin", "manage_vae_collective"]),
 ];
 
+const isAdminOrGestionnaireOfMaisonMereAAPOrOwnerOfAccount = [
+  hasRole(["admin", "gestion_maison_mere_aap", "manage_candidacy"]),
+  whenHasRole("gestion_maison_mere_aap", isGestionnaireOfMaisonMereAAP),
+  whenHasRoleButNotOthers(
+    "manage_candidacy",
+    ["admin", "gestion_maison_mere_aap"],
+    isOwnerOfAccount,
+  ),
+];
+
 export const resolversSecurityMap = {
   // cf https://the-guild.dev/graphql/tools/docs/resolvers-composition#supported-path-matcher-format
 
@@ -33,7 +45,7 @@ export const resolversSecurityMap = {
   "Query.organism_searchOrganisms": isAdminOrGestionnaireVaeCollective,
 
   "Query.organism_getCompteCollaborateurById":
-    isAdminOrGestionnaireOfMaisonMereAAP,
+    isAdminOrGestionnaireOfMaisonMereAAPOrOwnerOfAccount,
 
   "Mutation.organism_updateMaisonMereIsSignalized": isAdmin,
 
