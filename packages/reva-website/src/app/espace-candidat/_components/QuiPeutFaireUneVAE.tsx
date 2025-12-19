2e08c4df05af661dddd1e5d4e51ee08ee396d74a
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-12-19 10:45:59 +0100

    feat(website): new "QuiPeutFaireUneVAE" section in candidate homepage

--- packages/reva-website/src/app/espace-candidat/_components/QuiPeutFaireUneVAE.tsx
+++ packages/reva-website/src/app/espace-candidat/_components/QuiPeutFaireUneVAE.tsx
@@ -1,70 +1,43 @@
-import Image from "next/image";
+import { Card } from "@codegouvfr/react-dsfr/Card";
 
-const BlockCompetence = ({
-  imageUrl,
-  imageAlt,
-  label,
-}: {
-  imageUrl: string;
-  imageAlt: string;
-  label: string;
-}) => (
-  <div className="flex lg:flex-col items-center gap-4 lg:gap-0 w-full">
-    <Image
-      src={imageUrl}
-      width={88}
-      height={88}
-      alt={imageAlt}
-      className="max-w-14 max-h-14 lg:max-w-none lg:max-h-none"
-    />
-    <h3 className="text-xl text-[22px] leading-[28px] font-bold lg:w-2/3 mt-2 mb-0">
-      {label}
-    </h3>
-  </div>
-);
-
-const QuiPeutFaireUneVAE = () => (
+const QuiPeutFaireUneVAE = async () => (
   <section
     id="qui-peut-faire-une-vae"
-    className="w-full pt-14 lg:pt-20 pb-20 lg:pb-40 lg:mb-0 mx-auto flex flex-col items-center lg:text-center fr-container"
+    className="w-full pt-12 pb-32 lg:mb-0 mx-auto flex flex-col items-center fr-container"
   >
-    <h2 className="text-[28px] leading-9 lg:text-[32px] mb-8 mt-0 text-justify">
+    <h2 className="text-[28px] leading-9 lg:text-[32px] mb-12 text-justify">
       Qui peut faire une VAE sur France VAE ?
     </h2>
-    <span className="text-[20px] leading-8 text-justify lg:text-center">
-      Le parcours France VAE est accessible à toute personne ayant des
-      compétences liées au diplôme visé :
-    </span>
-    <div className="py-12 flex flex-col sm:flex-row gap-4 lg:gap-[88px] w-full">
-      <BlockCompetence
-        label="Salariés du secteur privé"
-        imageUrl="/candidate-space/blocks-competences/image-block-competence-salaries-du-secteur-prive.svg"
-        imageAlt="Document avec un stylo plume"
+    <div className="flex flex-col md:items-center md:grid grid-cols-2 gap-12">
+      <p className="text-xl">
+        <strong>
+          Toute personne qui possède une expérience en lien direct avec la
+          certification professionnelle visée.
+        </strong>
+        <br /> <br />
+        Cette certification professionnelle doit néanmoins être enregistrée au
+        répertoire national des certifications professionnelles.
+        <br /> <br />
+        <strong>La seule exception ?</strong> Si vous êtes{" "}
+        <strong>agent public</strong>, le parcours VAE ne s’effectue pas pour le
+        moment via notre plateforme. Contactez les organismes référents pour en
+        savoir plus.
+      </p>
+
+      <Card
+        title="Qui est éligible au parcours France VAE ?"
+        desc="Le nouveau parcours France VAE, plus simple et plus rapide, est accessible depuis 2023. Découvrez toutes les conditions pour réaliser ce parcours."
+        endDetail="Consultez l'article"
+        enlargeLink
+        imageUrl="/candidate-space/qui-peut-faire-une-vae/small_Qui_est_eligible_au_parcours_France_VAE.jpg"
+        imageAlt="homme en costume marchant avec une malette"
+        classes={{
+          imgTag: "h-[180px]",
+        }}
+        linkProps={{
+          href: "/savoir-plus/articles/France-VAE-qui-est-eligible/",
+        }}
       />
-      <BlockCompetence
-        label="Demandeurs d'emploi"
-        imageUrl="/candidate-space/blocks-competences/image-block-competence-demandeurs-demplois.svg"
-        imageAlt="logiciel de recherche d'emploi avec une loupe"
-      />
-      <BlockCompetence
-        label="Bénévoles"
-        imageUrl="/candidate-space/blocks-competences/image-block-competence-volontaires-et-benevoles.svg"
-        imageAlt="Mains réunies en signe de solidarité"
-      />
-      <BlockCompetence
-        label="Proches aidants"
-        imageUrl="/candidate-space/blocks-competences/image-block-competence-proches-aidants.svg"
-        imageAlt="Une main soutenant un coeur en signe de solidarité"
-      />
-    </div>
-    <div className="lg:flex flex-col text-[20px] leading-8 lg:text-xl">
-      <span>
-        Pour les autres cas, la VAE n'est pas encore possible via France VAE.
-      </span>
-      <span>
-        {" "}
-        Renseignez-vous auprès d'un des organismes listés ci-dessous.
-      </span>
     </div>
   </section>
 );
--- packages/reva-website/src/app/espace-candidat/page.tsx
+++ packages/reva-website/src/app/espace-candidat/page.tsx
@@ -5,21 +5,32 @@ import AccompagnementDemarche from "@/app/espace-candidat/_components/Accompagne
 import Articles from "@/app/espace-candidat/_components/Articles";
 import CommentFinancerVotreParcours from "@/app/espace-candidat/_components/CommentFinancerVotreParcours";
 import FaitesValiderVosCompetencesParUnDiplome from "@/app/espace-candidat/_components/FaitesValiderVosCompetencesParUnDiplome";
-import QuiPeutFaireUneVAE from "@/app/espace-candidat/_components/QuiPeutFaireUneVAE";
+import OldQuiPeutFaireUneVAE from "@/app/espace-candidat/_components/OldQuiPeutFaireUneVAE";
 import VousAvezBesoinDePlusDaide from "@/app/espace-candidat/_components/VousAvezBesoinDePlusDaide";
 import BackGroundUnions from "@/components/candidate-space/BackGroundUnions";
+import { isFeatureActive } from "@/utils/featureFlipping";
+
+import QuiPeutFaireUneVAE from "./_components/QuiPeutFaireUneVAE";
 
 export const metadata: Metadata = {
   title: "France VAE | L’outil qui facilite le suivi des candidats à la VAE",
   description: "Espace candidat France VAE",
 };
 
-const CandidateSpaceHomePage = () => {
+const CandidateSpaceHomePage = async () => {
+  const isPublicEligibleFeatureActive = await isFeatureActive(
+    "WEBSITE_PUBLIC_ELIGIBLE",
+  );
+
   return (
     <MainLayout className="w-full mx-auto relative flex flex-col items-center lg:pb-32">
       <BackGroundUnions />
       <FaitesValiderVosCompetencesParUnDiplome />
-      <QuiPeutFaireUneVAE />
+      {isPublicEligibleFeatureActive ? (
+        <QuiPeutFaireUneVAE />
+      ) : (
+        <OldQuiPeutFaireUneVAE />
+      )}
       <VousAvezBesoinDePlusDaide />
       <AccompagnementDemarche />
       <CommentFinancerVotreParcours />
