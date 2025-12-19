b2dbecb9b5dd87ce66d9c52d7c162eede58c80d0
Alexandre Garbe <alexandre.garbe@gmail.com>
2025-12-19 09:38:05 +0100

    feat(api): added a WEBSITE_PUBLIC_ELIGIBLE feature flag

---
+++ packages/reva-api/prisma/migrations/20251219083638_add_website_public_eligible_feature_flag/migration.sql
@@ -0,0 +1,7 @@
+insert into
+    features (key, label)
+values
+    (
+        'WEBSITE_PUBLIC_ELIGIBLE',
+        'Modification du wording relatif au public éligible sur le site institutionnel'
+    );
