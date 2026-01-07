1eb42191982441b00cf4b5bd4dc542e0380d043d
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-01-07 11:46:52 +0100

    feat(api): removed the AAP_USER_ACCOUNT_V2 feature flag

--- packages/reva-api/modules/organism/features/createOrganismAccount.ts
+++ packages/reva-api/modules/organism/features/createOrganismAccount.ts
@@ -3,119 +3,54 @@ import {
   logAAPAuditEvent,
 } from "@/modules/aap-log/features/logAAPAuditEvent";
 import { createAccount } from "@/modules/account/features/createAccount";
-import { isFeatureActiveForUser } from "@/modules/feature-flipping/feature-flipping.features";
 import { prismaClient } from "@/prisma/client";
 
 import { CreateOrganismAccountInput } from "../organism.types";
 
-import { updateOrganismOnAccountAssociation } from "./updateOrganismOnAccountAssociation";
-
 export const createOrganismAccount = async ({
-  organismId,
   maisonMereAAPId,
   accountEmail,
   accountFirstname,
   accountLastname,
   userInfo,
 }: CreateOrganismAccountInput & { userInfo: AAPAuditLogUserInfo }) => {
-  const isAApUserAccountV2FeatureActive = await isFeatureActiveForUser({
-    userKeycloakId: userInfo.userKeycloakId,
-    feature: "AAP_USER_ACCOUNT_V2",
+  if (!maisonMereAAPId) {
+    throw new Error("L'identifiant de la maison mère est obligatoire");
+  }
+
+  const maisonMereAAP = await prismaClient.maisonMereAAP.findUnique({
+    where: { id: maisonMereAAPId },
   });
 
-  if (isAApUserAccountV2FeatureActive) {
-    if (!maisonMereAAPId) {
-      throw new Error("L'identifiant de la maison mère est obligatoire");
-    }
-
-    const maisonMereAAP = await prismaClient.maisonMereAAP.findUnique({
-      where: { id: maisonMereAAPId },
-    });
-
-    if (!maisonMereAAP) {
-      throw new Error("La maison mère n'a pas été trouvée");
-    }
-
-    const account = await createAccount({
-      email: accountEmail,
-      username: accountEmail,
-      firstname: accountFirstname,
-      lastname: accountLastname,
-      group: "organism",
-    });
-
-    await prismaClient.maisonMereAAPOnAccount.create({
-      data: {
-        accountId: account.id,
-        maisonMereAAPId,
-      },
-    });
-
-    await logAAPAuditEvent({
-      eventType: "ORGANISM_ACCOUNT_CREATED_V2",
-      maisonMereAAPId: maisonMereAAP.id,
-      details: {
-        accountEmail,
-        maisonMereAAPId,
-        maisonMereAAPRaisonSociale: maisonMereAAP.raisonSociale,
-      },
-      userInfo,
-    });
-
-    return account;
-  } else {
-    if (!organismId) {
-      throw new Error("L'identifiant de l'organisme est obligatoire");
-    }
-
-    const organism = await prismaClient.organism.findUnique({
-      where: { id: organismId },
-      select: {
-        label: true,
-        maisonMereAAPId: true,
-        maisonMereAAP: {
-          select: {
-            raisonSociale: true,
-          },
-        },
-      },
-    });
-
-    if (!organism) {
-      throw new Error("L'organisme n'a pas été trouvé");
-    }
-
-    const account = await createAccount({
-      email: accountEmail,
-      username: accountEmail,
-      firstname: accountFirstname,
-      lastname: accountLastname,
-      group: "organism",
-      organismId,
-      maisonMereAAPRaisonSociale: organism.maisonMereAAP?.raisonSociale,
-    });
-
-    await updateOrganismOnAccountAssociation({
-      accountId: account.id,
-      organismIds: [organismId],
-    });
-
-    await prismaClient.maisonMereAAPOnAccount.create({
-      data: {
-        accountId: account.id,
-        maisonMereAAPId: maisonMereAAPId,
-      },
-    });
-
-    if (organism.maisonMereAAPId) {
-      await logAAPAuditEvent({
-        eventType: "ORGANISM_ACCOUNT_CREATED",
-        maisonMereAAPId: organism.maisonMereAAPId,
-        details: { organismId, organismLabel: organism?.label, accountEmail },
-        userInfo,
-      });
-    }
-
-    return account;
+  if (!maisonMereAAP) {
+    throw new Error("La maison mère n'a pas été trouvée");
   }
+
+  const account = await createAccount({
+    email: accountEmail,
+    username: accountEmail,
+    firstname: accountFirstname,
+    lastname: accountLastname,
+    group: "organism",
+  });
+
+  await prismaClient.maisonMereAAPOnAccount.create({
+    data: {
+      accountId: account.id,
+      maisonMereAAPId,
+    },
+  });
+
+  await logAAPAuditEvent({
+    eventType: "ORGANISM_ACCOUNT_CREATED_V2",
+    maisonMereAAPId: maisonMereAAP.id,
+    details: {
+      accountEmail,
+      maisonMereAAPId,
+      maisonMereAAPRaisonSociale: maisonMereAAP.raisonSociale,
+    },
+    userInfo,
+  });
+
+  return account;
 };
--- packages/reva-api/modules/organism/features/updateOrganismAccountAndOrganism.ts
+++ packages/reva-api/modules/organism/features/updateOrganismAccountAndOrganism.ts
@@ -4,29 +4,20 @@ import {
 } from "@/modules/aap-log/features/logAAPAuditEvent";
 import { getAccountById } from "@/modules/account/features/getAccount";
 import { updateAccountById } from "@/modules/account/features/updateAccount";
-import { isFeatureActiveForUser } from "@/modules/feature-flipping/feature-flipping.features";
 import { prismaClient } from "@/prisma/client";
 
 import { UpdateOrganimsAccountAndOrganismInput } from "../organism.types";
-
-import { updateOrganismOnAccountAssociation } from "./updateOrganismOnAccountAssociation";
 
 export const updateOrganismAccountAndOrganism = async ({
   accountId,
   accountEmail,
   accountFirstname,
   accountLastname,
-  organismId,
   maisonMereAAPId,
   userInfo,
 }: UpdateOrganimsAccountAndOrganismInput & {
   userInfo: AAPAuditLogUserInfo;
 }) => {
-  const isAApUserAccountV2FeatureActive = await isFeatureActiveForUser({
-    userKeycloakId: userInfo.userKeycloakId,
-    feature: "AAP_USER_ACCOUNT_V2",
-  });
-
   const account = await getAccountById({
     id: accountId,
   });
@@ -35,83 +26,36 @@ export const updateOrganismAccountAndOrganism = async ({
     throw Error("Compte utilisateur non trouvé");
   }
 
-  if (isAApUserAccountV2FeatureActive) {
-    if (!maisonMereAAPId) {
-      throw new Error("L'identifiant de la maison mère est obligatoire");
-    }
-
-    const maisonMereAAP = await prismaClient.maisonMereAAP.findUnique({
-      where: { id: maisonMereAAPId },
-    });
-
-    if (!maisonMereAAP) {
-      throw new Error("La maison mère n'a pas été trouvée");
-    }
-    const result = await updateAccountById({
-      accountId: account.id,
-      accountData: {
-        email: accountEmail,
-        firstname: accountFirstname,
-        lastname: accountLastname,
-      },
-    });
-
-    await logAAPAuditEvent({
-      eventType: "ORGANISM_ACCOUNT_UPDATED_V2",
-      maisonMereAAPId: maisonMereAAP.id,
-      details: {
-        accountEmail,
-        maisonMereAAPId,
-        maisonMereAAPRaisonSociale: maisonMereAAP.raisonSociale,
-      },
-      userInfo,
-    });
-
-    return result;
-  } else {
-    if (!organismId) {
-      throw Error("L'identifiant de l'organisme est obligatoire");
-    }
-
-    const organism = await prismaClient.organism.findUnique({
-      where: { id: organismId },
-    });
-
-    if (!organism) {
-      throw Error("L'organisme n'a pas été trouvé");
-    }
-
-    await updateAccountById({
-      accountId: account.id,
-      accountData: {
-        email: accountEmail,
-        firstname: accountFirstname,
-        lastname: accountLastname,
-      },
-    });
-
-    const result = await prismaClient.account.update({
-      where: { id: account.id },
-      data: { organismId },
-    });
-
-    await updateOrganismOnAccountAssociation({
-      accountId,
-      organismIds: [organismId], //TODO update this when we allow on account to be linked to multiple organisms
-    });
-
-    if (organism.maisonMereAAPId) {
-      await logAAPAuditEvent({
-        eventType: "ORGANISM_ACCOUNT_UPDATED",
-        maisonMereAAPId: organism.maisonMereAAPId,
-        details: {
-          accountEmail,
-          organismId,
-          organismLabel: organism.label,
-        },
-        userInfo,
-      });
-    }
-    return result;
+  if (!maisonMereAAPId) {
+    throw new Error("L'identifiant de la maison mère est obligatoire");
   }
+
+  const maisonMereAAP = await prismaClient.maisonMereAAP.findUnique({
+    where: { id: maisonMereAAPId },
+  });
+
+  if (!maisonMereAAP) {
+    throw new Error("La maison mère n'a pas été trouvée");
+  }
+  const result = await updateAccountById({
+    accountId: account.id,
+    accountData: {
+      email: accountEmail,
+      firstname: accountFirstname,
+      lastname: accountLastname,
+    },
+  });
+
+  await logAAPAuditEvent({
+    eventType: "ORGANISM_ACCOUNT_UPDATED_V2",
+    maisonMereAAPId: maisonMereAAP.id,
+    details: {
+      accountEmail,
+      maisonMereAAPId,
+      maisonMereAAPRaisonSociale: maisonMereAAP.raisonSociale,
+    },
+    userInfo,
+  });
+
+  return result;
 };
