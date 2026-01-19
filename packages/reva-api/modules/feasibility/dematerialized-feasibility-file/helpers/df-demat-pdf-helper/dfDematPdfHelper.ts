a015d2eb86aa3abc1063c29130163e1cff08aed9
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-01-19 14:22:18 +0100

    fix(api): dfDemat pdf helper addTitleBlock function was not resetting the doc.x value after completion

--- packages/reva-api/modules/feasibility/dematerialized-feasibility-file/helpers/df-demat-pdf-helper/dfDematPdfHelper.ts
+++ packages/reva-api/modules/feasibility/dematerialized-feasibility-file/helpers/df-demat-pdf-helper/dfDematPdfHelper.ts
@@ -250,6 +250,7 @@ export const addTitledBlock = ({
   widthInPt: number;
   doc: PDFKit.PDFDocument;
 }) => {
+  const oldX = doc.x;
   doc
     .fontSize(8)
     .font("assets/fonts/Marianne/Marianne-Medium.otf")
@@ -259,7 +260,7 @@ export const addTitledBlock = ({
     .text("", startInPt + pxToPt(16), doc.y)
     .font("assets/fonts/Marianne/Marianne-Regular.otf");
   content(doc);
-  doc.text("", startInPt, doc.y); //reset x position to start of block after block end
+  doc.text("", oldX, doc.y); //reset x position to start of block after block end
 };
 
 export const addDisabledCheckbox = ({
