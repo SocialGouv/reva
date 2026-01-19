9e72496485eddd1b5d641a565992ef128cfb7950
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-01-19 12:39:42 +0100

    fix(api): removed frame on new df demat pdf section since it does not handle multi page sections

--- packages/reva-api/modules/feasibility/dematerialized-feasibility-file/helpers/df-demat-pdf-helper/dfDematPdfHelper.ts
+++ packages/reva-api/modules/feasibility/dematerialized-feasibility-file/helpers/df-demat-pdf-helper/dfDematPdfHelper.ts
@@ -155,25 +155,18 @@ export const addSection = ({
   content: (doc: PDFKit.PDFDocument) => void;
   doc: PDFKit.PDFDocument;
 }) => {
-  addFrame({
-    doc,
-    startInPt: pxToPt(100),
-    widthInPt: pxToPt(1280),
-    content: (doc) => {
-      doc.image(iconPath, doc.x + pxToPt(40), doc.y + pxToPt(40), {
-        fit: [pxToPt(40), pxToPt(40)],
-      });
-
-      doc
-        .fontSize(14)
-        .font("assets/fonts/Marianne/Marianne-Bold.otf")
-        .text(title, doc.x + pxToPt(90), doc.y + pxToPt(32));
-
-      doc.moveDown(0.5);
-
-      content(doc);
-    },
+  doc.image(iconPath, doc.x, doc.y + pxToPt(8), {
+    fit: [pxToPt(40), pxToPt(40)],
   });
+
+  doc
+    .fontSize(14)
+    .font("assets/fonts/Marianne/Marianne-Bold.otf")
+    .text(title, doc.x + pxToPt(50), doc.y);
+
+  doc.moveDown(0.5);
+
+  content(doc);
 };
 
 export const pxToPt = (pixels: number) => pixels / 2.48;
