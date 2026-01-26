d2eed02fd0fa45f5c8d37e33dce7013cce29819f
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-01-26 09:49:48 +0100

    feat(api): added a VAE_COLLECTIVE_MULTI_CERTIFICATION feature flag

---
+++ packages/reva-api/prisma/migrations/20260126084653_add_vae_collective_multi_certification_feature_flag/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    features (key, label)
+VALUES
+    (
+        'VAE_COLLECTIVE_MULTI_CERTIFICATION',
+        'Permet de gérer plusieurs certifications dans une cohorte de VAE collective'
+    );
