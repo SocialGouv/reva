12482b2855ffaae5fcb06be5604bb1f8f23b3441
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-02-27 14:16:56 +0100

    chore(admin): migrated certification authority settings add local account pages tests to playwright

---
+++ packages/reva-admin-react/tests/(certificateur)/settings/add-local-account-general-information/certification-authority-settings-add-local-account-general-information.spec.ts
@@ -0,0 +1,128 @@
+import {
+  expect,
+  graphql,
+  test,
+} from "next/experimental/testmode/playwright/msw";
+
+import { login } from "../../../shared/helpers/auth/login";
+import { getCertificateurSettingsCommonHandlers } from "../../../shared/helpers/common-handlers/certificateur/getCertificateurSettingsCommon.handlers";
+import { graphQLResolver } from "../../../shared/helpers/network/msw";
+import { waitGraphQL } from "../../../shared/helpers/network/requests";
+
+const { certificateurSettingsCommonHandlers, certificateurSettingsCommonWait } =
+  getCertificateurSettingsCommonHandlers();
+
+const fvae = graphql.link("https://reva-api/api/graphql");
+
+const certificationAuthorityForAdd = graphQLResolver({
+  account_getAccountForConnectedUser: {
+    certificationAuthority: {
+      id: "c7399291-e79b-4e0f-b798-d3c97661e47f",
+      label: "certification authority label",
+    },
+  },
+});
+
+const addLocalAccountMutationResponse = graphQLResolver({
+  certification_authority_createCertificationAuthorityLocalAccount: {
+    id: "f7b5b065-f1c5-47d3-aa0c-c826deee8fa6",
+  },
+});
+
+test.describe("add local account general information page", () => {
+  test.use({
+    mswHandlers: [
+      [
+        ...certificateurSettingsCommonHandlers,
+        fvae.query(
+          "getCertificationAuthorityForAddLocalAccountGeneralInformationPage",
+          certificationAuthorityForAdd,
+        ),
+        fvae.mutation(
+          "addCertificationAuthorityLocalAccountGeneralInformationForAddLocalAccountGeneralInformationPage",
+          addLocalAccountMutationResponse,
+        ),
+      ],
+      { scope: "test" },
+    ],
+  });
+
+  test("when i access the add local account general information page - display the page with a correct title", async ({
+    page,
+  }) => {
+    await login({ role: "certificateur", page });
+    await page.goto(
+      "/admin2/certification-authorities/settings/local-accounts/add-local-account/general-information",
+    );
+    await Promise.all([
+      certificateurSettingsCommonWait(page),
+      waitGraphQL(
+        page,
+        "getCertificationAuthorityForAddLocalAccountGeneralInformationPage",
+      ),
+    ]);
+
+    await expect(
+      page
+        .getByTestId(
+          "add-certification-authority-local-account-general-information-page",
+        )
+        .locator("h1", { hasText: "Informations générales" }),
+    ).toBeVisible();
+  });
+
+  test("when i access the add local account general information page - let me fill the fields and submit the form", async ({
+    page,
+  }) => {
+    await login({ role: "certificateur", page });
+    await page.goto(
+      "/admin2/certification-authorities/settings/local-accounts/add-local-account/general-information",
+    );
+    await Promise.all([
+      certificateurSettingsCommonWait(page),
+      waitGraphQL(
+        page,
+        "getCertificationAuthorityForAddLocalAccountGeneralInformationPage",
+      ),
+    ]);
+
+    const pageRoot = page.getByTestId(
+      "add-certification-authority-local-account-general-information-page",
+    );
+    await pageRoot
+      .getByTestId("account-lastname-input")
+      .locator("input")
+      .fill("new account last name");
+    await pageRoot
+      .getByTestId("account-firstname-input")
+      .locator("input")
+      .fill("new account first name");
+    await pageRoot
+      .getByTestId("account-email-input")
+      .locator("input")
+      .fill("newaccount.email@example.com");
+    await pageRoot
+      .getByTestId("contact-full-name-input")
+      .locator("input")
+      .fill("new contact full name");
+    await pageRoot
+      .getByTestId("contact-email-input")
+      .locator("input")
+      .fill("newcontact.email@example.com");
+    await pageRoot
+      .getByTestId("contact-phone-input")
+      .locator("input")
+      .fill("9999999999");
+
+    const mutationPromise = waitGraphQL(
+      page,
+      "addCertificationAuthorityLocalAccountGeneralInformationForAddLocalAccountGeneralInformationPage",
+    );
+    await pageRoot.locator("button[type='submit']").click();
+    await mutationPromise;
+
+    await expect(page).toHaveURL(
+      /\/certification-authorities\/settings\/local-accounts\/f7b5b065-f1c5-47d3-aa0c-c826deee8fa6\//,
+    );
+  });
+});
---
+++ packages/reva-admin-react/tests/(certificateur)/settings/add-local-account/certification-authority-settings-add-local-account.spec.ts
@@ -0,0 +1,54 @@
+import { expect, test } from "next/experimental/testmode/playwright/msw";
+
+import { login } from "../../../shared/helpers/auth/login";
+import { getCertificateurSettingsCommonHandlers } from "../../../shared/helpers/common-handlers/certificateur/getCertificateurSettingsCommon.handlers";
+
+const { certificateurSettingsCommonHandlers, certificateurSettingsCommonWait } =
+  getCertificateurSettingsCommonHandlers();
+
+test.describe("add local acount page", () => {
+  test.use({
+    mswHandlers: [[...certificateurSettingsCommonHandlers], { scope: "test" }],
+  });
+
+  test("when i access the add local account page - display the page with a correct title", async ({
+    page,
+  }) => {
+    await login({ role: "certificateur", page });
+    await page.goto(
+      "/admin2/certification-authorities/settings/local-accounts/add-local-account",
+    );
+    await certificateurSettingsCommonWait(page);
+
+    await expect(
+      page
+        .getByTestId("add-certification-authority-local-account-page")
+        .locator("h1"),
+    ).toHaveText("Nouveau compte local");
+  });
+});
+
+test.describe("general information summary card", () => {
+  test.use({
+    mswHandlers: [[...certificateurSettingsCommonHandlers], { scope: "test" }],
+  });
+
+  test("when i click on the update button - redirect me to the add local account general information page", async ({
+    page,
+  }) => {
+    await login({ role: "certificateur", page });
+    await page.goto(
+      "/admin2/certification-authorities/settings/local-accounts/add-local-account",
+    );
+    await certificateurSettingsCommonWait(page);
+
+    await page
+      .getByTestId("local-account-general-information-summary-card")
+      .getByTestId("action-button")
+      .click();
+
+    await expect(page).toHaveURL(
+      /\/certification-authorities\/settings\/local-accounts\/add-local-account\/general-information/,
+    );
+  });
+});
