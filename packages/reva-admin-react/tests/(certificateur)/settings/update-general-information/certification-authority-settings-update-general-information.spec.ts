0486739dced8b2850b9a67767994ada69c303750
Pierre A <pierre@avizou.eu>
2026-03-02 15:26:12 +0100

    tests(admin):test the new websiteUrl field with optional/mandatory modalities

--- packages/reva-admin-react/tests/(certificateur)/settings/update-general-information/certification-authority-settings-update-general-information.spec.ts
+++ packages/reva-admin-react/tests/(certificateur)/settings/update-general-information/certification-authority-settings-update-general-information.spec.ts
@@ -31,6 +31,32 @@ const certificationAuthorityGeneralInfo = graphQLResolver({
   },
 });
 
+const certificationAuthorityGeneralInfoWithRequiredWebsiteUrl = graphQLResolver(
+  {
+    account_getAccountForConnectedUser: {
+      certificationAuthority: {
+        id: "c7399291-e79b-4e0f-b798-d3c97661e47f",
+        label: "certification authority label",
+        contactFullName: "jane doe",
+        contactEmail: "monemail@example.com",
+        contactPhone: "0101010101",
+        websiteUrl: "https://www.example.com",
+        certificationAuthorityStructures: [
+          {
+            hasReducedRequirements: true,
+            id: "123e4567-e89b-12d3-a456-426614174000",
+          },
+        ],
+        account: {
+          firstname: "jane admin",
+          lastname: "doe admin",
+          email: "monemaildeconnexion@example.com",
+        },
+      },
+    },
+  },
+);
+
 test.describe("update general information page", () => {
   test.use({
     mswHandlers: [
@@ -124,5 +150,71 @@ test.describe("update general information page", () => {
     await expect(
       page.getByTestId("certification-authority-submit-button"),
     ).toBeDisabled();
+  });
+});
+
+test.describe("update general information page with required website url", () => {
+  test.use({
+    mswHandlers: [
+      [
+        ...certificateurSettingsCommonHandlers,
+        fvae.query(
+          "getCertificationAuthorityGeneralInfoForEditPage",
+          certificationAuthorityGeneralInfoWithRequiredWebsiteUrl,
+        ),
+      ],
+      { scope: "test" },
+    ],
+  });
+
+  test("when i access the update general information page with required website url - display the correct form default values", async ({
+    page,
+  }) => {
+    await login({ role: "certificateur", page });
+    await page.goto(
+      "/admin2/certification-authorities/settings/informations-generales",
+    );
+    await Promise.all([
+      certificateurSettingsCommonWait(page),
+      waitGraphQL(page, "getCertificationAuthorityGeneralInfoForEditPage"),
+    ]);
+
+    await expect(
+      page.getByTestId("certification-authority-website-url").locator("input"),
+    ).toHaveValue("https://www.example.com");
+    await expect(
+      page.getByTestId("certification-authority-submit-button"),
+    ).toBeDisabled();
+  });
+
+  test("when i access the update general information page with required website url - display error message if the website url is not valid", async ({
+    page,
+  }) => {
+    await login({ role: "certificateur", page });
+    await page.goto(
+      "/admin2/certification-authorities/settings/informations-generales",
+    );
+    await Promise.all([
+      certificateurSettingsCommonWait(page),
+      waitGraphQL(page, "getCertificationAuthorityGeneralInfoForEditPage"),
+    ]);
+
+    await page
+      .getByTestId("certification-authority-website-url")
+      .locator("input")
+      .fill("not-a-valid-url");
+    await page.getByTestId("certification-authority-submit-button").click();
+
+    await expect(
+      page
+        .getByTestId("certification-authority-website-url")
+        .locator("input")
+        .getAttribute("aria-describedby"),
+    ).resolves.toContain("error");
+    await expect(
+      page
+        .getByTestId("certification-authority-website-url")
+        .locator(".fr-error-text"),
+    ).toHaveText("Le champ doit contenir une URL valide");
   });
 });
