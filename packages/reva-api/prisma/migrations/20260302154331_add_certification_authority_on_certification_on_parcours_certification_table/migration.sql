dab782dad0f5fc894670e755c7a874a71e9640a5
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-03-02 16:43:59 +0100

    feat(api): added certification_authority_on_certification_on_parcours_certification table

---
+++ packages/reva-api/prisma/migrations/20260302154331_add_certification_authority_on_certification_on_parcours_certification_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "certification_authority_on_certification_on_parcours_certification" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "certification_authority_on_certification_id" UUID NOT NULL,
+    "parcours_certification_id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "certification_authority_on_certification_on_parcours_certi_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "certification_authority_on_certification_on_parcours_certif_key" ON "certification_authority_on_certification_on_parcours_certification"("certification_authority_on_certification_id", "parcours_certification_id");
+
+-- AddForeignKey
+ALTER TABLE "certification_authority_on_certification_on_parcours_certification" ADD CONSTRAINT "cert_auth_on_cert_on_parcours" FOREIGN KEY ("certification_authority_on_certification_id") REFERENCES "certification_authority_on_certification"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "certification_authority_on_certification_on_parcours_certification" ADD CONSTRAINT "certification_authority_on_certification_on_parcours_certi_fkey" FOREIGN KEY ("parcours_certification_id") REFERENCES "parcours_certification"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
