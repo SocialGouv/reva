3c12ff431ca0d16fec3b9af5f99ca2284ad54cfc
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-03-03 15:39:41 +0100

    feat(api): added a test helper for the parcourCertification entity

---
+++ packages/reva-api/test/helpers/entities/create-parcours-certification-helper.ts
@@ -0,0 +1,27 @@
+import { faker } from "@faker-js/faker";
+import { Prisma } from "@prisma/client";
+
+import { prismaClient } from "@/prisma/client";
+
+import { createCertificationHelper } from "./create-certification-helper";
+
+export const createParcoursCertificationHelper = async (
+  parcoursCertificationArgs?: Partial<Prisma.ParcoursCertificationUncheckedCreateInput>,
+) => {
+  let certificationId;
+  if (parcoursCertificationArgs?.certificationId) {
+    certificationId = parcoursCertificationArgs.certificationId;
+  } else {
+    certificationId = (await createCertificationHelper()).id;
+  }
+  return prismaClient.parcoursCertification.create({
+    data: {
+      code: faker.string.uuid(),
+      label: faker.lorem.sentence(),
+      uai: faker.string.uuid(),
+      nomEtablissement: faker.lorem.sentence(),
+      certificationId,
+      ...parcoursCertificationArgs,
+    },
+  });
+};
