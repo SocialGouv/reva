2a1788923a55fe5933c62213aac99f0669891ad2
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-03-06 14:22:57 +0100

    feat(admin): added tests for the list on the certification authority settings certification parcours page

--- packages/reva-admin-react/tests/(certificateur)/settings/certifications/certification/parcours/certification-authority-settings-parcours-certification-page.spec.ts
+++ packages/reva-admin-react/tests/(certificateur)/settings/certifications/certification/parcours/certification-authority-settings-parcours-certification-page.spec.ts
@@ -19,7 +19,13 @@ const { certificateurSettingsCommonHandlers, certificateurSettingsCommonWait } =
 
 const fvae = graphql.link("https://reva-api/api/graphql");
 
-const createParcoursCertificationHandlers = () => {
+const PAGE_URL = `/admin2/certification-authorities/${CERTIFICATION_AUTHORITY_ID}/settings/certifications/1/parcours`;
+
+const createParcoursCertificationHandlers = (args?: {
+  withoutParcours?: boolean;
+}) => {
+  const { withoutParcours } = args ?? {};
+
   const getCertificationAndParcoursForCertificationAuthorityParcoursPageHandler =
     fvae.query(
       "getCertificationAndParcoursForCertificationAuthorityParcoursPageQuery",
@@ -28,20 +34,47 @@ const createParcoursCertificationHandlers = () => {
           getCertification: {
             id: "1",
             label: "Certification 1",
-            parcours: {
-              rows: [
-                {
-                  id: "1",
-                  label: "Parcours 1",
-                  nomEtablissement: "Etablissement 1",
+            parcours: withoutParcours
+              ? {
+                  rows: [],
+                  info: {
+                    totalRows: 0,
+                    totalPages: 0,
+                  },
+                }
+              : {
+                  rows: [
+                    {
+                      id: "1",
+                      label: "Parcours 1",
+                      nomEtablissement: "Etablissement 1",
+                    },
+                    {
+                      id: "2",
+                      label: "Parcours 2",
+                      nomEtablissement: "Etablissement 2",
+                    },
+                    {
+                      id: "3",
+                      label: "Parcours 3",
+                      nomEtablissement: "Etablissement 3",
+                    },
+                  ],
+                  info: {
+                    totalRows: 3,
+                    totalPages: 1,
+                  },
                 },
-              ],
-              info: {
-                totalRows: 1,
-                totalPages: 1,
-              },
-            },
           },
+          certification_authority_getParcoursForCertificationAndCertificationAuthority:
+            withoutParcours
+              ? []
+              : [
+                  {
+                    id: "2",
+                    label: "Parcours 2",
+                  },
+                ],
         },
       }),
     );
@@ -52,9 +85,21 @@ const createParcoursCertificationHandlers = () => {
       data: {},
     }),
   );
+
+  const updateParcoursForCertificationAndCertificationAuthorityHandler =
+    fvae.mutation(
+      "updateParcoursForCertificationAndCertificationAuthorityMutation",
+      graphQLResolver({
+        data: {
+          certification_authority_updateParcoursForCertificationAndCertificationAuthority: true,
+        },
+      }),
+    );
+
   return [
     getCertificationAndParcoursForCertificationAuthorityParcoursPageHandler,
     getCertificationAuthorityForCertificationsPageHandler,
+    updateParcoursForCertificationAndCertificationAuthorityHandler,
   ];
 };
 
@@ -68,34 +113,20 @@ async function waitForPageQueries(page: Page) {
   ]);
 }
 
-test.use({
-  mswHandlers: [
-    [
-      ...certificateurSettingsCommonHandlers,
-      ...createParcoursCertificationHandlers(),
-    ],
-    { scope: "test" },
-  ],
-});
-
-test.use({
-  mswHandlers: [
-    [
-      ...certificateurSettingsCommonHandlers,
-      ...createParcoursCertificationHandlers(),
-    ],
-    { scope: "test" },
-  ],
-});
-
 test.describe("when i access the parcours certification page", () => {
+  test.use({
+    mswHandlers: [
+      [
+        ...certificateurSettingsCommonHandlers,
+        ...createParcoursCertificationHandlers(),
+      ],
+      { scope: "test" },
+    ],
+  });
+
   test("it should display the page with a correct title", async ({ page }) => {
     await login({ page, role: "certificateur" });
-
-    await page.goto(
-      `/admin2/certification-authorities/${CERTIFICATION_AUTHORITY_ID}/settings/certifications/1/parcours`,
-    );
-
+    await page.goto(PAGE_URL);
     await waitForPageQueries(page);
 
     await expect(page.getByRole("heading", { level: 1 })).toHaveText(
@@ -104,16 +135,22 @@ test.describe("when i access the parcours certification page", () => {
   });
 });
 
-test.describe("breadcrumb", async () => {
+test.describe("breadcrumb", () => {
+  test.use({
+    mswHandlers: [
+      [
+        ...certificateurSettingsCommonHandlers,
+        ...createParcoursCertificationHandlers(),
+      ],
+      { scope: "test" },
+    ],
+  });
+
   test("when i click on the 'certifications gérées' link it should lead to the certifications page", async ({
     page,
   }) => {
     await login({ page, role: "certificateur" });
-
-    await page.goto(
-      `/admin2/certification-authorities/${CERTIFICATION_AUTHORITY_ID}/settings/certifications/1/parcours`,
-    );
-
+    await page.goto(PAGE_URL);
     await waitForPageQueries(page);
 
     await page
@@ -122,7 +159,7 @@ test.describe("breadcrumb", async () => {
       .click();
 
     await expect(page).toHaveURL(
-      /\/certification-authorities\/[\w-]+\/settings\/certifications\//,
+      `/admin2/certification-authorities/${CERTIFICATION_AUTHORITY_ID}/settings/certifications/`,
     );
   });
 
@@ -130,11 +167,7 @@ test.describe("breadcrumb", async () => {
     page,
   }) => {
     await login({ page, role: "certificateur" });
-
-    await page.goto(
-      `/admin2/certification-authorities/${CERTIFICATION_AUTHORITY_ID}/settings/certifications/1/parcours`,
-    );
-
+    await page.goto(PAGE_URL);
     await waitForPageQueries(page);
 
     await page
@@ -145,5 +178,137 @@ test.describe("breadcrumb", async () => {
     await expect(page).toHaveURL(
       `/admin2/certification-authorities/${CERTIFICATION_AUTHORITY_ID}/settings/`,
     );
+  });
+});
+
+test.describe("when I use the parcours list", () => {
+  test.describe("when the certification has parcours", () => {
+    test.use({
+      mswHandlers: [
+        [
+          ...certificateurSettingsCommonHandlers,
+          ...createParcoursCertificationHandlers(),
+        ],
+        { scope: "test" },
+      ],
+    });
+
+    test("it shows the correct parcours list", async ({ page }) => {
+      await login({ page, role: "certificateur" });
+      await page.goto(PAGE_URL);
+      await waitForPageQueries(page);
+
+      await expect(
+        page
+          .getByTestId("multi-select-list-item-1")
+          .getByRole("button", { name: "Ajouter" }),
+      ).toBeVisible();
+      await expect(
+        page
+          .getByTestId("multi-select-list-item-2")
+          .getByRole("button", { name: "Retirer" }),
+      ).toBeVisible();
+      await expect(
+        page
+          .getByTestId("multi-select-list-item-3")
+          .getByRole("button", { name: "Ajouter" }),
+      ).toBeVisible();
+    });
+
+    test("it let me add a parcours", async ({ page }) => {
+      await login({ page, role: "certificateur" });
+      await page.goto(PAGE_URL);
+      await waitForPageQueries(page);
+
+      const mutationPromise = waitGraphQL(
+        page,
+        "updateParcoursForCertificationAndCertificationAuthorityMutation",
+      );
+
+      await page
+        .getByTestId("multi-select-list-item-1")
+        .getByRole("button", { name: "Ajouter" })
+        .click();
+
+      await mutationPromise;
+    });
+
+    test("it let me remove a parcours", async ({ page }) => {
+      await login({ page, role: "certificateur" });
+      await page.goto(PAGE_URL);
+      await waitForPageQueries(page);
+
+      const mutationPromise = waitGraphQL(
+        page,
+        "updateParcoursForCertificationAndCertificationAuthorityMutation",
+      );
+
+      await page
+        .getByTestId("multi-select-list-item-2")
+        .getByRole("button", { name: "Retirer" })
+        .click();
+
+      await mutationPromise;
+    });
+
+    test("it let me toggle the only show added parcours switch", async ({
+      page,
+    }) => {
+      await login({ page, role: "certificateur" });
+      await page.goto(PAGE_URL);
+      await waitForPageQueries(page);
+
+      const queryPromise = waitGraphQL(
+        page,
+        "getCertificationAndParcoursForCertificationAuthorityParcoursPageQuery",
+      );
+
+      await page
+        .getByRole("checkbox", {
+          name: "Afficher uniquement les parcours ajoutés",
+        })
+        .click();
+
+      await queryPromise;
+    });
+
+    test("it let me search for a parcours", async ({ page }) => {
+      await login({ page, role: "certificateur" });
+      await page.goto(PAGE_URL);
+      await waitForPageQueries(page);
+
+      const queryPromise = waitGraphQL(
+        page,
+        "getCertificationAndParcoursForCertificationAuthorityParcoursPageQuery",
+      );
+
+      const searchBar = page.getByRole("searchbox", { name: "Rechercher" });
+      await searchBar.fill("Parcours 1");
+      await searchBar.press("Enter");
+
+      await queryPromise;
+    });
+  });
+
+  test.describe("when the certification has no parcours", () => {
+    test.use({
+      mswHandlers: [
+        [
+          ...certificateurSettingsCommonHandlers,
+          ...createParcoursCertificationHandlers({ withoutParcours: true }),
+        ],
+        { scope: "test" },
+      ],
+    });
+
+    test("it shows the empty state component", async ({ page }) => {
+      await login({ page, role: "certificateur" });
+      await page.goto(PAGE_URL);
+      await waitForPageQueries(page);
+
+      await expect(
+        page.getByRole("heading", { name: "Aucun parcours trouvé" }),
+      ).toBeVisible();
+    });
   });
 });
