68fe2420e7da91f73c262670d3470540cb454fec
Pierre A <pierre@avizou.eu>
2026-03-16 12:00:36 +0100

    feat(candidate):update logout page to new design

--- packages/reva-candidate/src/app/(auth)/logout-confirmation/page.tsx
+++ packages/reva-candidate/src/app/(auth)/logout-confirmation/page.tsx
@@ -1,30 +1,49 @@
 "use client";
 
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import { Panel } from "@/components/layout/Panel";
-
 export default function LogoutConfirmation() {
   const router = useRouter();
 
   return (
     <div data-testid="logout-confirmation" className="flex-1">
-      <Panel narrow>
-        <div className="flex flex-col items-center justify-center text-center p-6 pt-8">
-          <h1 className="text-3xl font-bold text-dsfrGray-800">
-            Vous êtes bien déconnecté du service France VAE
-          </h1>
-
-          <Button
-            data-testid="logout-confirmation-back-to-home"
-            className="mt-6"
-            onClick={() => {
-              router.push("/login");
-            }}
-          >
-            Retournez à la page d'accueil
-          </Button>
+      <Panel>
+        <div className="flex flex-row items-center justify-between gap-8">
+          <div className="flex flex-col justify-center p-6">
+            <h1 className="text-[2.5rem] font-bold text-dsfrGray-800">
+              Vous avez bien été déconnecté de votre espace France VAE
+            </h1>
+            <div className="flex flex-row gap-4">
+              <Button
+                data-testid="logout-confirmation-back-to-home"
+                className="mt-6"
+                onClick={() => {
+                  router.push(process.env.NEXT_PUBLIC_WEBSITE_BASE_URL || "/");
+                }}
+              >
+                Retourner à laccueil
+              </Button>
+              <Button
+                data-testid="logout-confirmation-reconnect"
+                className="mt-6"
+                priority="secondary"
+                onClick={() => {
+                  router.push("/login");
+                }}
+              >
+                Se reconnecter
+              </Button>
+            </div>
+          </div>
+          <Image
+            src="/candidat/images/error-hexagon.svg"
+            alt="Hexagone rouge"
+            width={282}
+            height={319}
+          />
         </div>
       </Panel>
     </div>
