9f803676a9563243740d019eee56992f48269482
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-03-17 11:52:13 +0100

    feat(api): updated candidacy_convention_collective table, added new ccns

---
+++ packages/reva-api/prisma/migrations/20260317104409_update_candidacy_convention_collective_table_add_new_ccns/migration.sql
@@ -0,0 +1,6 @@
+insert into
+  candidacy_convention_collective (idcc, label)
+values
+  ('9997', 'Jamais occupé d’emploi salarié'),
+  ('9998', 'Convention non encore en vigueur'),
+  ('9999', 'Sans convention collective');
