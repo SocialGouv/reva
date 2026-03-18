38d25f28ef81c7a7e01bd119a88d0fe40664efa7
Florian Bonniec <f.bonniec@me.com>
2026-03-18 15:36:39 +0100

    fix(api,candidate): fixed getCivilInformationCompletedByCandidateId feature and civilInformationSchema on profile v2

--- packages/reva-candidate/src/app/candidates/[candidateId]/(candidate)/profile/civil-informations/_components/civilInformationSchema.ts
+++ packages/reva-candidate/src/app/candidates/[candidateId]/(candidate)/profile/civil-informations/_components/civilInformationSchema.ts
@@ -18,7 +18,7 @@ export const civilInformationSchema = () =>
       firstname3: sanitizedOptionalText(),
       birthdate: sanitizedText(),
       country: sanitizedText().default("France"),
-      birthDepartment: sanitizedText(),
+      birthDepartment: sanitizedOptionalText(),
       birthCity: sanitizedText(),
       nationality: sanitizedText(),
       countryIsFrance: z.boolean(),
