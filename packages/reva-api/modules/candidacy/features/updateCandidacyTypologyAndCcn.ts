709b7757dd01d38d808d629b53c15032bbf57df8
Florian Bonniec <f.bonniec@me.com>
2026-03-18 15:08:53 +0100

    fix(api): fixed updateCandidacyTypologyAndCcn and getTypologyAndCollectiveAgreementCompletedByCandidateId features

--- packages/reva-api/modules/candidacy/features/updateCandidacyTypologyAndCcn.ts
+++ packages/reva-api/modules/candidacy/features/updateCandidacyTypologyAndCcn.ts
@@ -51,11 +51,12 @@ export const updateCandidacyTypologyAndCcn = async (
     typology === CandidateTypology.SALARIE_PRIVE ||
     typology === CandidateTypology.DEMANDEUR_EMPLOI ||
     typology === CandidateTypology.TRAVAILLEUR_NON_SALARIE ||
-    typology === CandidateTypology.TITULAIRE_MANDAT_ELECTIF;
+    typology === CandidateTypology.TITULAIRE_MANDAT_ELECTIF ||
+    typology === CandidateTypology.AIDANTS_FAMILIAUX_AGRICOLES;
 
   if (ccnRequired && !ccnId) {
     throw new Error(
-      'Les typologies "SALARIE_PRIVE" et "DEMANDEUR_EMPLOI" doivent être associées à une convention collective.',
+      'Les typologies "SALARIE_PRIVE", "DEMANDEUR_EMPLOI", "TRAVAILLEUR_NON_SALARIE", "TITULAIRE_MANDAT_ELECTIF" et "AIDANTS_FAMILIAUX_AGRICOLES" doivent être associées à une convention collective.',
     );
   }
 
