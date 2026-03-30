cf536ee8de09b28dbc6a4da8644ed52342056bf4
Pierre A <pierre@avizou.eu>
2026-03-30 16:08:07 +0200

    feat(api):add feature flag for jury results by block

---
+++ packages/reva-api/prisma/migrations/20260330140200_feature_jury_results_by_block/migration.sql
@@ -0,0 +1,8 @@
+insert into
+    features (key, label, description)
+values
+    (
+        'JURY_RESULTS_BY_BLOCK',
+        'Résultats du jury par bloc',
+        'Permet de remonter les résultats du jury par bloc'
+    );
