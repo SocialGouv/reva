31bb1575be2eb6335edb51b442217522144140f5
Pierre A <pierre@avizou.eu>
2026-04-16 17:11:22 +0200

    feat(api):revoking a jury decision also revokes jury competence blocks results

--- packages/reva-api/modules/jury/features/revokeJuryDecision.ts
+++ packages/reva-api/modules/jury/features/revokeJuryDecision.ts
@@ -38,6 +38,9 @@ export const revokeJuryDecision = async ({
       where: { id: juryId },
       data: {
         isActive: false,
+        juryResultByCompetenceBlocs: {
+          deleteMany: {},
+        },
       },
     });
 
