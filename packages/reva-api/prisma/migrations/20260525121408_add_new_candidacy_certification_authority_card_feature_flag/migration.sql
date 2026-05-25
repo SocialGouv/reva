1091c519e8d8f27de910877817c4ba223220e7a1
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-05-25 14:14:58 +0200

    feat(migration): add new feature flag for candidacy certification authority card

---
+++ packages/reva-api/prisma/migrations/20260525121408_add_new_candidacy_certification_authority_card_feature_flag/migration.sql
@@ -0,0 +1,7 @@
+insert into
+    features (key, label)
+values
+    (
+        'NEW_CANDIDACY_CERTIFICATION_AUTHORITY_CARD',
+        'Nouvelle carte certificateur dans la page de résumé de la candidature'
+    );
