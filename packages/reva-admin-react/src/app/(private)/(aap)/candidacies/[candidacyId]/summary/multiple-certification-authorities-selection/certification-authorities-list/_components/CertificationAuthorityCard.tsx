83f9fe0ed2e88d3581292fd65dfb00d74351a34a
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-07-02 11:36:18 +0200

    fix(admin): CertificationAuthorityCard was missing clickable hints

--- packages/reva-admin-react/src/app/(private)/(aap)/candidacies/[candidacyId]/summary/multiple-certification-authorities-selection/certification-authorities-list/_components/CertificationAuthorityCard.tsx
+++ packages/reva-admin-react/src/app/(private)/(aap)/candidacies/[candidacyId]/summary/multiple-certification-authorities-selection/certification-authorities-list/_components/CertificationAuthorityCard.tsx
@@ -12,22 +12,33 @@ export const CertificationAuthorityCard = ({
   contactPhone?: string | null;
   onClickHref?: string;
   onClick: () => void;
-}) => (
-  <Card
-    data-testid="certification-authority-card"
-    start={
-      <Tag small className="mb-3">
-        Gestionnaire de candidatures
-      </Tag>
-    }
-    title={label}
-    desc={
-      <span className="flex flex-col gap-1">
-        {contactEmail && <span>{contactEmail}</span>}
-        {contactPhone && <span>{contactPhone}</span>}
-      </span>
-    }
-    endDetail="Choisir"
-    nativeDivProps={{ onClick }}
-  />
-);
+}) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    onClick();
+  };
+
+  return (
+    <Card
+      data-testid="certification-authority-card"
+      start={
+        <Tag small className="mb-3">
+          Gestionnaire de candidatures
+        </Tag>
+      }
+      title={label}
+      desc={
+        <span className="flex flex-col gap-1">
+          {contactEmail && <span>{contactEmail}</span>}
+          {contactPhone && <span>{contactPhone}</span>}
+        </span>
+      }
+      endDetail="Choisir"
+      enlargeLink
+      linkProps={{
+        href: "/",
+        onClick: handleClick,
+      }}
+    />
+  );
+};
