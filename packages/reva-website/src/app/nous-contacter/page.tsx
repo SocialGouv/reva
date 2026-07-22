add59b5c990b8418b0a4bf1a80a980a6bec9feed
ThomasDos <dosanjos.thomas@gmail.com>
2026-07-22 14:58:15 +0200

    feat(website): redirect nous-contacter page to Crisp contact form

--- packages/reva-website/src/app/nous-contacter/page.tsx
+++ packages/reva-website/src/app/nous-contacter/page.tsx
@@ -1,26 +1,11 @@
-import { MainLayout } from "@/app/_components/layout/main-layout/MainLayout";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Nous contacter - France VAE",
 };
 
 const IndexNousContacterPage = () => {
-  return (
-    <MainLayout>
-      <div className="fr-container text-center pt-20">
-        <h1>Nous contacter</h1>
-        <iframe
-          title="Nous contacter - France VAE"
-          src="https://plugins.crisp.chat/urn:crisp.im:contact-form:0/contact/5d4404d7-da05-42b9-8e06-1e56d8c150b0"
-          referrerPolicy="origin"
-          sandbox="allow-forms allow-popups allow-scripts allow-same-origin"
-          width="100%"
-          height="780px"
-          frameBorder="0"
-        ></iframe>
-      </div>
-    </MainLayout>
-  );
+  redirect("https://francevae.crisp.help/fr/?contact");
 };
 
 export default IndexNousContacterPage;
