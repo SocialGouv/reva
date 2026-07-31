e99371bea7e5a16c186d606869a8d7596fb2b0c0
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-07-31 14:32:54 +0200

    fix(api): fixed wrongly named public resolver in vae collective security mao

--- packages/reva-api/modules/vae-collective/vae-collective.security.ts
+++ packages/reva-api/modules/vae-collective/vae-collective.security.ts
@@ -12,8 +12,10 @@ export const vaeCollectiveResolversSecurityMap = {
   "Mutation.*": defaultSecurity,
 
   // Define specific resolver security rules
-  "Query.getCohorteVAECollectiveByCodeInscription": isAnyone,
+  "Query.cohorteVaeCollective": isAnyone,
   "Query.cohortesVaeCollectivesForConnectedAap": isAnyone, // La sécurité est gérée dans la feature (filtre par rapport au rôle de l'utilisateur)
+  "Query.cohortesVaeCollectivesForConnectedCertificationAuthorityOrLocalAccount":
+    isAnyone, // La sécurité est gérée dans la feature (filtre par rapport au rôle de l'utilisateur)
   "Query.vaeCollective_getCommanditaireVaeCollective":
     isAdminOrGestionnaireOfCommanditaireVaeCollective,
 
