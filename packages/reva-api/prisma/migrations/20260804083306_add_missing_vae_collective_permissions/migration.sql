79540f2cf05abd7642e6502355b1a171d2d1a32e
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-08-04 10:33:25 +0200

    feat(api): added missing vae collective permissions

---
+++ packages/reva-api/prisma/migrations/20260804083306_add_missing_vae_collective_permissions/migration.sql
@@ -0,0 +1,13 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "PermissionVaeCollective" ADD VALUE 'MODIFIER_COHORTE';
+ALTER TYPE "PermissionVaeCollective" ADD VALUE 'SUPPRIMER_COHORTE';
+ALTER TYPE "PermissionVaeCollective" ADD VALUE 'VOIR_LISTE_COHORTES';
+ALTER TYPE "PermissionVaeCollective" ADD VALUE 'VOIR_COHORTE';
+ALTER TYPE "PermissionVaeCollective" ADD VALUE 'VOIR_STATISTIQUES';
