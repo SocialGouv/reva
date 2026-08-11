ad4c3cc15eadbf81f93dfe8cda5f078a1149f12e
Florian Bonniec <f.bonniec@me.com>
2026-08-11 17:17:38 +0200

    Revert "feat(api): updated sendDFFToCandidate feature to flag feasibility decison as DRAFT, added missing missing"
    This reverts commit 1db8c8dced04e200327f931e89451d01b8e1d442.

--- packages/reva-api/modules/feasibility/dematerialized-feasibility-file/features/sendDFFToCandidate.ts
+++ packages/reva-api/modules/feasibility/dematerialized-feasibility-file/features/sendDFFToCandidate.ts
@@ -1,5 +1,3 @@
-import { FeasibilityStatus } from "@prisma/client";
-
 import { logCandidacyAuditEvent } from "@/modules/candidacy-log/features/logCandidacyAuditEvent";
 import { DOSSIER_FAISABILITE_NON_TROUVE } from "@/modules/shared/errors/messages";
 import { prismaClient } from "@/prisma/client";
@@ -13,6 +11,12 @@ export const sendDFFToCandidate = async ({
   dematerializedFeasibilityFileId: string;
   context: GraphqlContext;
 }) => {
+  const now = new Date().toISOString();
+  await prismaClient.dematerializedFeasibilityFile.update({
+    where: { id: dematerializedFeasibilityFileId },
+    data: { sentToCandidateAt: now },
+  });
+
   const dff = await prismaClient.dematerializedFeasibilityFile.findUnique({
     where: { id: dematerializedFeasibilityFileId },
     include: {
@@ -25,26 +29,6 @@ export const sendDFFToCandidate = async ({
   if (!dff) {
     throw new Error(DOSSIER_FAISABILITE_NON_TROUVE);
   }
-
-  if (
-    dff.feasibility.decision !== FeasibilityStatus.DRAFT &&
-    dff.feasibility.decision !== FeasibilityStatus.INCOMPLETE
-  ) {
-    throw new Error(
-      "Le dossier de faisabilité n'est pas en état de draft ou incomplet pour être envoyé au candidat",
-    );
-  }
-
-  const now = new Date().toISOString();
-  await prismaClient.dematerializedFeasibilityFile.update({
-    where: { id: dematerializedFeasibilityFileId },
-    data: {
-      sentToCandidateAt: now,
-      feasibility: {
-        update: { decision: FeasibilityStatus.DRAFT },
-      },
-    },
-  });
 
   if (dff?.feasibility?.candidacy?.candidate?.email) {
     await sendDFFNotificationToCandidateEmail({
