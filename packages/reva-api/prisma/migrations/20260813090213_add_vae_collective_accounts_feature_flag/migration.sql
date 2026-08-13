802a5132a044c987860b779108bd7be729902c12
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-08-13 11:21:38 +0200

    feat(api): added VAE_COLLECTIVE_SUB_ACCOUNTS feature flag

---
+++ packages/reva-api/prisma/migrations/20260813090213_add_vae_collective_accounts_feature_flag/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    features (key, label)
+VALUES
+    (
+        'VAE_COLLECTIVE_ACCOUNTS',
+        'Permet de gérer les comptes utilisateursdans l''application VAE collective'
+    );
