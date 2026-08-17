88f0ad47e867d3053c037039380886fb707092c5
Cédric Soulas <soulascedric@gmail.com>
2026-08-17 17:44:18 +0200

    refactor(api): isolate candidate account authorization tests

--- packages/reva-api/modules/candidacy/candidacy.test.ts
+++ packages/reva-api/modules/candidacy/candidacy.test.ts
@@ -1,4 +1,3 @@
-import { NOT_AUTHORIZED_CANDIDACY_ACCESS } from "@/modules/shared/security/messages";
 import { prismaClient } from "@/prisma/client";
 import { authorizationHeaderForUser } from "@/test/helpers/authorization-helper";
 import { createCandidacyHelper } from "@/test/helpers/entities/create-candidacy-helper";
@@ -123,41 +122,6 @@ test("get existing Candidacy with admin user does not update the last opening da
   });
 
   expect(updatedCandidacy.lastOpenedAt).toBeNull();
-});
-
-test("a user can't modify the account information of another candidate", async () => {
-  const candidate = await createCandidateHelper();
-  const anotherCandidate = await createCandidateHelper();
-
-  const graphqlClient = getGraphQLClient({
-    headers: {
-      authorization: authorizationHeaderForUser({
-        role: "candidate",
-        keycloakId: candidate.keycloakId,
-      }),
-    },
-  });
-
-  const candidacy_updateContact = graphql(`
-    mutation candidacy_updateContact_of_different_candidate(
-      $candidateId: ID!
-      $candidateData: UpdateCandidateInput!
-    ) {
-      candidacy_updateContact(
-        candidateId: $candidateId
-        candidateData: $candidateData
-      ) {
-        id
-      }
-    }
-  `);
-
-  await expect(
-    graphqlClient.request(candidacy_updateContact, {
-      candidateId: anotherCandidate.id,
-      candidateData: { phone: "0612345678" },
-    }),
-  ).rejects.toThrowError(NOT_AUTHORIZED_CANDIDACY_ACCESS);
 });
 
 test("a candidate can modify all his account information", async () => {
