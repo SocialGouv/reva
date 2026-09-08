248fcf560d6b72906a1edb598b5c7c8e3508d341
Alexandre Garbe <alexandre.garbe@gmail.com>
2026-09-08 11:26:29 +0200

    feat(api): added ENABLE_EMAIL_OTP_ON_ACCOUNT_CREATION feature

---
+++ packages/reva-api/prisma/migrations/20260908091813_add_enable_email_otp_on_account_creation_feature_flag/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    features (key, label)
+VALUES
+    (
+        'ENABLE_EMAIL_OTP_ON_ACCOUNT_CREATION',
+        'Activation de l''authentification par email OTP lors de la création de compte'
+    );
